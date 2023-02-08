4fb3dcdccb91e2f50613bd36aa073dfc6874b6fc
Fritz Madden <fmadden@flexion.us>
2023-02-08 14:29:31 -0700

    111 - working on mssql connection attempt

--- api/src/config/mssql.ts
+++ api/src/config/mssql.ts
@@ -1,7 +1,55 @@
 import * as mssql from 'mssql';
 import config from './config';
 import logging from './logging';
+import { Connection, Request } from 'tedious';
+import { DefaultAzureCredential } from '@azure/identity';
 
+// Uncomment one of the two lines depending on the identity type
+//const credential = new DefaultAzureCredential(); // system-assigned identity
+const credential = new DefaultAzureCredential({ managedIdentityClientId: '784371ee-c2c8-4e71-8e19-3ae2ec348577' }); // user-assigned identity
+
+const Connect = async () => {
+  // Get token for Azure SQL Database
+  const accessToken = await credential.getToken('https://database.windows.net/.default');
+
+  // Create connection to database
+  const connection = new Connection({
+    server: 'boss-acms-dev.database.windows.net',
+    authentication: {
+      type: 'azure-active-directory-access-token',
+      options: {
+        token: accessToken.token
+      }
+    },
+    options: {
+      database: 'boss-dev',
+      encrypt: true,
+      port: 1433
+    }
+  });
+
+  // Open the database connection
+  connection.connect();
+
+  return connection;
+};
+
+export { Connect };
+/*
+const Query = async (connection: mssql.Connection, query: string) =>
+  new Promise((resolve, reject) => {
+    logging.info('MSSQL', `Query: ${query}`, connection);
+
+    const result = connection..query(query);
+      resolve(result);
+    } catch (error) {
+      reject(error);
+    }
+  });
+
+export { Connect, Query };
+
+/*
 const params: mssql.config = {
   user: config.mssql.user,
   password: config.mssql.pass,
@@ -42,4 +90,4 @@ const Query = async (connection: mssql.ConnectionPool, query: string) =>
     }
   });
 
-export { Connect, Query };
+*/
