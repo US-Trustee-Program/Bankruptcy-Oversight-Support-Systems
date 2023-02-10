1bd756e59c896b563cf0254891935c411ca89e92
SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
2023-02-10 11:45:37 -0800

    Added getCases method and related unit test.

--- api/src/main/java/gov/doj/CaseListUseCase.java
+++ api/src/main/java/gov/doj/CaseListUseCase.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class CaseListUseCase {
+
     public void getCases(String user) {
         List<String> caseOne = Arrays.asList("Chapter 11", "Debby Debtor", "Brian BusinessAnalyst");
         Map<String, List<String>> cases = new HashMap<>();
