e26d1ec8bca78bb7ebd0ea3a5aa2f2c40ef6589b
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-02-16 11:33:32 -0600

    111: Move local case list to local persistence gateway

--- api/src/main/java/gov/doj/usecases/CaseListUseCase.java
+++ api/src/main/java/gov/doj/usecases/CaseListUseCase.java
@@ -11,15 +11,10 @@ import java.util.Map;
 public class CaseListUseCase {
 
     public void getCases(String user) {
-        List<String> caseOne = Arrays.asList("Chapter 11", "Debby Debtor", "Brian BusinessAnalyst");
-        Map<String, List<String>> cases = new HashMap<>();
-        cases.put("477-14-12211", caseOne);
+        PersistenceGateway persistenceGateway = ObjectFactory.getObjectByAbstractClass(PersistenceGateway.class);
+        List<String> cases = persistenceGateway.getCases();
+
         Presenter presenter = ObjectFactory.getObjectByAbstractClass(Presenter.class);
         presenter.onSuccess(cases.toString());
-    }
-
-    public void addCase(String caseData) {
-        PersistenceGateway dbGateway = ObjectFactory.getObjectByAbstractClass(PersistenceGateway.class);
-        dbGateway.addCase(caseData);
     }
 }
