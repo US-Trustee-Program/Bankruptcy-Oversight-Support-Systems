f7cb09341ccadb33838a46c15a8f814c0b2f7669
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-02-24 15:50:57 -0600

    111: Get AzureSqlGateway only when needed

--- api/src/main/java/gov/doj/RuntimeObjectFactory.java
+++ api/src/main/java/gov/doj/RuntimeObjectFactory.java
@@ -4,16 +4,36 @@ import gov.doj.adapters.gateways.AzureSqlGateway;
 import gov.doj.adapters.gateways.LocalPersistenceGateway;
 import gov.doj.adapters.presenters.JsonPresenter;
 import gov.doj.usecases.PersistenceGateway;
+import io.quarkus.runtime.configuration.ConfigUtils;
+
+import java.util.List;
 
 public class RuntimeObjectFactory extends ObjectFactory {
     public static void init() {
         registerObject(Presenter.class, new JsonPresenter());
+        List<String> profiles = ConfigUtils.getProfiles();
+        if (profiles.contains("dev") && !profiles.contains("azure")) {
+            initLocal();
+        } else {
+            initProd();
+        }
+    }
+
+    private static void initLocal() {
         registerObject(PersistenceGateway.class, new LocalPersistenceGateway());
     }
 
-    public static void initCloud()
-    {
-        registerObject(Presenter.class, new JsonPresenter());
+    private static void initProd() {
         registerObject(PersistenceGateway.class, new AzureSqlGateway());
     }
+//    public static void init() {
+//        registerObject(Presenter.class, new JsonPresenter());
+//        registerObject(PersistenceGateway.class, new LocalPersistenceGateway());
+//    }
+//
+//    public static void initCloud()
+//    {
+//        registerObject(Presenter.class, new JsonPresenter());
+//        registerObject(PersistenceGateway.class, new AzureSqlGateway());
+//    }
 }
