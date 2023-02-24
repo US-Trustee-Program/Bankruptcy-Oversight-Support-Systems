1675a97c73b30edaf0adb3b3aeeae799525a0e58
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-02-24 15:50:57 -0600

    111: Get AzureSqlGateway only when needed

--- api/src/main/java/gov/doj/adapters/gateways/AzureSqlGateway.java
+++ api/src/main/java/gov/doj/adapters/gateways/AzureSqlGateway.java
@@ -1,28 +1,32 @@
 package gov.doj.adapters.gateways;
 
 import gov.doj.usecases.PersistenceGateway;
+import org.eclipse.microprofile.config.ConfigProvider;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class AzureSqlGateway implements PersistenceGateway {
+
     public List<String> connect() throws Exception{
 
         List<String> cases = new ArrayList<>();
         ResultSet resultSet = null;
 
+        String url =  ConfigProvider.getConfig().getValue("url", String.class);
         Properties properties = new Properties();
-        properties.load(AzureSqlGateway.class.getClassLoader().getResourceAsStream("application.properties"));
-
-        //read password from the env
-        String password =  org.eclipse.microprofile.config.ConfigProvider.getConfig().getValue("password", String.class);
+        String user =  ConfigProvider.getConfig().getValue("sql.user", String.class);
+        String password =  ConfigProvider.getConfig().getValue("password", String.class);
+        properties.put("user", user);
         properties.put("password", password);
 
-        try (Connection connection = DriverManager.getConnection(properties.getProperty("url"), properties);
+        try (Connection connection = DriverManager.getConnection(url, properties);
              Statement statement = connection.createStatement();){
 
             String selectSql = "SELECT case_id, case_number  from Cases";
