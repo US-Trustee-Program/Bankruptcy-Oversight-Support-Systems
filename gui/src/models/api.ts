7685fcefcbdfba83d0deb4d7fb06b597813c165b
Fritz Madden <fmadden@flexion.us>
2023-02-28 14:18:38 -0700

    Code to display a list of Cases in a simple unstyled screen

---
+++ gui/src/models/api.ts
@@ -0,0 +1,118 @@
+import config from '../configuration/apiConfiguration';
+
+export type ResponseData = {
+  message: string;
+  count: number;
+  body: object | Array<object>;
+};
+
+export type ResponseError = {
+  message: string;
+  error: object;
+};
+
+export default class Api {
+  private _host = `${config.protocol}://${config.server}:${config.port}`;
+
+  public async create(path: string, body: object): Promise<ResponseData> {
+    const response = await fetch(this._host + path, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(new Error(`500 Error - Invalid Request ${data?.toString()}`));
+    }
+  }
+
+  public async list(path: string): Promise<ResponseData> {
+    const response = await fetch(this._host + path, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(new Error(`404 Error - Not Found ${data?.toString()}`));
+    }
+  }
+
+  public async read(path: string, id: string): Promise<ResponseData> {
+    const response = await fetch(`${this._host}${path}/${id}`, {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(new Error(`404 Error - Not Found ${data?.toString()}`));
+    }
+  }
+
+  public async update(path: string, id: string, body: object): Promise<ResponseData> {
+    const response = await fetch(`${this._host}${path}/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(new Error(`500 Error - Failed Update ${data?.toString()}`));
+    }
+  }
+
+  public async replace(path: string, id: string, body: object): Promise<ResponseData> {
+    const response = await fetch(`${this._host}${path}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+      },
+      body: JSON.stringify(body),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(new Error(`500 Error - Failed Replace ${data?.toString()}`));
+    }
+  }
+
+  public async del(path: string, id: string) {
+    const response = await fetch(`${this._host}${path}/${id}`, {
+      method: 'DELETE',
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(new Error(`500 Error - Failed to Delete ${data?.toString()}`));
+    }
+  }
+}
