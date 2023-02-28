04b7ae61dd3162486f8cc08a02d49349e3b841c5
Fritz Madden <fmadden@flexion.us>
2023-02-28 09:50:27 -0700

    Finalized endpoint signatures

--- api/src/adapters/types/persistence-gateway.d.ts
+++ api/src/adapters/types/persistence-gateway.d.ts
@@ -1,9 +1,9 @@
-import { RecordObj, DbRecord } from './basic';
+import { RecordObj, DbResult } from './basic';
 
 export type PersistenceGateway = {
-  createRecord(table: string, fields: RecordObj[]): Promise<boolean>;
-  getAll(table: string): Promise<DbRecord>;
-  getRecord(table: string, id: number): Promise<DbRecord>;
-  updateRecord(table: string, id: number, fields: RecordObj[]): Promise<DbRecord>;
-  deleteRecord(table: string, id: number): Promise<boolean>;
+  createRecord(table: string, fields: RecordObj[]): Promise<DbResult>;
+  getAll(table: string): Promise<DbResult>;
+  getRecord(table: string, id: number): Promise<DbResult>;
+  updateRecord(table: string, id: number, fields: RecordObj[]): Promise<DbResult>;
+  deleteRecord(table: string, id: number): Promise<DbResult>;
 };
