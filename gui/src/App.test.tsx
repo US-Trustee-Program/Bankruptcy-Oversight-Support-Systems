962b3d1c25aa3f56ccf4d59d03bfc3e69c8a57f1
Fritz Madden <fmadden@flexion.us>
2023-03-08 13:44:12 -0700

    Fixed jest error in react related to routing

--- gui/src/App.test.tsx
+++ gui/src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders Total Active Cases', () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
 
   const h1 = screen.getByText(/Total Active Cases/i);
   expect(h1).toBeInTheDocument();
