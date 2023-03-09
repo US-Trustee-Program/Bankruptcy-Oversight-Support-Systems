293685be06b234276ce6dca78da414d973fa9e43
SwathiLexi <swathi.lexi@gmail.com>
2023-03-09 15:30:18 -0800

    DataLoader Implementation to load data into the Azure SQL Database tables for USTP. Initial Code checkin with Driver, LoaderMap, CMMCD Loader, resource files and initial code for the CMMHR loader.

---
+++ dataloader/src/main/java/gov/doj/Driver.java
@@ -0,0 +1,41 @@
+package gov.doj;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Driver {
+
+    private static LoaderMap loaderMap = new LoaderMap();
+    public static void main(String[] args) {
+
+        try {
+
+            Properties csvProperties = new Properties();
+            csvProperties.load(Driver.class.getClassLoader().getResourceAsStream("csvfiles.properties"));
+
+            if(!csvProperties.isEmpty()) {
+                csvProperties.forEach((k, v) -> {
+
+                    System.out.println("CSV Loader : " + k + " CSV File Path : " + v);
+
+                    String loaderKey = k.toString().toLowerCase();
+                    String loaderFilePath = v.toString().toLowerCase();
+
+                    IDataLoader dataLoader = (IDataLoader) loaderMap.map.get(loaderKey);
+
+                    System.out.println("Initializing the loader with the file : " + loaderFilePath);
+                    dataLoader.initialize(loaderFilePath);
+
+                    System.out.println("Starting the load run...");
+                    dataLoader.run();
+
+                    System.out.println("Load Complete.");
+
+                });
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ dataloader/src/main/java/gov/doj/IDataLoader.java
@@ -0,0 +1,11 @@
+package gov.doj;
+
+public interface IDataLoader {
+
+        void run();
+        void initialize(String filePath);
+        void loadTable();
+        void clearTable();
+
+
+}
