f7673d70ab2c5e8dffe40e27312be3cc5f3d820f
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-03-10 09:30:06 -0600

    111: Add pre-commit-java hooks
    * 111: Add pre-commit-java hooks
    
    * 111: Replace java pre-commit
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * 111: Run pre-commit on all files
    
    Uses Google Java style formatter
    
    * 111: Format Java code
    
    Skips Java formatter on pre-commit.ci
    Runs Java formatter in GitHub Actions workflow111: Format Java code
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- api/src/main/java/gov/doj/adapters/controllers/CaseListController.java
+++ api/src/main/java/gov/doj/adapters/controllers/CaseListController.java
@@ -4,77 +4,73 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gov.doj.entities.Case;
 import gov.doj.usecases.CasesUseCase;
-
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/cases")
 public class CaseListController {
 
-    private CasesUseCase caseUseCase = new CasesUseCase();
+  private CasesUseCase caseUseCase = new CasesUseCase();
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getCases() throws JsonProcessingException {
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public String getCases() throws JsonProcessingException {
 
-        List<Case> cases = caseUseCase.getCases();
+    List<Case> cases = caseUseCase.getCases();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = objectMapper.writeValueAsString(cases);
+    ObjectMapper objectMapper = new ObjectMapper();
+    String json = objectMapper.writeValueAsString(cases);
 
-        return json;
+    return json;
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/{caseId}")
+  public String getCaseById(long caseId) throws JsonProcessingException {
+
+    Optional<Case> _case = caseUseCase.getCaseById(caseId);
+
+    Case caseValue = _case.get();
+    if (caseValue.getCases_id() == 0) {
+      return "No Cases found with case Id : " + " " + caseId;
+    } else {
+
+      ObjectMapper objectMapper = new ObjectMapper();
+      String json = objectMapper.writeValueAsString(_case.get());
+
+      return json;
     }
+  }
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("/{caseId}")
-    public String getCaseById(long caseId) throws JsonProcessingException{
+  @Consumes(MediaType.APPLICATION_JSON)
+  @POST
+  @Path("/create")
+  public boolean create(String payload) {
+    long val = 0L; // case id generation value.
+    Case obj = new Case(val, "Betty", "Gates2", "Complete", Timestamp.from(Instant.now()), 3);
+    var response = Response.ok(caseUseCase.createCase(obj)).build();
+    return false;
+  }
 
-        Optional<Case> _case = caseUseCase.getCaseById(caseId);
+  @Consumes(MediaType.APPLICATION_JSON)
+  @PUT
+  @Path("/{caseId}")
+  public boolean update(long caseId, String payload) {
+    Case obj = new Case(caseId, "Betty", "Gates2", "Complete", Timestamp.from(Instant.now()), 3);
+    var response = Response.ok(caseUseCase.updateCase(obj)).build();
+    return false;
+  }
 
-        Case caseValue = _case.get();
-        if(caseValue.getCases_id() == 0){
-            return "No Cases found with case Id : " + " " + caseId;
-        }
-        else{
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            String json = objectMapper.writeValueAsString(_case.get());
-
-            return json;
-        }
-    }
-
-    @Consumes(MediaType.APPLICATION_JSON)
-    @POST
-    @Path("/create")
-    public boolean create(String payload){
-        long val = 0L; //case id generation value.
-        Case obj = new Case(val, "Betty", "Gates2", "Complete", Timestamp.from(Instant.now()), 3);
-        var response = Response.ok(caseUseCase.createCase(obj)).build();
-        return false;
-    }
-
-    @Consumes(MediaType.APPLICATION_JSON)
-    @PUT
-    @Path("/{caseId}")
-    public boolean update(long caseId, String payload)
-    {
-        Case obj = new Case(caseId, "Betty", "Gates2", "Complete", Timestamp.from(Instant.now()), 3);
-        var response = Response.ok(caseUseCase.updateCase(obj)).build();
-        return false;
-    }
-
-    @Produces(MediaType.APPLICATION_JSON)
-    @DELETE
-    @Path("/{caseId}")
-    public boolean delete(long caseId)
-    {
-        return caseUseCase.deleteCase(caseId);
-    }
+  @Produces(MediaType.APPLICATION_JSON)
+  @DELETE
+  @Path("/{caseId}")
+  public boolean delete(long caseId) {
+    return caseUseCase.deleteCase(caseId);
+  }
 }
--- api/src/main/java/gov/doj/adapters/gateways/AzureSqlGateway.java
+++ api/src/main/java/gov/doj/adapters/gateways/AzureSqlGateway.java
@@ -2,7 +2,6 @@ package gov.doj.adapters.gateways;
 
 import gov.doj.entities.Case;
 import gov.doj.usecases.PersistenceGateway;
-
 import java.sql.*;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -11,155 +10,162 @@ import java.util.Optional;
 
 public class AzureSqlGateway implements PersistenceGateway {
 
-    protected  ConnectionManager connectionManager;
+  protected ConnectionManager connectionManager;
 
-    public AzureSqlGateway(){
-        connectionManager = new ConnectionManager();
+  public AzureSqlGateway() {
+    connectionManager = new ConnectionManager();
+  }
+
+  @Override
+  public List<Case> getCases() {
+
+    List<Case> casesList = new ArrayList<>();
+    ResultSet resultSet = null;
+    String selectSql =
+        "SELECT cases_id, staff1, staff2, idi_status, idi_date, chapters_id from Cases";
+
+    try (Connection connection = this.connectionManager.getConnection();
+        Statement statement = connection.createStatement(); ) {
+
+      resultSet = statement.executeQuery(selectSql);
+      while (resultSet.next()) {
+        long id = resultSet.getLong("cases_id");
+        String staff1 = resultSet.getString("staff1");
+        String staff2 = resultSet.getString("staff2");
+        String idi_status = resultSet.getString("idi_status");
+        Timestamp idi_date = resultSet.getTimestamp("idi_date");
+        int chapters_id = resultSet.getInt("chapters_id");
+
+        Case _case = new Case(id, staff1, staff2, idi_status, idi_date, chapters_id);
+        casesList.add(_case);
+
+        System.out.println(
+            resultSet.getString(1)
+                + " "
+                + resultSet.getString(2)
+                + " "
+                + resultSet.getString(3)
+                + " "
+                + resultSet.getString(4)
+                + " "
+                + resultSet.getString(5)
+                + " "
+                + resultSet.getString(6)
+                + " ");
+      }
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
 
-    @Override
-    public List<Case> getCases() {
+    return casesList;
+  }
 
-        List<Case> casesList = new ArrayList<>();
-        ResultSet resultSet = null;
-        String selectSql = "SELECT cases_id, staff1, staff2, idi_status, idi_date, chapters_id from Cases";
+  @Override
+  public Optional<Case> getCase(long caseId) {
+    String selectSql =
+        "SELECT cases_id, staff1, staff2, idi_status, idi_date, chapters_id from Cases where"
+            + " cases_id = ?";
+    long cases_id = 0;
+    String staff1 = "", staff2 = "", idi_status = "";
+    Timestamp idi_date = Timestamp.from(Instant.now());
+    int chapters_id = 0;
 
-        try (Connection connection = this.connectionManager.getConnection();
-              Statement statement = connection.createStatement(); ){
+    try (Connection connection = this.connectionManager.getConnection();
+        PreparedStatement statement = connection.prepareStatement(selectSql); ) {
 
-            resultSet = statement.executeQuery(selectSql);
-            while (resultSet.next()) {
-                long id = resultSet.getLong("cases_id");
-                String staff1 = resultSet.getString("staff1");
-                String staff2 = resultSet.getString("staff2");
-                String idi_status = resultSet.getString("idi_status");
-                Timestamp idi_date = resultSet.getTimestamp("idi_date");
-                int chapters_id = resultSet.getInt("chapters_id");
+      statement.setString(1, String.valueOf(caseId));
+      ResultSet resultSet = statement.executeQuery();
 
-                Case _case = new Case(id, staff1, staff2, idi_status, idi_date, chapters_id);
-                casesList.add(_case);
+      if (resultSet.next()) {
+        cases_id = resultSet.getLong("cases_id");
+        staff1 = resultSet.getString("staff1");
+        staff2 = resultSet.getString("staff2");
+        idi_status = resultSet.getString("idi_status");
+        idi_date = resultSet.getTimestamp("idi_date");
+        chapters_id = resultSet.getInt("chapters_id");
+      }
 
+      return Optional.of(new Case(cases_id, staff1, staff2, idi_status, idi_date, chapters_id));
 
-                System.out.println(resultSet.getString(1) + " "
-                        + resultSet.getString(2) + " "
-                        + resultSet.getString(3) + " "
-                        + resultSet.getString(4) + " "
-                        + resultSet.getString(5) + " "
-                        + resultSet.getString(6) + " "
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-                );
-            }
+  @Override
+  public boolean createCase(Case aCaseObj) {
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    String sql =
+        "INSERT into Cases (staff1, staff2, idi_status, idi_date, chapters_id) VALUES (?, ?, ?, ?,"
+            + " ?)";
+    boolean rowInserted = false;
 
-        return casesList;
+    try (Connection connection = this.connectionManager.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql); ) {
+
+      statement.setString(1, aCaseObj.getStaff1());
+      statement.setString(2, aCaseObj.getStaff2());
+      statement.setString(3, aCaseObj.getIdi_status());
+      statement.setTimestamp(4, aCaseObj.getIdi_date());
+      statement.setInt(5, aCaseObj.getChapters_id());
+
+      rowInserted = statement.executeUpdate() > 0;
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
 
-    @Override
-    public Optional<Case> getCase(long caseId) {
-        String selectSql = "SELECT cases_id, staff1, staff2, idi_status, idi_date, chapters_id from Cases where cases_id = ?";
-        long cases_id = 0;
-        String staff1 = "", staff2 = "", idi_status = "";
-        Timestamp idi_date = Timestamp.from(Instant.now());
-        int chapters_id = 0;
+    return rowInserted;
+  }
 
-        try (Connection connection = this.connectionManager.getConnection();
-             PreparedStatement statement = connection.prepareStatement(selectSql); ) {
+  @Override
+  public boolean updateCase(Case aCaseObj) {
 
-            statement.setString(1, String.valueOf(caseId));
-            ResultSet resultSet = statement.executeQuery();
+    String sql =
+        "UPDATE Cases "
+            + "SET staff1 = ?, "
+            + "SET staff2 = ?, "
+            + "SET idi_status = ?,"
+            + "SET idi_date = ?,"
+            + "SET chapters_id = ?,";
+    sql += " WHERE cases_id = ?";
 
-            if(resultSet.next()) {
-                cases_id = resultSet.getLong("cases_id");
-                staff1 = resultSet.getString("staff1");
-                staff2 = resultSet.getString("staff2");
-                idi_status = resultSet.getString("idi_status");
-                idi_date = resultSet.getTimestamp("idi_date");
-                chapters_id = resultSet.getInt("chapters_id");
-            }
+    boolean rowUpdated = false;
+    try (Connection connection = this.connectionManager.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql); ) {
 
-            return Optional.of(new Case(cases_id, staff1, staff2, idi_status, idi_date, chapters_id));
+      statement.setString(1, aCaseObj.getStaff1());
+      statement.setString(2, aCaseObj.getStaff2());
+      statement.setString(3, aCaseObj.getIdi_status());
+      statement.setTimestamp(4, aCaseObj.getIdi_date());
+      statement.setInt(5, aCaseObj.getChapters_id());
+      statement.setLong(6, aCaseObj.getCases_id());
 
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
+      rowUpdated = statement.executeUpdate() > 0;
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
 
-    @Override
-    public boolean createCase(Case aCaseObj) {
+    return rowUpdated;
+  }
 
-        String sql = "INSERT into Cases (staff1, staff2, idi_status, idi_date, chapters_id) VALUES (?, ?, ?, ?, ?)";
-        boolean rowInserted = false;
+  @Override
+  public boolean deleteCase(long caseId) {
 
-        try (Connection connection = this.connectionManager.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql);){
+    String sql = "DELETE FROM cases where cases_id = ?";
+    boolean rowDeleted = false;
 
-            statement.setString(1, aCaseObj.getStaff1());
-            statement.setString(2, aCaseObj.getStaff2());
-            statement.setString(3,aCaseObj.getIdi_status());
-            statement.setTimestamp(4, aCaseObj.getIdi_date());
-            statement.setInt(5, aCaseObj.getChapters_id());
+    try (Connection connection = this.connectionManager.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql); ) {
 
-            rowInserted = statement.executeUpdate() > 0;
+      statement.setLong(1, caseId);
 
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
-
-
-        return rowInserted;
-    }
-
-    @Override
-    public boolean updateCase(Case aCaseObj) {
-
-        String sql = "UPDATE Cases " +
-                "SET staff1 = ?, " +
-                "SET staff2 = ?, " +
-                "SET idi_status = ?," +
-                "SET idi_date = ?," +
-                "SET chapters_id = ?,";
-        sql+= " WHERE cases_id = ?";
-
-        boolean rowUpdated = false;
-        try (Connection connection = this.connectionManager.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql);) {
-
-            statement.setString(1, aCaseObj.getStaff1());
-            statement.setString(2, aCaseObj.getStaff2());
-            statement.setString(3,aCaseObj.getIdi_status());
-            statement.setTimestamp(4, aCaseObj.getIdi_date());
-            statement.setInt(5, aCaseObj.getChapters_id());
-            statement.setLong(6, aCaseObj.getCases_id());
-
-            rowUpdated = statement.executeUpdate() > 0;
-
-        }catch(Exception e){
-            throw new RuntimeException(e);
-        }
-
-        return rowUpdated;
+      rowDeleted = statement.executeUpdate() > 0;
+    } catch (Exception e) {
 
     }
-
-    @Override
-    public boolean deleteCase(long caseId) {
-
-        String sql = "DELETE FROM cases where cases_id = ?";
-        boolean rowDeleted = false;
-
-        try (Connection connection = this.connectionManager.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql);) {
-
-            statement.setLong(1, caseId);
-
-            rowDeleted = statement.executeUpdate() > 0;
-        }catch(Exception e){
-
-
-        }
-        return false;
-    }
+    return false;
+  }
 }
