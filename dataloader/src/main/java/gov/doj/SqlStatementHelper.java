a3be42f8f8342f1220834e67be356e2efd8961a9
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-03-13 16:42:33 -0500

    111: Complete CMMDB insert

--- dataloader/src/main/java/gov/doj/SqlStatementHelper.java
+++ dataloader/src/main/java/gov/doj/SqlStatementHelper.java
@@ -6,8 +6,7 @@ import java.sql.Timestamp;
 import java.sql.Types;
 
 public class SqlStatementHelper {
-    public static void setString(int index, String data, PreparedStatement statement) {
-        System.out.println("Index: " + index + ", Data: " + data + ", setString.");
+    public static void setCharString(int index, String data, PreparedStatement statement) {
         try {
             if (data == null
                     || data.isEmpty()
@@ -20,9 +19,21 @@ public class SqlStatementHelper {
             throw new RuntimeException(e);
         }
     }
+    public static void setVarCharString(int index, String data, PreparedStatement statement) {
+        try {
+            if (data == null
+                    || data.isEmpty()
+                    || data.contains("NULL")) {
+                statement.setNull(index + 1, Types.VARCHAR);
+            } else {
+                statement.setString(index + 1, data);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
     
     public static void setInt(int index, String data, PreparedStatement statement) {
-        System.out.println("Index: " + index + ", Data: " + data + ", setInt.");
         try {
             if (data == null
                     || data.isEmpty()
@@ -35,9 +46,22 @@ public class SqlStatementHelper {
             throw new RuntimeException(e);
         }
     }
+
+    public static void setLong(int index, String data, PreparedStatement statement) {
+        try {
+            if (data == null
+                    || data.isEmpty()
+                    || data.contains("NULL")) {
+                statement.setNull(index + 1, Types.INTEGER);
+            } else {
+                statement.setLong(index + 1, Long.parseLong(data));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
     
     public static void setDouble(int index, String data, PreparedStatement statement) {
-        System.out.println("Index: " + index + ", Data: " + data + ", setDouble.");
         try {
             if (data == null
                     || data.isEmpty()
@@ -52,7 +76,6 @@ public class SqlStatementHelper {
     }
     
     public static void setTimestamp(int index, String data, PreparedStatement statement) {
-        System.out.println("Index: " + index + ", Data: " + data + ", setTimestamp.");
         try {
             if (data == null
                     || data.isEmpty()
@@ -63,6 +86,14 @@ public class SqlStatementHelper {
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static String maxChars(String data, int maxLength) {
+        if (data.length() <= maxLength) {
+            return data;
+        } else {
+            return data.substring(0, maxLength-1);
         }
     }
 }
