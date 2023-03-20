c0f578e4e8d9fceed31bf48f5a6b86fc2c8bcadf
SwathiLexi <swathi.lexi@gmail.com>
2023-03-20 13:23:58 -0700

    Removed the Static text on the home page - Total Active Cases.
    (cherry picked from commit 693125aa8f3d198c2588a4e0d35d6a92ebfa1e31)

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,13 +1,12 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './components/Home';
+//import { Home } from './components/Home';
 import { CaseList } from './components/CaseList';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}></Route>
         <Route path="/cases" element={<CaseList />}></Route>
       </Routes>
       <CaseList></CaseList>
