b8c29c4749b86397bb0030b807d72ad369a12a9a
Fritz Madden <fmadden@flexion.us>
2023-03-20 10:54:22 -0600

    111: frontend - Started using ENV Variables for config

--- gui/src/configuration/apiConfiguration.ts
+++ gui/src/configuration/apiConfiguration.ts
@@ -1,5 +1,5 @@
 export default {
-  server: 'localhost',
-  port: '8080',
-  protocol: 'http',
+  server: process.env['REACT_APP_SERVER_HOSTNAME'],
+  port: process.env['REACT_APP_SERVER_PORT'],
+  protocol: process.env['REACT_APP_SERVER_PROTOCOL'],
 };
