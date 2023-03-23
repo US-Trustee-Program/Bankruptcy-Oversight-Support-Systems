7d5def966298a473e4a857b9df140563c7a7f54c
Fritz Madden <fmadden@flexion.us>
2023-03-23 11:00:44 -0600

    Revert "111: frontend - mocha testing"
    This reverts commit c4dd6c10a41bad0b4076a1f9a6b566ba9f9c8305.
    
    Mocha Chai is being abandoned.

--- gui/tests/unit/App.test.tsx
+++ gui/tests/unit/App.test.tsx
@@ -1,29 +1,26 @@
 import 'jsdom-global/register';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { expect } from 'chai';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../../src/App';
 
-let rootContainer: HTMLDivElement;
-let root: ReactDOM.Root;
-
-before(() => {
-  rootContainer = document.createElement('div');
-  root = ReactDOM.createRoot(rootContainer);
-});
+const rootContainer = document.createElement('div');
+const root = ReactDOM.createRoot(rootContainer);
 
 describe('Test default route', () => {
-  it('Show display a list of cases', (): void => {
+  it('Show display a list of cases', () => {
     root.render(
       <BrowserRouter>
         <App />
       </BrowserRouter>,
     );
 
-    const h1 = document.querySelector('h1');
+    const h1 = rootContainer.querySelector('h1');
     if (h1) {
-      expect(h1).to.not.be.null;
-      expect((h1 as HTMLHeadingElement).textContent).to.equal('Case List');
+      expect(h1.textContent).to.equal('Case List');
+    } else {
+      return false;
     }
   });
 });
