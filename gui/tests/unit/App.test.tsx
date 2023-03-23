047341fbe914b27bd375926a5d98a620302b51b0
Fritz Madden <fmadden@flexion.us>
2023-03-23 10:56:22 -0600

    111: frontend - refactored testing
    After much trial and error, it seems best to stick with Reacts standard
      react testing library and jest framework.
    
    Mocha and Chai have been abandoned.
    
    However, to match the folder layout of the node api, all frontend tests
    will be placed above the src folder in the tests folder.

---
+++ gui/tests/unit/App.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../../src/App';
+
+test('renders Total Active Cases', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+
+  const h1 = screen.getByText(/Case List/i);
+  expect(h1).toBeInTheDocument();
+
+  const tableHeader = screen.getAllByRole('columnheader');
+  expect(tableHeader[0].textContent).toBe('Case Div');
+  expect(tableHeader[1].textContent).toBe('Case Year');
+  expect(tableHeader[2].textContent).toBe('Case Number');
+  expect(tableHeader[3].textContent).toBe('Chapter');
+  expect(tableHeader[4].textContent).toBe('Staff 1');
+  expect(tableHeader[5].textContent).toBe('Staff 2');
+});
