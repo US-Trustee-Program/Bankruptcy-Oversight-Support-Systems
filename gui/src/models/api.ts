8408d60f4aa5c6c10e7120d6ef3e9fc8d5d3b213
Fritz Madden <fmadden@flexion.us>
2023-03-27 14:03:05 -0600

    111: Frontend - troubleshooting mystery error

--- gui/src/models/api.ts
+++ gui/src/models/api.ts
@@ -58,10 +58,9 @@ export default class Api {
         );
       }
     } catch (e) {
+      console.error(e);
       return Promise.reject(
-        //new Error(`404 Error - Not found ${(e as Error).message} - caught error`),
-        //new Error(`404 Error - Not found - caught error`),
-        console.error(e),
+        new Error(`404 Error - Not found ${(e as Error).message} - caught error`),
       );
     }
   }
