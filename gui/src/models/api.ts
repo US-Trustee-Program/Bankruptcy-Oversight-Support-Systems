02df6553f6fd994e572ebe9076146329055757a5
Fritz Madden <fmadden@flexion.us>
2023-03-30 11:31:45 -0600

    111: Started Login screen for frontend homepage

--- gui/src/models/api.ts
+++ gui/src/models/api.ts
@@ -14,7 +14,7 @@ export type ResponseError = {
 export default class Api {
   private _host = `${config.protocol}://${config.server}:${config.port}`;
 
-  public async create(path: string, body: object): Promise<ResponseData> {
+  public static async create(path: string, body: object): Promise<ResponseData> {
     try {
       const response = await fetch(this._host + path, {
         method: 'POST',
@@ -36,7 +36,7 @@ export default class Api {
     }
   }
 
-  public async list(path: string): Promise<ResponseData> {
+  public static async list(path: string): Promise<ResponseData> {
     try {
       const response = await fetch(this._host + path, {
         method: 'GET',
@@ -65,7 +65,7 @@ export default class Api {
     }
   }
 
-  public async read(path: string, id: string): Promise<ResponseData> {
+  public static async read(path: string, id: string): Promise<ResponseData> {
     try {
       const response = await fetch(`${this._host}${path}/${id}`, {
         method: 'GET',
@@ -86,7 +86,7 @@ export default class Api {
     }
   }
 
-  public async update(path: string, id: string, body: object): Promise<ResponseData> {
+  public static async update(path: string, id: string, body: object): Promise<ResponseData> {
     try {
       const response = await fetch(`${this._host}${path}/${id}`, {
         method: 'PATCH',
@@ -108,7 +108,7 @@ export default class Api {
     }
   }
 
-  public async replace(path: string, id: string, body: object): Promise<ResponseData> {
+  public static async replace(path: string, id: string, body: object): Promise<ResponseData> {
     try {
       const response = await fetch(`${this._host}${path}/${id}`, {
         method: 'PUT',
@@ -130,7 +130,7 @@ export default class Api {
     }
   }
 
-  public async del(path: string, id: string) {
+  public static async del(path: string, id: string) {
     try {
       const response = await fetch(`${this._host}${path}/${id}`, {
         method: 'DELETE',
@@ -142,6 +142,28 @@ export default class Api {
         return data;
       } else {
         return Promise.reject(new Error(`500 Error - Failed to Delete ${data?.toString()}`));
+      }
+    } catch (e: unknown) {
+      return Promise.reject(new Error(`500 Error - Invalid Request ${(e as Error).message}`));
+    }
+  }
+
+  public static async post(path: string, body: object): Promise<ResponseData> {
+    try {
+      const response = await fetch(this._host + path, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json;charset=UTF-8',
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(new Error(`500 Error - Invalid Request ${data?.toString()}`));
       }
     } catch (e: unknown) {
       return Promise.reject(new Error(`500 Error - Invalid Request ${(e as Error).message}`));
