283fbcaa4d456f2fe14aae27ae0853631bfa2b1a
Fritz Madden <fmadden@flexion.us>
2023-04-03 15:32:51 -0600

    111: Node API - Updated live in-memory data
    Updated live in-memory data for new endpoint and case list end point to
    reflect new data being returned by endpoints in live MSSQL gateway.

--- api/src/adapters/gateways/users.local.inmemory.gateway.ts
+++ api/src/adapters/gateways/users.local.inmemory.gateway.ts
@@ -1,20 +1,20 @@
-import log from '../logging.service.js';
-import { getProperty, mockData } from '../mock-data/index.js';
-import { ObjectKeyVal, RecordObj } from '../types/basic.js';
 import { DbResult } from '../types/database.js';
 
 const NAMESPACE = 'USERS-LOCAL-INMEMORY-DATA-MODULE';
-
-async function initializeCases(): Promise<ObjectKeyVal[]> {
-  return [];
-}
 
 const login = async (userName: { firstName: string, lastName: string }): Promise<DbResult> => {
   const results = {
     success: true,
     message: `User record`,
-    count: 0,
-    body: [],
+    count: 1,
+    body: [
+      {
+          "first_name": "Foo             ",
+          "middle_initial": " ",
+          "last_name": "Bar                        ",
+          "professional_id": 123
+      }
+    ],
   };
   return results;
 };
