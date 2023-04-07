22a8e8d4d73175f490001a69e749f16a01ec9bcd
SwathiLexi <swathi.lexi@gmail.com>
2023-04-07 14:08:29 -0700

    Updated the SQL Query to fetch Debtor name and concatenate the first and last names of Staff members. Added additional changes as per the customer feedback for story 111.

--- api/src/adapters/controllers/users.controller.ts
+++ api/src/adapters/controllers/users.controller.ts
@@ -1,8 +1,8 @@
-import log from "../logging.service";
-import { httpError, httpSuccess } from "../utils/http";
+import log from "../logging.service.js";
+import { httpError, httpSuccess } from "../utils/http.js";
 import useCase from '../../use-cases/index.js';
-import { UserPersistenceGateway } from "../types/persistence-gateway";
-import proxyData from "../data-access.proxy";
+import { UserPersistenceGateway } from "../types/persistence-gateway.js";
+import proxyData from "../data-access.proxy.js";
 
 const NAMESPACE = 'USERS-CONTROLLER';
 
