c7360969871569e6118f1aaf907b0f122c3f52cd
Fritz Madden <fmadden@flexion.us>
2023-04-20 16:11:04 -0600

    111: Started building out a node function
    Started building out a node function.  In it's very early stages.

--- functions/node/users/index.ts
+++ functions/node/users/index.ts
@@ -1,8 +1,12 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { UsersController } from '../adapters/controllers/users.controller';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
+    const firstName = (req.query.first_name || (req.body && req.body.first_name));
+    const lastName = (req.query.last_name || (req.body && req.body.last_name));
+
+    const HEADERS = {'Content-Type': 'application/json'}
+
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
