ddd0ce7b4c5fe021c357214f1d3d30784f546453
Fritz Madden <fmadden@flexion.us>
2023-04-25 17:35:55 -0600

    112: Fleshed out user login node function
    Code added but not yet tested successfully.

---
+++ functions/node/adapters/services/logger.service.ts
@@ -0,0 +1,44 @@
+import pine from 'pine';
+
+const logger = pine();
+
+const consoleMap = {
+  info: logger.info,
+  warn: logger.warn,
+  error: logger.error,
+  debug: logger.debug,
+};
+
+type ObjectKey = keyof typeof consoleMap;
+
+export default class log {
+  private static logMessage(logType: string, namespace: string, message: string, data?: any) {
+    logType = logType.toLowerCase();
+
+    if (!['info', 'warn', 'error', 'debug'].includes(logType)) {
+        throw new Error(`Log type ${logType} must be 'info', 'warn', 'error', or 'debug'`);
+    }
+
+    if (data) {
+        consoleMap[logType as ObjectKey](`[${logType.toUpperCase()}] [${namespace}] ${message} ${undefined != data ? JSON.stringify(data) : ''}`);
+    } else {
+        consoleMap[logType as ObjectKey](`[${logType.toUpperCase}] [${namespace}] ${message}`);
+    }
+  }
+
+  public static info(namespace: string, message: string, data?: any) {
+    log.logMessage('info', namespace, message, data);
+  }
+
+  public static warn(namespace: string, message: string, data?: any) {
+    log.logMessage('warn', namespace, message, data);
+  }
+
+  public static error(namespace: string, message: string, data?: any) {
+    log.logMessage('error', namespace, message, data);
+  }
+
+  public static debug(namespace: string, message: string, data?: any) {
+    log.logMessage('debug', namespace, message, data);
+  }
+}
