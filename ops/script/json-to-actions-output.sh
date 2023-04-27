5d9306f28a721fe7550a374c28a9405254be62d3
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-04-27 11:54:18 -0700

    base64 encode job outputs

--- ops/script/json-to-actions-output.sh
+++ ops/script/json-to-actions-output.sh
@@ -7,12 +7,12 @@ fi
 # test=$(cat outputs.json | jp -u "[keys(@), *.value]")
 # len=$(cat outputs.json | jp -u "length([keys(@), *.value] | [0])")
 
-value1=$(cat $1 | jq -r .functionAppId.value)
-value2=$(cat $1 | jq -r .functionAppName.value)
-value3=$(cat $1 | jq -r .webappId.value)
-value4=$(cat $1 | jq -r .webappName.value)
+value1=$(cat $1 | jq -r .functionAppId.value | base64)
+value2=$(cat $1 | jq -r .functionAppName.value | base64)
+value3=$(cat $1 | jq -r .webappId.value | base64)
+value4=$(cat $1 | jq -r .webappName.value | base64)
 
-echo "functionAppId=${value1}" >> $GITHUB_OUTPUT
-echo "functionAppName=${value2}" >> $GITHUB_OUTPUT
-echo "webappId=${value3}" >> $GITHUB_OUTPUT
-echo "webappName=${value4}" >> $GITHUB_OUTPUT
+echo "functionAppId=${value1}" >>$GITHUB_OUTPUT
+echo "functionAppName=${value2}" >>$GITHUB_OUTPUT
+echo "webappId=${value3}" >>$GITHUB_OUTPUT
+echo "webappName=${value4}" >>$GITHUB_OUTPUT
