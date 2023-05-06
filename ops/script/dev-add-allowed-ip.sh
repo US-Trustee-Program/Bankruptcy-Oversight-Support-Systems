0ae5d917afe26c0b05d34fe2ec23b4940c87cf03
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-05-05 18:14:20 -0700

    Include scripts to help with adding and removing access restriction rules specific to webapp and api
    [skipci]

--- ops/script/dev-add-allowed-ip.sh
+++ ops/script/dev-add-allowed-ip.sh
@@ -12,9 +12,19 @@ app_rg=$1
 stack_name=$2
 priority=$3
 
+if [[ -z "${app_rg}" || -z "${stack_name}" || -z "${priority}" ]]; then
+    echo "Error: Missing parameters. Usage: dev-add-allowed-ip.sh <resource_group_name:str> <stack_name:str> <priority:int>"
+    exit 1
+fi
+
 agentIp=$(curl -s https://api.ipify.org)
+
 ruleName="dev-agent-${agentIp:0:16}"
+
+echo "Attempting to add Ip allow rule (${ruleName})"
+
 az functionapp config access-restriction add -g $app_rg -n "${stack_name}-node-api" --rule-name $ruleName --action Allow --ip-address $agentIp --priority "${priority}1" 1>/dev/null
+
 az functionapp config access-restriction add -g $app_rg -n "${stack_name}-webapp" --rule-name $ruleName --action Allow --ip-address $agentIp --priority "${priority}2" 1>/dev/null
 
 echo "Done"
