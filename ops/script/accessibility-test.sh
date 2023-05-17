81f0018e2f9f4e21e9cad1690ba9754f2a377586
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-05-17 09:38:23 -0700

    CAMS-12 Move gha inline script to separate script file

---
+++ ops/script/accessibility-test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+source pa11y.env
+npm run build
+npm install serve -g
+serve -s build &
+timeout 120s sh -c 'until nc -z localhost 3000; do sleep 1; done'
+if (( $? != 0 )); then
+    echo "Webapp check timed out"
+    exit 1
+fi
+
+echo "Starting pa11y accessibility test"
+ERRORS=$(pa11y-ci -j | jq .errors)
+if [ $ERRORS -eq 0 ]; then
+  echo "pa11y found no errors."
+else
+  echo "pa11y found ${ERRORS} errors."
+  exit 1
+fi
