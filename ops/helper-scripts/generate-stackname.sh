b7fa058a3f55e24145c96f11a4af75a7eae6593a
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-05-25 09:11:14 -0700

    CAMS-21 fixes for generating stackname for Azure resource naming

---
+++ ops/helper-scripts/generate-stackname.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Description: Helper script to generate stack name for naming Azure resources
+# Usage: generate-stackname.sh <environment:str> <application_name:str> <suffix:str> <branchName:str>
+
+set -euo pipefail
+
+if [[ $1 == "Main" ]]; then
+    echo $2
+    exit 0
+else
+    stackname=$2
+
+    if [[ -n $3 ]]; then
+        stackname="${stackname}$3"
+    fi
+
+    if [[ -n $4 ]]; then
+        if [[ $(uname) == "Darwin" ]]; then
+            hash=$(echo -n $4 | openssl sha256 | awk '{print $1}')
+        else
+            hash=$(echo -n $4 | openssl sha256 | awk '{print $2}')
+        fi
+        short_hash=${hash:0:6}
+        stackname="${stackname}-$short_hash"
+    fi
+    echo $stackname
+    exit 0
+fi
