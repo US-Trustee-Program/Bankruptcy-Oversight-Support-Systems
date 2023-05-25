d34406801f44420742be3e43b421d2c953d18739
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-05-25 15:20:40 -0700

    CAMS-21 Allow access for GHA runner to run healthcheck test

--- ops/helper-scripts/dev-add-allowed-ip.sh
+++ ops/helper-scripts/dev-add-allowed-ip.sh
@@ -4,13 +4,18 @@
 # Prerequisite:
 #   - curl
 #   - Azure CLI
-# Usage: dev-add-allowed-ip.sh <resource_group_name:str> <stack_name:str> <priority:int>
+# Usage: dev-add-allowed-ip.sh <resource_group_name:str> <stack_name:str> <priority:int> <isCICD:bool>
 
 set -euo pipefail
 
 app_rg=$1
 stack_name=$2
 priority=$3
+if (($# == 3)); then
+    ci=$4
+else
+    ci=
+fi
 
 if [[ -z "${app_rg}" || -z "${stack_name}" || -z "${priority}" ]]; then
     echo "Error: Missing parameters. Usage: dev-add-allowed-ip.sh <resource_group_name:str> <stack_name:str> <priority:int>"
@@ -19,10 +24,12 @@ fi
 
 agentIp=$(curl -s https://api.ipify.org)
 
-ruleName="dev-agent-${agentIp:0:16}"
-
+if [[ -n $ci ]]; then
+    ruleName="gha-agent-${stack_name:0:16}"
+else
+    ruleName="dev-agent-${agentIp:0:16}"
+fi
 echo "Attempting to add Ip allow rule (${ruleName})"
-
 az functionapp config access-restriction add -g $app_rg -n "${stack_name}-node-api" --rule-name $ruleName --action Allow --ip-address $agentIp --priority "${priority}1" 1>/dev/null
 
 az functionapp config access-restriction add -g $app_rg -n "${stack_name}-webapp" --rule-name $ruleName --action Allow --ip-address $agentIp --priority "${priority}2" 1>/dev/null
