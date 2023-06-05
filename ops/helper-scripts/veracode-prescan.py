5cdc0ecea8a1648cbbab2f39623df89d2672e78f
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-06-05 13:21:41 -0700

    CAMS-21 update prescan script to check prescan status

--- ops/helper-scripts/veracode-prescan.py
+++ ops/helper-scripts/veracode-prescan.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import json
+import time
 from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 
 api_base = "https://api.veracode.us/was/configservice/v1"
@@ -17,6 +18,23 @@ except requests.RequestException as e:
 
 if response.ok:
     print(response.status_code)
+    counter = 0
+    while counter < 20:
+      counter = counter + 1
+      time.sleep(30)
+      statusResp = requests.get(api_base + "/analyses/b022cc5c5616b85d0653acb6f5a5d5e0/scans", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
+
+      data = statusResp.json()
+      status = data['_embedded']['scans'][0]['latest_occurrence_status']['status_type']
+      print("{} {}".format(count, status))
+      if status == 'FINISHED_VERIFYING_RESULTS':
+          print("Veracode Prescan Status Good")
+          sys.exit(0)
+
+    print("Veracode Prescan timeout")
+    sys.exit(2)
+
 else:
     print(response.status_code)
     print(response.text)
+    sys.exit(1)
