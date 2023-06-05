7f992a3fbb3d394475d9211469c8ccbd152968e6
Fritz Madden <fmadden@flexion.us>
2023-06-05 11:22:55 -0600

    CAMS-37: Added local pacer gateway code\n\nCo-authored-by: SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>\nCo-authored-by: Fritz Madden <fmadden@flexion.us>\nCo-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>\n

---
+++ backend/functions/lib/adapters/gateways/local.pacer.gateway.ts
@@ -0,0 +1,18 @@
+import { PacerGatewayInterface } from './pacer.gateway.interface';
+import { Chapter15Case } from '../types/cases';
+
+class LocalPacerGateway implements PacerGatewayInterface {
+  async getChapter15Cases(startingMonth: number): Promise<Chapter15Case[]> {
+    let chapter15Case: Chapter15Case = {
+      caseNumber: "22-44449",
+      caseTitle: "Flo Esterly and Neas Van Sampson",
+      dateFiled: "2005-05-04",
+    };
+
+    const chapter15CaseArray: Chapter15Case[] = [chapter15Case];
+
+    return chapter15CaseArray;
+  }
+}
+
+export { LocalPacerGateway }
