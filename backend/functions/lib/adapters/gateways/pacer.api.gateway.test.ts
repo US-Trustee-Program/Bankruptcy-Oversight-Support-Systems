8785da6cf3973f815ea1b18d85febeae068c297c
SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
2023-06-05 14:46:48 -0700

    Chapter 15 Cases : Started the unit test on the pacer.api.gateway.test. Need to update the fetch method mock call.
    Co-authored-by: SwathiLexi <30849469+swathilexi@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>

--- backend/functions/lib/adapters/gateways/pacer.api.gateway.test.ts
+++ backend/functions/lib/adapters/gateways/pacer.api.gateway.test.ts
@@ -1,5 +1,21 @@
+///<reference path="../../../node_modules/@types/node/globals.global.d.ts"/>
+//import {getGlobalObject} from "./misc";
+import { PacerApiGateway } from "./pacer.api.gateway";
+import * as db from "./local.inmemory.gateway";
+
 describe('PACER API gateway tests', () => {
     test('should return error message for non-200 response', async () => {
 
+         jest.mock('fetch', () => {
+             json:() => ({status: 401, message: 'Unauthorized user' })
+         });
+
+         const gateway = new PacerApiGateway();
+         const response = await gateway.getChapter15Cases()
+
+        expect(response).toEqual({status: 401, message: 'Unauthorized user' });
+
     });
 });
+
+
