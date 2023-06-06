cde943cbf425ca01f6b4e0727abaa9d728161d7a
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-06-05 17:12:14 -0700

    CAMS-21 Refactoring to workflow file and DAST scan script

--- ops/helper-scripts/veracode-dast-scan.py
+++ ops/helper-scripts/veracode-dast-scan.py
@@ -1,25 +1,48 @@
 import sys
 import requests
-import json
-import time
 from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 
-api_base = "https://api.veracode.us/was/configservice/v1"
-headers = {"User-Agent": "USTP Script", "Content-Type":"application/json"}
+API_BASE = "https://api.veracode.us/was/configservice/v1"
+REQUEST_HEADERS = {"User-Agent": "USTP Script", "Content-Type":"application/json"}
+
+DEFAULT_DAST_NAME = "CAMS Flexion"
+
+def get_analysis_id(name):
+    name = name.replace(" ", "%%20").strip()
+    response = requests.get(API_BASE + "/analyses", params={ 'name' : name }, auth=RequestsAuthPluginVeracodeHMAC(), headers=REQUEST_HEADERS)
+    if response.ok:
+        data = response.json()
+        return data['_embedded']['analyses'][0]['analysis_id']
+    else:
+        print(response.status_code)
+        print(response.text)
+        sys.exit(1) # Request failed to retrieve analysis id
+
+def schedule_dast_analysis(name, id):
+    payload = {}
+    payload['name'] = name
+    payload['schedule'] = {
+        'now' : True,
+        'duration' : {
+            'length' : 1,
+            'unit' : 'DAY'
+        }
+    }
+    response = requests.put("{}/analyses/{}".format(API_BASE, id), params={ 'method' : 'PATCH' }, auth=RequestsAuthPluginVeracodeHMAC(), headers=REQUEST_HEADERS, json=payload)
+    if response.ok:
+        print(response.status_code)
+        sys.exit(0)
+    else:
+        print(response.status_code)
+        print(response.text)
+        sys.exit(2) # Request failed to schedule DAST scan
 
 try:
-    payload = json.loads('{"name":"CAMS Flexion","schedule":{"now":true,"duration":{"length":1,"unit":"DAY"}}}')
+    dast_name = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_DAST_NAME
 
-    response = requests.put(api_base + "/analyses/b022cc5c5616b85d0653acb6f5a5d5e0?method=PATCH", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers, json=payload)
+    analysis_id = get_analysis_id(dast_name)
+    schedule_dast_analysis(dast_name, analysis_id)
 
 except requests.RequestException as e:
     print(e)
-    sys.exit(1)
-
-if response.ok:
-    print(response.status_code)
-    sys.exit(0)
-else:
-    print(response.status_code)
-    print(response.text)
-    sys.exit(1)
+    sys.exit(3)
