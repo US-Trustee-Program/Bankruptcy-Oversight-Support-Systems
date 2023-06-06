5a5d73a8c61ed4d4938bd83fdcb94111b4fcb335
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-06-06 09:54:19 -0700

    CAMS-21 Refactoring for prescan script

--- ops/helper-scripts/veracode-prescan.py
+++ ops/helper-scripts/veracode-prescan.py
@@ -1,28 +1,48 @@
 import sys
 import requests
-import json
 import time
 from veracode_api_signing.plugin_requests import RequestsAuthPluginVeracodeHMAC
 
-api_base = "https://api.veracode.us/was/configservice/v1"
-headers = {"User-Agent": "USTP Script", "Content-Type":"application/json"}
+API_BASE = "https://api.veracode.us/was/configservice/v1"
+REQUEST_HEADERS = {"User-Agent": "USTP Script", "Content-Type":"application/json"}
 
-try:
-    payload = json.loads('{"name":"CAMS Flexion"}')
+DEFAULT_DAST_NAME = "CAMS Flexion"
 
-    response = requests.put(api_base + "/analyses/b022cc5c5616b85d0653acb6f5a5d5e0?method=PATCH&run_verification=true", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers, json=payload)
+def get_analysis_id(name):
+    name = name.replace(" ", "%%20").strip()
+    response = requests.get(API_BASE + "/analyses", params={ 'name' : name }, auth=RequestsAuthPluginVeracodeHMAC(), headers=REQUEST_HEADERS)
+    if response.ok:
+        data = response.json()
+        return data['_embedded']['analyses'][0]['analysis_id']
+    else:
+        print(response.status_code)
+        print(response.text)
+        sys.exit(1) # Request failed to retrieve analysis id
 
-except requests.RequestException as e:
-    print(e)
-    sys.exit(1)
+def schedule_dast_prescan(name, id):
+    payload = {}
+    payload['name'] = name
 
-if response.ok:
-    print(response.status_code)
+    response = requests.put(
+        "{}/analyses/{}".format(API_BASE, id),
+        params={ 'method' : 'PATCH', 'run_verification' : 'true' },
+        auth=RequestsAuthPluginVeracodeHMAC(),
+        headers=REQUEST_HEADERS,
+        json=payload)
+
+    if response.ok:
+        print(response.status_code)
+    else:
+        print(response.status_code)
+        print(response.text)
+        sys.exit(1)
+
+def status_check_dast_prescan(id):
     counter = 0
     while counter < 20:
       counter = counter + 1
       time.sleep(30)
-      statusResp = requests.get(api_base + "/analyses/b022cc5c5616b85d0653acb6f5a5d5e0/scans", auth=RequestsAuthPluginVeracodeHMAC(), headers=headers)
+      statusResp = requests.get("{}/analyses/{}/scans".format(API_BASE, id), auth=RequestsAuthPluginVeracodeHMAC(), headers=REQUEST_HEADERS)
 
       data = statusResp.json()
       status = data['_embedded']['scans'][0]['latest_occurrence_status']['status_type']
@@ -30,11 +50,15 @@ if response.ok:
       if status == 'FINISHED_VERIFYING_RESULTS':
           print("Veracode Prescan Status Good")
           sys.exit(0)
+      elif status == 'VERIFICATION_FAILED':
+          print("VERIFICATION_FAILED")
+          sys.exit(2)
 
     print("Veracode Prescan timeout")
     sys.exit(2)
 
-else:
-    print(response.status_code)
-    print(response.text)
-    sys.exit(1)
+dast_name = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_DAST_NAME
+
+analysis_id = get_analysis_id(dast_name)
+schedule_dast_prescan(dast_name, analysis_id)
+status_check_dast_prescan(analysis_id)
