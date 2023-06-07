a21a5577e3442c81acb68d2abe8948baab2b292e
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-06-07 13:07:05 -0500

    CAMS-37: Fix pacer api test [skip ci]
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>
    Co-authored-by: SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>

--- backend/functions/lib/adapters/gateways/pacer.api.gateway.test.ts
+++ backend/functions/lib/adapters/gateways/pacer.api.gateway.test.ts
@@ -2,9 +2,12 @@
 //import {getGlobalObject} from "./misc";
 import { PacerApiGateway } from "./pacer.api.gateway";
 import { Chapter15Case } from '../types/cases';
+import {GatewayHelper} from "./gateway-helper";
 const http = require('../utils/http');
 
 describe('PACER API gateway tests', () => {
+    const gatewayHelper = new GatewayHelper();
+
     test('should return error message for non-200 response', async () => {
         const responseValue = {status: 401, message: 'Unauthorized user' };
         jest.spyOn(http, 'httpPost').mockImplementation(() => {
@@ -19,21 +22,28 @@ describe('PACER API gateway tests', () => {
     });
 
     test('should return content for 200 response', async () => {
+        const mockedApiResponse = gatewayHelper.pacerMockExtract().slice(0, 2);
         const expectedResponseValue: Chapter15Case[] = [
             {
-                caseNumber: '23-1234',
-                caseTitle: '',
-                dateFiled: '',
+                caseNumber: '04-44449',
+                caseTitle: 'Flo Esterly and Neas Van Sampson',
+                dateFiled: '2005-05-04',
             },
+            {
+                caseNumber: '06-1122',
+                caseTitle: 'Jennifer Millhouse',
+                dateFiled: '2006-03-27',
+            }
         ];
         jest.spyOn(http, 'httpPost').mockImplementation(() => {
             return {
-                json:() => ({ content: expectedResponseValue }),
+                json:() => ({ content: mockedApiResponse }),
                 status: 200,
             };
         });
 
         const gateway = new PacerApiGateway();
-        await expect(gateway.getChapter15Cases()).toEqual(expectedResponseValue);
+
+        expect(await gateway.getChapter15Cases()).toEqual(expectedResponseValue);
     });
 });
