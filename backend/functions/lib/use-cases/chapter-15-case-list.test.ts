313c374457ab5012ade3c3db3c55c053b4673c1b
SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
2023-06-07 12:28:25 -0700

    Chapter 15 Cases : Removed redundant test for testing the Unauthorized use for local gateway. Tried fixing the pacer local gateway test.
    Co-authored-by: SwathiLexi <30849469+swathilexi@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>

--- backend/functions/lib/use-cases/chapter-15-case-list.test.ts
+++ backend/functions/lib/use-cases/chapter-15-case-list.test.ts
@@ -1,6 +1,13 @@
+import * as http from "http";
+
 const context = require('azure-function-context-mock');
-import { CaseListDbResult } from '../adapters/types/cases';
+import { CaseListDbResult, Chapter15Case } from "../adapters/types/cases";
 import Chapter15CaseList from './chapter-15-case-list';
+import { PacerApiGateway } from "../adapters/gateways/pacer.api.gateway";
+import { PacerGatewayInterface } from "./pacer.gateway.interface";
+import { getPacerGateway } from "../../factory";
+
+
 
 const mockChapterList: CaseListDbResult = {
   success: true,
@@ -12,10 +19,31 @@ const mockChapterList: CaseListDbResult = {
 }
 
 describe('Chapter 15 case tests', () => {
-  xtest('Calling getChapter15CaseList should return valid chapter 15 data', async () => {
+  test('Calling getChapter15CaseList should return valid chapter 15 data', async () => {
     const chapter15CaseList = new Chapter15CaseList;
     const results = await chapter15CaseList.getChapter15CaseList(context);
-
+    const expectedResponseValue: Chapter15Case[] = [
+      {
+        caseNumber: '04-44449',
+        caseTitle: 'Flo Esterly and Neas Van Sampson',
+        dateFiled: '2005-05-04',
+      },
+      {
+        caseNumber: '06-1122',
+        caseTitle: 'Jennifer Millhouse',
+        dateFiled: '2006-03-27',
+      }
+    ];
+    const pacerGateway: PacerGatewayInterface = getPacerGateway();
+    jest.mock('../adapters/gateways/pacer.api.gateway');
+    const fakeGetChapter15Cases = jest.fn(async () => {
+      return expectedResponseValue
+    });
+    jest.mocked(PacerApiGateway).mockImplementation(  () => {
+      return{
+        method: fakeGetChapter15Cases
+      }
+    })
     expect(results).toEqual(mockChapterList);
   });
 });
