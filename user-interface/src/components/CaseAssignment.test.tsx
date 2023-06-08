37f9fb856b67a1ec26dcbbeb9dd70bbe360de684
Fritz Madden <fmadden@flexion.us>
2023-06-08 12:59:12 -0600

    CAMS-49: Planned tests for CaseAssignment module
    Co-authored-by: SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>

---
+++ user-interface/src/components/CaseAssignment.test.tsx
@@ -0,0 +1,6 @@
+// test that Loading... is displayed while we wait for the api to return data
+// if api.list returns an empty set, then screen should contain an empty table with valid header but no content
+// if api.list returns more than 0 results, then all results will be displayed in table body content
+
+// for UX, it might be good to put a time limit on the api call to return results, and display an appropriate screen message to user.
+// for UX, do we want to limit number of results to display on screen (pagination discussion to table for now)
