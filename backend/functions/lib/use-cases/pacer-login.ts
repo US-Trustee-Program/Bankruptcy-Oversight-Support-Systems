6a78fe906de0fce8714209f6aaca7607c43777a2
Fritz Madden <fmadden@flexion.us>
2023-06-14 13:33:52 -0600

    CAMS-39: Debugged promise rejection for getPacerToken
    Co-authored-by: Fritz Madden <fmadden@flexion.us>
    Co-authored-by: SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>

--- backend/functions/lib/use-cases/pacer-login.ts
+++ backend/functions/lib/use-cases/pacer-login.ts
@@ -2,34 +2,39 @@ import { Context } from '../adapters/types/basic';
 import * as dotenv from 'dotenv';
 import { axiosPost } from '../adapters/utils/http';
 
-dotenv.config ();
-
-
+dotenv.config();
 
 namespace UseCases {
   export class PacerLogin {
     private readonly functionContext: Context;
 
     async getPacerToken(context: Context): Promise<string> {
-      const response = await axiosPost({
-        url: 'https://qa-login.uscourts.gov/services/cso-auth',
-        body: {
-          'loginId': 'username',
-          'password': ''
-        },
-      });
-      if (response.status == 200){
-        if (response.data.loginResult != 1){
-          Promise.resolve(response.data.nextGenCSO);
+      // Get existing token OR login if no existing token
+      try {
+        const response = await axiosPost({
+          url: 'https://qa-login.uscourts.gov/services/cso-auth',
+          body: {
+            loginId: 'username',
+            password: '',
+          },
+        });
+        if (response.status == 200) {
+          if (response.data.loginResult == 0) {
+            return response.data.nextGenCSO;
+          } else if (response.data.loginResult == 1) {
+            throw Error(response.data.errorDescription);
+          } else {
+            throw Error('Error retrieving token');
+          }
         } else {
-          Promise.reject('Login Failed');
+          throw Error('Failed to Connect to PACER API');
         }
-      } else { 
-        Promise.reject('Failed to Connect to PACER API');
+        // - should we also check if the existing token is still valid?
+        return response;
+      } catch (e) {
+        console.log('PacerLogin.getPacerToken uscourts.gov rejected with a Failed Response');
+        console.log(e);
       }
-      return response;
-    // Get existing token OR login if no existing token
-    // - should we also check if the existing token is still valid?
     }
   }
 }
