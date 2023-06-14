d8799c6fff8715a76a6f67219035e26297176e27
Arthur Morrow <amorrow@flexion.us>
2023-06-14 14:55:45 -0400

    CAMS-39 added getPacerToken function login within pacer-login.ts

--- backend/functions/lib/use-cases/pacer-login.ts
+++ backend/functions/lib/use-cases/pacer-login.ts
@@ -1,16 +1,35 @@
 import { Context } from '../adapters/types/basic';
 import * as dotenv from 'dotenv';
+import { axiosPost } from '../adapters/utils/http';
 
-dotenv.config();
+dotenv.config ();
+
+
 
 namespace UseCases {
   export class PacerLogin {
     private readonly functionContext: Context;
 
     async getPacerToken(context: Context): Promise<string> {
-      // Get existing token OR login if no existing token
-      // - should we also check if the existing token is still valid?
-      return process.env.PACER_TOKEN;
+      const response = await axiosPost({
+        url: 'https://qa-login.uscourts.gov/services/cso-auth',
+        body: {
+          'loginId': 'username',
+          'password': ''
+        },
+      });
+      if (response.status == 200){
+        if (response.data.loginResult != 1){
+          Promise.resolve(response.data.nextGenCSO);
+        } else {
+          Promise.reject('Login Failed');
+        }
+      } else { 
+        Promise.reject('Failed to Connect to PACER API');
+      }
+      return response;
+    // Get existing token OR login if no existing token
+    // - should we also check if the existing token is still valid?
     }
   }
 }
