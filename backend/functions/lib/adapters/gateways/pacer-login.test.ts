0e4268680f86452e012291e3686b61358e4c1d64
Fritz Madden <fmadden@flexion.us>
2023-06-16 10:27:47 -0600

    CAMS-39: Added 5 tests to pacer-login.ts

--- backend/functions/lib/adapters/gateways/pacer-login.test.ts
+++ backend/functions/lib/adapters/gateways/pacer-login.test.ts
@@ -2,7 +2,7 @@ import { PacerLogin } from './pacer-login';
 const http = require('../utils/http');
 
 describe('PACER API gateway tests', () => {
-  test('should reject when pacer login fails', async () => {
+  test('should throw error when pacer login fails', async () => {
     const pacerLogin = new PacerLogin();
     const responseValue = {
       status: 200,
@@ -13,5 +13,55 @@ describe('PACER API gateway tests', () => {
     });
 
     expect(pacerLogin.getPacerToken()).rejects.toEqual(Error('Login Failed'));
+  });
+
+  test('should throw error when pacer returns a result > 1', async () => {
+    const pacerLogin = new PacerLogin();
+    const responseValue = {
+      status: 200,
+      data: { errorDescription: 'Some random error', loginResult: 2 },
+    };
+    jest.spyOn(http, 'httpPost').mockImplementation(() => {
+      return responseValue;
+    });
+
+    expect(pacerLogin.getPacerToken()).rejects.toEqual(Error('Error retrieving token'));
+  });
+
+  test('should throw error when pacer returns a status that is not 200', async () => {
+    const pacerLogin = new PacerLogin();
+    const responseValue = {
+      status: 400,
+      data: {},
+    };
+    jest.spyOn(http, 'httpPost').mockImplementation(() => {
+      return responseValue;
+    });
+
+    expect(pacerLogin.getPacerToken()).rejects.toEqual(Error('Failed to Connect to PACER API'));
+  });
+
+  test('should throw error when httpPost throws an error', async () => {
+    const pacerLogin = new PacerLogin();
+    const message = 'something went really wrong';
+    jest.spyOn(http, 'httpPost').mockImplementation(() => {
+      throw Error(message);
+    });
+
+    expect(pacerLogin.getPacerToken()).rejects.toEqual(Error(message));
+  });
+
+  test('should return token when valid response is received from Pacer', async () => {
+    const pacerLogin = new PacerLogin();
+    const expectedValue = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    const responseValue = {
+      status: 200,
+      data: { nextGenCSO: expectedValue, loginResult: 0 },
+    };
+    jest.spyOn(http, 'httpPost').mockImplementation(() => {
+      return responseValue;
+    });
+
+    expect(await pacerLogin.getPacerToken()).toEqual(expectedValue);
   });
 });
