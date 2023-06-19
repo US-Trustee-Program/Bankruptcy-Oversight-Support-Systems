d1272ae2edf47367a9c86c331a145b887e570569
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-06-19 12:23:12 -0500

    CAMS-39: WIP - Get PACER token from keyvault
    Co-authored-by: Fritz Madden <fmadden@flexion.us> [skip ci]

--- backend/functions/lib/adapters/gateways/pacer-login.ts
+++ backend/functions/lib/adapters/gateways/pacer-login.ts
@@ -1,12 +1,48 @@
 import { httpPost } from '../utils/http';
 import * as dotenv from 'dotenv';
+import { AzurePacerTokenSecretGateway } from './azure-pacer-token-secret.gateway';
+import { NoPacerToken } from './pacer-exceptions';
 
 dotenv.config();
 
 export class PacerLogin {
   public async getPacerToken(): Promise<string> {
+    const azurePacerTokenSecretGateway = new AzurePacerTokenSecretGateway();
     // - should we also check if the existing token is still valid?
     // Get existing token OR login if no existing token
+    let token;
+    try {
+      token = await azurePacerTokenSecretGateway.getPacerTokenFromSecrets();
+    } catch(e) {
+      if (e instanceof NoPacerToken) {
+        const response = await httpPost({
+          url: process.env.PACER_TOKEN_URL,
+          body: {
+            loginId: process.env.PACER_TOKEN_LOGIN_ID,
+            password: process.env.PACER_TOKEN_PASSWORD,
+          },
+        });
+
+        if (response.status == 200) {
+          if (response.data.loginResult == 0) {
+            return response.data.nextGenCSO;
+          } else if (response.data.loginResult == 1) {
+            throw Error(response.data.errorDescription);
+          } else {
+            throw Error('Error retrieving token');
+          }
+        } else {
+          throw Error('Failed to Connect to PACER API');
+        }
+      }
+
+      // store token
+    }
+    // validate token
+    // - if valid, return it
+    // - if no token or invalid, get a new one
+    //   - store the new one
+
     try {
       const response = await httpPost({
         url: process.env.PACER_TOKEN_URL,
