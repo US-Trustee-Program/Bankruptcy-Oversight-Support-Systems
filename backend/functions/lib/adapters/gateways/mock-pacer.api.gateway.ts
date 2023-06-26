505567e8dc77265906a741bf29139c80ac7c6a85
SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
2023-06-26 14:36:49 -0700

    Removed the case data which was not required for the mock.

--- backend/functions/lib/adapters/gateways/mock-pacer.api.gateway.ts
+++ backend/functions/lib/adapters/gateways/mock-pacer.api.gateway.ts
@@ -38,57 +38,25 @@ export class MockPacerApiGateway implements PacerGatewayInterface {
 
     oldCases.push(
       {
-        // "courtId": "cm8bk",
-        // "caseId": 127352,
-        // "caseYear": today.getFullYear()-4,
         caseNumber: '44449',
-        // "caseOffice": "4",
-        // "caseType": "bk",
         caseTitle: 'Flo Esterly and Neas Van Sampson',
         dateFiled: new Date(today.getFullYear() - 4, today.getMonth() - 10, today.getDate())
           .toISOString()
-          .split('T')[0],
-        // "bankruptcyChapter": "15",
-        // "jointBankruptcyFlag": "n",
-        // "jurisdictionType": "Bankruptcy",
-        // "caseNumberFull": "4:2004bk44449"
+          .split('T')[0]
       },
       {
-        // "courtId": "cm8bk",
-        // "caseId": 127761,
-        // "caseYear": today.getFullYear(),
         caseNumber: '1122',
-        // "caseOffice": "1",
-        // "caseType": "bk",
         caseTitle: 'Jennifer Millhouse',
         dateFiled: new Date(today.getFullYear(), today.getMonth() - 7, today.getDate())
           .toISOString()
-          .split('T')[0],
-        // "dateTermed": "2008-08-11",
-        // "dateDischarged": "2008-08-11",
-        // "bankruptcyChapter": "15",
-        // "dispositionMethod": "Standard Discharge",
-        // "jointBankruptcyFlag": "n",
-        // "jurisdictionType": "Bankruptcy",
-        // "effectiveDateClosed": "2008-08-11",
-        // "caseNumberFull": "1:2006bk01122"
+          .split('T')[0]
       },
       {
-        // "courtId": "cm8bk",
-        // "caseId": 127827,
-        // "caseYear": today.getFullYear()-10,
         caseNumber: '1166',
-        // "caseOffice": "1",
-        // "caseType": "bk",
         caseTitle: 'Heather Anne Real',
         dateFiled: new Date(today.getFullYear() - 10, today.getMonth(), today.getDate())
           .toISOString()
-          .split('T')[0],
-        // "dateDischarged": "2007-10-26",
-        // "bankruptcyChapter": "15",
-        // "jointBankruptcyFlag": "n",
-        // "jurisdictionType": "Bankruptcy",
-        // "caseNumberFull": "1:2006bk01166"
+          .split('T')[0]
       },
     );
     this.chapter15CaseList.push(oldCases[0], oldCases[1], oldCases[2]);
@@ -98,55 +66,25 @@ export class MockPacerApiGateway implements PacerGatewayInterface {
 
     newCases.push(
       {
-        // "courtId": "cm8bk",
-        // "caseId": 127828,
-        // "caseYear": today.getFullYear(),
         caseNumber: '1167',
-        // "caseOffice": "1",
-        // "caseType": "bk",
         caseTitle: 'Heather Anne Real',
         dateFiled: new Date(today.getFullYear(), today.getMonth() - 1, today.getDate())
           .toISOString()
-          .split('T')[0],
-        // "dateDischarged": "2007-10-26",
-        // "bankruptcyChapter": "15",
-        // "jointBankruptcyFlag": "n",
-        // "jurisdictionType": "Bankruptcy",
-        // "caseNumberFull": "1:2006bk01167"
+          .split('T')[0]
       },
       {
-        // "courtId": "cm8bk",
-        // "caseId": 127836,
-        // "caseYear": today.getFullYear(),
         caseNumber: '1175',
-        // "caseOffice": "1",
-        // "caseType": "bk",
         caseTitle: 'James P. Tennor',
         dateFiled: new Date(today.getFullYear(), today.getMonth() - 3, today.getDate())
           .toISOString()
-          .split('T')[0],
-        // "dateDischarged": "2007-10-26",
-        // "bankruptcyChapter": "15",
-        // "jointBankruptcyFlag": "n",
-        // "jurisdictionType": "Bankruptcy",
-        // "caseNumberFull": "1:2006bk01175"
+          .split('T')[0]
       },
       {
-        // "courtId": "cm8bk",
-        // "caseId": 127837,
-        // "caseYear": today.getFullYear(),
         caseNumber: '1176',
-        // "caseOffice": "1",
-        // "caseType": "bk",
         caseTitle: 'Tommy Testformiddlena tennor',
         dateFiled: new Date(today.getFullYear(), today.getMonth() - 2, today.getDate())
           .toISOString()
-          .split('T')[0],
-        // "dateDischarged": "2007-10-26",
-        // "bankruptcyChapter": "15",
-        // "jointBankruptcyFlag": "n",
-        // "jurisdictionType": "Bankruptcy",
-        // "caseNumberFull": "1:2006bk01176"
+          .split('T')[0]
       },
     );
     this.chapter15CaseList.push(newCases[0], newCases[1], newCases[2]);
