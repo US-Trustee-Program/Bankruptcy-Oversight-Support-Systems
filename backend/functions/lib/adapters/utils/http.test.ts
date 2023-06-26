9cfd6971769623763adb40e3a89f3172936f0ead
SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
2023-06-26 13:53:32 -0700

    Updated the http.test with a test for the httpPost
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>

--- backend/functions/lib/adapters/utils/http.test.ts
+++ backend/functions/lib/adapters/utils/http.test.ts
@@ -1,6 +1,28 @@
+import { httpPost } from "./http";
+
 const fetchSpy = jest
   .spyOn(global, 'fetch')
-  .mockImplementation((_url: string, { method: string, headers: {}, body: string }) => {
+  .mockImplementation((_url: URL,  requestInit: RequestInit):Promise<Response> => {
     // has to return a Promise<Response>
     return;
   });
+
+describe('Tests out the http calls', () => {
+  test('should call fetch with correct headers', async () => {
+    const expectedHeaders = { mimeType:'application/json', 'Content-Type': 'application/json',
+      Accept: 'application/json' };
+
+    const data = {
+      url: 'urlString',
+      body: {},
+      headers: { mimeType:'application/json' }
+    }
+    try {
+      const response = await httpPost(data);
+    }catch(e){
+      ;
+    }
+
+    expect(fetchSpy).toHaveBeenCalledWith(data.url, expect.objectContaining(expect.objectContaining({headers: expectedHeaders})));
+  });
+});
