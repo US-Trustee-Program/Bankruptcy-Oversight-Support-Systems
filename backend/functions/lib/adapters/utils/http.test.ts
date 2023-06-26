aaee1244bd27977f35afeb2c3c8f1cd7a4e96e0a
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-06-26 16:06:42 -0500

    CAMS-39: Finish testing http calls fetch with correct headers
    Co-authored-by: Fritz Madden <fmadden@flexion.us>
    Co-authored-by: SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>

--- backend/functions/lib/adapters/utils/http.test.ts
+++ backend/functions/lib/adapters/utils/http.test.ts
@@ -1,4 +1,4 @@
-import { httpPost } from "./http";
+import {httpGet, httpPost} from './http';
 
 const fetchSpy = jest
   .spyOn(global, 'fetch')
@@ -8,7 +8,7 @@ const fetchSpy = jest
   });
 
 describe('Tests out the http calls', () => {
-  test('should call fetch with correct headers', async () => {
+  test('should call fetch with correct headers from httpPost', async () => {
     const expectedHeaders = { mimeType:'application/json', 'Content-Type': 'application/json',
       Accept: 'application/json' };
 
@@ -18,11 +18,30 @@ describe('Tests out the http calls', () => {
       headers: { mimeType:'application/json' }
     }
     try {
-      const response = await httpPost(data);
-    }catch(e){
-      ;
+      await httpPost(data);
+    } catch(e) {
+      // Because we are not returning a Promise<Response> we catch the error and ignore as the test only
+      //  cares about how fetch was called.
     }
 
-    expect(fetchSpy).toHaveBeenCalledWith(data.url, expect.objectContaining(expect.objectContaining({headers: expectedHeaders})));
+    expect(fetchSpy).toHaveBeenCalledWith(data.url, expect.objectContaining({ headers: expectedHeaders }));
+  });
+
+  test('should call fetch with correct headers from httpGet', async () => {
+    const expectedHeaders = { mimeType:'application/json', 'Content-Type': 'application/json',
+      Accept: 'application/json' };
+
+    const data = {
+      url: 'urlString',
+      headers: { mimeType:'application/json' }
+    }
+    try {
+      await httpGet(data);
+    } catch(e) {
+      // Because we are not returning a Promise<Response> we catch the error and ignore as the test only
+      //  cares about how fetch was called.
+    }
+
+    expect(fetchSpy).toHaveBeenCalledWith(data.url, expect.objectContaining({ headers: expectedHeaders }));
   });
 });
