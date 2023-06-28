b4ad00203afc6caec792a08d3083e7b4ee39b742
Fritz Madden <fmadden@flexion.us>
2023-06-28 11:30:42 -0600

    CAMS-15: Updated dev-add-allowed-ip.sh and continuous deployment
    Co-authored-by: Fritz Madden <fmadden@flexion.us>
    Co-authored-by: Michael Ly <105381067+magile32@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <amorrow@Arthurs-MacBook-Pro.local>

--- ops/helper-scripts/dev-add-allowed-ip.sh
+++ ops/helper-scripts/dev-add-allowed-ip.sh
@@ -8,27 +8,59 @@
 
 set -euo pipefail # ensure job step fails in CI pipeline when error occurs
 
-app_rg=$1
-stack_name=$2
-priority=$3
-if (($# == 4)); then
-    ci=$4
-else
-    ci=
-fi
+ci=
+ip=
+while [[ $# > 0 ]]; do
+    case $1 in
+    -h | --help)
+        echo ""
+        echo "Usage: dev-add-allowed-ip.sh -g <resource_group_name:str> -s <stack_name:str> -p <priority:int> --is-cicd <isCICD:bool> -ip <IP_Address>"
+        echo ""
+        shift
+        ;;
+    -g | --resource-group)
+        app_rg="${2}"
+        shift 2
+        ;;
+    -s | --stack-name)
+        stack_name="${2}"
+        shift 2
+        ;;
+    -p | --priority)
+        priority="${2}"
+        shift 2
+        ;;
+    --is-cicd)
+        ci=true
+        shift
+        ;;
+    -ip | --ip-address)
+        ip="${2}"
+        shift 2
+        ;;
+    *)
+        exit 2 # error on unknown flag/switch
+        ;;
+    esac
+done
 
 if [[ -z "${app_rg}" || -z "${stack_name}" || -z "${priority}" ]]; then
     echo "Error: Missing parameters. Usage: dev-add-allowed-ip.sh <resource_group_name:str> <stack_name:str> <priority:int>"
     exit 1
 fi
 
-agentIp=$(curl -s https://api.ipify.org)
-
-if [[ $ci ]]; then
-    ruleName="gha-agent-${stack_name}"
+if [[ -z "${ip}" ]]; then
+    agentIp=$(curl -s https://api.ipify.org)
 else
-    ruleName="dev-agent-${agentIp}"
+    agentIp=${ip}
 fi
+
+if [[ -z "${ci}" ]]; then
+    ruleName="dev-agent-${agentIp}"
+else
+    ruleName="gha-agent-${stack_name}"
+fi
+
 ruleName=${ruleName:0:32} # trim up to 32 character limit
 echo "Attempting to add Ip allow rule (${ruleName})"
 az functionapp config access-restriction add -g $app_rg -n "${stack_name}-node-api" --rule-name $ruleName --action Allow --ip-address $agentIp --priority "${priority}1" 1>/dev/null
