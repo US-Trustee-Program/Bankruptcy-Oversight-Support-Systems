a856d1a19d14c01aa08baaa87a48332f0fc8c0ca
SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
2023-07-24 15:12:39 -0700

    Updated the region and office models to be interfaces. Removed the gateway and usecase for the region and office.

--- backend/functions/lib/adapters/gateways/office.gateway.ts
+++
@@ -1,5 +0,0 @@
-import { Office } from '../types/office';
-
-export interface IOfficeGateway {
-  getOffice(professionalId: string): Promise<Office>;
-}
---
+++ backend/functions/lib/adapters/types/office.d.ts
@@ -0,0 +1,5 @@
+export interface Office {
+  _courtId: string; //varchar(4)
+  _officeCode: string; //char(1)
+  _officeName: string; //varchar(110)
+}
