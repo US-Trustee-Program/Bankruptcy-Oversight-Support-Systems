4763ecea45cecdf36f1bc2debe42c1e2af0714ed
Fritz Madden <fmadden@flexion.us>
2023-08-01 10:39:47 -0600

    CAMS-87: Updated accessibility-test.sh to run start:pa11y
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>

--- ops/helper-scripts/accessibility-test.sh
+++ ops/helper-scripts/accessibility-test.sh
@@ -2,10 +2,7 @@
 
 set -euo pipefail # ensure job step fails in CI pipeline when error occurs
 
-export REACT_APP_PA11Y=true
-npm run build
-npm install serve -g
-serve -s build &
+npm run start:pa11y &
 timeout 120s sh -c 'until nc -z localhost 3000; do sleep 1; done'
 if (( $? != 0 )); then
     echo "Webapp check timed out"
