c347bae63464cd30f7efdcff4c1a5dfb16cf61de
Fritz Madden <fmadden@flexion.us>
2023-08-01 10:55:09 -0600

    CAMS-87: Added modification to vite.config to test a hunch about github build process
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>

--- ops/helper-scripts/accessibility-test.sh
+++ ops/helper-scripts/accessibility-test.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail # ensure job step fails in CI pipeline when error occurs
 
-npm run start:pa11y &
+export CAMS_PA11Y=true
+npm run build
+npm install serve -g
+serve -s build &
 timeout 120s sh -c 'until nc -z localhost 3000; do sleep 1; done'
 if (( $? != 0 )); then
     echo "Webapp check timed out"
