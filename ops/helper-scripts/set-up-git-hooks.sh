3a8641cc18ffdd8aa7f3d1abfbaa9b3782de92cb
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-08-02 17:37:53 -0500

    CAMS-111: Add script to set up pre-commit hooks
    Co-authored-by: Fritz Madden <fmadden@flexion.us>

---
+++ ops/helper-scripts/set-up-git-hooks.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# pre-commit
+pre-commit --version
+if [ $? -ne 0 ]
+then
+    echo "Please follow the instructions to install the pre-commit python tool. The instructions can be found at https://pre-commit.com/index.html#install"
+    exit 1
+fi
+pre-commit install
+
+# Append the branch naming convention check
+cat pre-commit >> ../../.git/hooks/pre-commit
