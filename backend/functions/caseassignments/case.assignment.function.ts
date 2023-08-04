e3304ae5b2c225d02ac93dfecdf9d18c0d38a2b8
SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
2023-08-03 18:55:03 -0700

    CAMS-84 Added the azure function test for the case assignment azure function and updated function.json for bindings.

--- backend/functions/caseassignments/case.assignment.function.ts
+++ backend/functions/caseassignments/case.assignment.function.ts
@@ -7,46 +7,79 @@ import { CaseAssignmentRequest } from '../lib/adapters/types/case.assignment.req
 import { CaseAssignmentRole } from '../lib/adapters/types/case.assignment.role';
 
 const NAMESPACE = 'CASE-ASSIGNMENT-FUNCTION' as const;
+const REQUIRED_CASE_ID_MESSAGE = 'Required parameter caseId is absent.';
+const REQUIRED_PROFESSIONAL_ID_MESSAGE = 'Required parameter professionalId is absent.';
+const REQUIRED_ROLE_MESSAGE = 'Required parameter role of the professional is absent.';
+const INVALID_ROLE_MESSAGE =
+  'Invalid role for the professional. Please pass in a valid professional role to assign';
+
 const httpTrigger: AzureFunction = async function (
   functionContext: Context,
-  caseAssignmentRequest: HttpRequest,
+  request: HttpRequest,
 ): Promise<void> {
-  let caseId: string = '';
-  let professionalId: string = '';
-  let role: string = '';
-
-  if (caseAssignmentRequest.query.caseId) caseId = caseAssignmentRequest.query.caseId;
-  else if (caseAssignmentRequest.body && caseAssignmentRequest.body.caseId)
-    caseId = caseAssignmentRequest.body.caseId;
-  if (caseAssignmentRequest.query.professionalId)
-    professionalId = caseAssignmentRequest.query.professionalId;
-  else if (caseAssignmentRequest.body && caseAssignmentRequest.body.professionalId)
-    caseId = caseAssignmentRequest.body.professionalId;
-  if (caseAssignmentRequest.query.role) role = caseAssignmentRequest.query.role;
-  else if (caseAssignmentRequest.body && caseAssignmentRequest.body.role)
-    caseId = caseAssignmentRequest.body.role;
-
-  const assignmentRequest: CaseAssignmentRequest = new CaseAssignmentRequest(
-    caseId,
-    professionalId,
-    role as unknown as CaseAssignmentRole,
-  );
-
-  const caseAssignmentController: CaseAssignmentController = new CaseAssignmentController(
-    functionContext,
-  );
+  const caseId = request.query.caseId || (request.body && request.body.caseId);
+  const professionalId =
+    request.query.professionalId || (request.body && request.body.professionalId);
+  const role = request.query.role || (request.body && request.body.role);
 
   try {
+    const assignmentRequest: CaseAssignmentRequest = createAssignmentRequest(
+      functionContext,
+      caseId,
+      professionalId,
+      role,
+    );
+    const caseAssignmentController: CaseAssignmentController = new CaseAssignmentController(
+      functionContext,
+    );
+
     const newAssignmentId = await caseAssignmentController.createCaseAssignment(assignmentRequest);
-    const responseBody = {
+    /*const responseBody = {
       message: 'A new assignment has been created successfully',
       data: { assignmentId: newAssignmentId },
-    };
-    functionContext.res = httpSuccess(functionContext, responseBody);
+    };*/
+    functionContext.res = httpSuccess(functionContext, { assignmentId: newAssignmentId });
   } catch (exception) {
     log.error(applicationContextCreator(functionContext), NAMESPACE, exception.message, exception);
     functionContext.res = httpError(functionContext, exception, 400);
   }
 };
+
+function createAssignmentRequest(
+  functionContext: Context,
+  caseId: string,
+  professionalId: string,
+  role: string,
+): CaseAssignmentRequest {
+  if (!caseId) {
+    log.error(applicationContextCreator(functionContext), NAMESPACE, REQUIRED_CASE_ID_MESSAGE);
+    functionContext.res = httpError(functionContext, new Error(REQUIRED_CASE_ID_MESSAGE), 400);
+  }
+  if (!professionalId) {
+    log.error(
+      applicationContextCreator(functionContext),
+      NAMESPACE,
+      REQUIRED_PROFESSIONAL_ID_MESSAGE,
+    );
+    functionContext.res = httpError(
+      functionContext,
+      new Error(REQUIRED_PROFESSIONAL_ID_MESSAGE),
+      400,
+    );
+  }
+  if (!role) {
+    log.error(applicationContextCreator(functionContext), NAMESPACE, REQUIRED_ROLE_MESSAGE);
+    functionContext.res = httpError(functionContext, new Error(REQUIRED_ROLE_MESSAGE), 400);
+  }
+
+  let professionalRole: CaseAssignmentRole;
+  if (role in CaseAssignmentRole) {
+    professionalRole = role as unknown as CaseAssignmentRole;
+    return new CaseAssignmentRequest(caseId, professionalId, professionalRole);
+  } else {
+    log.error(applicationContextCreator(functionContext), NAMESPACE, INVALID_ROLE_MESSAGE);
+    functionContext.res = httpError(functionContext, new Error(INVALID_ROLE_MESSAGE), 400);
+  }
+}
 
 export default httpTrigger;
