930fd9a71ac28d69574ab4bf7664ddb00eba4352
Arthur Morrow <amorrow@flexion.us>
2023-08-08 09:01:49 -0400

    CAMS-103 configured reporting on error for react

--- user-interface/src/App.tsx
+++ user-interface/src/App.tsx
@@ -7,22 +7,39 @@ import { CaseAssignment } from './components/CaseAssignment';
 import { HeaderNavBar } from './components/HeaderNavBar';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
+import React from 'react';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
+import { AppInsightsErrorBoundary } from "@microsoft/applicationinsights-react-js";
+
+const api = import.meta.env['APPINSIGHTS_CONNECTION_STRING']
+var reactPlugin = new ReactPlugin();
+var appInsights = new ApplicationInsights({
+    config: {
+        connectionString: 'YOUR_CONNECTION_STRING_GOES_HERE',
+        enableAutoRouteTracking: true,
+        extensions: [reactPlugin]
+    }
+});
+appInsights.loadAppInsights();
 
 function App() {
   return (
-    <div className="App">
-      <Provider store={store}>
-        <HeaderNavBar />
-        <div className="body">
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/cases" element={<CaseList />}></Route>
-            <Route path="/case-assignment" element={<CaseAssignment />}></Route>
-            <Route path="*" element={<NotFound />}></Route>
-          </Routes>
-        </div>
-      </Provider>
-    </div>
+    <AppInsightsErrorBoundary onError={() => <h1>Something Went Wrong</h1>} appInsights={reactPlugin}>
+      <div className="App">
+        <Provider store={store}>
+          <HeaderNavBar />
+          <div className="body">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/cases" element={<CaseList />}></Route>
+              <Route path="/case-assignment" element={<CaseAssignment />}></Route>
+              <Route path="*" element={<NotFound />}></Route>
+            </Routes>
+          </div>
+        </Provider>
+      </div>
+  </AppInsightsErrorBoundary>
   );
 }
 
