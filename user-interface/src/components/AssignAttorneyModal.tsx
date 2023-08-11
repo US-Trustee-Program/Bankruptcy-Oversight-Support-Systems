90a18d3188098cc67fb064a61ac82d3d5abdcc0f
Fritz Madden <fmadden@flexion.us>
2023-08-10 17:26:06 -0700

    CAMS-87: WIP - Adding assigned attorneys to case

--- user-interface/src/components/AssignAttorneyModal.tsx
+++ user-interface/src/components/AssignAttorneyModal.tsx
@@ -1,11 +1,25 @@
 import { forwardRef, useRef, useImperativeHandle } from 'react';
 import Modal, { ModalRefType } from './uswds/Modal';
 import { Chapter15Type } from '../type-declarations/chapter-15';
+import React from 'react';
 
 export interface AssignAttorneyModalProps {
   bCase: Chapter15Type | undefined;
   modalId: string;
   openerId: string;
+  callBack: (props: object) => void;
+}
+
+export interface AssignedAttorney {
+  id: number;
+  name: string;
+  caseCount: number;
+}
+
+export interface callBackProps {
+  openerId: string;
+  bCase: Chapter15Type;
+  attorneyList: AssignedAttorney[];
 }
 
 function AssignAttorneyModalComponent(
@@ -18,15 +32,22 @@ function AssignAttorneyModalComponent(
     modalId: props.modalId,
     submitButton: {
       label: 'Assign',
+      onClick: submitValues,
     },
     cancelButton: {
       label: 'Go back',
+      onClick: cancelModal,
     },
   };
 
-  const attorneyList = [
+  const attorneyList: AssignedAttorney[] = [
     { id: 1, name: 'Alan Shore', caseCount: 3 },
     { id: 4, name: 'Denny Crane', caseCount: 5 },
+    { id: 5, name: 'Jane Doe', caseCount: 4 },
+    { id: 6, name: 'John Doe', caseCount: 15 },
+    { id: 7, name: 'Roger Morre', caseCount: 256 },
+    { id: 8, name: 'Roger Wilco', caseCount: 5 },
+    { id: 390, name: 'Cee Threepeeo', caseCount: 0 },
   ];
 
   useImperativeHandle(ref, () => {
@@ -39,6 +60,31 @@ function AssignAttorneyModalComponent(
       };
     }
   });
+
+  let checkListValues: number[] = [];
+  function updateCheckList(ev: React.ChangeEvent<HTMLInputElement>, id: number) {
+    if (ev.target.checked && !checkListValues.includes(id)) {
+      checkListValues.push(id);
+    }
+  }
+
+  function cancelModal() {
+    checkListValues = [];
+  }
+
+  function submitValues() {
+    // send attorney IDs to API and if successful, then
+    //if (true) {
+    let finalAttorneyList: AssignedAttorney[] = [];
+    // call callback from parent with IDs and names of attorneys, and case id.
+    finalAttorneyList = attorneyList.filter((attorney) => checkListValues.includes(attorney.id));
+    props.callBack({
+      openerId: props.openerId,
+      bCase: props.bCase,
+      attorneyList: finalAttorneyList,
+    });
+    //}
+  }
 
   return (
     <Modal
@@ -67,7 +113,7 @@ function AssignAttorneyModalComponent(
                           id={`${idx}-checkbox`}
                           name="selectedItems"
                           value={attorney.id}
-                          // onChange={(event) => updateCheckList(event, attorney.id)}
+                          onChange={(event) => updateCheckList(event, attorney.id)}
                           className="attorney-list-checkbox"
                         />
                       </td>
