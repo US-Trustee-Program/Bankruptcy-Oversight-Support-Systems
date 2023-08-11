87e12463336bc7cdb7847fa7eac0bde537e3d2ee
SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
2023-08-11 12:44:04 -0700

    CAMS-84 Add functionality to the repository to find an Assignment by case Id. Update the relevant tests to throw an error when creating an assignment, when a case already has one.
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>

--- backend/functions/lib/adapters/controllers/case.assignment.controller.ts
+++ backend/functions/lib/adapters/controllers/case.assignment.controller.ts
@@ -6,8 +6,12 @@ import { CaseAssignment } from '../../use-cases/case.assignment';
 import { CaseAssignmentRepositoryInterface } from '../../interfaces/case.assignment.repository.interface';
 import { TrialAttorneysAssignmentRequest } from '../types/trial.attorneys.assignment.request';
 import { ITrialAttorneyAssignmentResponse } from '../types/case.assignment';
+import log from '../services/logger.service';
+import { httpError } from '../utils/http';
+import { CaseAssignmentException } from '../../../caseassignments/case.assignment.exception';
 
-//const NAMESPACE = 'ASSIGNMENT-CONTROLLER'; // Will need this when implementing telemetry
+const NAMESPACE = 'ASSIGNMENT-CONTROLLER';
+
 export class CaseAssignmentController {
   private readonly applicationContext: ApplicationContext;
   private readonly caseAssignmentRepository: CaseAssignmentRepositoryInterface;
@@ -20,20 +24,30 @@ export class CaseAssignmentController {
   public async createTrailAttorneyAssignments(
     assignmentRequest: TrialAttorneysAssignmentRequest,
   ): Promise<ITrialAttorneyAssignmentResponse> {
-    const listOfAssignments: CaseAttorneyAssignment[] = [];
+    try {
+      const listOfAssignments: CaseAttorneyAssignment[] = [];
 
-    assignmentRequest.listOfAttorneyIds.forEach((attorney) => {
-      const assignment: CaseAttorneyAssignment = new CaseAttorneyAssignment(
-        assignmentRequest.caseId,
-        attorney,
-        assignmentRequest.role,
+      assignmentRequest.listOfAttorneyIds.forEach((attorney) => {
+        const assignment: CaseAttorneyAssignment = new CaseAttorneyAssignment(
+          assignmentRequest.caseId,
+          attorney,
+          assignmentRequest.role,
+        );
+        listOfAssignments.push(assignment);
+      });
+      const assignmentService = new CaseAssignment(this.caseAssignmentRepository);
+      return assignmentService.createTrialAttorneyAssignments(
+        this.applicationContext,
+        listOfAssignments,
       );
-      listOfAssignments.push(assignment);
-    });
-    const assignmentService = new CaseAssignment(this.caseAssignmentRepository);
-    return assignmentService.createTrialAttorneyAssignments(
-      this.applicationContext,
-      listOfAssignments,
-    );
+    } catch (exception) {
+      log.error(applicationContextCreator(this.applicationContext), NAMESPACE, exception.message);
+      this.applicationContext.res = httpError(
+        this.applicationContext,
+        new Error(exception.message),
+        exception.status,
+      );
+      throw new CaseAssignmentException(exception.status, exception.message);
+    }
   }
 }
