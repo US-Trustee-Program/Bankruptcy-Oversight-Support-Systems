d2f9fad1d4ed07571667abfbfd409facc69da2d4
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-08-15 15:45:09 -0700

    CAMS-103 Update webapp instrumentation key for new environment

--- user-interface/src/ApplicationInsightsService.tsx
+++ user-interface/src/ApplicationInsightsService.tsx
@@ -2,7 +2,7 @@ import { ApplicationInsights, ITelemetryItem } from '@microsoft/applicationinsig
 import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
 
 const appInsightsConnectionString =
-  'InstrumentationKey=208578d0-a120-f0af-a1fa-f7443385b9c1;EndpointSuffix=applicationinsights.us;IngestionEndpoint=https://usgovvirginia-1.in.applicationinsights.azure.us/;AADAudience=https://monitor.azure.us/'; // TODO : Testing instrumentation with temporary environment
+  'InstrumentationKey=340f749a-f435-fa6a-96c6-acf683a99a18;EndpointSuffix=applicationinsights.us;IngestionEndpoint=https://usgovvirginia-1.in.applicationinsights.azure.us/;AADAudience=https://monitor.azure.us/'; // TODO : Testing instrumentation with temporary environment
 const reactPlugin = new ReactPlugin();
 
 const appInsights = new ApplicationInsights({
