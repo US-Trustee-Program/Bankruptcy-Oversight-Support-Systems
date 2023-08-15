1437dfc99f5fa9479bd5ee535281dea681e0012f
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-08-15 13:35:47 -0700

    CAMS-103 Experiment with client side app insight instrumentation

--- user-interface/src/ApplicationInsightsService.tsx
+++ user-interface/src/ApplicationInsightsService.tsx
@@ -1,7 +1,8 @@
 import { ApplicationInsights, ITelemetryItem } from '@microsoft/applicationinsights-web';
 import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
 
-const appInsightsConnectionString = import.meta.env['APPLICATIONINSIGHTS_CONNECTION_STRING']; // TODO : NEED TO VERIFY THIS SNIPPET
+const appInsightsConnectionString =
+  'InstrumentationKey=208578d0-a120-f0af-a1fa-f7443385b9c1;EndpointSuffix=applicationinsights.us;IngestionEndpoint=https://usgovvirginia-1.in.applicationinsights.azure.us/;AADAudience=https://monitor.azure.us/'; // TODO : Testing instrumentation with temporary environment
 const reactPlugin = new ReactPlugin();
 
 const appInsights = new ApplicationInsights({
