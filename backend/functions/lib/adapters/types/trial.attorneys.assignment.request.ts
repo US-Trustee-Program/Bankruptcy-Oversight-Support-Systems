4590c49e9bdf664e85853c17f229b9d2f9b7137f
SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
2023-08-16 14:52:48 -0700

    CAMS-84 Updated the trial.attorneys.assignment.request - made the fields public and removed the getters and setters. Deleted the unused class and file case.assignment.request.ts
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>

--- backend/functions/lib/adapters/types/trial.attorneys.assignment.request.ts
+++ backend/functions/lib/adapters/types/trial.attorneys.assignment.request.ts
@@ -1,10 +1,10 @@
 import { CaseAssignmentRole } from './case.assignment.role';
 
 export class TrialAttorneysAssignmentRequest {
-  private _caseId: string;
-  private _caseTitle: string;
-  private _listOfAttorneyIds: string[];
-  private _role: CaseAssignmentRole;
+  caseId: string;
+  caseTitle: string;
+  listOfAttorneyIds: string[];
+  role: CaseAssignmentRole;
 
   constructor(
     caseId: string,
@@ -12,39 +12,9 @@ export class TrialAttorneysAssignmentRequest {
     role: CaseAssignmentRole,
     caseTitle?: string,
   ) {
-    this._caseId = caseId;
-    this._listOfAttorneyIds = listOfAttorneyIds;
-    this._role = role;
-    this._caseTitle = caseTitle;
-  }
-
-  get role(): CaseAssignmentRole {
-    return this._role;
-  }
-
-  set role(value: CaseAssignmentRole) {
-    this._role = value;
-  }
-  get caseTitle(): string {
-    return this._caseTitle;
-  }
-
-  set caseTitle(value: string) {
-    this._caseTitle = value;
-  }
-  get caseId(): string {
-    return this._caseId;
-  }
-
-  set caseId(value: string) {
-    this._caseId = value;
-  }
-
-  get listOfAttorneyIds(): string[] {
-    return this._listOfAttorneyIds;
-  }
-
-  set listOfAttorneyIds(value: string[]) {
-    this._listOfAttorneyIds = value;
+    this.caseId = caseId;
+    this.listOfAttorneyIds = listOfAttorneyIds;
+    this.role = role;
+    this.caseTitle = caseTitle;
   }
 }
