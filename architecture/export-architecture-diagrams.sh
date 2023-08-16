b133dd6ee705eddd6a9f78ed68559e1c301c6b3e
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-08-16 12:52:58 -0500

    CAMS-109: Generate docsify pages to display mermaid diagrams

--- architecture/export-architecture-diagrams.sh
+++ architecture/export-architecture-diagrams.sh
@@ -4,5 +4,51 @@
 structurizr.sh export -workspace architecture/cams.dsl -format mermaid
 
 # Produce Docsify Content
-#   This should replace the content of the appropriate markdown file,
-#     wrapping the mermaid in a code tag.
+pushd architecture
+
+## CAMS System Context
+mermaid_file="./structurizr-SystemLandscape.mmd"
+{
+  echo "# CAMS System Context\n\n\`\`\`mermaid"
+  cat "$mermaid_file"
+  echo "\n\`\`\`"
+} > temp_file.md
+mv temp_file.md ../docs/architecture/diagrams/cams-context.md
+
+## CAMS Containers
+mermaid_file="./structurizr-CAMSContainers.mmd"
+{
+  echo "# CAMS Containers\n\n\`\`\`mermaid"
+  cat "$mermaid_file"
+  echo "\n\`\`\`"
+} > temp_file.md
+mv temp_file.md ../docs/architecture/diagrams/cams-containers.md
+
+## CAMS Functions API with Webapp
+mermaid_file="./structurizr-FunctionsAPIwithWebapp.mmd"
+{
+  echo "# CAMS Webapp with Functions API\n\n\`\`\`mermaid"
+  cat "$mermaid_file"
+  echo "\n\`\`\`"
+} > temp_file.md
+mv temp_file.md ../docs/architecture/diagrams/cams-webapp-with-functions-api.md
+
+## CAMS Webapp Components
+mermaid_file="./structurizr-CAMSWebapp.mmd"
+{
+  echo "# CAMS Webapp Components\n\n\`\`\`mermaid"
+  cat "$mermaid_file"
+  echo "\n\`\`\`"
+} > temp_file.md
+mv temp_file.md ../docs/architecture/diagrams/cams-webapp-components.md
+
+## CAMS Functions API Components
+mermaid_file="./structurizr-FunctionsAPI.mmd"
+{
+  echo "# CAMS Functions API Components\n\n\`\`\`mermaid"
+  cat "$mermaid_file"
+  echo "\n\`\`\`"
+} > temp_file.md
+mv temp_file.md ../docs/architecture/diagrams/cams-functions-api-components.md
+
+popd
