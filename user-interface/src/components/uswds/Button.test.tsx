d0c7ec82d6cd4edddeef287cb1162f9a2b595d71
Fritz Madden <fmadden@flexion.us>
2023-08-21 17:23:22 -0600

    CAMS-87: Added tests for button component [skip ci]
    Co-authored-by: SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>

--- user-interface/src/components/uswds/Button.test.tsx
+++ user-interface/src/components/uswds/Button.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Button, { UswdsButtonState, UswdsButtonStyle } from './Button';
+import Button, { ButtonRef, UswdsButtonState, UswdsButtonStyle } from './Button';
 
 describe('Test button component', () => {
   test('Should add the proper css class when uswdsButtonStyle is passed in', () => {
@@ -28,5 +28,46 @@ describe('Test button component', () => {
 
     const button = screen.getByTestId('button');
     expect(button).toHaveClass('usa-focus');
+  });
+
+  test('Should disable button when disable attribute is set on the Button', () => {
+    render(
+      <React.StrictMode>
+        <BrowserRouter>
+          <Button disabled={true}>Button text</Button>
+        </BrowserRouter>
+      </React.StrictMode>,
+    );
+
+    const button = screen.getByTestId('button');
+    expect(button).toHaveAttribute('disabled');
+    expect(button).toHaveAttribute('aria-disabled');
+  });
+
+  test('Should disable button when the disable method is called on the Button ref', () => {
+    const buttonRef = React.createRef<ButtonRef>();
+    render(
+      <React.StrictMode>
+        <BrowserRouter>
+          <Button ref={buttonRef}>Button text</Button>
+        </BrowserRouter>
+      </React.StrictMode>,
+    );
+
+    const button = screen.getByTestId('button');
+
+    expect(button).not.toHaveAttribute('disabled');
+
+    act(() => {
+      buttonRef.current?.disableButton(true);
+    });
+
+    expect(button).toHaveAttribute('disabled');
+
+    act(() => {
+      buttonRef.current?.disableButton(false);
+    });
+
+    expect(button).not.toHaveAttribute('disabled');
   });
 });
