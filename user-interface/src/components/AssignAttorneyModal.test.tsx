44577fa1461da34d843639122d405ad3b642b074
SwathiLexi <30849469+SwathiLexi@users.noreply.github.com>
2023-08-22 13:09:41 -0700

    CAMS-87 Added tests to test out AssignAttorneyModal to test for the modal to be visible.
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>

--- user-interface/src/components/AssignAttorneyModal.test.tsx
+++ user-interface/src/components/AssignAttorneyModal.test.tsx
@@ -1,8 +1,10 @@
-import { render } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import AssignAttorneyModal from './AssignAttorneyModal';
 import React from 'react';
 import { Chapter15Type } from '../type-declarations/chapter-15';
+import { ModalRefType } from './uswds/Modal';
+import { ToggleModalButton } from './uswds/ToggleModalButton';
 
 describe('Test Assign Attorney Modal Component', () => {
   test('Should open modal with submit disabled, and enable button when item is checked, and disable when there are no more items checked.', async () => {
@@ -14,19 +16,34 @@ describe('Test Assign Attorney Modal Component', () => {
 
     const modalRef = React.createRef<ModalRefType>();
     const callback = vi.fn();
-
+    const modalId = 'some-modal-id';
     render(
       <React.StrictMode>
         <BrowserRouter>
-          <AssignAttorneyModal
-            ref={modalRef}
-            bCase={bCase}
-            modalId="some-modal-id"
-            openerId="opener-123"
-            callBack={callback}
-          ></AssignAttorneyModal>
+          <>
+            <ToggleModalButton toggleAction={'open'} modalId={modalId} modalRef={modalRef}>
+              Open Modal
+            </ToggleModalButton>
+            <AssignAttorneyModal
+              ref={modalRef}
+              bCase={bCase}
+              modalId={modalId}
+              openerId="opener-123"
+              callBack={callback}
+            ></AssignAttorneyModal>
+          </>
         </BrowserRouter>
       </React.StrictMode>,
     );
+    const button = screen.getByTestId('toggle-modal-button');
+    const modal = screen.getByTestId(`modal-${modalId}`);
+    const submitButton = screen.getByTestId('toggle-modal-button-submit');
+
+    act(() => {
+      fireEvent.click(button);
+    });
+
+    expect(modal).toHaveClass('is-visible');
+    //expect(button).
   });
 });
