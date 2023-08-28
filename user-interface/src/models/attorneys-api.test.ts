3f69cbecee0e5a2952de99114d663fcf79ef2bb9
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-08-28 10:52:50 -0500

    CAMS-87: Test Attorneys API

---
+++ user-interface/src/models/attorneys-api.test.ts
@@ -0,0 +1,56 @@
+import AttorneysApi from './attorneys-api';
+import { ObjectKeyVal } from '../type-declarations/basic';
+import { ResponseData } from '../type-declarations/api';
+
+describe('Attorneys API test', () => {
+  test('should return only the list of attorneys', async () => {
+    vi.mock('./api', () => {
+      return {
+        default: {
+          list: (path: string, options?: ObjectKeyVal): Promise<ResponseData> => {
+            return Promise.resolve({
+              message: '',
+              count: 2,
+              body: {
+                attorneyList: [
+                  {
+                    firstName: 'Marilyn',
+                    middleName: 'Jane',
+                    lastName: 'Jenkins',
+                    generation: '',
+                    office: 'Manhattan',
+                  },
+                  {
+                    firstName: 'Thomas',
+                    middleName: 'Stephen',
+                    lastName: 'Koenig',
+                    generation: '',
+                    office: 'Manhattan',
+                  },
+                ],
+              },
+            });
+          },
+        },
+      };
+    });
+
+    const attorneys = await AttorneysApi.getAttorneys();
+    expect(attorneys).toEqual([
+      {
+        firstName: 'Marilyn',
+        middleName: 'Jane',
+        lastName: 'Jenkins',
+        generation: '',
+        office: 'Manhattan',
+      },
+      {
+        firstName: 'Thomas',
+        middleName: 'Stephen',
+        lastName: 'Koenig',
+        generation: '',
+        office: 'Manhattan',
+      },
+    ]);
+  });
+});
