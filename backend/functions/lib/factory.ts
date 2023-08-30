6fdf586b494f32c603092b591bbb28b0f19e476c
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-08-30 10:48:11 -0500

    CAMS-87: Use local attorney gateway in deployed environment

--- backend/functions/lib/factory.ts
+++ backend/functions/lib/factory.ts
@@ -19,6 +19,7 @@ export const getAttorneyGateway = (): AttorneyGatewayInterface => {
   if (config.get('dbMock')) {
     return new AttorneyLocalGateway();
   } else {
+    return new AttorneyLocalGateway();
     // return new AttorneyApiGateway(); // not yet implemented
   }
 };
