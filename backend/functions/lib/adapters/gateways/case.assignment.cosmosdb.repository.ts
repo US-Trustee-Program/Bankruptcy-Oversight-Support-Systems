c23872777553f8cc8a68124df5229b392e87152c
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-09-07 17:03:36 -0500

    CAMS-83: Remove console log [skip ci]
    Co-authored-by: Fritz Madden <fmadden@flexion.us>

--- backend/functions/lib/adapters/gateways/case.assignment.cosmosdb.repository.ts
+++ backend/functions/lib/adapters/gateways/case.assignment.cosmosdb.repository.ts
@@ -81,7 +81,6 @@ export class CaseAssignmentCosmosDbRepository implements CaseAssignmentRepositor
         .container(this.containerName)
         .items.readAll()
         .fetchAll();
-      console.log(results);
       return results;
     } catch (e) {
       throw new AssignmentException(500, 'Failed to retrieve assignments from the database.');
