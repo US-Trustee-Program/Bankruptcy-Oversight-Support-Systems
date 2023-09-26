2a85ecd82a077e0dcd14363c961f51b9432f637b
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-09-25 19:59:41 -0500

    WIP - Add pa11y test of assignments screen
    Adds a test of the assignments screen which should click on an Assign
    button so that we can test the modal. The selector for the button is
    not currently working.
    
    [skip ci]
    
    Jira ticket: CAMS-47

--- user-interface/src/models/chapter15-mock.api.cases.ts
+++ user-interface/src/models/chapter15-mock.api.cases.ts
@@ -14,6 +14,13 @@ export default class Chapter15MockApi extends Api {
           },
         };
         break;
+      case '/attorneys':
+        response = {
+          message: 'attorneys list',
+          count: Chapter15MockApi.attorneyList.length,
+          body: { attorneyList: Chapter15MockApi.attorneyList },
+        };
+        break;
       default:
         response = {
           message: 'not found',
@@ -30,42 +37,80 @@ export default class Chapter15MockApi extends Api {
     {
       caseNumber: '23-44463',
       caseTitle: 'Flo Esterly and Neas Van Sampson',
-      dateFiled: '2023-05-04',
+      dateFiled: '05-04-2023',
     },
     {
       caseNumber: '23-44462',
       caseTitle: 'Bridget Maldonado',
-      dateFiled: '2023-04-14',
+      dateFiled: '04-14-2023',
     },
     {
       caseNumber: '23-44461',
       caseTitle: 'Talia Torres and Tylor Stevenson',
-      dateFiled: '2023-04-04',
+      dateFiled: '04-04-2023',
     },
     {
       caseNumber: '23-44460',
       caseTitle: 'Asia Hodges',
-      dateFiled: '2023-03-01',
+      dateFiled: '03-01-2023',
     },
     {
       caseNumber: '23-44459',
       caseTitle: 'Marilyn Lawson',
-      dateFiled: '2023-02-14',
+      dateFiled: '02-14-2023',
     },
     {
       caseNumber: '23-44458',
       caseTitle: 'April Pierce and Leah Pierce',
-      dateFiled: '2023-02-04',
+      dateFiled: '02-04-2023',
     },
     {
       caseNumber: '23-44457',
       caseTitle: 'Corinne Gordon',
-      dateFiled: '2023-01-14',
+      dateFiled: '01-14-2023',
     },
     {
       caseNumber: '23-44456',
       caseTitle: 'Marilyn Lang and Rudy Bryant',
-      dateFiled: '2023-01-04',
+      dateFiled: '01-04-2023',
+    },
+  ];
+
+  static attorneyList = [
+    {
+      caseLoad: 3,
+      firstName: 'Debora',
+      generation: '',
+      lastName: 'Henderson',
+      middleName: 'Cindy',
+    },
+    {
+      caseLoad: 6,
+      firstName: 'Adele',
+      generation: '',
+      lastName: 'Greene',
+      middleName: 'Joann',
+    },
+    {
+      caseLoad: 2,
+      firstName: 'Brock',
+      generation: '',
+      lastName: 'Scott',
+      middleName: 'Stuart',
+    },
+    {
+      caseLoad: 7,
+      firstName: 'Katherine',
+      generation: '',
+      lastName: 'Hess',
+      middleName: 'Jewell',
+    },
+    {
+      caseLoad: 4,
+      firstName: 'Lawrence',
+      generation: '',
+      lastName: 'Heath',
+      middleName: 'Josiah',
     },
   ];
 }
