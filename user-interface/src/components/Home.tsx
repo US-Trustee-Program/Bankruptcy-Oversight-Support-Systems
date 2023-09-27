56eccafd3b386bd07055878caf7975efc4d7fe59
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-09-27 12:54:29 -0700

    CAMS-47 Default home page to case assignment view.
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>
    Co-authored-by: Arthur Morrow <amorrow@flexion.us>

--- user-interface/src/components/Home.tsx
+++ user-interface/src/components/Home.tsx
@@ -1,7 +1,12 @@
+import { CaseAssignment } from './CaseAssignment';
 import './Home.scss';
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <CaseAssignment />
+    </div>
+  );
 }
 
 export default Home;
