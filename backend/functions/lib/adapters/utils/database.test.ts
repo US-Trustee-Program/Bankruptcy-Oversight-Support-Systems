18856a816a36f57399b181991dbe62526a4a4142
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-09-27 10:31:00 -0700

    CAMS-47 Additional update to database test setup
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>

--- backend/functions/lib/adapters/utils/database.test.ts
+++ backend/functions/lib/adapters/utils/database.test.ts
@@ -1,5 +1,11 @@
+import { executeQuery } from '../utils/database';
+import { QueryResults, IDbConfig } from '../types/database';
+// import log from '../services/logger.service';
+import { ApplicationContext } from '../types/basic';
+
 type sqlConnect = {
   request: () => void;
+  close: () => void;
   query: () => void;
 };
 
@@ -8,7 +14,10 @@ function mssqlMock() {
     public connect = jest.fn().mockImplementation(
       (): Promise<sqlConnect> =>
         Promise.resolve({
-          request: jest.fn(),
+          request: jest.fn().mockImplementation(() => ({
+            input: jest.fn(),
+          })),
+          close: jest.fn(),
           query: jest
             .fn()
             .mockImplementation((): Promise<string> => Promise.resolve('test string')),
@@ -17,12 +26,38 @@ function mssqlMock() {
   };
 }
 
+function logMock() {
+  return class log {
+    public static info = jest.fn();
+    public static error = jest.fn();
+  };
+}
+
 describe('Tests database', () => {
   test('???', async () => {
     // setup test
+    jest.mock('../services/logger.service', logMock);
     jest.mock('mssql', mssqlMock);
+
     // execute method under test
+    const context = {} as ApplicationContext;
+    const config = {} as IDbConfig;
+    const query = 'SELECT * FROM foo WHERE data = @param1 AND name=@param2';
+    const input = [
+      {
+        name: 'param1',
+        type: null,
+        value: 'dataValue',
+      },
+      {
+        name: 'param2',
+        type: null,
+        value: 'nameValue',
+      },
+    ];
+    const queryResult: QueryResults = await executeQuery(context, config, query, input);
 
     // assert
+    console.log(queryResult);
   });
 });
