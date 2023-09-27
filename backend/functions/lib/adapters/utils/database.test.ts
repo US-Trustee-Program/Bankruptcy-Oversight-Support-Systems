bda1fae38b16fab653ccdbec51c42cdda5353d1e
Fritz Madden <fmadden@flexion.us>
2023-09-27 10:48:00 -0600

    CAMS-47: Added mock class for ConnectionPool
    Co-authored-by: Michael Ly <105381067+magile32@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <amorrow@Arthurs-MacBook-Pro.local>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>

--- backend/functions/lib/adapters/utils/database.test.ts
+++ backend/functions/lib/adapters/utils/database.test.ts
@@ -1,18 +1,26 @@
 type sqlConnect = {
   request: () => void;
   query: () => void;
+};
+
+function mssqlMock() {
+  return class ConnectionPool {
+    public connect = jest.fn().mockImplementation(
+      (): Promise<sqlConnect> =>
+        Promise.resolve({
+          request: jest.fn(),
+          query: jest
+            .fn()
+            .mockImplementation((): Promise<string> => Promise.resolve('test string')),
+        }),
+    );
+  };
 }
+
 describe('Tests database', () => {
   test('???', async () => {
     // setup test
-    jest.mock('mssql', () => ({
-      ConnectionPool: {
-        connect: jest.fn().mockImplementation((): Promise<sqlConnect> => (Promise.resolve({
-          request: jest.fn(),
-          query: jest.fn().mockImplementation((): Promise<string> => Promise.resolve('test string')),
-        }))),
-      },
-    }));
+    jest.mock('mssql', mssqlMock);
     // execute method under test
 
     // assert
