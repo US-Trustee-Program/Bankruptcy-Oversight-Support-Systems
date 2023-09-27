08d8114efdccb1cba160cf24e959bbc5876dc801
Arthur Morrow <amorrow@flexion.us>
2023-09-27 12:28:52 -0400

    CAMS-47: finished mock implementation for databasse.test\n\nCo-authored-by: Fritz Madden <fmadden@flexion.us>\nCo-authored-by: Michael Ly <105381067+magile32@users.noreply.github.com>\nCo-authored-by: Arthur Morrow <amorrow@Arthurs-MacBook-Pro.local>\n

--- backend/functions/lib/adapters/utils/database.test.ts
+++ backend/functions/lib/adapters/utils/database.test.ts
@@ -1,12 +1,16 @@
+type sqlConnect = {
+  request: () => void;
+  query: () => void;
+}
 describe('Tests database', () => {
   test('???', async () => {
     // setup test
     jest.mock('mssql', () => ({
       ConnectionPool: {
-        connect: jest.fn().mockImplementation(() => ({
+        connect: jest.fn().mockImplementation((): Promise<sqlConnect> => (Promise.resolve({
           request: jest.fn(),
-          query: jest.fn().mockImplementation(() => 'test string'),
-        })),
+          query: jest.fn().mockImplementation((): Promise<string> => Promise.resolve('test string')),
+        }))),
       },
     }));
     // execute method under test
