202d881c96dc89a22912598073293d097891c93b
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-09-29 17:07:01 -0500

    Fix mock of api response
    When I wrote the mock for api, I based it on how I mocked the attorneys
    api, but that works differently. We were returning just case details
    when we should have been returning an object with a body property which
    had a value of another object with a caseDetails property.
    
    Jira ticket: CAMS-164
    
    Co-authored-by: Michael Ly <105381067+magile32@users.noreply.github.com>

--- user-interface/src/components/CaseDetail.test.tsx
+++ user-interface/src/components/CaseDetail.test.tsx
@@ -1,5 +1,5 @@
 import { vi } from 'vitest';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, waitFor, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
@@ -8,6 +8,7 @@ import { CaseDetail } from './CaseDetail';
 const sleep = (milliseconds: number) =>
   new Promise((callback) => setTimeout(callback, milliseconds));
 
+const caseId = '101-23-12345';
 describe('Case Detail screen tests', () => {
   beforeEach(() => {
     vi.stubEnv('CAMS_PA11Y', 'true');
@@ -20,20 +21,26 @@ describe('Case Detail screen tests', () => {
           get: async () => {
             await sleep(1000);
             return {
-              caseId: '101-23-12345',
-              caseTitle: 'The Beach Boys',
-              dateFiled: '01-04-1962',
-              dateClosed: '01-08-1963',
-              assignedStaff: [
-                {
-                  name: 'Brian Wilson',
-                  type: 'Trial Attorney',
+              message: '',
+              count: 1,
+              body: {
+                caseDetails: {
+                  caseId: caseId,
+                  caseTitle: 'The Beach Boys',
+                  dateFiled: '01-04-1962',
+                  dateClosed: '01-08-1963',
+                  assignedStaff: [
+                    {
+                      name: 'Brian Wilson',
+                      type: 'Trial Attorney',
+                    },
+                    {
+                      name: 'Carl Wilson',
+                      type: 'Trial Attorney',
+                    },
+                  ],
                 },
-                {
-                  name: 'Carl Wilson',
-                  type: 'Trial Attorney',
-                },
-              ],
+              },
             };
           },
         },
@@ -45,14 +52,14 @@ describe('Case Detail screen tests', () => {
     vi.unstubAllEnvs();
   });
 
-  test('should display dates for the case', async () => {
+  test('should display case title, case number, dates, and assignees for the case', async () => {
     vi.mock('react-router-dom', async () => {
       const actual = (await vi.importActual('react-router-dom')) as object;
       return {
         ...actual,
         useParams: () => {
           return {
-            caseId: '101-23-12345',
+            caseId,
           };
         },
       };
@@ -64,21 +71,28 @@ describe('Case Detail screen tests', () => {
         </Provider>
       </BrowserRouter>,
     );
+    await waitForElementToBeRemoved(screen.getByTestId('loading-indicator')).then(() => {
+      sleep(100);
+    });
+
     await waitFor(
       async () => {
-        const loadingText = screen.getByTestId('loading-indicator');
-        if (loadingText) throw Error('is still loading');
-      },
-      { timeout: 10000 },
-    );
-    const title = screen.getByTestId('case-detail-heading');
-    expect(title).toEqual('The Beach Boys');
-    /*
-    const dateFiled = screen.getByTestId('case-detail-filed-date');
-    const dateClosed = screen.getByTestId('case-detail-closed-date');
+        const title = screen.getByTestId('case-detail-heading');
+        expect(title.innerHTML).toEqual('The Beach Boys');
+        const caseNumber = document.querySelector('.case-number');
+        expect(caseNumber?.innerHTML).toEqual(caseId);
 
-    expect(dateFiled.innerHTML).toEqual('01-04-1962');
-    expect(dateClosed.innerHTML).toEqual('01-08-1963');
-    */
+        const dateFiled = screen.getByTestId('case-detail-filed-date');
+        const dateClosed = screen.getByTestId('case-detail-closed-date');
+        expect(dateFiled.innerHTML).toEqual('01-04-1962');
+        expect(dateClosed.innerHTML).toEqual('01-08-1963');
+
+        /*const assignees = document.querySelectorAll('.assigned-staff-list .individual-assignee');
+        assignees?.forEach((assignee) => {
+
+        });*/
+      },
+      { timeout: 5000 },
+    );
   }, 20000);
 });
