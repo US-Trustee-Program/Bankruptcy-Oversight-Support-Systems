946c0daccfc313578d8ff6e61bf90daaa3ad3c5f
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-10-04 11:07:40 -0700

    CAMS-132 refactor test related to updates to index.ts
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>
    Co-authored-by: Arthur Morrow <amorrow@flexion.us>
    Co-authored-by: Brian Posey <bposey@flexion.us>

--- backend/functions/lib/adapters/controllers/cases.controller.ts
+++ backend/functions/lib/adapters/controllers/cases.controller.ts
@@ -2,7 +2,7 @@ import { ApplicationContext } from '../types/basic';
 import { applicationContextCreator } from '../utils/application-context-creator';
 import { Context } from '@azure/functions';
 import log from '../services/logger.service';
-import Chapter15Case from '../../use-cases/chapter-15.case';
+import { Chapter15CaseList } from '../../use-cases/chapter-15.case';
 import InvalidChapterCaseList from '../../use-cases/invalid-chapter.case-list';
 
 const NAMESPACE = 'CASES-CONTROLLER';
@@ -17,7 +17,7 @@ export class CasesController {
   public async getCaseList(requestQueryFilters: { caseChapter: string }) {
     log.info(this.applicationContext, NAMESPACE, 'Getting case list.');
     if (requestQueryFilters.caseChapter == '15') {
-      const chapter15CaseList = new Chapter15Case();
+      const chapter15CaseList = new Chapter15CaseList();
       return await chapter15CaseList.getChapter15CaseList(this.applicationContext);
     } else {
       const invalidChapterCaseList = new InvalidChapterCaseList();
@@ -26,7 +26,7 @@ export class CasesController {
   }
 
   public async getCaseDetails(requestQueryFilters: { caseId: string }) {
-    const chapter15CaseDetail = new Chapter15Case();
+    const chapter15CaseDetail = new Chapter15CaseList();
     return chapter15CaseDetail.getChapter15CaseDetail(
       this.applicationContext,
       requestQueryFilters.caseId,
