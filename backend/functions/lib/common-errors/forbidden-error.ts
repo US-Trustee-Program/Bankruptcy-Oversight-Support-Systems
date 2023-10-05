2170cfa1739b17037454283082d78c8123e12c3f
Arthur Morrow <amorrow@flexion.us>
2023-10-05 16:03:32 -0400

    CAMS-142: removed findAssignment and added ForbiddenError throw
    Co-authored-by: Brian <bposey@flexion.us>
    Co-authored-by: Michael Ly <105381067+magile32@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <amorrow@Arthurs-MacBook-Pro.local>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Fritz Madden <fmadden@flexion.us>

--- backend/functions/lib/common-errors/forbidden-error.ts
+++ backend/functions/lib/common-errors/forbidden-error.ts
@@ -3,8 +3,8 @@ import { CamsError } from './cams-error';
 export class ForbiddenError extends CamsError {
   originalError: Error;
 
-  constructor(module: string, error: Error) {
-    const message = error.message || 'Unknown error';
+  constructor(module: string, error: Error, message?: string) {
+    message = message || error.message || 'Request is Forbidden';
     super(403, message, module);
     this.originalError = error;
   }
