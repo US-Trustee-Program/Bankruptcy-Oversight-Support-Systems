96737b282b7712c45f5feeb55b2f911741cf4599
Brian Posey <bposey@flexion.us>
2023-10-06 16:13:11 -0400

    Fixing breaking tests and adding tests for coverage
    Jira ticket: CAMS-142
    
    Co-authored-by: Fritz Madden <fmadden@flexion.us>
    Co-authored-by: Arthur Morrow <amorrow@Arthurs-MacBook-Pro.local>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Michael Ly <105381067+magile32@users.noreply.github.com>
    Co-authored-by: Brian Posey <bposey@flexion.us>

--- backend/functions/lib/adapters/utils/http-response.test.ts
+++ backend/functions/lib/adapters/utils/http-response.test.ts
@@ -1,4 +1,6 @@
-import { httpSuccess } from './http-response';
+import { CamsError } from '../../common-errors/cams-error';
+import { INTERNAL_SERVER_ERROR } from '../../common-errors/constants';
+import { httpError, httpSuccess } from './http-response';
 
 describe('Tests out http responses', () => {
   test('Should return properly formatted http success response', () => {
@@ -6,6 +8,17 @@ describe('Tests out http responses', () => {
     const actualResult = httpSuccess(expectedBody);
 
     expect(actualResult.statusCode).toEqual(200);
+    expect(actualResult.body).toEqual(expectedBody);
+    expect(actualResult.headers).toHaveProperty('Content-Type', 'application/json');
+    expect(actualResult.headers).toHaveProperty('Last-Modified');
+  });
+  test('Should return properly formatted http error response', () => {
+    const moduleName = 'TEST-MODULE';
+    const camsError = new CamsError(moduleName, { message: 'Foo', status: INTERNAL_SERVER_ERROR });
+    const expectedBody = { error: camsError.message };
+    const actualResult = httpError(camsError);
+
+    expect(actualResult.statusCode).toEqual(camsError.status);
     expect(actualResult.body).toEqual(expectedBody);
     expect(actualResult.headers).toHaveProperty('Content-Type', 'application/json');
     expect(actualResult.headers).toHaveProperty('Last-Modified');
