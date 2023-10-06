860641398d47147690d48af6931aa4f25c89822f
Fritz Madden <fmadden@flexion.us>
2023-10-06 12:01:51 -0600

    Added log.camsError method to the logger service.
    [skip ci]
    
    Jira ticket: CAMS-142
    
    Co-authored-by: Fritz Madden <fmadden@flexion.us>
    Co-authored-by: Arthur Morrow <amorrow@Arthurs-MacBook-Pro.local>
    Co-authored-by: Mrinalini Sinha <mrinalinisinha05@gmail.com>
    Co-authored-by: Brian Posey <bposey@flexion.us>

--- backend/functions/lib/common-errors/utility.ts
+++ backend/functions/lib/common-errors/utility.ts
@@ -2,7 +2,7 @@ import { CamsError } from './cams-error';
 import { FORBIDDEN } from './constants';
 import { ForbiddenError } from './forbidden-error';
 
-export function toCamsError(moduleName: string, originalError: Error, statusCode: number) {
+export function toCamsError(moduleName: string, originalError: Error, statusCode?: number) {
   if (originalError instanceof CamsError) return originalError;
 
   // TODO: pull statusCode out of params and get it from originalError
