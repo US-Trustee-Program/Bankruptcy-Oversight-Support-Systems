faf53c6496ee0406d92ac405abea84755c342b5d
Mrinalini Sinha <20514925+mrinsin@users.noreply.github.com>
2023-10-09 10:45:55 -0600

    Setup jest config to clear all mocks after each test is run, updated git hook setup script
    Jira ticket: CAMS-142 CAMS-190
    
     Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
     Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
     Co-authored-by: Michael Ly <105381067+magile32@users.noreply.github.com>
     Co-authored-by: Mrinalini Sinha <20514925+mrinsin@users.noreply.github.com>
     Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- backend/functions/attorneys/attorneys.function.test.ts
+++ backend/functions/attorneys/attorneys.function.test.ts
@@ -5,13 +5,8 @@ import httpTrigger from './attorneys.function';
 import * as httpResponseModule from '../lib/adapters/utils/http-response';
 import { AttorneysController } from '../lib/adapters/controllers/attorneys.controller';
 import { CamsError } from '../lib/common-errors/cams-error';
-import clearAllMocks = jest.clearAllMocks;
 
 describe('Attorneys Azure Function tests', () => {
-  beforeEach(() => {
-    clearAllMocks();
-  });
-
   it('Should call getAttorneyList with office id if parameter was passed in URL', async () => {
     const officeId = '123';
     const request = {
