b12f76494b04204e93ddbe2ddefa4a21f9e8a765
Fritz Madden <fmadden@flexion.us>
2023-10-10 09:48:32 -0600

    Added comment to remind us to add info about Icon
    Added comment to remind us to add info about Icon naming options so that
    it's easier to use this component without having to search USWDS
    
    Jira ticket: CAMS-189

--- user-interface/src/components/uswds/Icon.tsx
+++ user-interface/src/components/uswds/Icon.tsx
@@ -1,3 +1,7 @@
+/*
+ * The possible strings for icon names should be added to the code in order to make it easier
+ * to determine what the possible options are.
+ */
 export interface IconProps {
   name: string;
   focusable?: boolean;
