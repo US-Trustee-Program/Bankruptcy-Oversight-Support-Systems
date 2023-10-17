65803b12e4090d57533bafb73f3ebf5790c20ca1
Mrinalini Sinha <20514925+mrinsin@users.noreply.github.com>
2023-10-17 11:20:40 -0600

    Updated build process to include common package
    Jira ticket: CAMS-197
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

---
+++ common/src/feature-flags.ts
@@ -0,0 +1,12 @@
+// This internal interface aligns with the LaunchDarkly LDFlagSet interface that
+// types the return of the useFlags hook. It is more restrictive than the `any` type
+// used for the value which could include JSON / object literal payloads. If we were
+// to use JSON feature flag values out of LaunchDarkly then this definition would
+// need to be revisited.
+export interface FeatureFlagSet {
+  [key: string]: boolean | string | number;
+}
+
+export const defaultFeatureFlags: FeatureFlagSet = {
+  'chapter-twelve-enabled': true,
+};
