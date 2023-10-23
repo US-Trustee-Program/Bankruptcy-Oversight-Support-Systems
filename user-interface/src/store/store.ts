d0d3ef75d5aba3ac23d0cde1974d150a9d4f6fe8
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-10-23 10:48:01 -0700

    Remove unused provider related to store.ts
    Jira ticket: CAMS-193
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Mrinalini Sinha <20514925+mrinsin@users.noreply.github.com>

--- user-interface/src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-
-export const store = configureStore({
-  reducer: {},
-});
-
-export const useAppDispatch: () => typeof store.dispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<ReturnType<typeof store.getState>> = useSelector;
