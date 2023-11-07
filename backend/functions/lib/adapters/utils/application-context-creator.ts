1851543fe15a8c40acea3bc1a19c49128f191fdf
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-11-06 22:13:54 -0600

    WIP - troubleshooting
    Jira ticket: CAMS-141

--- backend/functions/lib/adapters/utils/application-context-creator.ts
+++ backend/functions/lib/adapters/utils/application-context-creator.ts
@@ -2,6 +2,7 @@ import { Context } from '@azure/functions';
 import { ApplicationContext } from '../types/basic';
 import { ApplicationConfiguration } from '../../configs/application-configuration';
 import { getFeatureFlags } from './feature-flag';
+import log from '../services/logger.service';
 
 export async function applicationContextCreator(
   functionContext: Context,
@@ -9,9 +10,11 @@ export async function applicationContextCreator(
   const config = new ApplicationConfiguration();
   const featureFlags = await getFeatureFlags(config);
 
-  return {
+  const appContext = {
     ...functionContext,
     config,
     featureFlags,
-  };
+  } as ApplicationContext;
+  log.info(appContext, 'context-creator', 'context contents -------', appContext);
+  return appContext;
 }
