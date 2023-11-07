6295942a0796da731b1fef502e65f9efcc122703
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-11-07 08:43:50 -0600

    Fix logger service test
    Jira ticket: CAMS-141
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>

--- backend/functions/lib/adapters/utils/application-context-creator.ts
+++ backend/functions/lib/adapters/utils/application-context-creator.ts
@@ -2,7 +2,6 @@ import { Context } from '@azure/functions';
 import { ApplicationContext } from '../types/basic';
 import { ApplicationConfiguration } from '../../configs/application-configuration';
 import { getFeatureFlags } from './feature-flag';
-import log from '../services/logger.service';
 
 export async function applicationContextCreator(
   functionContext: Context,
@@ -15,6 +14,5 @@ export async function applicationContextCreator(
     config,
     featureFlags,
   } as ApplicationContext;
-  log.info(appContext, 'context-creator', 'context contents -------', appContext);
   return appContext;
 }
