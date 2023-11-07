b33501228138d6e0049676e3a184f1c90224332a
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-11-07 09:08:40 -0600

    Remove commented out tests
    We had some tests that were relying on behavior that was a consequence
    of treating the case assignment repository as if it persisted accross
    api calls, which it never did. Removing the repository from the context
    resulted in the tests failing because the repository is now instantiated
     for each call to the controller which is the same behavior as at
    runtime. Several attempts to test exceptional behavior via mocks were
    not successful. We will address in a DevEx task.
    
    Jira ticket: CAMS-141
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- backend/functions/lib/adapters/controllers/case.assignment.controller.test.ts
+++ backend/functions/lib/adapters/controllers/case.assignment.controller.test.ts
@@ -15,6 +15,8 @@ describe('Case Assignment Creation Tests', () => {
     };
   });
 
+  // TODO: figure out a way to test exceptional behavior
+
   test('A case is assigned to an attorney when requested', async () => {
     const testCaseAssignment = {
       caseId: '001-18-12345',
@@ -34,49 +36,6 @@ describe('Case Assignment Creation Tests', () => {
     expect(assignmentResponse.body.length).toBe(1);
     expect(assignmentResponse.body[0]).toBeTruthy();
   });
-
-  // TODO: figure out a way to test exceptional behavior
-  // test('should throw an assignment exception, if one already exists in the repository for the case', async () => {
-  //   const testCaseAssignment = {
-  //     caseId: '001-18-12345',
-  //     listOfAttorneyNames: ['Jane'],
-  //     role: trialAttorneyRole,
-  //   };
-  //
-  //   const assignmentController = new CaseAssignmentController(appContext);
-  //
-  //   try {
-  //     await assignmentController.createTrialAttorneyAssignments(testCaseAssignment);
-  //     await assignmentController.createTrialAttorneyAssignments(testCaseAssignment);
-  //     expect(true).toBeFalsy();
-  //   } catch (exception) {
-  //     expect(exception.message).toEqual(
-  //       'A trial attorney assignment already exists for this case. Cannot create another assignment on an existing case assignment.',
-  //     );
-  //   }
-  // });
-  //
-  // test('creating a new trial attorney assignment on a case with an existing assignment throws error', async () => {
-  //   const testCaseAssignment1 = {
-  //     caseId: '001-18-12345',
-  //     listOfAttorneyNames: ['Jane'],
-  //     role: trialAttorneyRole,
-  //   };
-  //
-  //   const testCaseAssignment2 = {
-  //     caseId: '001-18-12345',
-  //     listOfAttorneyNames: ['John', 'Jane'],
-  //     role: trialAttorneyRole,
-  //   };
-  //
-  //   const assignmentController = new CaseAssignmentController(appContext);
-  //
-  //   await assignmentController.createTrialAttorneyAssignments(testCaseAssignment1);
-  //
-  //   await expect(
-  //     assignmentController.createTrialAttorneyAssignments(testCaseAssignment2),
-  //   ).rejects.toThrow(AssignmentError);
-  // });
 
   test('should assign all attorneys in the list', async () => {
     const listOfAttorneyNames = ['Jane', 'Tom', 'Adrian'];
