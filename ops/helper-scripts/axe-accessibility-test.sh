1d1a7149563d268a70efa3272c91b180015f966e
Mrinalini Sinha <20514925+mrinsin@users.noreply.github.com>
2023-11-09 12:06:29 -0700

    CAMS-202 Updates accesibility step
    Jira ticket: CAMS-202
    
    Co-authored-by: Michael Ly <105381067+magile32@users.noreply.github.com>

---
+++ ops/helper-scripts/axe-accessibility-test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail # ensure job step fails in CI pipeline when error occurs
+
+export CAMS_PA11Y=true
+npm run build
+npm install serve -g
+serve -s build &
+timeout 120s sh -c 'until nc -z localhost 3000; do sleep 1; done'
+if (( $? != 0 )); then
+    echo "Webapp check timed out"
+    exit 1
+fi
+
+echo "Starting axe accessibility test"
+docker run axe http://localhost:3000 --tags section508,wcag22aa,best-practice --exit
