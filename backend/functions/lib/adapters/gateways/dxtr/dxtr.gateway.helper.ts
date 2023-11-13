5d15ecccb14bcf6624cf02851d83a442f945db22
Fritz Madden <fmadden@flexion.us>
2023-11-13 11:01:42 -0700

    Switched from regex to fixed character position
    Switched from regex to fixed character position in the helper
    
    Jira ticket: CAMS-214
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Mrinalini Sinha <20514925+mrinsin@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- backend/functions/lib/adapters/gateways/dxtr/dxtr.gateway.helper.ts
+++ backend/functions/lib/adapters/gateways/dxtr/dxtr.gateway.helper.ts
@@ -43,8 +43,10 @@ export function parseDebtorType(record: DxtrTransactionRecord): string {
   return getDebtorTypeLabel(debtorType);
 }
 
+// 1081231056523-10565            15IB00-0000000     000000000000000000230411999992304119999923041110308230411VP000000                                 NNNNN
 export function parsePetitionType(record: DxtrTransactionRecord): string {
-  const petitionMatch = record.txRecord.match(/\d{13}-\d{5}\s+\d{2}\w{2}[\w-]*\s+\d{57}\w{2}/);
-  const petition = petitionMatch ? petitionMatch[0].slice(-2) : undefined;
+  const codeLength = 2;
+  const codeIndex = 107;
+  const petition = record.txRecord.slice(codeIndex, codeIndex + codeLength);
   return getPetitionLabel(petition);
 }
