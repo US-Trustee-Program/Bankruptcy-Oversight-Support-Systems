a5338cb8b3e17f18ff9cc0430274326dcc1ce28a
Brian Posey <15091170+btposey@users.noreply.github.com>
2023-11-15 09:44:16 -0500

    Add dev tools support to add AO_DE records
    Jira ticket: CAMS-219

--- dev-tools/test-data/index.ts
+++ dev-tools/test-data/index.ts
@@ -1,8 +1,11 @@
 // Import fixtures to generate.
-import { toAoAtInsertStatements } from './tables/AO_AT';
-import { toAoCsInsertStatements } from './tables/AO_CS';
-import { toAoPyInsertStatements } from './tables/AO_PY';
-import { toAoTxInsertStatements } from './tables/AO_TX';
+import { AO_AT_Record, toAoAtInsertStatements } from './tables/AO_AT';
+import { AO_CS_Record, toAoCsInsertStatements } from './tables/AO_CS';
+import { AO_DE_Record, toAoDeInsertStatements } from './tables/AO_DE';
+import { AO_GRP_DES_Record, toAoGrpDesInsertStatements } from './tables/AO_GRP_DES';
+import { AO_PY_Record, toAoPyInsertStatements } from './tables/AO_PY';
+import { AO_REGION_Record, toAoRegionInsertStatements } from './tables/AO_REGION';
+import { AO_TX_Record, toAoTxInsertStatements } from './tables/AO_TX';
 import {
   BCase,
   BCaseTransactionTypeOrder,
@@ -12,12 +15,12 @@ import {
 } from './domain/bcase';
 import { CaseDetailInterface } from './cases';
 import { concatenateCityStateZipCountry, concatenateName } from './utility';
+import { TxCode } from './types';
 
 import { createChapter15Cases } from './fixtures/chapter15Cases';
 import { createNoJudgeAssignedCases } from './fixtures/noJudgeAssignedCases';
 import { CreateCaseOptions, createAttorney } from './fixtures/lib/common';
 import { createReopenedCases } from './fixtures/reopenedCases';
-import { TxCode } from './types';
 
 const validFormats = ['json', 'sql'];
 const format = process.argv[2];
@@ -90,28 +93,31 @@ if (format === 'json') {
   const dbRecordBundle = toDbRecords(bCases);
 
   // Output all the fixtures to SQL.
-  console.log('\n-- AO_AT Attorneys');
-  if (!dbRecordBundle.AO_AT.length) console.log('-- No AO_AT records.');
-  toAoAtInsertStatements(dbRecordBundle.AO_AT).forEach((statement) => {
-    console.log(statement.trim());
-  });
-  console.log('\n-- AO_CS Cases');
-  if (!dbRecordBundle.AO_CS.length) console.log('-- No AO_CS records.');
-  toAoCsInsertStatements(dbRecordBundle.AO_CS).forEach((statement) => {
-    console.log(statement.trim());
-  });
-  console.log('\n-- AO_PY Parties');
-  if (!dbRecordBundle.AO_PY.length) console.log('-- No AO_PY records.');
-  toAoPyInsertStatements(dbRecordBundle.AO_PY).forEach((statement) => {
-    console.log(statement.trim());
-  });
-  console.log('\n-- AO_TX Transactions');
-  if (!dbRecordBundle.AO_TX.length) console.log('-- No AO_TX records.');
-  toAoTxInsertStatements(dbRecordBundle.AO_TX).forEach((statement) => {
-    console.log(statement.trim());
-  });
+  generateSql<AO_AT_Record>('AO_AT Attorneys', toAoAtInsertStatements, dbRecordBundle.AO_AT);
+  generateSql<AO_CS_Record>('AO_CS Cases', toAoCsInsertStatements, dbRecordBundle.AO_CS);
+  generateSql<AO_GRP_DES_Record>(
+    'AO_GRP_DES ',
+    toAoGrpDesInsertStatements,
+    dbRecordBundle.AO_GRP_DES,
+  );
+  generateSql<AO_DE_Record>('AO_DE Docket Entries', toAoDeInsertStatements, dbRecordBundle.AO_DE);
+  generateSql<AO_PY_Record>('AO_AT Attorneys', toAoPyInsertStatements, dbRecordBundle.AO_PY);
+  generateSql<AO_REGION_Record>(
+    'AO_PY Parties',
+    toAoRegionInsertStatements,
+    dbRecordBundle.AO_REGION,
+  );
+  generateSql<AO_TX_Record>('AO_TX Transactions', toAoTxInsertStatements, dbRecordBundle.AO_TX);
 }
 
 function pickTop(list: Array<BCaseTransactionTypeOrder>, code: TxCode) {
   return list.filter((i) => i.code === code).sort((a, b) => (a.date < b.date ? 1 : -1))[0];
+}
+
+function generateSql<T>(label: string, mapper: (n: Array<T>) => Array<string>, data: Array<T>) {
+  console.log('\n-- ' + label);
+  if (!data.length) console.log('-- No records.');
+  mapper(data).forEach((statement) => {
+    console.log(statement.trim());
+  });
 }
