767044aa9983cd4a4e1277f13b02a8097fc31193
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2023-11-16 15:09:02 -0600

    Make UI improvements
    Cherry picked changes from another branch that had some bad changes
    merged into it.
    
    The left nav content is known but was only being shown when isLoading is
    false.
    The docket record content was taking up too much space.
    The mock data for use with pa11y was not mocking the docket record. The
    backend mock was also not taking advantage of the new pattern we've
    begun using to get the data from a constant in a neighboring TypeScript
    file.
    
    Jira ticket: CAMS-219
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Mrinalini Sinha <20514925+mrinsin@users.noreply.github.com>
    co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Michael Ly <105381067+magile32@users.noreply.github.com>

--- backend/functions/lib/adapters/gateways/dxtr/case-docket.mock.gateway.ts
+++ backend/functions/lib/adapters/gateways/dxtr/case-docket.mock.gateway.ts
@@ -2,65 +2,19 @@ import { NotFoundError } from '../../../common-errors/not-found-error';
 import { CaseDocket } from '../../../use-cases/case-docket/case-docket.model';
 import { ApplicationContext } from '../../types/basic';
 import { CaseDocketGateway } from '../gateways.types';
+import { GatewayHelper } from '../gateway-helper';
 
 const MODULENAME = 'CASE-DOCKET-MOCK-GATEWAY';
-
-export const DOCKET: CaseDocket = [
-  {
-    sequenceNumber: 0,
-    documentNumber: null,
-    dateFiled: '11-05-2015',
-    summaryText: 'Motion for Joint Administration',
-    fullText:
-      'Clamo bellum repellendus conservo patrocinor commemoro. Coniuratio victus blanditiis ulterius voluptate territo utrimque tam umerus. Repellendus creta cum carpo laudantium adhuc volva provident dolores aqua. Tredecim demens acsi consectetur adfectus compello pecus sed complectus. Conspergo caecus absorbeo.',
-  },
-  {
-    sequenceNumber: 1,
-    documentNumber: null,
-    dateFiled: '07-12-2016',
-    summaryText: 'Order Re: Motion for Joint Administration',
-    fullText:
-      'Vorax venio comminor quasi toties eaque soluta. Statua denique asper desino. Voluptatibus inventore cupiditate. Vigilo crastinus contigo aestus credo.',
-  },
-  {
-    sequenceNumber: 2,
-    documentNumber: null,
-    dateFiled: '08-15-2016',
-    summaryText: 'Auto- docket of credit card',
-    fullText: 'Textor combibo virtus eum stillicidium tabella tempus sub audax.',
-  },
-  {
-    sequenceNumber: 3,
-    documentNumber: null,
-    dateFiled: '06-09-2018',
-    summaryText: 'Case Association - Joint Administration',
-    fullText:
-      'Centum vis auctor cupiditate voluptatibus usus demoror valeo summopere. Demens fuga tumultus comes caput charisma. Clarus tardus approbo comes trepide dolores.\nComes enim velit provident quas votum vis tenax timidus eius. Vociferor sponte ipsam. Vinum cinis corporis delectatio.\nAufero timidus aggero deludo capio summisse ambitus blanditiis maxime. Acerbitas pecus occaecati comparo vado. Aveho autus eligendi illo quae praesentium soluta cupio.',
-  },
-  {
-    sequenceNumber: 4,
-    documentNumber: '4',
-    dateFiled: '01-01-2018',
-    summaryText: 'Petition for Recognition of Foreign Proceeding',
-    fullText:
-      'Corpus truculenter astrum cui tamen tribuo. Sodalitas qui carcer alias vallum sponte. Addo conturbo utique.',
-  },
-  {
-    sequenceNumber: 5,
-    documentNumber: '5',
-    dateFiled: '11-30-2015',
-    summaryText: 'Case Association - Joint Administration',
-    fullText:
-      'Ustilo basium teneo abeo urbanus terminatio somniculosus sapiente tollo capto. Curatio curo abbas. Adulatio curo sufficio comminor conicio. Sed quia argentum campana admiratio. Ut odio admoveo adsidue confero ambulo urbanus tenus.',
-  },
-];
 
 export const NORMAL_CASE_ID = '111-11-11111';
 
 export class MockCaseDocketGateway implements CaseDocketGateway {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async getCaseDocket(_context: ApplicationContext, caseId: string): Promise<CaseDocket> {
-    if (caseId === NORMAL_CASE_ID) return Promise.resolve(DOCKET);
+    const gatewayHelper = new GatewayHelper();
+    if (caseId === NORMAL_CASE_ID) {
+      return Promise.resolve(gatewayHelper.getCaseDocketEntriesMockExtract());
+    }
     return Promise.reject(new NotFoundError(MODULENAME, { data: { caseId } }));
   }
 }
--- backend/functions/lib/adapters/gateways/gateway-helper.ts
+++ backend/functions/lib/adapters/gateways/gateway-helper.ts
@@ -5,6 +5,8 @@ import { ApplicationContext } from '../types/basic';
 import { CamsError } from '../../common-errors/cams-error';
 import { ATTORNEYS } from '../../testing/mock-data/debtor-attorneys.mock';
 import { DEBTORS } from '../../testing/mock-data/debtors.mock';
+import { CASE_DOCKET_ENTRIES } from '../../testing/mock-data/case-docket-entries.mock';
+import { CaseDocketEntry } from '../../use-cases/case-docket/case-docket.model';
 
 export class GatewayHelper {
   getAllCasesMockExtract(): CaseDetailInterface[] {
@@ -16,6 +18,10 @@ export class GatewayHelper {
     } catch (err) {
       throw Error(err);
     }
+  }
+
+  getCaseDocketEntriesMockExtract(): CaseDocketEntry[] {
+    return CASE_DOCKET_ENTRIES;
   }
 
   getAllDebtorsMockExtract(): Map<string, Party> {
