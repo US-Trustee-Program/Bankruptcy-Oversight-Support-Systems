e77e79bcaf2deda52388991e3594f1c7f14620a3
Brian Posey <15091170+btposey@users.noreply.github.com>
2023-11-17 16:23:02 -0500

    Format case docket dates to expected MM-dd-yyyy
    Jira ticket: CAMS-219
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- backend/functions/lib/adapters/gateways/dxtr/case-docket.mock.gateway.ts
+++ backend/functions/lib/adapters/gateways/dxtr/case-docket.mock.gateway.ts
@@ -9,7 +9,7 @@ export const DOCKET: CaseDocket = [
   {
     sequenceNumber: 0,
     documentNumber: null,
-    dateFiled: '2015-11-05T00:00:00.000Z',
+    dateFiled: '11-05-2015',
     summaryText: 'Motion for Joint Administration',
     fullText:
       'Clamo bellum repellendus conservo patrocinor commemoro. Coniuratio victus blanditiis ulterius voluptate territo utrimque tam umerus. Repellendus creta cum carpo laudantium adhuc volva provident dolores aqua. Tredecim demens acsi consectetur adfectus compello pecus sed complectus. Conspergo caecus absorbeo.',
@@ -17,7 +17,7 @@ export const DOCKET: CaseDocket = [
   {
     sequenceNumber: 1,
     documentNumber: null,
-    dateFiled: '2016-07-12T00:00:00.000Z',
+    dateFiled: '07-12-2016',
     summaryText: 'Order Re: Motion for Joint Administration',
     fullText:
       'Vorax venio comminor quasi toties eaque soluta. Statua denique asper desino. Voluptatibus inventore cupiditate. Vigilo crastinus contigo aestus credo.',
@@ -25,14 +25,14 @@ export const DOCKET: CaseDocket = [
   {
     sequenceNumber: 2,
     documentNumber: null,
-    dateFiled: '2016-08-15T00:00:00.000Z',
+    dateFiled: '08-15-2016',
     summaryText: 'Auto- docket of credit card',
     fullText: 'Textor combibo virtus eum stillicidium tabella tempus sub audax.',
   },
   {
     sequenceNumber: 3,
     documentNumber: null,
-    dateFiled: '2018-06-09T00:00:00.000Z',
+    dateFiled: '06-09-2018',
     summaryText: 'Case Association - Joint Administration',
     fullText:
       'Centum vis auctor cupiditate voluptatibus usus demoror valeo summopere. Demens fuga tumultus comes caput charisma. Clarus tardus approbo comes trepide dolores.\nComes enim velit provident quas votum vis tenax timidus eius. Vociferor sponte ipsam. Vinum cinis corporis delectatio.\nAufero timidus aggero deludo capio summisse ambitus blanditiis maxime. Acerbitas pecus occaecati comparo vado. Aveho autus eligendi illo quae praesentium soluta cupio.',
@@ -40,7 +40,7 @@ export const DOCKET: CaseDocket = [
   {
     sequenceNumber: 4,
     documentNumber: '4',
-    dateFiled: '2018-01-01T00:00:00.000Z',
+    dateFiled: '01-01-2018',
     summaryText: 'Petition for Recognition of Foreign Proceeding',
     fullText:
       'Corpus truculenter astrum cui tamen tribuo. Sodalitas qui carcer alias vallum sponte. Addo conturbo utique.',
@@ -48,7 +48,7 @@ export const DOCKET: CaseDocket = [
   {
     sequenceNumber: 5,
     documentNumber: '5',
-    dateFiled: '2015-11-30T00:00:00.000Z',
+    dateFiled: '11-30-2015',
     summaryText: 'Case Association - Joint Administration',
     fullText:
       'Ustilo basium teneo abeo urbanus terminatio somniculosus sapiente tollo capto. Curatio curo abbas. Adulatio curo sufficio comminor conicio. Sed quia argentum campana admiratio. Ut odio admoveo adsidue confero ambulo urbanus tenus.',
