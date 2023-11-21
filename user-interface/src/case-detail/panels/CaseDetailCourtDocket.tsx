acca279551034d668b8a81893ed5f3b334da7b41
Brian Posey <15091170+btposey@users.noreply.github.com>
2023-11-21 09:39:19 -0500

    Preliminary WIP to highlight search text
    Jira ticket: CAMS-221
    
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- user-interface/src/case-detail/panels/CaseDetailCourtDocket.tsx
+++ user-interface/src/case-detail/panels/CaseDetailCourtDocket.tsx
@@ -1,9 +1,9 @@
+import { useEffect, useState } from 'react';
 import IconInput from '@/lib/components/IconInput';
-import './CaseDetailCourtDocket.scss';
 import LoadingIndicator from '@/lib/components/LoadingIndicator';
 import { CaseDocketEntry } from '@/lib/type-declarations/chapter-15';
-import { useEffect, useState } from 'react';
 import useFeatureFlags, { DOCKET_SEARCH_ENABLED } from '@/lib/hooks/UseFeatureFlags';
+import './CaseDetailCourtDocket.scss';
 
 export interface CaseDetailCourtDocketProps {
   caseId: string | undefined;
@@ -12,22 +12,25 @@ export interface CaseDetailCourtDocketProps {
 
 export default function CaseDetailCourtDocket(props: CaseDetailCourtDocketProps) {
   const { docketEntries } = props;
+  // TODO: Replace with useTransition
   const [isLoading, setIsLoading] = useState(true);
+  const [searchString, setSearchString] = useState('');
+  const [filteredDocketEntries, setFilteredDocketEntries] = useState(docketEntries);
+
   const flags = useFeatureFlags();
-  const [visibleDocketEntries, setVisibleDocketEntries] = useState(docketEntries);
   const searchFeature = flags[DOCKET_SEARCH_ENABLED];
+
+  function docketSearchFilter(docketEntry: CaseDocketEntry) {
+    return (
+      docketEntry.summaryText.toLowerCase().includes(searchString) ||
+      docketEntry.fullText.toLowerCase().includes(searchString)
+    );
+  }
 
   function search(ev: React.ChangeEvent<HTMLInputElement>) {
     const searchString = ev.target.value.toLowerCase();
-    if (docketEntries) {
-      const filteredDocketEntries = docketEntries.filter((docketEntry) => {
-        return (
-          docketEntry.summaryText.toLowerCase().includes(searchString) ||
-          docketEntry.fullText.toLowerCase().includes(searchString)
-        );
-      });
-      setVisibleDocketEntries(filteredDocketEntries);
-    }
+    setSearchString(searchString || '');
+    setFilteredDocketEntries(docketEntries?.filter(docketSearchFilter));
   }
 
   useEffect(() => {
@@ -35,8 +38,64 @@ export default function CaseDetailCourtDocket(props: CaseDetailCourtDocketProps)
   }, [docketEntries]);
 
   useEffect(() => {
-    console.log('this docket entries have change');
-  }, [visibleDocketEntries]);
+    // TODO: Need to find type declaration for the browser window and replace this adhoc type def.
+    const browserApi = window as unknown as {
+      CSS: {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        highlights?: any;
+      };
+    };
+    if (browserApi.CSS.highlights) {
+      console.log('Highlight API is supported.');
+      if (!searchString || searchString.length === 0) {
+        browserApi.CSS.highlights.clear();
+        return;
+      }
+      // TODO: Don't like having to get by the entire class name.... Find a better way.
+      const docketNode = document.getElementById('searchable-docket');
+      if (docketNode) {
+        const treeWalker = document.createTreeWalker(docketNode, NodeFilter.SHOW_TEXT);
+        const allTextNodes = [];
+        let currentNode = treeWalker.nextNode();
+        while (currentNode) {
+          allTextNodes.push(currentNode);
+          currentNode = treeWalker.nextNode();
+        }
+
+        const ranges = allTextNodes
+          .map((el) => {
+            return { el, text: el.textContent?.toLowerCase() || '' };
+          })
+          .map(({ text, el }) => {
+            const indices = [];
+            let startPos = 0;
+            while (startPos < text.length) {
+              const index = text.indexOf(searchString, startPos);
+              if (index === -1) break;
+              indices.push(index);
+              startPos = index + searchString.length;
+            }
+
+            // Create a range object for each instance of
+            // str we found in the text node.
+            return indices.map((index) => {
+              const range = new Range();
+              range.setStart(el, index);
+              range.setEnd(el, index + searchString.length);
+              return range;
+            });
+          });
+
+        console.log('ranges', ranges);
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const searchResultsHighlight = new Highlight(...ranges.flat());
+        browserApi.CSS.highlights.set('search-results', searchResultsHighlight);
+      }
+    } else {
+      console.log('Highlight API is not supported.');
+    }
+  }, [filteredDocketEntries]);
 
   return (
     <div id="case-detail-court-docket-panel">
@@ -62,11 +121,11 @@ export default function CaseDetailCourtDocket(props: CaseDetailCourtDocketProps)
           </div>
         </form>
       )}
-      {isLoading && <LoadingIndicator />}
-      {!isLoading &&
-        visibleDocketEntries &&
-        (visibleDocketEntries as Array<CaseDocketEntry>)?.map(
-          (docketEntry: CaseDocketEntry, idx: number) => {
+      <div id="searchable-docket">
+        {isLoading && <LoadingIndicator />}
+        {!isLoading &&
+          filteredDocketEntries &&
+          filteredDocketEntries.map((docketEntry: CaseDocketEntry, idx: number) => {
             return (
               <div
                 className="grid-row grid-gap-lg docket-entry"
@@ -98,8 +157,8 @@ export default function CaseDetailCourtDocket(props: CaseDetailCourtDocketProps)
                 </div>
               </div>
             );
-          },
-        )}
+          })}
+      </div>
     </div>
   );
 }
