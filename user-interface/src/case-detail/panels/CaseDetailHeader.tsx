1a421eb94cd741ef39a51edd8871d6b63f35e1ed
Fritz Madden <fmadden@flexion.us>
2023-11-27 17:33:54 -0700

    Sub Header of Case Detail info is fixed on scroll
    When the data on screen is scrolled such that the sub heading appears at
    the top of the screen, it becomes fixed in place.  It goes back to
    normal when screen is scrolled up.
    
    Jira ticket: CAMS-246

--- user-interface/src/case-detail/panels/CaseDetailHeader.tsx
+++ user-interface/src/case-detail/panels/CaseDetailHeader.tsx
@@ -1,7 +1,9 @@
+import './CaseDetailHeader.scss';
 import { Link } from 'react-router-dom';
 import Icon from '@/lib/components/uswds/Icon';
 import { getCaseNumber } from '@/lib/utils/formatCaseNumber';
 import { CaseDetailType } from '@/lib/type-declarations/chapter-15';
+import { useEffect } from 'react';
 
 export interface CaseDetailHeaderProps {
   isLoading: boolean;
@@ -13,6 +15,21 @@ export default function CaseDetailHeader(props: CaseDetailHeaderProps) {
   const courtInformation = `${props.caseDetail?.courtName} - ${props.caseDetail?.courtDivisionName}`;
   // u00A0 is a non-breaking space. Using &nbsp; in the string literal does not display correctly.
   const chapterInformation = `${props.caseDetail?.petitionLabel} Chapter\u00A0${props.caseDetail?.chapter}`;
+
+  useEffect(() => {
+    const caseH1 = document.querySelector('.case-detail-header h1');
+    const caseSubHeaders = document.querySelector('.case-detail-header .sub-headers');
+    if (caseH1 && caseSubHeaders) {
+      window.addEventListener('scroll', () => {
+        if (caseH1.getBoundingClientRect().bottom <= 20) {
+          if (!caseSubHeaders.classList.contains('fixed')) caseSubHeaders.classList.add('fixed');
+        } else {
+          if (caseSubHeaders.classList.contains('fixed')) caseSubHeaders.classList.remove('fixed');
+        }
+      });
+    }
+  }, [props.isLoading == false]);
+
   return (
     <div className="case-detail-header">
       <div className="grid-row grid-gap-lg">
@@ -50,7 +67,7 @@ export default function CaseDetailHeader(props: CaseDetailHeaderProps) {
       )}
 
       {!props.isLoading && (
-        <div className="grid-row grid-gap-lg" data-testid="h2-with-case-info">
+        <div className="grid-row grid-gap-lg sub-headers" data-testid="h2-with-case-info">
           <div className="grid-col-1"></div>
           <div className="grid-col-2">
             <h2 className="case-number text-no-wrap" title="Case Number">
