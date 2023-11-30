4e748a9f0a9bf4a1d5043460f915f6550887cc9b
Michael Ly <105381067+magile32@users.noreply.github.com>
2023-11-30 15:18:36 -0800

    CAMS-14 Add health check on sql db connection and update test

--- backend/functions/healthcheck/healthcheck.test.ts
+++ backend/functions/healthcheck/healthcheck.test.ts
@@ -1,6 +1,29 @@
 import httpTrigger from './healthcheck.function';
 const context = require('azure-function-context-mock');
 
+const mockRequestFunc = jest.fn().mockImplementation(() => ({
+  input: jest.fn(),
+  query: mockQueryFunc,
+}));
+const mockCloseFunc = jest.fn();
+const mockQueryFunc = jest.fn();
+const mockConnect = jest.fn().mockImplementation(
+  (): Promise<unknown> =>
+    Promise.resolve({
+      request: mockRequestFunc,
+      close: mockCloseFunc,
+    }),
+);
+jest.mock('mssql', () => {
+  return {
+    ConnectionPool: jest.fn().mockImplementation(() => {
+      return {
+        connect: mockConnect,
+      };
+    }),
+  };
+});
+
 test('Healthcheck endpoint should return an ALIVE status', async () => {
   const request = {
     query: {},
