67d512fffe83b066d3dea8130bfd7a1bcd485eeb
Fritz Madden <fmadden@flexion.us>
2023-12-04 17:24:04 -0700

    Fixed formatting on Facet Multi-select for USWDS
    Jira ticket: CAMS-223
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Michael Ly <105381067+magile32@users.noreply.github.com>

--- user-interface/src/lib/components/MultiSelect.tsx
+++ user-interface/src/lib/components/MultiSelect.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import './MultiSelect.scss';
 import ReactSelect from 'react-select';
 
 export declare type MultiSelectOptionList<Option> = readonly Option[];
@@ -11,12 +13,68 @@ export interface MultiSelectProps {
 
 export default function MultiSelect(props: MultiSelectProps) {
   //
+  const customStyles = {
+    control: (provided: any, state: { isFocused: any }) => ({
+      ...provided,
+      background: '#fff',
+      borderColor: '#565c65',
+      minHeight: '40px',
+      boxShadow: state.isFocused ? null : null,
+      outline: state.isFocused ? '0.25rem solid #2491ff' : null,
+      borderRadius: 0,
+      '&:hover': {
+        borderColor: '#565c65',
+      },
+    }),
+
+    placeholder: (provided: any) => ({
+      ...provided,
+      color: '#565C65',
+    }),
+
+    valueContainer: (provided: any) => ({
+      ...provided,
+      padding: '0 6px',
+    }),
+
+    input: (provided: any) => ({
+      ...provided,
+      margin: '0px',
+    }),
+
+    indicatorSeparator: () => ({
+      display: 'none',
+    }),
+
+    indicatorsContainer: (provided: any) => ({
+      ...provided,
+      height: '40px',
+    }),
+
+    clearIndicator: (provided: any) => ({
+      ...provided,
+      color: '#1b1b1b',
+    }),
+
+    dropdownIndicator: (provided: any) => ({
+      ...provided,
+      color: '#1b1b1b',
+    }),
+
+    multiValueLabel: (provided: any) => ({
+      ...provided,
+      color: '#1b1b1b',
+    }),
+  };
+
   return (
     <ReactSelect
       options={props.options}
       isMulti
       closeMenuOnSelect={props.closeMenuOnSelect}
       onChange={props.onChange}
+      className={`${props.className || ''} cams-multi-select`}
+      styles={customStyles}
     ></ReactSelect>
   );
 }
