748c34ae5023b34e4bc08ac17b4cc9faa8f0e6fb
Fritz Madden <fmadden@flexion.us>
2023-12-07 16:38:20 -0700

    Added tests for min and max dates
    Made sure attributes set correctly initially, and then modified properly
    when start and end date are updated.
    
    Jira ticket: CAMS-254

--- user-interface/src/lib/components/uswds/DateRangePicker.test.tsx
+++ user-interface/src/lib/components/uswds/DateRangePicker.test.tsx
@@ -32,4 +32,76 @@ describe('Test DateRangePicker component', async () => {
     });
     expect(mockHandlerEnd).toHaveBeenCalled();
   });
+
+  test('should set min and max attributes when minDate and maxDate is supplied', async () => {
+    render(
+      <React.StrictMode>
+        <DateRangePicker
+          id={'date-picker'}
+          minDate="01/01/2024"
+          maxDate="01/01/2025"
+        ></DateRangePicker>
+      </React.StrictMode>,
+    );
+
+    const startDateText = screen.getByTestId('date-picker-date-start');
+    expect(startDateText).toBeInTheDocument();
+    expect(startDateText).toHaveAttribute('min', '01/01/2024');
+    expect(startDateText).toHaveAttribute('max', '01/01/2025');
+
+    const endDateText = screen.getByTestId('date-picker-date-end');
+    expect(endDateText).toBeInTheDocument();
+    expect(endDateText).toHaveAttribute('min', '01/01/2024');
+    expect(endDateText).toHaveAttribute('max', '01/01/2025');
+  });
+
+  test('should adjust min and max attributes when start or end date is changed, and reset when cleared', async () => {
+    render(
+      <React.StrictMode>
+        <DateRangePicker
+          id={'date-picker'}
+          minDate="01/01/2020"
+          maxDate="01/01/2035"
+        ></DateRangePicker>
+      </React.StrictMode>,
+    );
+
+    let startDateText = screen.getByTestId('date-picker-date-start');
+    let endDateText = screen.getByTestId('date-picker-date-end');
+    expect(startDateText).toBeInTheDocument();
+    expect(startDateText).toHaveAttribute('max', '01/01/2035');
+    expect(endDateText).toHaveAttribute('min', '01/01/2020');
+
+    act(() => {
+      fireEvent.change(startDateText, { target: { value: '2022-05-01' } });
+    });
+
+    startDateText = screen.getByTestId('date-picker-date-start');
+    endDateText = screen.getByTestId('date-picker-date-end');
+    expect(startDateText).toHaveAttribute('max', '01/01/2035');
+    expect(endDateText).toHaveAttribute('min', '2022-05-01');
+
+    act(() => {
+      fireEvent.change(endDateText, { target: { value: '2025-07-01' } });
+    });
+
+    startDateText = screen.getByTestId('date-picker-date-start');
+    endDateText = screen.getByTestId('date-picker-date-end');
+    expect(startDateText).toHaveAttribute('max', '2025-07-01');
+    expect(endDateText).toHaveAttribute('min', '2022-05-01');
+
+    act(() => {
+      fireEvent.change(startDateText, { target: { value: '' } });
+    });
+
+    expect(startDateText).toHaveAttribute('max', '2025-07-01');
+    expect(endDateText).toHaveAttribute('min', '01/01/2020');
+
+    act(() => {
+      fireEvent.change(endDateText, { target: { value: '' } });
+    });
+
+    expect(startDateText).toHaveAttribute('max', '01/01/2035');
+    expect(endDateText).toHaveAttribute('min', '01/01/2020');
+  });
 });
