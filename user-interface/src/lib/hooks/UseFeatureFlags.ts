37108981dfe0b305d929a7ad5691ffc940b1ebf5
Brian Posey <15091170+btposey@users.noreply.github.com>
2023-12-19 14:22:04 -0500

    Add test coverage for useFeatureFlag hook
    Jira ticket: CAMS-249

--- user-interface/src/lib/hooks/UseFeatureFlags.ts
+++ user-interface/src/lib/hooks/UseFeatureFlags.ts
@@ -1,11 +1,13 @@
 import { useFlags } from 'launchdarkly-react-client-sdk';
-import config from '@/configuration/featureFlagConfiguration';
 import { defaultFeatureFlags, FeatureFlagSet } from '@common/feature-flags';
+import { getFeatureFlagConfiguration } from '../../configuration/featureFlagConfiguration';
 
 export const CHAPTER_ELEVEN_ENABLED = 'chapter-eleven-enabled';
 export const CHAPTER_TWELVE_ENABLED = 'chapter-twelve-enabled';
 
 export default function useFeatureFlags(): FeatureFlagSet {
+  const config = getFeatureFlagConfiguration();
+  console.log(config.clientId);
   if (!config.clientId) return defaultFeatureFlags;
 
   const featureFlags = useFlags();
