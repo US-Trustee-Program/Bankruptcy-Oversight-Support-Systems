92f66bba051e141c1c1a57f8a46f95fb3299eba4
Brian Posey <15091170+btposey@users.noreply.github.com>
2023-12-22 10:42:15 -0500

    Add court and office name to order API response
    Jira ticket: CAMS-262

--- backend/functions/lib/adapters/gateways/dxtr/orders.dxtr.gateway.ts
+++ backend/functions/lib/adapters/gateways/dxtr/orders.dxtr.gateway.ts
@@ -13,22 +13,34 @@ export class DxtrOrdersGateway implements OrdersGateway {
   async getOrders(context: ApplicationContext): Promise<Array<Order>> {
     const input: DbTableFieldSpec[] = [];
     const query = `
-    SELECT
-      CS.CS_DIV+'-'+CS.CASE_ID as caseId,
-      CS.CS_SHORT_TITLE as caseTitle,
-      CS.CS_CHAPTER as chapter,
-      'transfer' AS orderType,
-      FORMAT(TX_DATE, 'yyyy-MM-dd') AS orderDate,
-      DE.DE_SEQNO AS sequenceNumber,
-      DE.DE_DOCUMENT_NUM AS documentNumber,
-      DE.DO_SUMMARY_TEXT AS summaryText,
-      DE.DT_TEXT AS fullText,
-      FORMAT(DE.DE_DATE_FILED, 'yyyy-MM-dd') AS dateFiled
-    FROM AO_TX AS TX
-    JOIN AO_DE AS DE ON TX.CS_CASEID=DE.CS_CASEID AND TX.DE_SEQNO=DE.DE_SEQNO
-    JOIN AO_CS AS CS ON TX.CS_CASEID=CS.CS_CASEID
-    WHERE TX.TX_CODE = 'CTO'
-    `;
+      SELECT
+        CS.CS_DIV+'-'+CS.CASE_ID as caseId,
+        CS.CS_SHORT_TITLE as caseTitle,
+        CS.CS_CHAPTER as chapter,
+        C.COURT_NAME as courtName,
+        O.OFFICE_NAME as courtDivisionName,
+        G.REGION_ID as regionId,
+        'transfer' AS orderType,
+        FORMAT(TX.TX_DATE, 'yyyy-MM-dd') AS orderDate,
+        DE.DE_SEQNO AS sequenceNumber,
+        DE.DE_DOCUMENT_NUM AS documentNumber,
+        DE.DO_SUMMARY_TEXT AS summaryText,
+        DE.DT_TEXT AS fullText,
+        FORMAT(DE.DE_DATE_FILED, 'yyyy-MM-dd') AS dateFiled
+      FROM AO_TX AS TX
+      JOIN AO_DE AS DE ON TX.CS_CASEID=DE.CS_CASEID AND TX.DE_SEQNO=DE.DE_SEQNO
+      JOIN AO_CS AS CS ON TX.CS_CASEID=CS.CS_CASEID
+      JOIN AO_GRP_DES AS G
+        ON CS.GRP_DES = G.GRP_DES
+      JOIN AO_COURT AS C
+        ON CS.COURT_ID = C.COURT_ID
+      JOIN AO_CS_DIV AS CSD
+        ON CS.CS_DIV = CSD.CS_DIV
+      JOIN AO_OFFICE AS O
+        ON CS.COURT_ID = O.COURT_ID
+        AND CSD.OFFICE_CODE = O.OFFICE_CODE
+      WHERE TX.TX_CODE = 'CTO'
+      `;
 
     const queryResult: QueryResults = await executeQuery(
       context,
@@ -37,7 +49,6 @@ export class DxtrOrdersGateway implements OrdersGateway {
       input,
     );
 
-    // TODO: Add court information. Look at the case detail SQL for example.
     // TODO: Need to add the document links for the docket entry to the DTO if they exist.
 
     if (queryResult.success) {
--- backend/functions/lib/use-cases/orders/orders.model.ts
+++ backend/functions/lib/use-cases/orders/orders.model.ts
@@ -4,6 +4,9 @@ export type Order = CaseDocketEntry & {
   caseId: string;
   caseTitle: string;
   chapter: string;
+  courtName: string;
+  courtDivisionName: string;
+  regionId: string;
   orderType: 'transfer';
   orderDate: string;
 };
