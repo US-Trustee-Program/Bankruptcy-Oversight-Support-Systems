d0f4520e02b6226d3f698bed89acbe19a8f23d6f
Brian Posey <15091170+btposey@users.noreply.github.com>
2023-12-22 15:27:24 -0500

    Add document links to orders returned from API
    Jira ticket: CAMS-262

--- dev-tools/test-data/scripts/transferOrders.ts
+++ dev-tools/test-data/scripts/transferOrders.ts
@@ -2,9 +2,14 @@ import { fakerEN_US as faker } from '@faker-js/faker';
 import readline from 'node:readline';
 import { AO_TX_Record, buildRecFromTxRecord, toAoTxInsertStatements } from '../tables/AO_TX';
 import { AO_DE_Record, toAoDeInsertStatements } from '../tables/AO_DE';
+import { AO_DC_Record, toAoDcInsertStatements } from '../tables/AO_DC';
+import { randomInt } from '../utility';
+
+const SIZE_MB_5 = 5 * 1024 * 1024;
 
 async function main() {
   const docketEntries: Array<AO_DE_Record> = [];
+  const docketDocuments: Array<AO_DC_Record> = [];
   const transferOrders: Array<AO_TX_Record> = [];
 
   const rl = readline.createInterface({
@@ -25,8 +30,18 @@ async function main() {
       DE_TYPE: 'order',
       DO_SELECT_TEXT: 'Order Re: Transfer Case',
       DO_SUMMARY_TEXT: 'Order Re: Transfer Case',
-      DT_TEXT: faker.lorem.paragraph(1),
+      DT_TEXT: faker.lorem.sentences(8),
     });
+    const FILE_NAME = `${courtId}-${csCaseId}-${nextDocketSequenceNumber}-${nextDocketSequenceNumber}-0.pdf`;
+    const documentEntry = new AO_DC_Record({
+      FILE_NAME,
+      COURT_ID: docketEntry.COURT_ID,
+      CS_CASEID: docketEntry.CS_CASEID,
+      DE_SEQNO: docketEntry.DE_SEQNO,
+      PDF_SIZE: randomInt(SIZE_MB_5),
+      COURT_STATUS: 'pdf',
+    });
+    docketDocuments.push(documentEntry);
     const transferOrder = new AO_TX_Record({
       CS_CASEID: csCaseId,
       COURT_ID: courtId,
@@ -37,12 +52,15 @@ async function main() {
       TX_CODE: 'CTO',
       TX_DATE: orderDate,
     });
-    transferOrder.REC = buildRecFromTxRecord(transferOrder, div, '15', 'Transfer order');
+    transferOrder.REC = buildRecFromTxRecord(transferOrder, div, '15', '23 WARN: 23-50607 ');
     transferOrders.push(transferOrder);
     docketEntries.push(docketEntry);
   }
 
   toAoDeInsertStatements(docketEntries).forEach((statement) => {
+    console.log(statement.trim());
+  });
+  toAoDcInsertStatements(docketDocuments).forEach((statement) => {
     console.log(statement.trim());
   });
   toAoTxInsertStatements(transferOrders).forEach((statement) => {
