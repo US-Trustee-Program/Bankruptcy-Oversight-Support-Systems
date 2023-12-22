93d740233efcd980a66fabc52e9f12131db62fdd
Fritz Madden <fmadden@flexion.us>
2023-12-21 17:48:51 -0700

    WIP working on front end form
    Jira ticket: CAMS-262

--- user-interface/src/review-orders/ReviewOrdersScreen.tsx
+++ user-interface/src/review-orders/ReviewOrdersScreen.tsx
@@ -1,50 +1,108 @@
 import './ReviewOrdersScreen.scss';
+import { useState } from 'react';
 import { Accordion, AccordionGroup } from '@/lib/components/uswds/Accordion';
 
+type ReviewOrderCourtInfo = {
+  name: string;
+  region: string;
+  location: string;
+};
+
 export default function ReviewOrders() {
+  const [courtSelection, setCourtSelection] = useState<ReviewOrderCourtInfo>();
+  const [, setCaseSelection] = useState<string>('');
+
+  /****  START MOCK DATA  ****/
+
   const regionNumber = '02';
   const orderList = [
     {
       caseId: '23-12345',
-      title: 'Some case title',
-      orderDate: '01/23/2024',
+      region: '02',
+      location: 'Manhattan',
+      caseTitle: 'Some case title',
+      courtName: 'court 1',
       orderType: 'transfer',
+      orderDate: '01/23/2024',
       status: 'pending',
-      content: 'here is some content hidden inside the 1st accordion',
+      summaryText: '1st accordion',
+      fullText: 'here is some content hidden inside the 1st accordion',
     },
     {
       caseId: '23-12346',
-      title: 'Some case title 2',
+      region: '02',
+      location: 'Manhattan',
+      caseTitle: 'Some case title 2',
+      courtName: 'court 2',
       orderDate: '01/24/2024',
       orderType: 'consolidation',
       status: 'pending',
-      content: 'here is some content hidden inside the 2nd accordion',
+      summaryText: '2nd accordion',
+      fullText: 'here is some content hidden inside the 2nd accordion',
     },
     {
       caseId: '23-12347',
-      title: 'Some case title 3',
+      region: '02',
+      location: 'Manhattan',
+      caseTitle: 'Some case title 3',
+      courtName: 'court 2',
       orderDate: '01/25/2024',
       orderType: 'transfer',
       status: 'approved',
-      content: 'here is some content hidden inside the 3rd accordion',
+      summaryText: '3rd accordion',
+      fullText: 'here is some content hidden inside the 3rd accordion',
     },
     {
       caseId: '23-12348',
-      title: 'Some case title 4',
+      region: '02',
+      location: 'Manhattan',
+      caseTitle: 'Some case title 4',
+      courtName: 'court 1',
       orderDate: '01/26/2024',
       orderType: 'transfer',
       status: 'pending',
-      content: 'here is some content hidden inside the 4th accordion',
+      summaryText: '4th accordion',
+      fullText: 'here is some content hidden inside the 4th accordion',
     },
     {
       caseId: '23-12349',
-      title: 'Some case title 5',
+      region: '02',
+      location: 'Manhattan',
+      caseTitle: 'Some case title 5',
+      courtName: 'court 3',
       orderDate: '01/27/2024',
       orderType: 'transfer',
       status: 'rejected',
-      content: 'here is some content hidden inside the 5th accordion',
+      summaryText: '5th accordion',
+      fullText: 'here is some content hidden inside the 5th accordion',
     },
   ];
+
+  const courtList: ReviewOrderCourtInfo[] = [
+    {
+      name: 'court 1',
+      region: '02',
+      location: 'Manhattan',
+    },
+    {
+      name: 'court 2',
+      region: '02',
+      location: 'Manhattan',
+    },
+    {
+      name: 'court 3',
+      region: '02',
+      location: 'Manhattan',
+    },
+  ];
+  const courtListHashMap = new Map();
+  courtList.map((court) => {
+    courtListHashMap.set(court.name, court);
+  });
+
+  const caseList = ['case 1', 'case 2', 'case 3'];
+
+  /****  END MOCK DATA  ****/
 
   const statusType = new Map();
   statusType.set('pending', 'Pending Review');
@@ -54,6 +112,14 @@ export default function ReviewOrders() {
   const orderType = new Map();
   orderType.set('transfer', 'Transfer');
   orderType.set('consolidation', 'Consolidation');
+
+  function handleCourtSelection(ev: React.ChangeEvent<HTMLSelectElement>) {
+    setCourtSelection(courtListHashMap.get(ev.target.value));
+  }
+
+  function handleCaseSelection(ev: React.ChangeEvent<HTMLSelectElement>) {
+    setCaseSelection(ev.target.value);
+  }
 
   return (
     <div data-testid="review-orders-screen" className="review-orders-screen">
@@ -68,9 +134,9 @@ export default function ReviewOrders() {
               {orderList.map((order, idx: number) => {
                 return (
                   <Accordion key={idx} id={`order-list-${idx}`}>
-                    <div className="grid-row grid-gap-lg">
+                    <div className="accordion-heading grid-row grid-gap-lg">
                       <div className="grid-col-1 case-id text-no-wrap">{order.caseId}</div>
-                      <div className="grid-col-3 case-title text-no-wrap">{order.title}</div>
+                      <div className="grid-col-3 case-title text-no-wrap">{order.caseTitle}</div>
                       <div className="grid-col-1 order-date text-no-wrap">{order.orderDate}</div>
                       <div className="grid-col-3"></div>
                       <div className="grid-col-2 order-type text-no-wrap">
@@ -80,7 +146,60 @@ export default function ReviewOrders() {
                         <span className={order.status}>{statusType.get(order.status)}</span>
                       </div>
                     </div>
-                    {order.content}
+                    <div className="accordion-content">
+                      <div className="order-legal-statement">
+                        <h4>
+                          {order.orderDate} - Order to {orderType.get(order.orderType)}
+                        </h4>
+                        <p>
+                          Court order transferring to new court district. The legal definitions of
+                          the terms bankruptcy, insolvency, liquidation and dissolution are
+                          contested in the Indian legal system. There is no regulation or statute
+                          legislated upon bankruptcy which denotes a condition of inability...
+                        </p>
+                      </div>
+                      <section className="order-form">
+                        <div className="court-selection">
+                          <div>
+                            Transfer {order.caseId} from {order.caseTitle} to
+                          </div>
+                          <div>
+                            <label>New Court</label>
+                            <select
+                              className="usa-select"
+                              id={`court-selection-${order.caseId}`}
+                              onChange={handleCourtSelection}
+                            >
+                              {courtList.map((court, index) => (
+                                <option value={court.name} key={index}>
+                                  {court.name}
+                                </option>
+                              ))}
+                            </select>
+                          </div>
+                        </div>
+                        <div className="case-selection">
+                          <label>New Case</label>
+                          <select
+                            className="usa-select"
+                            id={`case-selection-${order.caseId}`}
+                            onChange={handleCaseSelection}
+                          >
+                            {caseList.map((bCase, index) => (
+                              <option value={bCase} key={index}>
+                                {bCase}
+                              </option>
+                            ))}
+                          </select>
+                        </div>
+                        <div className="preview-results">
+                          <CaseSelection
+                            fromCourt={courtListHashMap.get(order.courtName)}
+                            toCourt={courtSelection}
+                          ></CaseSelection>
+                        </div>
+                      </section>
+                    </div>
                   </Accordion>
                 );
               })}
@@ -90,5 +209,31 @@ export default function ReviewOrders() {
         <div className="grid-col-1"></div>
       </div>
     </div>
+  );
+}
+
+interface CaseSelectionProps {
+  toCourt: ReviewOrderCourtInfo | undefined;
+  fromCourt: ReviewOrderCourtInfo;
+}
+
+function CaseSelection(props: CaseSelectionProps) {
+  const { fromCourt, toCourt }: CaseSelectionProps = props;
+
+  return (
+    <>
+      {fromCourt && toCourt && (
+        <>
+          USTP Office: transfer from
+          <span className="from-location">
+            {fromCourt.region} - {fromCourt.location}
+          </span>
+          to
+          <span className="to-location">
+            {toCourt.region} - {toCourt.location}
+          </span>
+        </>
+      )}
+    </>
   );
 }
