be5ebcc27c71ab4880574d384647cde8cbc7fc0f
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-01-08 10:36:08 -0500

    Increase test coverage of factory.ts
    Jira ticket: CAMS-263

--- backend/functions/lib/factory.test.ts
+++ backend/functions/lib/factory.test.ts
@@ -1,31 +1,117 @@
+import { CaseAssignmentCosmosDbRepository } from './adapters/gateways/case.assignment.cosmosdb.repository';
 import CasesDxtrGateway from './adapters/gateways/dxtr/cases.dxtr.gateway';
-import { applicationContextCreator } from './adapters/utils/application-context-creator';
-import { getCaseDocketUseCase, getCasesGateway } from './factory';
+import { MockOfficesGateway } from './adapters/gateways/dxtr/mock.offices.gateway';
+import { MockOrdersGateway } from './adapters/gateways/dxtr/mock.orders.gateway';
+import OfficesDxtrGateway from './adapters/gateways/dxtr/offices.gateway';
+import { DxtrOrdersGateway } from './adapters/gateways/dxtr/orders.dxtr.gateway';
+import { CasesLocalGateway } from './adapters/gateways/mock.cases.gateway';
+import { OrdersCosmosDbRepository } from './adapters/gateways/orders.cosmosdb.repository';
+import { RuntimeStateCosmosDbRepository } from './adapters/gateways/runtime-state.cosmosdb.repository';
+import { ApplicationContext } from './adapters/types/basic';
+import CosmosClientHumble from './cosmos-humble-objects/cosmos-client-humble';
+import FakeAssignmentsCosmosClientHumble from './cosmos-humble-objects/fake.assignments.cosmos-client-humble';
+import FakeOrdersCosmosClientHumble from './cosmos-humble-objects/fake.orders.cosmos-client-humble';
+import FakeRuntimeStateCosmosClientHumble from './cosmos-humble-objects/fake.runtime.cosmos-client-humble';
+import {
+  getAssignmentRepository,
+  getAssignmentsCosmosDbClient,
+  getCaseDocketUseCase,
+  getCasesGateway,
+  getOfficesGateway,
+  getOrdersCosmosDbClient,
+  getOrdersGateway,
+  getOrdersRepository,
+  getRuntimeCosmosDbClient,
+  getRuntimeStateRepository,
+} from './factory';
+import { createMockApplicationContext } from './testing/testing-utilities';
 import { CaseDocketUseCase } from './use-cases/case-docket/case-docket';
 
-const functionContext = require('azure-function-context-mock');
+describe('Factory functions', () => {
+  let dbContext: ApplicationContext;
+  let mockDbContext: ApplicationContext;
 
-describe('Verify select factory methods that depend on config flag DATABASE_MOCK', () => {
-  const ORIGINAL_ENV = process.env;
-
-  beforeEach(() => {
-    jest.resetModules();
-    process.env.DATABASE_MOCK = 'false';
+  beforeAll(async () => {
+    dbContext = await createMockApplicationContext({
+      DATABASE_MOCK: 'false',
+      COSMOS_ENDPOINT: 'https://cosmos-ustp-cams-dev.documents.azure.us:443/',
+    });
+    mockDbContext = await createMockApplicationContext({ DATABASE_MOCK: 'true' });
   });
 
-  afterAll(() => {
-    process.env = ORIGINAL_ENV;
+  test('getAttorneyGateway', async () => {
+    const obj = getCaseDocketUseCase(dbContext);
+    expect(obj).toBeInstanceOf(CaseDocketUseCase);
   });
 
-  test('should return object of type CasesDxtrGateway', async () => {
-    const applicationContext = await applicationContextCreator(functionContext);
-    const factoryObj = getCasesGateway(applicationContext);
-    expect(factoryObj).toBeInstanceOf(CasesDxtrGateway);
+  test('getCasesGateway', async () => {
+    const mockObj = getCasesGateway(mockDbContext);
+    expect(mockObj).toBeInstanceOf(CasesLocalGateway);
+
+    const obj = getCasesGateway(dbContext);
+    expect(obj).toBeInstanceOf(CasesDxtrGateway);
   });
 
-  test('should return object of type CaseDocketUseCase', async () => {
-    const applicationContext = await applicationContextCreator(functionContext);
-    const factoryObj = getCaseDocketUseCase(applicationContext);
-    expect(factoryObj).toBeInstanceOf(CaseDocketUseCase);
+  test('getAssignmentRepository', async () => {
+    const obj = getAssignmentRepository(dbContext);
+    expect(obj).toBeInstanceOf(CaseAssignmentCosmosDbRepository);
+  });
+
+  test('getAssignmentsCosmosDbClient', async () => {
+    const mockObj = getAssignmentsCosmosDbClient(mockDbContext);
+    expect(mockObj).toBeInstanceOf(FakeAssignmentsCosmosClientHumble);
+
+    const obj = getAssignmentsCosmosDbClient(dbContext);
+    expect(obj).toBeInstanceOf(CosmosClientHumble);
+  });
+
+  test('getOrdersCosmosDbClient', async () => {
+    const mockObj = getOrdersCosmosDbClient(mockDbContext);
+    expect(mockObj).toBeInstanceOf(FakeOrdersCosmosClientHumble);
+
+    const obj = getOrdersCosmosDbClient(dbContext);
+    expect(obj).toBeInstanceOf(CosmosClientHumble);
+  });
+
+  test('getCaseDocketUseCase', async () => {
+    const mockObj = getCaseDocketUseCase(mockDbContext);
+    expect(mockObj).toBeInstanceOf(CaseDocketUseCase);
+
+    const obj = getCaseDocketUseCase(dbContext);
+    expect(obj).toBeInstanceOf(CaseDocketUseCase);
+  });
+
+  test('getOrdersGateway', async () => {
+    const mockObj = getOrdersGateway(mockDbContext);
+    expect(mockObj).toBeInstanceOf(MockOrdersGateway);
+
+    const obj = getOrdersGateway(dbContext);
+    expect(obj).toBeInstanceOf(DxtrOrdersGateway);
+  });
+
+  test('getOfficesGateway', async () => {
+    const mockObj = getOfficesGateway(mockDbContext);
+    expect(mockObj).toBeInstanceOf(MockOfficesGateway);
+
+    const obj = getOfficesGateway(dbContext);
+    expect(obj).toBeInstanceOf(OfficesDxtrGateway);
+  });
+
+  test('getOrdersRepository', async () => {
+    const obj = getOrdersRepository(dbContext);
+    expect(obj).toBeInstanceOf(OrdersCosmosDbRepository);
+  });
+
+  test('getRuntimeCosmosDbClient', async () => {
+    const mockObj = getRuntimeCosmosDbClient(mockDbContext);
+    expect(mockObj).toBeInstanceOf(FakeRuntimeStateCosmosClientHumble);
+
+    const obj = getRuntimeCosmosDbClient(dbContext);
+    expect(obj).toBeInstanceOf(CosmosClientHumble);
+  });
+
+  test('getRuntimeStateRepository', async () => {
+    const obj = getRuntimeStateRepository(dbContext);
+    expect(obj).toBeInstanceOf(RuntimeStateCosmosDbRepository);
   });
 });
