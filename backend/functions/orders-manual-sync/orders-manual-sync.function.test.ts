88cce34a730fb3b7d58da39c77f25f87e1c9ad8d
Fritz Madden <fmadden@flexion.us>
2024-01-10 22:54:32 -0700

    Got testing coverage over 90%
    Jira ticket: CAMS-263
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

---
+++ backend/functions/orders-manual-sync/orders-manual-sync.function.test.ts
@@ -0,0 +1,53 @@
+import { LoggerImpl } from '../lib/adapters/utils/application-context-creator';
+import { CamsError } from '../lib/common-errors/cams-error';
+import { OrdersController } from '../lib/controllers/orders/orders.controller';
+import { SyncOrdersStatus } from '../lib/use-cases/orders/orders';
+import httpTrigger from './orders-manual-sync.function';
+const context = require('azure-function-context-mock');
+
+const syncResponse: SyncOrdersStatus = {
+  options: {
+    txIdOverride: 10,
+  },
+  initialSyncState: {
+    documentType: 'ORDERS_SYNC_STATE',
+    txId: 464,
+    id: '28e35739-58cd-400b-9d4b-26969773618b',
+  },
+  finalSyncState: {
+    documentType: 'ORDERS_SYNC_STATE',
+    txId: 464,
+    id: '28e35739-58cd-400b-9d4b-26969773618b',
+  },
+  length: 13,
+  startingTxId: 10,
+  maxTxId: 464,
+};
+
+describe('Orders Sync Function tests', () => {
+  test('Should call orders controller method syncOrders', async () => {
+    const request = {
+      params: {},
+      method: 'POST',
+    };
+    const syncOrders = jest
+      .spyOn(OrdersController.prototype, 'syncOrders')
+      .mockResolvedValue(syncResponse);
+    await httpTrigger(context, request);
+    expect(syncOrders).toHaveBeenCalled();
+  });
+
+  test('Should log a camsError if syncOrders throws a CamsError', async () => {
+    const request = {
+      params: {},
+      method: 'POST',
+    };
+    const syncOrders = jest
+      .spyOn(OrdersController.prototype, 'syncOrders')
+      .mockRejectedValue(new CamsError('TEST_MODULE', { message: 'error' }));
+    const camsError = jest.spyOn(LoggerImpl.prototype, 'camsError').mockImplementation(() => {});
+    await httpTrigger(context, request);
+    expect(syncOrders).toHaveBeenCalled();
+    expect(camsError).toHaveBeenCalled();
+  });
+});
