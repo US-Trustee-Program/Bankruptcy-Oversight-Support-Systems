9efe936a3a40116ef632ad854331575026fc56e2
Fritz Madden <fmadden@flexion.us>
2024-01-12 12:15:31 -0700

    Added coverage
    Added coverage for SearchableSelect setValue, resetValue, and
    clearValue.
    
    Added full coverage for Input component.
    
    Jira ticket: CAMS-263
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

---
+++ user-interface/src/lib/components/uswds/Input.test.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { InputRef } from '@/lib/type-declarations/input-fields';
+import Input from './Input';
+
+describe('Tests for USWDS Input component.', () => {
+  const ref = React.createRef<InputRef>();
+  const youChangedMe = vi.fn();
+
+  beforeEach(() => {
+    render(
+      <div>
+        <Input ref={ref} id="input-1" inputmode="numeric" value="1" onChange={youChangedMe}></Input>
+      </div>,
+    );
+  });
+
+  test('Should change value when ref.setValue() is called and set value back to original when ref.resetValue() is called.', async () => {
+    const inputEl = screen.getByTestId('input-1');
+    expect(inputEl).toHaveValue('1');
+
+    ref.current?.setValue('2');
+    await waitFor(() => {
+      expect(inputEl).toHaveValue('2');
+    });
+
+    ref.current?.resetValue();
+    await waitFor(() => {
+      expect(inputEl).toHaveValue('1');
+    });
+  });
+
+  test('Should clear value when ref.clearValue is called.', async () => {
+    const inputEl = screen.getByTestId('input-1');
+    ref.current?.setValue('2');
+
+    await waitFor(() => {
+      expect(inputEl).toHaveValue('2');
+    });
+
+    ref.current?.clearValue();
+    await waitFor(() => {
+      expect(inputEl).toHaveValue('');
+    });
+  });
+
+  test('Should call props.onChange when a change is made to input by keypress, but not by ref.', async () => {
+    const inputEl = screen.getByTestId('input-1');
+
+    ref.current?.setValue('2');
+    await waitFor(() => {
+      expect(inputEl).toHaveValue('2');
+    });
+    expect(youChangedMe).not.toHaveBeenCalled();
+
+    fireEvent.change(inputEl, { target: { value: '5' } });
+    await waitFor(() => {
+      expect(inputEl).toHaveValue('5');
+    });
+    expect(youChangedMe).toHaveBeenCalled();
+  });
+});
+
+describe('Tests for USWDS Input component when no value is initially set.', () => {
+  const ref = React.createRef<InputRef>();
+
+  test('Should change value to empty string when props.input is not set and ref.setValue() is called.', async () => {
+    render(
+      <div>
+        <Input ref={ref} id="input-1"></Input>
+      </div>,
+    );
+
+    const inputEl = screen.getByTestId('input-1');
+    expect(inputEl).toHaveValue('');
+
+    ref.current?.setValue('12345');
+    await waitFor(() => {
+      expect(inputEl).toHaveValue('12345');
+    });
+
+    ref.current?.resetValue();
+    await waitFor(() => {
+      expect(inputEl).toHaveValue('');
+    });
+  });
+});
