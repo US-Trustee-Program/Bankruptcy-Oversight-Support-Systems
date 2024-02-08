c9726c8ed4f4802cf631990647aed9e40a9fc0b9
Arthur Morrow <amorrow@flexion.us>
2024-02-08 12:25:55 -0500

    added parentheses to court div and updated tests
    Jira ticket: CAMS-325

--- user-interface/src/data-verification/TransferOrderAccordion.tsx
+++ user-interface/src/data-verification/TransferOrderAccordion.tsx
@@ -702,13 +702,13 @@ export function CaseSelection(props: CaseSelectionProps) {
     <>
       USTP Office: transfer from
       <span className="from-location transfer-highlight__span">
-        Region {safeToInt(fromCourt.region)} - {fromCourt.courtDivisionName}
+        Region {safeToInt(fromCourt.region)} - ({fromCourt.courtDivisionName})
       </span>
       {toCourt.region && toCourt.courtDivisionName && (
         <>
           to
           <span className="to-location transfer-highlight__span">
-            Region {safeToInt(toCourt.region)} - {toCourt.courtDivisionName}
+            Region {safeToInt(toCourt.region)} - ({toCourt.courtDivisionName})
           </span>
         </>
       )}
