efd0b8e3f184481d9d1a769cba33b4229a3ec150
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-02-13 09:02:08 -0500

    Add to mock data in common - WIP
    [skip ci]
    
    Jira ticket: CAMS-270

--- common/src/cams/test-utilities/test.ts
+++
@@ -1,7 +0,0 @@
-import { Mock } from './mock-data';
-
-console.log(JSON.stringify(Mock.getTransferOrder('person'), null, 2));
-console.log(JSON.stringify(Mock.getTransferOrder('company'), null, 2));
-
-console.log(JSON.stringify(Mock.getTransferOrder('company', { status: 'approved' }), null, 2));
-console.log(JSON.stringify(Mock.getTransferOrder('company', { status: 'rejected' }), null, 2));
