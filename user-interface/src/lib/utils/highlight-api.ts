a368cf285ee2f1e1c72bb84b2dcd5076d6e07081
Michael Ly <105381067+magile32@users.noreply.github.com>
2024-02-16 08:32:33 -0800

    CAMS-332 Fix RangeError on docket search (#502)
    * Fix for docket search. Parameterize search character size and improve range mapping
    
    ---------
    
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- user-interface/src/lib/utils/highlight-api.ts
+++ user-interface/src/lib/utils/highlight-api.ts
@@ -5,36 +5,36 @@ export function handleHighlight(
   _document: Document,
   elementId: string,
   searchString: string,
+  minSearchStringLength: number = 1,
 ) {
-  // See https://developer.mozilla.org/en-US/docs/Web/API/HighlightRegistry/clear#browser_compatibility
-  const browserApi = _window as unknown as WindowExperimental;
+  try {
+    // See https://developer.mozilla.org/en-US/docs/Web/API/HighlightRegistry/clear#browser_compatibility
+    const browserApi = _window as unknown as WindowExperimental;
 
-  // API is not supported in this browser.
-  if (!browserApi.CSS.highlights) return;
+    // API is not supported in this browser.
+    if (!browserApi.CSS.highlights) return;
 
-  // Clear all highlighting if we don't have a search string.
-  if (!searchString.length) {
-    browserApi.CSS.highlights.clear();
-    return;
-  }
+    // Clear all highlighting if we don't have a search string.
+    if (searchString.length < minSearchStringLength) {
+      browserApi.CSS.highlights.clear();
+      return;
+    }
 
-  // Apply highlighting to the docket nodes.
-  const docketNode = _document.getElementById(elementId);
-  if (!docketNode) return;
+    // Apply highlighting to the docket nodes.
+    const docketNode = _document.getElementById(elementId);
+    if (!docketNode) return;
 
-  const treeWalker = _document.createTreeWalker(docketNode, NodeFilter.SHOW_TEXT);
-  const allTextNodes = [];
-  let currentNode = treeWalker.nextNode();
-  while (currentNode) {
-    allTextNodes.push(currentNode);
-    currentNode = treeWalker.nextNode();
-  }
+    const treeWalker = _document.createTreeWalker(docketNode, NodeFilter.SHOW_TEXT);
+    const allTextNodes = [];
+    let currentNode = treeWalker.nextNode();
+    while (currentNode) {
+      allTextNodes.push(currentNode);
+      currentNode = treeWalker.nextNode();
+    }
 
-  const ranges = allTextNodes
-    .map((el) => {
-      return { el, text: el.textContent?.toLowerCase() || '' };
-    })
-    .map(({ text, el }) => {
+    const ranges = allTextNodes.reduce((accumulator, el) => {
+      const text = el.textContent?.toLowerCase() ?? '';
+
       const indices = [];
       let startPos = 0;
       while (startPos < text.length) {
@@ -44,17 +44,22 @@ export function handleHighlight(
         startPos = index + searchString.length;
       }
 
-      return indices.map((index) => {
+      const innerRanges = indices.map((index) => {
         const range = new Range();
         range.setStart(el, index);
         range.setEnd(el, index + searchString.length);
         return range;
       });
-    });
+      if (innerRanges.length) accumulator.push(...innerRanges);
+      return accumulator;
+    }, [] as Range[]);
 
-  // TypeScript does not ship experimental browser type definitions which are being used here.
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const searchResultsHighlight = new Highlight(...ranges.flat());
-  browserApi.CSS.highlights.set('search-results', searchResultsHighlight);
+    // TypeScript does not ship experimental browser type definitions which are being used here.
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const searchResultsHighlight = new Highlight(...ranges.flat());
+    browserApi.CSS.highlights.set('search-results', searchResultsHighlight);
+  } catch {
+    // We want to silently fail and not crash the browser.
+  }
 }
