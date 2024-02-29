fa437c63a439f61734d31697f282512c254e2b7f
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-02-29 11:50:33 -0500

    Fix bug preventing cosmos docs from being deleted
    [skip ci]
    
    Jira ticket: CAMS-270

--- dev-tools/cosmos-records/delete-orders.ts
+++ dev-tools/cosmos-records/delete-orders.ts
@@ -1,11 +1,15 @@
 import { deleteDocuments } from './lib';
 
 export default function deleteOrders() {
-  deleteDocuments('orders', 'SELECT * FROM c');
-  deleteDocuments('cases', 'SELECT * FROM c WHERE c.documentType = "AUDIT_TRANSFER"');
-  deleteDocuments('cases', 'SELECT * FROM c WHERE c.documentType LIKE "TRANSFER_%"');
-  deleteDocuments('cases', 'SELECT * FROM c WHERE c.documentType = "AUDIT_CONSOLIDATIONS"');
-  deleteDocuments('consolidations', 'SELECT * FROM c');
+  deleteDocuments('orders', 'caseId', 'SELECT * FROM c');
+  deleteDocuments('cases', 'caseId', 'SELECT * FROM c WHERE c.documentType = "AUDIT_TRANSFER"');
+  deleteDocuments('cases', 'caseId', 'SELECT * FROM c WHERE c.documentType LIKE "TRANSFER_%"');
+  deleteDocuments(
+    'cases',
+    'caseId',
+    'SELECT * FROM c WHERE c.documentType = "AUDIT_CONSOLIDATIONS"',
+  );
+  deleteDocuments('consolidations', 'consolidationId', 'SELECT * FROM c');
 }
 
 deleteOrders();
