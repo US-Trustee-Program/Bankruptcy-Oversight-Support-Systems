3854e0f916995c8c40106d4d9209c62db0d9b32c
Arthur Morrow <amorrow@flexion.us>
2024-03-06 15:42:31 -0500

    added proper check for bicep
    Jira ticket: CAMS-322

--- ops/scripts/pipeline/check-deploy-bicep.sh
+++ ops/scripts/pipeline/check-deploy-bicep.sh
@@ -6,21 +6,25 @@
 # Exitcodes
 # ==========
 # 1   missing parameter
-#Testing Bicep Deployment
 
 set -euo pipefail # ensure job step fails in CI pipeline when error occurs
 
 app_rg=$1
 deployBicep=false
+containsBicep=false
 
 rg_response=$(az group show --name "${app_rg}" --query "[name]" -o tsv  || true)  #continue if rg show fails
 
 #compares current commit with previous commit for changes in bicep directory
-changes=$(git diff HEAD^ HEAD -- ./ops/cloud-deployment/)
+changes=$(git diff HEAD^ HEAD)
+
+if [[ $changes == *".bicep"* ]]; then
+    containsBicep=true
+fi
 
 
 
-if [[ $changes != "" && $rg_response != "" ]] || [[ $rg_response == "" ]]; then
+if [[ $containsBicep && $rg_response != "" ]] || [[ $rg_response == "" ]]; then
     deployBicep=true
 else
     deployBicep=false
