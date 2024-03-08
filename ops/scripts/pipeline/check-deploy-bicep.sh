24c2d501d5ac70d3316213557fd5c8f19f7af75e
Arthur Morrow <amorrow@flexion.us>
2024-03-08 09:06:02 -0500

    added workflow dispatch param for manual bicep deployment
    Jira ticket: CAMS-322

--- ops/scripts/pipeline/check-deploy-bicep.sh
+++ ops/scripts/pipeline/check-deploy-bicep.sh
@@ -3,14 +3,13 @@
 # Title:        check-deploy-bicep.sh
 # Description:  Helper script to check if bicep changes have been made and if bicep should be deployed
 #
-# Exitcodes
-# ==========
-# 1   missing parameter
 
 set -euo pipefail # ensure job step fails in CI pipeline when error occurs
 
 app_rg=$1
-deployBicep=false
+deploy_flag=$2 #workflow dispatch parameter enableBicepDeployment from GHA
+
+deployBicep=false #default bicep deployment to false
 containsBicep=false
 
 rg_response=$(az group show --name "${app_rg}" --query "[name]" -o tsv  || true)  #continue if rg show fails
@@ -25,10 +24,8 @@ fi
 
 
 
-if [[ $containsBicep == true || $rg_response == "" ]]; then
+if [[ $containsBicep == true || $rg_response == "" || $deploy_flag == "true" ]]; then
     deployBicep=true
-else
-    deployBicep=false
 fi
 
 echo $deployBicep
