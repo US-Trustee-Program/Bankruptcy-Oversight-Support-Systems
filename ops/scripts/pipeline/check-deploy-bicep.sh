3bee2b5fe007cf946d08bf5d24cb0afcd0bf2d48
Arthur Morrow <amorrow@flexion.us>
2024-03-08 11:37:52 -0500

    changed diff to check against main for bicep changes
    Jira ticket: CAMS-322

--- ops/scripts/pipeline/check-deploy-bicep.sh
+++ ops/scripts/pipeline/check-deploy-bicep.sh
@@ -14,7 +14,7 @@ containsBicep=false
 
 rg_response=$(az group show --name "${app_rg}" --query "[name]" -o tsv  || true)  #continue if rg show fails
 
-changes=$(git diff HEAD^ HEAD)
+changes=$(git diff HEAD origin/main)
 if [[ $changes == *".bicep"* && $changes == *"cloud-deployment"* ]] || [[ $changes == *"continuous-deployment.yml"* ]]; then
  #checks if most recent commit contains changes in any .bicep or in the workflow file. Changes in the workflow file have the potential to affect bicep resources withiout modifying the bicep directly
     containsBicep=true
