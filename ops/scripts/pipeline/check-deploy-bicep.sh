cb8de156293d8de8c1e09f9b588295473408bf45
Arthur Morrow <amorrow@flexion.us>
2024-03-11 10:13:39 -0400

    updated script to use last merge commit if one exists
    [ci-skip]
    
    Jira ticket: CAMS-322

--- ops/scripts/pipeline/check-deploy-bicep.sh
+++ ops/scripts/pipeline/check-deploy-bicep.sh
@@ -2,7 +2,6 @@
 
 # Title:        check-deploy-bicep.sh
 # Description:  Helper script to check if bicep changes have been made and if bicep should be deployed
-#
 
 set -euo pipefail # ensure job step fails in CI pipeline when error occurs
 
@@ -14,8 +13,16 @@ deployBicep=false #default bicep deployment to false
 #If rg show fails, we want to continue instead of erroring out
 rg_response=$(az group show --name "${app_rg}" --query "[name]" -o tsv  || true)
 
+#gets only the last merge commit SHA
+lastMergeCommitSha=$(git log --pretty=format:"%H" --merges -n 1)
+
+if [[ $lastMergeCommitSha != "" ]]; then
+    changes=$(git diff "${lastMergeCommitSha}" HEAD -- ./ops/cloud-deployment/ ./.github/workflows/continuous-deployment.yml)
+else
+    changes=$(git diff HEAD^@ -- ./ops/cloud-deployment/ ./.github/workflows/continuous-deployment.yml)
+
+fi
 #diff of previous merge commit  and current head only on folders containing potential infrastructure changes
-changes=$(git diff HEAD^@ -- ./ops/cloud-deployment/ ./.github/workflows/continuous-deployment.yml)
 
 if [[ $changes != "" || $rg_response == "" || $deploy_flag == true  ]]; then
     deployBicep=true
