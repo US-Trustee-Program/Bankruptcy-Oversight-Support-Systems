d64980d0d34d1075d44d19f902cddd960605c9d8
amorrow-flexion <133667008+amorrow-flexion@users.noreply.github.com>
2024-03-12 16:16:51 -0400

    Cams 322 conditional bicep execution (#519)
    Jira ticket: CAMS-322
    Added functionality to conditionally execute bicep within the github workflow

---
+++ ops/scripts/pipeline/check-deploy-bicep.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# Title:        check-deploy-bicep.sh
+# Description:  Helper script to check if bicep changes have been made and if bicep should be deployed
+
+set -euo pipefail # ensure job step fails in CI pipeline when error occurs
+
+app_rg=$1
+deploy_flag=$2 #workflow dispatch parameter enableBicepDeployment from GHA
+
+deployBicep=false #default bicep deployment to false
+
+#If rg show fails, we want to continue instead of erroring out
+rg_response=$(az group show --name "${app_rg}" --query "[name]" -o tsv  || true)
+
+#uses branch to get previous merge commits hash
+branch=$(git branch --show-current)
+# shellcheck disable=SC2086 # REASON: Quotes renders the branch unusable
+lastMergeCommitSha=$(git log ${branch} --first-parent --pretty=format:"%H" --merges -n 1)
+if [[ $lastMergeCommitSha != "" && $branch == "main" ]] ; then
+    # shellcheck disable=SC2086 # REASON: Qoutes render the commit sha unusable
+    changes=$(git diff ${lastMergeCommitSha} HEAD -- ./ops/cloud-deployment/ ./.github/workflows/continuous-deployment.yml)
+else
+    changes=$(git diff HEAD origin/main -- ./ops/cloud-deployment/ ./.github/workflows/continuous-deployment.yml)
+fi
+
+if [[ $changes != "" || $rg_response == "" || $deploy_flag == true  ]]; then
+    deployBicep=true
+fi
+
+echo $deployBicep
