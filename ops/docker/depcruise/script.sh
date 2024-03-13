0f89c671dde2be2450e747e32da573cc36c809b3
Michael Ly <105381067+magile32@users.noreply.github.com>
2024-03-12 19:23:52 -0700

    Update dependencies install on docker container. Update script executing depcruise to experiment.

--- ops/docker/depcruise/script.sh
+++ ops/docker/depcruise/script.sh
@@ -1,20 +1,35 @@
 #!/usr/bin/env bash
 
-DEFAULT_DIR=$(pwd)
+set -eu
 
-echo "Executing depcruise on frontend"
+CURRENT_TIMESTAMP=$(date +'%Y-%m-%d_%H%M')
+DEFAULT_DIR=$(pwd)
+DOCUMENT_DIR="${DEFAULT_DIR}/docs/architecture/dependency-cruiser"
+
+echo "Executing depcruise on common"
 cd common || exit
-npm run dependency-cruiser-graphical
+npx depcruise \
+    --output-type dot \
+    --do-not-follow '^node_modules($|/)' \
+    src | dot -T svg > "${DOCUMENT_DIR}/common/dp_common_${CURRENT_TIMESTAMP}.svg"
 cd "${DEFAULT_DIR}" || exit
+echo "Completed common"
 
 echo "Executing depcruise on backend"
 cd backend/functions || exit
-npm run dependency-cruiser-graphical
+npx depcruise \
+    --output-type dot \
+    --do-not-follow '^node_modules($|/)' \
+    attorneys case-assignments case-docket case-history case-summary cases consolidations healthcheck offices orders orders-manual-sync orders-sync lib | dot -T svg > "${DOCUMENT_DIR}/functions/dp_api_${CURRENT_TIMESTAMP}.svg"
 cd "${DEFAULT_DIR}" || exit
+echo "Completed backend"
 
 echo "Executing depcruise on frontend"
 cd user-interface || exit
-npm run dependency-cruiser-graphical
+npx depcruise \
+    --output-type dot \
+    --do-not-follow '^node_modules($|/)' \
+    --exclude 'test|scss' \
+    public src | dot -T svg > "${DOCUMENT_DIR}/user-interface/dp_${CURRENT_TIMESTAMP}.svg"
 cd "${DEFAULT_DIR}" || exit
-
-echo "depcruise completed"
+echo "Completed frontend"
