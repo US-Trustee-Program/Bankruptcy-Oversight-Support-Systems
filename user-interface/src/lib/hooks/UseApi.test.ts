d4e3f550f57233415b5159f925f1a65358b44b93
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-03-13 13:36:53 -0500

    Finish testing UseApi hook
    Jira ticket: CAMS-335
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

--- user-interface/src/lib/hooks/UseApi.test.ts
+++ user-interface/src/lib/hooks/UseApi.test.ts
@@ -1,10 +1,10 @@
 import Api from '../models/api';
 import MockApi from '../models/chapter15-mock.api.cases';
 import { setApiContext, useApi } from './UseApi';
+import { vi } from 'vitest';
 
 describe('UseApi Hook', () => {
-  test.skip('should return the concrete API by default', () => {
-    // TODO: We need to be able to reset the ../models/api module with the new env var.
+  test('should return the concrete API by default', () => {
     vi.stubEnv('CAMS_PA11Y', '');
     vi.resetModules();
     const actualApi = useApi();
