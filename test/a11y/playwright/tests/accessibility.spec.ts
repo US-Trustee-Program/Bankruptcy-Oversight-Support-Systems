0958300926e7203f2126d31d91e4482a5eae3321
Michael Ly <105381067+magile32@users.noreply.github.com>
2024-03-18 09:56:01 -0700

    Remove trycatch so assert statement will fail test [skip ci]

--- test/a11y/playwright/tests/accessibility.spec.ts
+++ test/a11y/playwright/tests/accessibility.spec.ts
@@ -1,16 +1,12 @@
 import { AxeBuilder } from "@axe-core/playwright";
 import { test, expect } from "@playwright/test";
 
-test("First a11y test", async ({
-  page,
-}) => {
-  await page.goto('');
+test("First a11y test", async ({ page }) => {
+  await page.goto("");
 
-  try {
-    const results = await new AxeBuilder({ page }).withTags('best-practice').analyze();
-    expect(results.violations).toEqual([]);
-    console.log(results.violations);
-  } catch (e) {
-    console.error(e);
-  }
+  const results = await new AxeBuilder({ page })
+    .withTags("best-practice")
+    .analyze();
+  expect(results.violations).toEqual([]);
+  console.log(results.violations);
 });
