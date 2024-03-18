c424d426794d7215c0d6aa6398b2c9763735276f
Michael Ly <105381067+magile32@users.noreply.github.com>
2024-03-18 15:06:18 -0700

    CAMS-269 Wire up playwright execute axe testing. Eslinting configs updated.

--- test/a11y/playwright/tests/accessibility.spec.ts
+++ test/a11y/playwright/tests/accessibility.spec.ts
@@ -1,29 +1,26 @@
-import { AxeBuilder } from "@axe-core/playwright";
-import { test, expect } from "@playwright/test";
+import { AxeBuilder } from '@axe-core/playwright';
+import { test, expect } from '@playwright/test';
 
-test.describe("Experiment with a11y tests", () => {
-  test("First a11y test", async ({ page }) => {
-    await page.goto("");
+test.describe('Experiment with a11y tests', () => {
+  test('First a11y test', async ({ page }) => {
+    await page.goto('');
 
-    const results = await new AxeBuilder({ page })
-      .withTags("best-practice")
-      .analyze();
+    const results = await new AxeBuilder({ page }).withTags('best-practice').analyze();
     expect(results.violations).toEqual([]);
-    console.log(results.violations);
   });
 
-  test("Navigate to different page and use multiple tags", async ({ page }) => {
-    await page.goto("/data-verification");
-    expect(
-      page.getByRole("heading", { name: "Data Verification" })
-    ).toBeVisible();
+  test('Navigate to different page and use multiple tags', async ({ page }) => {
+    await page.goto('/data-verification');
+    // wait for list of orders to render
+    await expect(page.getByText('Loading court orders...')).not.toBeVisible();
+
+    expect(page.getByRole('heading', { name: 'Data Verification' })).toBeVisible();
 
     const results = await new AxeBuilder({ page })
-      .withTags("wcag2a")
-      .withTags("best-practice")
+      .withTags('wcag22aa')
+      // .withTags("best-practice")
       .analyze();
 
     expect(results.violations).toEqual([]);
-    console.log(results.violations);
   });
 });
