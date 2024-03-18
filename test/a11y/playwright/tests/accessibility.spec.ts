f6a284b3e47983bc0ae0800a401c58ff73928993
Michael Ly <105381067+magile32@users.noreply.github.com>
2024-03-18 10:01:18 -0700

    Example to navigate to different view and leverage multiple tags [skip ci]

--- test/a11y/playwright/tests/accessibility.spec.ts
+++ test/a11y/playwright/tests/accessibility.spec.ts
@@ -1,12 +1,29 @@
 import { AxeBuilder } from "@axe-core/playwright";
 import { test, expect } from "@playwright/test";
 
-test("First a11y test", async ({ page }) => {
-  await page.goto("");
+test.describe("Experiment with a11y tests", () => {
+  test("First a11y test", async ({ page }) => {
+    await page.goto("");
 
-  const results = await new AxeBuilder({ page })
-    .withTags("best-practice")
-    .analyze();
-  expect(results.violations).toEqual([]);
-  console.log(results.violations);
+    const results = await new AxeBuilder({ page })
+      .withTags("best-practice")
+      .analyze();
+    expect(results.violations).toEqual([]);
+    console.log(results.violations);
+  });
+
+  test("Navigate to different page and use multiple tags", async ({ page }) => {
+    await page.goto("/data-verification");
+    expect(
+      page.getByRole("heading", { name: "Data Verification" })
+    ).toBeVisible();
+
+    const results = await new AxeBuilder({ page })
+      .withTags("wcag2a")
+      .withTags("best-practice")
+      .analyze();
+
+    expect(results.violations).toEqual([]);
+    console.log(results.violations);
+  });
 });
