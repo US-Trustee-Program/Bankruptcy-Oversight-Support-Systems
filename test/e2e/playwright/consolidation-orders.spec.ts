b6ff51cdfe93151b941ea16e1f0e13365b26237a
Michael Ly <105381067+magile32@users.noreply.github.com>
2024-03-21 17:09:29 -0700

    Implement consolidation test
    [skip ci]
    
    Jira ticket: CAMS-272
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

--- test/e2e/playwright/consolidation-orders.spec.ts
+++ test/e2e/playwright/consolidation-orders.spec.ts
@@ -62,4 +62,75 @@ test.describe('Consolidation Orders', () => {
 
     expect(jointAdminType).toBeInViewport();
   });
+
+  test('should open consolidation modal, fill form and close modal by clicking cancel', async ({
+    page,
+  }) => {
+    // get pending consolidation order id
+    const pendingConsolidationOrder: Order = orderResponseBody.find(
+      (o) => o.orderType === 'consolidation' && o.status === 'pending',
+    );
+    expect(pendingConsolidationOrder).not.toBeFalsy();
+
+    // Action update filter
+    await page.getByTestId('order-status-filter-transfer').click();
+
+    // Assert state of all filters
+    expect(page.getByTestId('order-status-filter-pending').locator('svg')).toBeVisible();
+    expect(page.getByTestId('order-status-filter-approved').locator('svg')).not.toBeVisible();
+    expect(page.getByTestId('order-status-filter-rejected').locator('svg')).not.toBeVisible();
+    expect(page.getByTestId('order-status-filter-transfer').locator('svg')).not.toBeVisible();
+    expect(page.getByTestId('order-status-filter-consolidation').locator('svg')).toBeVisible();
+
+    // Action open accordian
+    await page.getByTestId(`accordion-button-order-list-${pendingConsolidationOrder.id}`).click();
+
+    await page.getByTestId(`${pendingConsolidationOrder.id}-case-list-checkbox-0`).check();
+
+    await page
+      .getByTestId(`button-accordion-approve-button-${pendingConsolidationOrder.id}`)
+      .click();
+
+    // Assert modal opened
+    expect(
+      page.getByTestId(`modal-overlay-confirmation-modal-${pendingConsolidationOrder.id}`),
+    ).toBeVisible();
+    expect(
+      page
+        .getByTestId(`modal-content-confirmation-modal-${pendingConsolidationOrder.id}`)
+        .getByTestId('toggle-modal-button-submit'),
+    ).toBeDisabled();
+
+    // Action fill modal dialog form
+    await page
+      .getByTestId(
+        `radio-administrative-confirmation-modal-${pendingConsolidationOrder.id}-click-target`,
+      )
+      .check();
+
+    await page
+      .locator('div')
+      .filter({ hasText: /^Select\.\.\.$/ })
+      .nth(1)
+      .click();
+    await page.getByRole('option', { name: /Brooklyn/ }).click();
+
+    await page
+      .getByTestId(`lead-case-input-confirmation-modal-${pendingConsolidationOrder.id}`)
+      .fill('11-11111');
+
+    // Action click cancel
+    await page
+      .getByTestId(`modal-content-confirmation-modal-${pendingConsolidationOrder.id}`)
+      .getByTestId('toggle-modal-button-cancel')
+      .click();
+
+    // Assert modal closed and approve button is disabled
+    expect(
+      page.getByTestId(`modal-overlay-confirmation-modal-${pendingConsolidationOrder.id}`),
+    ).not.toBeVisible();
+    expect(
+      page.getByTestId(`button-accordion-approve-button-${pendingConsolidationOrder.id}`),
+    ).toBeDisabled();
+  });
 });
