9c5b96e08aa77bd78b542624964945cfadcfe80e
Michael Ly <105381067+magile32@users.noreply.github.com>
2024-03-25 08:25:16 -0700

    Debug e2e test in pipeline [skip ci]

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -31,7 +31,6 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
   },
 
   /* Configure projects for major browsers */
