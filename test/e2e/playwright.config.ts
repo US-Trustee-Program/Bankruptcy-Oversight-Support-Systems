15874d1295aa1f147d221091e31831eaa14e2a7b
Michael Ly <105381067+magile32@users.noreply.github.com>
2024-03-25 10:20:57 -0700

    Enable trace for playwright tests

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     baseURL: process.env.TARGET_HOST || 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
