524a452609cb639e686b06c9b135a153b6729bb4
Fritz Madden <fmadden@flexion.us>
2024-03-26 16:33:11 -0600

    Working on Tests for CaseDetailAssociatedCases
    Jira ticket: CAMS-277
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>

--- user-interface/src/case-detail/panels/CaseDetailAssociatedCases.test.tsx
+++ user-interface/src/case-detail/panels/CaseDetailAssociatedCases.test.tsx
@@ -1,119 +1,102 @@
 import { render, screen } from '@testing-library/react';
-//import { MockData } from '@common/cams/test-utilities/mock-data';
+import { MockData } from '@common/cams/test-utilities/mock-data';
 import CaseDetailAssociatedCases from './CaseDetailAssociatedCases';
-import { EventCaseReference } from '@common/cams/events';
+import { ConsolidationFrom, ConsolidationTo, EventCaseReference } from '@common/cams/events';
+import { ConsolidationType } from '@common/cams/orders';
+import { BrowserRouter } from 'react-router-dom';
+import { getCaseNumber } from '@/lib/utils/formatCaseNumber';
 
 /*
 dxtrId, debtor, officeName, officeCode,
 */
-const associatedCasesMock: EventCaseReference[] = [
-  {
-    caseId: '081-93-87181',
-    otherCase: {
-      //orderType: 'consolidation',
-      //status: 'pending',
-      caseId: '081-34-34811',
-      caseTitle: 'Hall, Gill and Romero',
-      dateFiled: '2016-09-22',
-      //divisionCode: '081',
-      chapter: '15',
-      courtName: 'Southern District of New York',
-      courtDivisionName: 'Manhattan',
-      regionId: '02',
-      regionName: 'NEW YORK',
-      //orderDate: '2016-09-28',
-      //jobId: 4840265,
-      //leadCaseIdHint: null,
-    },
-    orderDate: '2016-09-28',
-    consolidationType: 'substantive',
-    documentType: 'CONSOLIDATION_TO',
-    id: '4d67d58b-7025-4ceb-ac2b-bee3c00b1ad4',
-  },
-  {
-    caseId: '081-34-34811',
-    otherCase: {
-      orderType: 'consolidation',
-      status: 'pending',
+function getAssociatedCasesMock(caseId: string, consolidationType: ConsolidationType) {
+  return [
+    {
       caseId: '081-93-87181',
-      caseTitle: 'Reed, Williams and Marquez',
-      dateFiled: '2016-09-22',
-      divisionCode: '081',
-      chapter: '15',
-      courtName: 'Southern District of New York',
-      courtDivisionName: 'Manhattan',
-      regionId: '02',
-      regionName: 'NEW YORK',
+      otherCase: MockData.getCaseSummary({ override: { caseId, caseTitle: 'Mr Joe' } }),
       orderDate: '2016-09-28',
-      jobId: 4840265,
-      leadCaseIdHint: '081-16-12681',
-    },
-    orderDate: '2016-09-28',
-    consolidationType: 'substantive',
-    documentType: 'CONSOLIDATION_FROM',
-    id: '19a22328-819d-4ee1-a3c9-2077ffe3e339',
-  },
-  {
-    caseId: '081-34-34811',
-    otherCase: {
-      orderType: 'consolidation',
-      status: 'pending',
-      caseId: '081-64-09681',
-      caseTitle: 'Jackson PLC',
-      dateFiled: '2016-09-22',
-      divisionCode: '081',
-      chapter: '15',
-      courtName: 'Southern District of New York',
-      courtDivisionName: 'Manhattan',
-      regionId: '02',
-      regionName: 'NEW YORK',
+      documentType: 'CONSOLIDATION_TO',
+      consolidationType,
+    } as ConsolidationTo,
+    {
+      caseId,
+      otherCase: MockData.getCaseSummary(),
       orderDate: '2016-09-28',
-      jobId: 4840265,
-      leadCaseIdHint: '081-16-12681',
-    },
-    orderDate: '2016-09-28',
-    consolidationType: 'substantive',
-    documentType: 'CONSOLIDATION_FROM',
-    id: '1bd90f17-c3da-4681-88e6-a8b12de36a78',
-  },
-  {
-    caseId: '081-34-34811',
-    otherCase: {
-      orderType: 'consolidation',
-      status: 'pending',
-      caseId: '081-55-57121',
-      caseTitle: 'Walters-Lynn',
-      dateFiled: '2016-09-22',
-      divisionCode: '081',
-      chapter: '15',
-      courtName: 'Southern District of New York',
-      courtDivisionName: 'Manhattan',
-      regionId: '02',
-      regionName: 'NEW YORK',
+      documentType: 'CONSOLIDATION_FROM',
+      consolidationType,
+    } as ConsolidationFrom,
+    {
+      caseId,
+      otherCase: MockData.getCaseSummary(),
       orderDate: '2016-09-28',
-      jobId: 4840265,
-      leadCaseIdHint: '081-16-12681',
-    },
-    orderDate: '2016-09-28',
-    consolidationType: 'substantive',
-    documentType: 'CONSOLIDATION_FROM',
-    id: '9991a623-633c-4efc-8707-0953eb5a15d6',
-  },
-];
+      documentType: 'CONSOLIDATION_FROM',
+      consolidationType,
+    } as ConsolidationFrom,
+    {
+      caseId,
+      otherCase: MockData.getCaseSummary(),
+      orderDate: '2016-09-28',
+      documentType: 'CONSOLIDATION_FROM',
+      consolidationType,
+    } as ConsolidationFrom,
+  ];
+}
 
 describe('associated cases tests', () => {
+  function renderComponent(mock: EventCaseReference[], isLoading: boolean) {
+    render(
+      <BrowserRouter>
+        <CaseDetailAssociatedCases associatedCases={mock} isAssociatedCasesLoading={isLoading} />
+      </BrowserRouter>,
+    );
+  }
+
   test('should display loading indicator if loading', async () => {
-    render(<CaseDetailAssociatedCases associatedCases={[]} />);
+    renderComponent([], true);
 
     const loadingIndicator = screen.queryByTestId('loading-indicator');
     expect(loadingIndicator).toBeInTheDocument();
   });
 
-  test('should display associated case table when data exists', async () => {
-    render(<CaseDetailAssociatedCases associatedCases={associatedCasesMock} />);
+  test.only('should display associated case table when data exists', async () => {
+    const mock: EventCaseReference[] = getAssociatedCasesMock('081-34-34811', 'substantive');
+    renderComponent(mock, false);
+
+    const sortedMock = mock
+      .sort((a, b) => (a.otherCase.caseId > b.otherCase.caseId ? 1 : -1))
+      .sort((a, _b) => (a.documentType === 'CONSOLIDATION_FROM' ? 1 : -1));
+
+    let header3, header4;
+    await vi.waitFor(() => {
+      header3 = document.querySelector('.associated-cases h3');
+      header4 = document.querySelector('.associated-cases h4');
+    });
+
+    expect(header3).toHaveTextContent('Consolidated cases (4)');
+    expect(header4).toHaveTextContent('Substantive Consolidation');
+
+    const tableRow1Cells = document.querySelectorAll('#associated-cases-table tr:nth-child(1) td');
+    const tableRow3Cells = document.querySelectorAll('#associated-cases-table tr:nth-child(3) td');
+    expect(tableRow1Cells[1]).toHaveTextContent(
+      `${getCaseNumber(sortedMock[0].otherCase.caseTitle)} (Lead)`,
+    );
+    expect(tableRow3Cells[0]).toHaveTextContent(
+      `${getCaseNumber(sortedMock[2].otherCase.caseId)} (${sortedMock[2].otherCase.courtDivisionName})`,
+    );
+    expect(tableRow3Cells[1]).toHaveTextContent(
+      `${getCaseNumber(sortedMock[2].otherCase.caseTitle)}`,
+    );
+    expect(tableRow3Cells[2]).toHaveTextContent(
+      `${getCaseNumber(sortedMock[2].otherCase.dateFiled)}`,
+    );
+    expect(tableRow3Cells[3]).toHaveTextContent(`${getCaseNumber(sortedMock[2].orderDate)}`);
   });
 
-  test('should display lead case in the top of the list.', async () => {});
+  test('should display lead case in the top of the list.', async () => {
+    renderComponent([], false);
+  });
 
-  test('should display cases in order by case ID.', async () => {});
+  test('should display cases in order by case ID.', async () => {
+    renderComponent([], false);
+  });
 });
