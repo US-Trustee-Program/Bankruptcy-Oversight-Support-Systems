49ea0e3133d0842cbf0291c1a264b1bb50eedc64
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-03-26 10:19:20 -0400

    Fix case associated function test
    Jira ticket: CAMS-277

--- backend/functions/case-associated/case-associated.function.test.ts
+++ backend/functions/case-associated/case-associated.function.test.ts
@@ -1,19 +1,16 @@
-import { CaseDetail } from '../../../common/src/cams/cases';
-import { MockData } from '../../../common/src/cams/test-utilities/mock-data';
+import { EventCaseReference } from '../../../common/src/cams/events';
 import { NotFoundError } from '../lib/common-errors/not-found-error';
-import { CaseSummaryController } from '../lib/controllers/case-summary/case-summary.controller';
+import { CaseAssociatedController } from '../lib/controllers/case-associated/case-associated.controller';
 import { CamsResponse } from '../lib/controllers/controller-types';
 import httpTrigger from './case-associated.function';
 
 const context = require('azure-function-context-mock');
 
 describe('Case summary function', () => {
-  test('Should return case summary for an existing case ID', async () => {
-    const caseDetail: CaseDetail = MockData.getCaseDetail();
-
-    const expectedResponseBody: CamsResponse<CaseDetail> = {
+  test('Should return associated cases response.', async () => {
+    const expectedResponseBody: CamsResponse<Array<EventCaseReference>> = {
       success: true,
-      body: caseDetail,
+      body: [],
     };
     const request = {
       params: {
@@ -21,18 +18,18 @@ describe('Case summary function', () => {
       },
     };
     jest
-      .spyOn(CaseSummaryController.prototype, 'getCaseSummary')
+      .spyOn(CaseAssociatedController.prototype, 'getAssociatedCases')
       .mockResolvedValue(expectedResponseBody);
 
     await httpTrigger(context, request);
     expect(context.res.body).toEqual(expectedResponseBody);
   });
 
-  test('Should return an error response for', async () => {
-    const error = new NotFoundError('CASE-MANAGEMENT-USE-CASE', {
+  test('Should return an error response', async () => {
+    const error = new NotFoundError('CASE-ASSOCIATED-USE-CASE', {
       message: 'Case summary not found for case ID.',
     });
-    jest.spyOn(CaseSummaryController.prototype, 'getCaseSummary').mockRejectedValue(error);
+    jest.spyOn(CaseAssociatedController.prototype, 'getAssociatedCases').mockRejectedValue(error);
     const request = {
       params: {
         caseId: '000-00-00000',
