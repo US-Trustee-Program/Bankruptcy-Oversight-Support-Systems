9b615be463da51ffda83d663dcff73a06e0ccff8
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-03-29 10:13:26 -0400

    Add mock associated cases to test
    Jira ticket: CAMS-277

--- backend/functions/lib/controllers/case-associated/case-associated.controller.test.ts
+++ backend/functions/lib/controllers/case-associated/case-associated.controller.test.ts
@@ -7,6 +7,7 @@ import {
 } from '../../testing/testing-constants';
 import { NotFoundError } from '../../common-errors/not-found-error';
 import { CaseAssociatedUseCase } from '../../use-cases/case-associated/case-associated';
+import { MockData } from '../../../../../common/src/cams/test-utilities/mock-data';
 
 describe('Test case-history controller', () => {
   let applicationContext;
@@ -16,8 +17,10 @@ describe('Test case-history controller', () => {
   });
 
   test('should return associated cases when getAssociatedCases is called', async () => {
-    // TODO: Need to mock the associated cases.
-    const associatedCases = [];
+    const associatedCases = [
+      MockData.getConsolidationReference({ override: { documentType: 'CONSOLIDATION_FROM' } }),
+      MockData.getConsolidationReference(),
+    ];
     jest
       .spyOn(CaseAssociatedUseCase.prototype, 'getAssociatedCases')
       .mockResolvedValue(associatedCases);
