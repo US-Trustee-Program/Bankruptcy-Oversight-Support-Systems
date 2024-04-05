f4a38be9284ac4970ebcb9a007f733e210384daf
Michael Ly <105381067+magile32@users.noreply.github.com>
2024-04-05 10:55:39 -0700

    Update test ids for e2e testing
    [skip ci]
    
    Jira ticket: CAMS-329
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>

--- test/e2e/playwright/consolidation-orders.spec.ts
+++ test/e2e/playwright/consolidation-orders.spec.ts
@@ -91,9 +91,7 @@ test.describe('Consolidation Orders', () => {
       page.getByTestId(`modal-overlay-confirmation-modal-${pendingConsolidationOrder.id}`),
     ).toBeVisible();
     expect(
-      page
-        .getByTestId(`modal-content-confirmation-modal-${pendingConsolidationOrder.id}`)
-        .getByTestId('toggle-modal-button-submit'),
+      page.getByTestId(`button-confirmation-modal-${pendingConsolidationOrder.id}-submit-button`),
     ).toBeDisabled();
 
     // Action fill modal dialog form
@@ -112,8 +110,7 @@ test.describe('Consolidation Orders', () => {
 
     // Action click cancel
     await page
-      .getByTestId(`modal-content-confirmation-modal-${pendingConsolidationOrder.id}`)
-      .getByTestId('toggle-modal-button-cancel')
+      .getByTestId(`button-confirmation-modal-${pendingConsolidationOrder.id}-cancel-button`)
       .click();
 
     // Assert modal closed and approve button is disabled
