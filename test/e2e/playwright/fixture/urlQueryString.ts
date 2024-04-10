2799bcffc3b0805dc649b21aede32ba48b425d72
Arthur Morrow <amorrow@flexion.us>
2024-04-10 10:52:49 -0400

    Added step to change cosmosdb name back after e2e
    Jira ticket: CAMS-273
    
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

--- test/e2e/playwright/fixture/urlQueryString.ts
+++ test/e2e/playwright/fixture/urlQueryString.ts
@@ -9,7 +9,7 @@ export const test = base.extend({
       url += 'x-ms-routing-name=staging';
       return goto(url, options);
     }
-
+    //TODO: When doing an initial deployment we want to be able to test the Production slot
     page.goto = modifiedGoto; // replace original goto
 
     await use(page);
