5c42774fe856a88c26ae05aa84691f835abff01e
Michael Ly <105381067+magile32@users.noreply.github.com>
2024-04-10 16:16:22 -0700

    CAMS-237 Revert back changes on script [skip ci]

--- ops/scripts/pipeline/dev-rm-allowed-ip.sh
+++ ops/scripts/pipeline/dev-rm-allowed-ip.sh
@@ -11,25 +11,16 @@ set -euo pipefail # ensure job step fails in CI pipeline when error occurs
 app_rg=$1
 stack_name=$2
 rule_name=$3
-slot_name=${4: ""}
 
 if [[ -z "${app_rg}" || -z "${stack_name}" || -z "${rule_name}" ]]; then
     echo "Error: Missing parameters. Usage: dev-rm-allowed-ip.sh <resource_group_name:str> <stack_name:str> <rule_name:str>"
     exit 1
 fi
 
-apiRemoveCmd="az functionapp config access-restriction remove -g ${app_rg} -n ${stack_name}-node-api --rule-name ${rule_name} 1>/dev/null"
-webAppRemoveCmd="az functionapp config access-restriction remove -g ${app_rg} -n ${stack_name}-webapp --rule-name ${rule_name} 1>/dev/null"
-if [[ $slot_name != "" ]]; then
-    #If slot_name is passed add slot to command
-    apiRemoveCmd+=" --slot ${slot_name}"
-    webAppRemoveCmd+=" --slot ${slot_name}"
-fi
+echo "Removing Ip allow rule by name (${rule_name})"
 
-echo "Removing Ip allow rule by name (${rule_name})         From: ${stack_name}"
+az functionapp config access-restriction remove -g "${app_rg}" -n "${stack_name}-node-api" --rule-name "${rule_name}" 1>/dev/null
 
-eval "$apiRemoveCmd"
-eval "$webAppRemoveCmd"
-
+az functionapp config access-restriction remove -g "${app_rg}" -n "${stack_name}-webapp" --rule-name "${rule_name}" 1>/dev/null
 
 echo "Done"
