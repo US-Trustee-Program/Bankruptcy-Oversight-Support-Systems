bda44ae42ac5b27096343b6815f45637208124c3
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-04-22 16:39:40 -0500

    Create workflow to automatically run updates

--- ops/scripts/utility/update-dependencies.sh
+++ ops/scripts/utility/update-dependencies.sh
@@ -4,7 +4,7 @@
 
 # Usage
 #   From the root directory, run the following command:
-#     ./ops/scripts/utility/update-dependencies.sh [-r]
+#     ./ops/scripts/utility/update-dependencies.sh [-h]
 
 ############################################################
 # Help                                                     #
@@ -13,12 +13,11 @@ Help()
 {
    # Display Help
    echo "This script runs 'npm update' for all Node projects in the repository."
+   echo "It is intended to be run by a scheduled GitHub Actions Workflow."
    echo
-   echo "Syntax: ./ops/scripts/utility/update-dependencies.sh [-h|r|u]"
+   echo "Syntax: ./ops/scripts/utility/update-dependencies.sh [-h]"
    echo "options:"
    echo "h     Print this Help and exit."
-   echo "r     Run the script but remain on dependency-updates branch."
-   echo "u     Run the script but keep the existing dependency-updates branch."
    echo
 }
 
@@ -27,37 +26,18 @@ Help()
 # Main program                                             #
 ############################################################
 ############################################################
-while getopts ":hru" option; do
+while getopts ":h" option; do
   case $option in
     h) # display help
       Help
       exit;;
-    r) # stay on dependency-updates branch upon completion
-      REMAIN=true
-      ;;
-    u) # update existing dependency-updates branch
-      UPDATE=true
-      ;;
     \?) # Invalid option
       echo "'-r' is the only supported option. It is used to remain on the dependency-updates branch at the end of the script."
       ;;
   esac
 done
 
-CURRENT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-if [[ -n $(git status -s) ]]; then
-  STASHED_CHANGE=true
-  git stash
-fi
-if [[ -z "${UPDATE}" ]]; then
-  git checkout main
-  git pull --rebase
-  git branch -D dependency-updates
-  git checkout -b dependency-updates
-else
-  git checkout dependency-updates
-  git pull --rebase
-fi
+git checkout -b dependency-updates-auto
 
 PROJECTS=("backend/functions" "common" "dev-tools" "test/e2e" "user-interface")
 
@@ -71,15 +51,4 @@ done
 
 git add .
 git commit -m "Update all npm projects"
-git push -u origin dependency-updates
-
-if [[ -z "${REMAIN}" ]]; then
-  git checkout "$CURRENT_BRANCH"
-  if [[ -n "${STASHED_CHANGE}" ]]; then
-    git stash pop
-  fi
-elif [[ -n "${STASHED_CHANGE}" ]]; then
-  echo "Remaining on 'dependency-updates' branch, but don't forget you have changes to ${CURRENT_BRANCH} that were stashed."
-fi
-
-open "https://github.com/US-Trustee-Program/Bankruptcy-Oversight-Support-Systems/compare/main...dependency-updates?template=dependencies.md";
+git push -u origin dependency-updates-auto
