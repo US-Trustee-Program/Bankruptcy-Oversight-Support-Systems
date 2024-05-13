476f7530659cf73206d50c8fd60d86a17a678e7b
Fritz Madden <fmadden@flexion.us>
2024-05-13 10:29:46 -0600

    Added some modal tests
    Jira ticket: CAMS-338
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

---
+++ user-interface/src/data-verification/transfer/RejectedTransferOrder.test.tsx
@@ -0,0 +1,5 @@
+import { describe } from 'vitest';
+
+describe('RejectedTransferOrder component', () => {
+  test('should ', () => {});
+});
---
+++ user-interface/src/data-verification/transfer/TransferConfirmationModal.test.tsx
@@ -0,0 +1,123 @@
+import { describe } from 'vitest';
+import {
+  TransferConfirmationModal,
+  TransferConfirmationModalProps,
+} from './TransferConfirmationModal';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { MockData } from '@common/cams/test-utilities/mock-data';
+import { getCaseNumber } from '@/lib/utils/formatCaseNumber';
+import React from 'react';
+import { TransferConfirmationModalImperative } from './TransferConfirmationModal';
+
+const modalId = 'test';
+const fromCase = MockData.getCaseSummary();
+const toCase = MockData.getCaseSummary();
+
+describe('TransferConfirmationModal component', () => {
+  const modalRef = React.createRef<TransferConfirmationModalImperative>();
+  function renderWithProps(props: Partial<TransferConfirmationModalProps> = {}) {
+    const onCancel = vitest.fn();
+    const onConfirm = vitest.fn();
+    const defaultProps: TransferConfirmationModalProps = {
+      id: modalId,
+      fromCaseId: fromCase.caseId,
+      fromDivisionName: fromCase.courtDivisionName,
+      fromCourtName: fromCase.courtName,
+      toCaseId: toCase.caseId,
+      toCourtName: toCase.courtName,
+      toDivisionName: toCase.courtDivisionName,
+      onCancel,
+      onConfirm,
+    };
+
+    const renderProps = { ...defaultProps, ...props };
+    render(<TransferConfirmationModal {...renderProps} ref={modalRef} />);
+    return {
+      onCancel,
+      onConfirm,
+      modalRef,
+    };
+  }
+
+  test.skip('should call onCancel callback when cancelled', () => {
+    const { onCancel } = renderWithProps();
+    expect(onCancel).toHaveBeenCalled();
+  });
+
+  test('should call onConfirm callback when approved', async () => {
+    const { onConfirm, modalRef } = renderWithProps();
+
+    modalRef.current?.show({
+      status: 'approved',
+    });
+
+    const confirmButton = screen.getByTestId(`button-confirm-modal-${modalId}-submit-button`);
+    fireEvent.click(confirmButton);
+
+    await waitFor(() => {
+      expect(onConfirm).toHaveBeenCalledWith('approved');
+    });
+  });
+
+  test('should display static text for confirmation modal', async () => {
+    const expectedText = `This will approve the transfer of case${getCaseNumber(fromCase.caseId)}in${fromCase.courtName} (${fromCase.courtDivisionName})to case${getCaseNumber(toCase.caseId)}in${toCase.courtName} (${toCase.courtDivisionName}).`;
+    const { modalRef } = renderWithProps();
+
+    modalRef.current?.show({
+      status: 'approved',
+    });
+
+    await waitFor(() => {
+      const titleBlock = document.querySelector('.usa-modal__heading');
+      expect(titleBlock).toHaveTextContent('Approve case transfer?');
+    });
+
+    const textBlock = document.querySelector('.usa-modal__main section');
+    expect(textBlock).toHaveTextContent(expectedText);
+  });
+
+  test('should display static text for rejection modal without destination court and case number', async () => {
+    const expectedText = `This will stop the transfer of case${getCaseNumber(fromCase.caseId)}in${fromCase.courtName} (${fromCase.courtDivisionName}).`;
+    const { modalRef } = renderWithProps({
+      toCaseId: undefined,
+      toCourtName: undefined,
+      toDivisionName: undefined,
+    });
+
+    modalRef.current?.show({
+      status: 'rejected',
+    });
+
+    const textBlock = document.querySelector('.usa-modal__main section');
+    expect(textBlock).toHaveTextContent(expectedText);
+  });
+
+  test('should display static text for rejection modal with destination court and case number', async () => {
+    const expectedText = `This will stop the transfer of case${getCaseNumber(fromCase.caseId)}in${fromCase.courtName} (${fromCase.courtDivisionName})to case${getCaseNumber(toCase.caseId)}in${toCase.courtName} (${toCase.courtDivisionName}).`;
+    const { modalRef, onConfirm } = renderWithProps();
+
+    modalRef.current?.show({
+      status: 'rejected',
+    });
+
+    await waitFor(() => {
+      const titleBlock = document.querySelector('.usa-modal__heading');
+      expect(titleBlock).toHaveTextContent('Reject case transfer?');
+    });
+
+    const textBlock = document.querySelector('.usa-modal__main section');
+    expect(textBlock).toHaveTextContent(expectedText);
+
+    let rejectReasonTextArea = screen.getByTestId(`rejection-reason-input-${modalId}`);
+    const rejectReasonText = 'rejected because it has already been completed';
+    fireEvent.change(rejectReasonTextArea, { target: { value: rejectReasonText } });
+    await waitFor(() => {
+      rejectReasonTextArea = screen.getByTestId(`rejection-reason-input-${modalId}`);
+      expect(rejectReasonTextArea).toHaveValue(rejectReasonText);
+    });
+
+    const confirmButton = screen.getByTestId(`button-confirm-modal-${modalId}-submit-button`);
+    fireEvent.click(confirmButton);
+    expect(onConfirm).toHaveBeenCalledWith('rejected', rejectReasonText);
+  });
+});
