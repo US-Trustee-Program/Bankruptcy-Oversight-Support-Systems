52e79630e1f9aa6371e3660e3e713eb53af88c82
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-05-16 17:25:28 -0400

    Clear PendingTransferOrder when accordian is collapsed
    Jira ticket: CAMS-338

--- user-interface/src/data-verification/TransferOrderAccordion.tsx
+++ user-interface/src/data-verification/TransferOrderAccordion.tsx
@@ -1,13 +1,17 @@
+import { useRef } from 'react';
 import { Accordion } from '@/lib/components/uswds/Accordion';
 import { OfficeDetails } from '@common/cams/courts';
 import { TransferOrder } from '@common/cams/orders';
 import { formatDate } from '@/lib/utils/datetime';
 import { getCaseNumber } from '@/lib/utils/formatCaseNumber';
 import { AlertDetails } from '@/lib/components/uswds/Alert';
+import {
+  PendingTransferOrder,
+  PendingTransferOrderImperative,
+} from './transfer/PendingTransferOrder';
+import { ApprovedTransferOrder } from './transfer/ApprovedTransferOrder';
+import { RejectedTransferOrder } from './transfer/RejectedTransferOrder';
 import './TransferOrderAccordion.scss';
-import { PendingTransferOrder } from './transfer/PendingTransferOrder';
-import { ApprovedTransferOrder } from '@/data-verification/transfer/ApprovedTransferOrder';
-import { RejectedTransferOrder } from '@/data-verification/transfer/RejectedTransferOrder';
 
 export interface TransferOrderAccordionProps {
   order: TransferOrder;
@@ -23,72 +27,69 @@ export interface TransferOrderAccordionProps {
 
 export function TransferOrderAccordion(props: TransferOrderAccordionProps) {
   const { order, hidden, statusType, orderType, officesList, expandedId, onExpand } = props;
-  // const [activeButtonId, setActiveButtonId] = useState<string>(`suggested-cases-${order.id}`);
+
+  const pendingTransferOrderRef = useRef<PendingTransferOrderImperative>(null);
 
   function onCollapse() {
-    // TODO: Need imperitive API on pendingtransferOrder component to expose reset/
-    // cancelUpdate();
-    // approveButtonRef.current?.disableButton(true);
-    // setActiveButtonId(`enter-case-${order.id}`);
+    pendingTransferOrderRef?.current?.cancel();
   }
 
   return (
-    <>
-      <Accordion
-        key={order.id}
-        id={`order-list-${order.id}`}
-        expandedId={expandedId}
-        onExpand={onExpand}
-        onCollapse={onCollapse}
-        hidden={hidden}
+    <Accordion
+      key={order.id}
+      id={`order-list-${order.id}`}
+      expandedId={expandedId}
+      onExpand={onExpand}
+      onCollapse={onCollapse}
+      hidden={hidden}
+    >
+      <section
+        className="accordion-heading grid-row grid-gap-lg"
+        data-testid={`accordion-heading-${order.id}`}
       >
-        <section
-          className="accordion-heading grid-row grid-gap-lg"
-          data-testid={`accordion-heading-${order.id}`}
+        <div
+          className="grid-col-6 text-no-wrap"
+          aria-label={`Court district – ${getCaseNumber(order.courtName)}`}
         >
-          <div
-            className="grid-col-6 text-no-wrap"
-            aria-label={`Court district – ${getCaseNumber(order.courtName)}`}
+          {order.courtName}
+        </div>
+        <div
+          className="grid-col-2 text-no-wrap"
+          title="Event date"
+          aria-label={`Event date – ${formatDate(order.orderDate)}`}
+        >
+          {formatDate(order.orderDate)}
+        </div>
+        <div className="grid-col-2 order-type text-no-wrap">
+          <span aria-label={`Event type ${orderType.get(order.orderType)}`}>
+            {orderType.get(order.orderType)}
+          </span>
+        </div>
+        <div className="grid-col-2 order-status text-no-wrap">
+          <span
+            className={order.status}
+            aria-label={`Event status ${statusType.get(order.status)}`}
           >
-            {order.courtName}
-          </div>
-          <div
-            className="grid-col-2 text-no-wrap"
-            title="Event date"
-            aria-label={`Event date – ${formatDate(order.orderDate)}`}
-          >
-            {formatDate(order.orderDate)}
-          </div>
-          <div className="grid-col-2 order-type text-no-wrap">
-            <span aria-label={`Event type ${orderType.get(order.orderType)}`}>
-              {orderType.get(order.orderType)}
-            </span>
-          </div>
-          <div className="grid-col-2 order-status text-no-wrap">
-            <span
-              className={order.status}
-              aria-label={`Event status ${statusType.get(order.status)}`}
-            >
-              {statusType.get(order.status)}
-            </span>
-          </div>
-        </section>
-        <section className="accordion-content" data-testid={`accordion-content-${order.id}`}>
-          {order.status === 'pending' && (
-            <PendingTransferOrder
-              order={order}
-              onOrderUpdate={props.onOrderUpdate}
-              officesList={officesList}
-            />
-          )}
-          {order.status === 'approved' && (
-            <ApprovedTransferOrder order={order} onOrderUpdate={props.onOrderUpdate} />
-          )}
-          {order.status === 'rejected' && (
-            <RejectedTransferOrder order={order} onOrderUpdate={props.onOrderUpdate} />
-          )}
-        </section>
-      </Accordion>
-    </>
+            {statusType.get(order.status)}
+          </span>
+        </div>
+      </section>
+      <section className="accordion-content" data-testid={`accordion-content-${order.id}`}>
+        {order.status === 'pending' && (
+          <PendingTransferOrder
+            order={order}
+            onOrderUpdate={props.onOrderUpdate}
+            officesList={officesList}
+            ref={pendingTransferOrderRef}
+          />
+        )}
+        {order.status === 'approved' && (
+          <ApprovedTransferOrder order={order} onOrderUpdate={props.onOrderUpdate} />
+        )}
+        {order.status === 'rejected' && (
+          <RejectedTransferOrder order={order} onOrderUpdate={props.onOrderUpdate} />
+        )}
+      </section>
+    </Accordion>
   );
 }
