7e2cc0a7814be46e0a84f925ab87a4588aa3227f
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-06-03 12:09:14 -0400

    Remove logout from Session component
    [skip ci]
    
    Jira ticket: CAMS-347

--- user-interface/src/login/Session.tsx
+++ user-interface/src/login/Session.tsx
@@ -3,12 +3,10 @@ import { CamsUser } from './login-helpers';
 
 export type SessionContextType = {
   user: CamsUser | null;
-  // logout: () => void;
 };
 
 export const SessionContext = createContext<SessionContextType>({
   user: null,
-  // logout: () => {},
 });
 
 export type SessionProps = PropsWithChildren & {
@@ -16,16 +14,12 @@ export type SessionProps = PropsWithChildren & {
 };
 
 export function Session(props: SessionProps) {
+  // TODO: We need to store the user in local storage.
   // if (window.localStorage) {
   //   window.localStorage.setItem(LOCAL_STORAGE_USER_KEY, JSON.stringify(props.user));
   // }
 
-  // function logout() {
-  //   // TODO
-  // }
-
   // TODO: We need to change the '/login' route to go to '/'.
-
   return (
     <SessionContext.Provider value={{ user: props.user }}>{props.children}</SessionContext.Provider>
   );
