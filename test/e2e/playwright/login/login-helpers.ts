bc6d38f805e7e764484b8eb6c96de6c1b4d278c8
Arthur Morrow <amorrow@flexion.us>
2024-06-06 13:30:21 -0400

    Cleaned up e2e tests and login help button ids
    Jira ticket: CAMS-373
    
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

--- test/e2e/playwright/login/login-helpers.ts
+++ test/e2e/playwright/login/login-helpers.ts
@@ -5,7 +5,7 @@ const LOGOUT_PATH = '/logout';
 
 async function mockLogin(page: Page) {
   await page.goto(LOGIN_PATH);
-  await page.getByTestId('button-undefined').click(); //test-id for confirm on prelogin is button-undefined. we should change this
+  await page.getByTestId('button-auo-confirm').click();
   await expect(page.getByTestId('modal-content-login-modal')).toBeVisible();
   await page.getByTestId('radio-role-0-click-target').click();
   await page.getByTestId('button-login-modal-submit-button').click();
@@ -15,7 +15,7 @@ async function mockLogin(page: Page) {
 async function logout(page: Page) {
   await page.goto(LOGOUT_PATH);
   await expect(page.getByTestId('alert-message')).toBeVisible();
-  await expect(page.getByTestId('button-undefined')).toBeVisible(); //test-id for confirm on prelogin is button-undefined. we should change this
+  await expect(page.getByTestId('button-login')).toBeVisible();
 }
 
 export function useAuthentication(_provider: string) {
