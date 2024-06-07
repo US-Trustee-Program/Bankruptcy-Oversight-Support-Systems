4917ac52dd5d7840f26aacf41936774345ea463d
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-06-07 10:27:26 -0400

    Added additional test coverage
    Jira ticket: CAMS-347

--- user-interface/src/login/SessionEnd.test.tsx
+++ user-interface/src/login/SessionEnd.test.tsx
@@ -1,7 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { describe } from 'vitest';
 import { SessionEnd } from './SessionEnd';
 import { BrowserRouter } from 'react-router-dom';
+import * as reactRouter from 'react-router';
+import { LOGIN_PATH } from './login-helpers';
 
 describe('SessionEnd', () => {
   test('should render alert', () => {
@@ -11,5 +13,31 @@ describe('SessionEnd', () => {
       </BrowserRouter>,
     );
     expect(screen.queryByTestId('alert-container')).toBeInTheDocument();
+  });
+  test('should clear session from local storage', () => {
+    render(
+      <BrowserRouter>
+        <SessionEnd></SessionEnd>
+      </BrowserRouter>,
+    );
+    // TODO
+  });
+  test('should allow the user to redirect to login', () => {
+    const navigate = vi.fn();
+    const useNavigate = vi.spyOn(reactRouter, 'useNavigate').mockImplementation(() => {
+      return navigate;
+    });
+    render(
+      <BrowserRouter>
+        <SessionEnd></SessionEnd>
+      </BrowserRouter>,
+    );
+    const loginRedirectButton = screen.queryByTestId('button-login');
+    expect(loginRedirectButton).toBeInTheDocument();
+
+    fireEvent.click(loginRedirectButton!);
+
+    expect(useNavigate).toHaveBeenCalled();
+    expect(navigate).toHaveBeenCalledWith(LOGIN_PATH);
   });
 });
