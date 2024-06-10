9c6b7b465cb31495749974cb590cd4ba40893d83
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-06-10 11:24:44 -0400

    Stub Session tests
    Jira ticket: CAMS-401

---
+++ user-interface/src/login/Session.test.tsx
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Session, SessionProps } from './Session';
+import { CamsUser } from './login-helpers';
+
+describe('Session', () => {
+  const testUser: CamsUser = {
+    name: 'Test User',
+  };
+  const testProvider = 'mock';
+
+  function renderWithProps(props: Partial<SessionProps> = {}) {
+    const defaultProps: SessionProps = {
+      user: testUser,
+      provider: testProvider,
+    };
+
+    render(
+      <BrowserRouter>
+        <Session {...defaultProps} {...props}></Session>
+      </BrowserRouter>,
+    );
+  }
+
+  // TODO: Complete these tests.
+  test('should write the session to local storage', () => {
+    renderWithProps();
+  });
+
+  test('should redirect to "/" if path is "/login"', () => {
+    renderWithProps();
+  });
+
+  test('should redirect to "/" if path is "/logout"', () => {
+    renderWithProps();
+  });
+});
