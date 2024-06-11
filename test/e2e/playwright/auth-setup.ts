2216d92540c8f70a42ae5a96dc8b08146a5e2e1d
Arthur Morrow <amorrow@flexion.us>
2024-06-11 14:51:04 -0400

    changed auth url to proper okta tenant
    Jira ticket: CAMS-401

--- test/e2e/playwright/auth-setup.ts
+++ test/e2e/playwright/auth-setup.ts
@@ -3,7 +3,7 @@ import { expect, test as setup } from '@playwright/test';
 const authFile = 'playwright/.auth/user.json';
 const userName = process.env.OKTA_USER_NAME;
 const password = process.env.OKTA_PASSWORD;
-const OKTA_HOST = 'https://dev-34608149.okta.com/';
+const OKTA_HOST = 'https://dev-34608149.okta.com/api/v1/authn';
 setup('authenticate', async ({ request }) => {
   // Send authentication request. Replace with your own.
   await request.post(OKTA_HOST, {
