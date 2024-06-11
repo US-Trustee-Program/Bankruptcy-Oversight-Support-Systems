2271441fec758b2b5f8624c73cc5ebfc7cae0a87
Fritz Madden <fmadden@flexion.us>
2024-06-11 16:19:08 -0600

    Fixed colors in header etc. in all environments
    Jira ticket: CAMS-401
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,
    Co-authored-by: Sam Johnson <171059116+samjohnsonflexion@users.noreply.github.com>

--- user-interface/src/lib/components/uswds/Banner.tsx
+++ user-interface/src/lib/components/uswds/Banner.tsx
@@ -6,9 +6,9 @@ import Icon from './Icon';
 
 export const Banner = () => {
   const launchDarklyEnvironment = import.meta.env['CAMS_LAUNCH_DARKLY_ENV'];
-  const envHeaderClassName =
-    'usa-banner__header ' +
-    (launchDarklyEnvironment === 'production' ? '' : `${launchDarklyEnvironment}`);
+  const environmentClass =
+    launchDarklyEnvironment === 'production' ? '' : `${launchDarklyEnvironment}`;
+  const envHeaderClassName = 'usa-banner__header ' + environmentClass;
 
   const session = useContext(SessionContext);
 
@@ -17,42 +17,42 @@ export const Banner = () => {
       className="usa-banner cams-banner"
       aria-label="Official website of the United States government"
     >
-      <div className="usa-accordion">
-        <header className={envHeaderClassName} data-testid="banner-header">
-          <div className="usa-banner__inner flex-box">
-            <div className="header-flag-container">
-              <img
-                aria-hidden="true"
-                className="usa-banner__header-flag banner-image"
-                src="/assets/styles/img/us_flag_small.png"
-                alt=""
-              />
-              <span className="banner-text usa-banner__header-text">
-                An official website of the United States government
-              </span>
-            </div>
-            <div className="environment-text-container usa-banner__header-text">
-              {launchDarklyEnvironment != 'production' && (
-                <span className="environment-text">{launchDarklyEnvironment} ENVIRONMENT</span>
-              )}
-            </div>
-
-            <div className="login-info">
-              {session.user && (
-                <>
-                  <span className="user-icon">
-                    <Icon name="person"></Icon>
-                  </span>
-                  <span className="user-name">{session.user?.name ?? 'UNKNOWN'} </span>
-                  <span className="logout-link">
-                    <a href={LOGOUT_PATH}>logout</a>
-                  </span>
-                </>
-              )}
-            </div>
+      <header className={envHeaderClassName} data-testid="banner-header">
+        <div className="usa-banner__inner header-container">
+          <div className="header-flag-container">
+            <img
+              aria-hidden="true"
+              className="usa-banner__header-flag banner-image"
+              src="/assets/styles/img/us_flag_small.png"
+              alt=""
+            />
+            <span className="banner-text usa-banner__header-text">
+              An official website of the United States government
+            </span>
           </div>
-        </header>
-      </div>
+          <div className="environment-text-container usa-banner__header-text">
+            {launchDarklyEnvironment != 'production' && (
+              <span className="environment-text">{launchDarklyEnvironment} ENVIRONMENT</span>
+            )}
+          </div>
+
+          <div className="login-info">
+            {session.user && (
+              <>
+                <span className="user-icon">
+                  <Icon name="person"></Icon>
+                </span>
+                <span className="user-name">{session.user?.name ?? 'UNKNOWN'} </span>
+                <span className="logout-link">
+                  <a href={LOGOUT_PATH} className={environmentClass}>
+                    logout
+                  </a>
+                </span>
+              </>
+            )}
+          </div>
+        </div>
+      </header>
     </section>
   );
 };
