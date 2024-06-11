5a33a85e28470592e700abf110e11315cabb8ec9
Arthur Morrow <amorrow@flexion.us>
2024-06-11 16:59:24 -0400

    adjust banner to properly display items with flex-box
    Jira ticket: CAMS-401
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>

--- user-interface/src/lib/components/uswds/Banner.tsx
+++ user-interface/src/lib/components/uswds/Banner.tsx
@@ -18,28 +18,27 @@ export const Banner = () => {
     >
       <div className="usa-accordion">
         <header className={envHeaderClassName} data-testid="banner-header">
-          <div className="usa-banner__inner grid-row grid-gap-sm">
-            <div className="grid-col-1 header-flag-container">
+          <div className="usa-banner__inner flex-box">
+            <div className="header-flag-container">
               <img
                 aria-hidden="true"
                 className="usa-banner__header-flag"
                 src="/assets/styles/img/us_flag_small.png"
                 alt=""
               />
-            </div>
-            <div className="grid-col-auto tablet:grid-col-auto" aria-hidden="true">
-              <span className="usa-banner__header-text">
+              <span className="banner-text usa-banner__header-text">
                 An official website of the United States government
               </span>
             </div>
-            {launchDarklyEnvironment != 'production' && (
-              <div className="grid-col-5 usa-banner__header-text">
+            <div className="environment-text-container usa-banner__header-text">
+              {launchDarklyEnvironment != 'production' && (
                 <span className="environment-text">{launchDarklyEnvironment} ENVIRONMENT</span>
-              </div>
-            )}
-            <div className="grid-col-2 usa-banner__header-text">
+              )}
+            </div>
+
+            <div className="login-info">
               {session.user && (
-                <span className=" environment-text">
+                <span>
                   {session.user?.name ?? 'UNKNOWN'} <a href={LOGOUT_PATH}>logout</a>
                 </span>
               )}
