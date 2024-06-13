5f23e0f0b0f3a4ae86ed3a2d5a4a71f88f253b52
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-06-13 15:53:06 -0400

    Add OktaLogin tests
    Jira ticket: CAMS-401

--- user-interface/src/login/providers/okta/OktaLogin.test.tsx
+++ user-interface/src/login/providers/okta/OktaLogin.test.tsx
@@ -1,5 +1,33 @@
 import { describe } from 'vitest';
+import * as oktaReactModule from '@okta/okta-react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { OktaLogin } from './OktaLogin';
 
 describe('OktaLogin', () => {
-  test('should allow the user to login via Okta', () => {});
+  const authState = {
+    isAuthenticated: false,
+  };
+  const signInWithRedirect = vi.fn();
+  const useOktaAuth = vi.fn().mockImplementation(() => {
+    return {
+      oktaAuth: {
+        signInWithRedirect,
+      },
+      authState,
+    };
+  });
+  vi.spyOn(oktaReactModule, 'useOktaAuth').mockImplementation(useOktaAuth);
+
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test('should allow the user to login via Okta', async () => {
+    render(<OktaLogin></OktaLogin>);
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('interstitial-login')).toBeInTheDocument();
+    });
+    expect(signInWithRedirect).toHaveBeenCalled();
+  });
 });
