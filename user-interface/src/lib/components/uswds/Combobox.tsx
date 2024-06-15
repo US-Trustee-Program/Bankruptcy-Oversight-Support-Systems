69332cda042809dbf678da186684586af9c138ba
Fritz Madden <fmadden@flexion.us>
2024-06-14 18:01:30 -0600

    Got basic selection working including...
    Key down events through the dropdown and in the input element.
    Got all focus highlights working and styled well.
    Got pills working and styled well.
    
    ToDo:
    
    Pills probably ought to be tabbable and focusable.
    Input handling for filtering dropdown.
    Also, when items in listed are filtered out (hidden) they also should
    have tabIndex set to -1 so that you can't focus on them and the arrow
    keys continue to work.
    
    Jira ticket: CAMS-375
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Sam Johnson <171059116+samjohnsonflexion@users.noreply.github.com>

--- user-interface/src/lib/components/uswds/Combobox.tsx
+++ user-interface/src/lib/components/uswds/Combobox.tsx
@@ -11,10 +11,7 @@ import {
 import { InputRef } from '../../type-declarations/input-fields';
 import Icon from './Icon';
 import Button, { UswdsButtonStyle } from './Button';
-import ComboboxMultiSelectInput from './ComboboxMultiSelectInput';
-
-// Alias for readability.
-//const debounce = setTimeout;
+import PillBox from './PillBox';
 
 export type ComboOption = {
   value: string;
@@ -33,21 +30,23 @@ interface ComboboxProps extends PropsWithChildren, Omit<InputProps, 'onChange'> 
   position?: 'left' | 'right';
   value?: string;
   icon?: string;
-  includeClearButton?: boolean;
   options: ComboOption[];
   onChange: (options: ComboOption[]) => void;
   multiSelect: boolean;
 }
 
 function ComboboxComponent(props: ComboboxProps, ref: React.Ref<InputRef>) {
-  const { label, includeClearButton, options, value, onChange, multiSelect, ...otherProps } = props;
+  const { label, options, value, onChange, multiSelect, ...otherProps } = props;
   const [inputDisabled, setInputDisabled] = useState<boolean>(otherProps.disabled ?? false);
   const [selections, setSelections] = useState<ComboOption[]>();
   const [expandIcon, setExpandIcon] = useState<string>('expand_more');
   const [expanded, setExpanded] = useState<boolean>(false);
   const [expandedClass, setExpandedClass] = useState<string>('closed');
+  const [dropdownLocation, setDropdownLocation] = useState<{ bottom: number } | null>(null);
+  const [currentSelection, setCurrentSelection] = useState<number>(0);
 
   function emitChange(_value: string) {
+    // TODO may not need... this came from input component
     /*
     if (onChange) {
       const ev = { target: { value } } as React.ChangeEvent<HTMLInputElement>;
@@ -57,24 +56,39 @@ function ComboboxComponent(props: ComboboxProps, ref: React.Ref<InputRef>) {
   }
 
   function getValue() {
+    // TODO this should be reimplemented to return the selections list
     return '';
   }
 
   function resetValue() {
+    // TODO this will need to reset the values back to what they were originally...
     //setInputValue(value || '');
   }
 
   function clearValue() {
+    // TODO this should be setup to clear a single value... may not need this.
     //setInputValue('');
     emitChange('');
   }
 
+  function clearAll() {
+    setSelections([]);
+  }
+
   function setValue(_value: string) {
+    // TODO may not need.  Useful for setting a list of values.  You could instead rely on props.
     //setInputValue(value);
   }
 
   function disable(value: boolean) {
     setInputDisabled(value);
+  }
+
+  function setListItemClass(index: number, option: ComboOption) {
+    const classNames = [];
+    if (isSelected(option)) classNames.push('selected');
+    if (currentSelection === index) classNames.push('highlighted');
+    return classNames.join(' ');
   }
 
   function isSelected(option: ComboOption) {
@@ -115,7 +129,23 @@ function ComboboxComponent(props: ComboboxProps, ref: React.Ref<InputRef>) {
     }
   }
 
-  function toggleDropdown() {
+  function toggleDropdown(_ev: React.MouseEvent<HTMLButtonElement>) {
+    const screenBottom = window.scrollY + window.innerHeight;
+    const inputContainer = document.querySelector(`#${props.id} .input-container`);
+    const topYPos = inputContainer?.getBoundingClientRect().top;
+    const bottomYPos = inputContainer?.getBoundingClientRect().bottom;
+
+    if (bottomYPos && options.length * 43 > screenBottom - bottomYPos) {
+      if (topYPos && bottomYPos) {
+        const inputHeight = bottomYPos - topYPos;
+        setDropdownLocation({ bottom: inputHeight });
+      } else {
+        setDropdownLocation(null);
+      }
+    } else {
+      setDropdownLocation(null);
+    }
+
     if (expanded) {
       setExpandIcon('expand_more');
       setExpanded(false);
@@ -135,6 +165,51 @@ function ComboboxComponent(props: ComboboxProps, ref: React.Ref<InputRef>) {
     console.log(ev);
   }
 
+  function handleKeyDown(ev: React.KeyboardEvent, index: number) {
+    const list = document.querySelector(`#${props.id} .item-list-container ul`);
+    const input = document.querySelector(`#${props.id} .input-container input`);
+
+    switch (ev.key) {
+      case 'Escape':
+        setExpandIcon('expand_more');
+        setExpanded(false);
+        setExpandedClass('closed');
+        setCurrentSelection(0);
+        break;
+      case 'ArrowDown':
+        if (index >= 0 && index < options.length) {
+          setCurrentSelection(index + 1);
+          if (list) (list.children[index] as HTMLElement).focus({ preventScroll: true });
+          ev.stopPropagation();
+        } else {
+          setCurrentSelection(0);
+          if (input) (input as HTMLElement).focus({ preventScroll: true });
+          ev.stopPropagation();
+        }
+        break;
+      case 'ArrowUp':
+        if (index > 1 && index <= options.length) {
+          setCurrentSelection(index - 1);
+          if (list) (list.children[index - 2] as HTMLElement).focus({ preventScroll: true });
+          ev.stopPropagation();
+        } else if (index === 1) {
+          setCurrentSelection(0);
+          if (input) (input as HTMLElement).focus({ preventScroll: true });
+          ev.stopPropagation();
+        } else if (index === 0) {
+          setCurrentSelection(options.length);
+          if (list)
+            (list.children[list.children.length - 1] as HTMLElement).focus({ preventScroll: true });
+          ev.stopPropagation();
+        }
+        break;
+      case 'Enter':
+        // TODO: we need to add code here to select an item in the list
+        console.log('Enter is not implemented yet -- see TODO');
+        break;
+    }
+  }
+
   useEffect(() => {
     //setInputValue(value || '');
   }, [value]);
@@ -142,30 +217,38 @@ function ComboboxComponent(props: ComboboxProps, ref: React.Ref<InputRef>) {
   useImperativeHandle(ref, () => ({ clearValue, resetValue, setValue, getValue, disable }));
 
   return (
-    <div className="usa-form-group">
-      <label className="usa-label" id={props.id + '-label'} htmlFor={props.id}>
-        {label}
-      </label>
-      <div className="usa-combo-box">
-        {includeClearButton && !inputDisabled && (
-          <div className="usa-input-suffix" aria-hidden="true">
-            <Button uswdsStyle={UswdsButtonStyle.Unstyled} onClick={clearValue}>
-              <Icon name="close"></Icon>
-            </Button>
-          </div>
+    <div id={props.id} className="usa-form-group combo-box-form-group">
+      <div className={`chapter-label ${multiSelect ? 'multi-select' : ''}`}>
+        <label className="usa-label" id={props.id + '-label'} htmlFor={props.id}>
+          {label}
+        </label>
+        {multiSelect && selections && selections.length > 0 && (
+          <Button uswdsStyle={UswdsButtonStyle.Unstyled} onClick={clearAll}>
+            clear
+          </Button>
         )}
+      </div>
+      {multiSelect && (
+        <PillBox selections={selections ?? []} onSelectionChange={handlePillSelection}></PillBox>
+      )}
+      <div className="usa-combo-box">
         <div className="input-container usa-input">
-          {multiSelect && (
-            <ComboboxMultiSelectInput
-              selections={selections}
-              onSelectionChange={handlePillSelection}
+          <div className="combo-box-input-container">
+            <input
+              {...otherProps}
+              className={`usa-tooltip combo-box-input`}
               onChange={handleInputFilter}
-            ></ComboboxMultiSelectInput>
-          )}
+              onKeyDown={(ev) => handleKeyDown(ev, 0)}
+              value={value}
+              disabled={inputDisabled}
+            />
+          </div>
           <Button
             className="expand-button"
             uswdsStyle={UswdsButtonStyle.Unstyled}
             onClick={toggleDropdown}
+            onKeyDown={(ev) => handleKeyDown(ev, 0)}
+            disabled={inputDisabled}
           >
             <Icon name={expandIcon}></Icon>
           </Button>
@@ -175,14 +258,17 @@ function ComboboxComponent(props: ComboboxProps, ref: React.Ref<InputRef>) {
           id={`${props.id}-item-list`}
           aria-hidden={`${expanded}`}
           tabIndex={-1}
+          style={dropdownLocation ?? undefined}
         >
           <ul>
             {options.map((option, idx) => (
               <li
-                className={isSelected(option) ? 'selected' : ''}
+                className={setListItemClass(idx, option)}
                 key={idx}
                 data-value={option.value}
                 onClick={() => handleDropdownItemSelection(option)}
+                onKeyDown={(ev) => handleKeyDown(ev, idx + 1)}
+                tabIndex={expanded ? 0 : -1}
               >
                 {option.label}
               </li>
