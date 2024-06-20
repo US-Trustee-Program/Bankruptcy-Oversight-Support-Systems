83a850a898c6c6f44b6719a95f63cc4d61bf2322
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-06-20 16:09:15 -0500

    Add session to cache without user info
    Jira ticket: CAMS-401
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,
    Co-authored-by: Sam Johnson <171059116+samjohnsonflexion@users.noreply.github.com>

--- backend/functions/lib/adapters/gateways/user-session.gateway.test.ts
+++ backend/functions/lib/adapters/gateways/user-session.gateway.test.ts
@@ -1,20 +1,38 @@
 import { UserSessionGateway } from './user-session.gateway';
 import OktaGateway from './okta/okta-gateway';
 import { JwtHeader } from '../types/authorization';
+import { ApplicationContext } from '../types/basic';
+import { createMockApplicationContext } from '../../testing/testing-utilities';
+import { MockHumbleItem, MockHumbleItems } from '../../testing/mock.cosmos-client-humble';
+import { CamsSession } from '../../../../../common/src/cams/session';
 
 describe('user-session.gateway test', () => {
   const jwtClaims = {
-    iss: 'https://dev-31938913.okta.com/oauth2/default',
+    iss: 'https://nonsense-3wjj23473kdwh2.okta.com/oauth2/default',
     sub: 'user@fake.com',
     aud: 'api://default',
     iat: 0,
     exp: 1,
   };
-  beforeEach(() => {
+  let token: string;
+  let mockGetValue: CamsSession;
+  let context: ApplicationContext;
+  beforeEach(async () => {
+    context = await createMockApplicationContext({
+      DATABASE_MOCK: 'true',
+      AUTH_ISSUER: 'https://nonsense-3wjj23473kdwh2.okta.com/oauth2/default',
+    });
     const jwtHeader = {
       alg: 'RS256',
       typ: undefined,
       kid: '',
+    };
+    token = 't3st1d';
+    mockGetValue = {
+      user: { name: '' },
+      apiToken: token,
+      provider: 'okta',
+      validatedClaims: jwtClaims,
     };
     jest.spyOn(OktaGateway, 'verifyToken').mockResolvedValue({
       claims: jwtClaims,
@@ -29,17 +47,27 @@ describe('user-session.gateway test', () => {
         return false;
       },
     });
+    jest.spyOn(MockHumbleItem.prototype, 'read').mockResolvedValue({
+      resource: mockGetValue,
+    });
   });
 
-  test('should return valid session when not in cache', async () => {
-    const token = 't3st1d';
-    const gateway = new UserSessionGateway();
-    const session = await gateway.lookup(token);
-    expect(session).toEqual({
-      user: { name: '' },
-      apiToken: token,
-      provider: '',
-      validatedClaims: jwtClaims,
+  test('should return valid session and add to cache when cache miss is encountered', async () => {
+    jest.spyOn(MockHumbleItem.prototype, 'read').mockResolvedValue({
+      resource: undefined,
     });
+    const createSpy = jest.spyOn(MockHumbleItems.prototype, 'create');
+    const gateway = new UserSessionGateway();
+    const session = await gateway.lookup(context, token);
+    expect(session).toEqual(mockGetValue);
+    expect(createSpy).toHaveBeenCalled();
+  });
+
+  test('should return valid session on cache hit', async () => {
+    const createSpy = jest.spyOn(MockHumbleItems.prototype, 'create');
+    const gateway = new UserSessionGateway();
+    const session = await gateway.lookup(context, token);
+    expect(session).toEqual(mockGetValue);
+    expect(createSpy).not.toHaveBeenCalled();
   });
 });
