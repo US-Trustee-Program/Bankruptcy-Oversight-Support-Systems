e9b402d933961a5f76409796c120b81f67828266
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-06-20 20:44:15 -0500

    Add user to cached session
    Jira ticket: CAMS-401

--- backend/functions/lib/adapters/gateways/user-session.gateway.ts
+++ backend/functions/lib/adapters/gateways/user-session.gateway.ts
@@ -2,6 +2,9 @@ import { CamsSession } from '../../../../../common/src/cams/session';
 import { SessionCache } from '../utils/sessionCache';
 import { getAuthorizationGateway, getUserSessionCacheRepository } from '../../factory';
 import { ApplicationContext } from '../types/basic';
+import { ForbiddenError } from '../../common-errors/forbidden-error';
+
+const MODULE_NAME = 'USER-SESSION-GATEWAY';
 
 export class UserSessionGateway implements SessionCache {
   async lookup(context: ApplicationContext, token: string, provider: string): Promise<CamsSession> {
@@ -14,8 +17,14 @@ export class UserSessionGateway implements SessionCache {
 
     const authGateway = getAuthorizationGateway(provider);
     const jwt = await authGateway.verifyToken(token);
+    if (!jwt) {
+      throw new ForbiddenError(MODULE_NAME, {
+        message: 'Unable to verify token.',
+      });
+    }
+    const user = await authGateway.getUser(token);
     const session: CamsSession = {
-      user: { name: '' },
+      user,
       apiToken: token,
       provider: provider,
       validatedClaims: jwt.claims,
