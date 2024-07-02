a1803aadb66bb14dfc2d07a3bd614a7128ee9914
Arthur Morrow <amorrow@flexion.us>
2024-07-02 15:01:24 -0400

    created script for login provider
    Jira ticket: CAMS-401

---
+++ ops/scripts/pipeline/set-login-provider-config.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Description: Helper script to set the login provider configuration for mock login
+
+set -eou pipefail
+
+camsLoginProvider=$1
+camsLoginProviderConfig=$2
+apiName=$3
+
+loginProviderConfigOutput=$camsLoginProviderConfig
+
+if [[ $camsLoginProvider == 'mock' ]]; then
+    # shellcheck disable=SC2089 # REASON: We need to retain the single quotes
+    loginProviderConfigOutput='{ "issuer": "https://'$apiName'.azurewebsites.us/api/oauth2/default", "clientId": ""}'
+fi
+
+echo "$loginProviderConfigOutput"
