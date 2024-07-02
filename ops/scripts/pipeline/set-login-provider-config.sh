fde695c45518f36838512b21d1eb1d66c4d04708
Arthur Morrow <amorrow@flexion.us>
2024-07-02 15:35:40 -0400

    removed clientId from mock provider config
    Jira ticket: CAMS-401

--- ops/scripts/pipeline/set-login-provider-config.sh
+++ ops/scripts/pipeline/set-login-provider-config.sh
@@ -12,7 +12,7 @@ loginProviderConfigOutput=$camsLoginProviderConfig
 
 if [[ $camsLoginProvider == 'mock' ]]; then
     # shellcheck disable=SC2089 # REASON: We need to retain the single quotes
-    loginProviderConfigOutput='{ "issuer": "https://'$apiName'.azurewebsites.us/api/oauth2/default", "clientId": ""}'
+    loginProviderConfigOutput='{ "issuer": "https://'$apiName'.azurewebsites.us/api/oauth2/default" }'
 fi
 
 echo "$loginProviderConfigOutput"
