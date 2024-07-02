5001a6c5282bbe4b31db95a179fb8076707dc7ff
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-07-02 17:51:55 -0400

    Added mock issuer generation test for MockLogin
    Jira ticket: CAMS-401
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,
    Co-authored-by: Sam Johnson <171059116+samjohnsonflexion@users.noreply.github.com>

--- user-interface/src/login/providers/mock/MockLogin.test.tsx
+++ user-interface/src/login/providers/mock/MockLogin.test.tsx
@@ -5,19 +5,56 @@ import { MockLogin } from './MockLogin';
 import { MockData } from '@common/cams/test-utilities/mock-data';
 
 describe('MockLogin', () => {
+  const fetchSpy = vi
+    .spyOn(global, 'fetch')
+    .mockImplementation(
+      (_input: string | URL | Request, _init?: RequestInit): Promise<Response> => {
+        return Promise.resolve({
+          ok: true,
+          json: vi.fn().mockResolvedValue({ token: MockData.getJwt() }),
+        } as unknown as Response);
+      },
+    );
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  test('should report a bad mock issuer to the console', async () => {
+    const errorSpy = vi.spyOn(global.console, 'error');
+
+    vi.stubEnv('CAMS_BASE_PATH', '');
+    vi.stubEnv('CAMS_SERVER_HOSTNAME', '');
+    vi.stubEnv('CAMS_SERVER_PORT', '');
+    vi.stubEnv('CAMS_SERVER_PROTOCOL', '');
+
+    vi.resetModules();
+    const mod = await import('./MockLogin');
+    const { MockLogin } = mod;
+
+    render(
+      <BrowserRouter>
+        <MockLogin user={null}>
+          <div>Hello World!!</div>
+        </MockLogin>
+      </BrowserRouter>,
+    );
+
+    const radioButton = screen.queryByTestId('radio-role-0-click-target');
+    expect(radioButton).toBeInTheDocument();
+    fireEvent.click(radioButton!);
+
+    const loginButton = screen.queryByTestId('button-login-modal-submit-button');
+    expect(loginButton).toBeInTheDocument();
+    fireEvent.click(loginButton!);
+
+    expect(fetchSpy).not.toHaveBeenCalled();
+    expect(errorSpy).toHaveBeenCalled();
+  });
+
   test('should allow the user to select a role', async () => {
     const testId = 'child-div';
     const childText = 'TEST';
-    const fetchSpy = vi
-      .spyOn(global, 'fetch')
-      .mockImplementation(
-        (_input: string | URL | Request, _init?: RequestInit): Promise<Response> => {
-          return Promise.resolve({
-            ok: true,
-            json: vi.fn().mockResolvedValue({ token: MockData.getJwt() }),
-          } as unknown as Response);
-        },
-      );
 
     const children = <div data-testid={testId}>{childText}</div>;
     render(
