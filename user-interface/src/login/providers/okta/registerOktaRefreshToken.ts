e5298b4a4f447e1360dac29587996c035f3b4890
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-07-15 12:38:45 -0400

    Added logic to dedupe before hooks to Okta refresh
    [skip ci]
    
    Jira ticket: CAMS-401

--- user-interface/src/login/providers/okta/registerOktaRefreshToken.ts
+++ user-interface/src/login/providers/okta/registerOktaRefreshToken.ts
@@ -1,21 +1,14 @@
+import OktaAuth from '@okta/okta-auth-js';
 import { addApiBeforeHook } from '@/lib/hooks/UseApi';
 import LocalStorage from '@/lib/utils/local-storage';
 import { registerSemaphore, useSemaphore } from '@/lib/utils/semaphore';
-import { getLoginConfigurationFromEnv } from '@/login/login-library';
-import OktaAuth, { OktaAuthOptions } from '@okta/okta-auth-js';
 
 const SAFE_LIMIT = 30; // Seconds
 
 const OKTA_TOKEN_REFRESH = 'OKTA_TOKEN_REFRESH';
 registerSemaphore(OKTA_TOKEN_REFRESH);
 
-export function registerOktaRefreshToken() {
-  const config = getLoginConfigurationFromEnv<OktaAuthOptions>();
-  const oktaAuth = new OktaAuth(config);
-  // const { oktaAuth, authState } = useOktaAuth();
-
-  // TODO: How to not repeatedly add the hook to the API?
-  // Use OktaAuth() and `oktaAuth.authStateManager.getAuthState()` to NOT use the React component lifecycle so we can register ONCE on app load?
+export function registerOktaRefreshToken(oktaAuth: OktaAuth) {
   addApiBeforeHook(async () => {
     const now = Math.floor(Date.now() / 1000);
     const session = LocalStorage.getSession();
