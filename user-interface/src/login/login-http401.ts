da421aaa31720823ba211cf3273c1158b25c860b
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-07-16 10:45:44 -0400

    Replace react router with vanilla browser redirection
    Jira ticket: CAMS-401

--- user-interface/src/login/login-http401.ts
+++ user-interface/src/login/login-http401.ts
@@ -1,9 +1,10 @@
 import { isCamsApi } from '@/configuration/apiConfiguration';
-import { useNavigate } from 'react-router-dom';
+import { LOGOUT_PATH } from './login-library';
 
 export async function http401Hook(response: Response) {
   if (response.status === 401 && isCamsApi(response.url)) {
-    const navigate = useNavigate();
-    navigate('/logout');
+    const { host, protocol } = window.location;
+    const logoutUri = protocol + '//' + host + LOGOUT_PATH;
+    window.location.assign(logoutUri);
   }
 }
