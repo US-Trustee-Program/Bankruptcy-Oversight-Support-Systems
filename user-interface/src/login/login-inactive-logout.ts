27400894cc9ffdc960f9762662e9bd91a06fa0a6
Fritz Madden <fmadden@flexion.us>
2024-07-22 12:06:32 -0600

    Added keypress event listener and externalized config
    Jira ticket: CAMS-402
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,
    Co-authored-by: Sam Johnson <171059116+samjohnsonflexion@users.noreply.github.com>

--- user-interface/src/login/login-inactive-logout.ts
+++ user-interface/src/login/login-inactive-logout.ts
@@ -1,8 +1,9 @@
 import LocalStorage from '@/lib/utils/local-storage';
 import { LOGOUT_PATH } from './login-library';
 
-const POLLING_INTERVAL = 20000; // milliseconds
-const TIMEOUT = 15000;
+const POLLING_INTERVAL = 60000; // milliseconds
+const TIMEOUT_MINUTES = import.meta.env['CAMS_INACTIVE_TIMEOUT'] ?? 30;
+const TIMEOUT = TIMEOUT_MINUTES * 60 * 1000;
 
 export function resetLastInteraction() {
   LocalStorage.setLastInteraction(Date.now());
@@ -12,6 +13,9 @@ export function checkForInactivity() {
   const now = Date.now();
   const lastInteraction = LocalStorage.getLastInteraction();
   if (!lastInteraction || now > lastInteraction + TIMEOUT) {
+    document.body.removeEventListener('click', resetLastInteraction);
+    document.body.removeEventListener('keypress', resetLastInteraction);
+
     const { host, protocol } = window.location;
     const logoutUri = protocol + '//' + host + LOGOUT_PATH;
     window.location.assign(logoutUri);
@@ -21,4 +25,5 @@ export function checkForInactivity() {
 export function initializeInactiveLogout() {
   setInterval(checkForInactivity, POLLING_INTERVAL);
   document.body.addEventListener('click', resetLastInteraction);
+  document.body.addEventListener('keypress', resetLastInteraction);
 }
