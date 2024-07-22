dd41f3be56ccf03106fdae13c75a039a43e946c8
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-07-22 12:57:22 -0400

    Implement timout logic
    Jira ticket: CAMS-402

--- user-interface/src/login/Login.tsx
+++ user-interface/src/login/Login.tsx
@@ -17,6 +17,7 @@ import { CamsSession, CamsUser } from '@common/cams/session';
 import { MockData } from '@common/cams/test-utilities/mock-data';
 import { addApiAfterHook } from '@/lib/models/api';
 import { http401Hook } from './login-http401';
+import { initializeInactiveLogout } from './login-inactive-logout';
 
 export type LoginProps = PropsWithChildren & {
   provider?: LoginProvider;
@@ -37,6 +38,7 @@ export function Login(props: LoginProps): React.ReactNode {
   }
 
   addApiAfterHook(http401Hook);
+  initializeInactiveLogout();
 
   const session: CamsSession | null = LocalStorage.getSession();
   if (session) {
--- user-interface/src/login/login-inactive-logout.ts
+++ user-interface/src/login/login-inactive-logout.ts
@@ -1,10 +1,24 @@
+import LocalStorage from '@/lib/utils/local-storage';
 import { LOGOUT_PATH } from './login-library';
 
-export async function inactiveLogoutHook() {
-  const someTimoutCondition = true;
-  if (someTimoutCondition) {
+const POLLING_INTERVAL = 20000; // milliseconds
+const TIMEOUT = 15000;
+
+export function resetLastInteraction() {
+  LocalStorage.setLastInteraction(Date.now());
+}
+
+export function checkForInactivity() {
+  const now = Date.now();
+  const lastInteraction = LocalStorage.getLastInteraction();
+  if (!lastInteraction || now > lastInteraction + TIMEOUT) {
     const { host, protocol } = window.location;
     const logoutUri = protocol + '//' + host + LOGOUT_PATH;
     window.location.assign(logoutUri);
   }
+}
+
+export function initializeInactiveLogout() {
+  setInterval(checkForInactivity, POLLING_INTERVAL);
+  document.body.addEventListener('click', resetLastInteraction);
 }
