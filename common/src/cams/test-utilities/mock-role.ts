fb4e7550f5cf9edaa222c06335d1ac9a4ce028ae
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-07-29 10:47:33 -0400

    Remove unused group from model
    Jira ticket: CAMS-406

--- common/src/cams/test-utilities/mock-role.ts
+++ common/src/cams/test-utilities/mock-role.ts
@@ -5,51 +5,43 @@ export type MockRole = {
   sub: string;
   label: string;
   user: CamsUser;
-  // groups: string[];
 };
 
-// TODO: Make some aliases to make managing these relationships easier.
 const offices = MockData.getOffices();
 
-const MANHATTAN = offices.filter((office) => office.courtDivisionCode === '081');
-const BUFFALO = offices.filter((office) => office.courtDivisionCode === '091');
+const MANHATTAN = offices.filter((office) => office.courtDivisionCode === '081')[0];
+const BUFFALO = offices.filter((office) => office.courtDivisionCode === '091')[0];
 
 export const usersWithRole: MockRole[] = [
   {
     sub: 'jpearson@fake.com',
     label: 'Jessica Pearson - Trial Attorney (Manhattan)',
-    user: { name: 'Jessica Pearson', roles: [CamsRole.TrialAttorney], offices: MANHATTAN },
-    // groups: ['USTP CAMS Trial Attorney', 'USTP CAMS Region 2 Office Manhattan'],
+    user: { name: 'Jessica Pearson', roles: [CamsRole.TrialAttorney], offices: [MANHATTAN] },
   },
   {
     sub: 'jmccoy@fake.com',
     label: 'Jack McCoy - Trial Attorney (Manhattan)',
-    user: { name: 'Jack McCoy', roles: [CamsRole.TrialAttorney], offices: MANHATTAN },
-    // groups: ['USTP CAMS Trial Attorney', 'USTP CAMS Region 2 Office Manhattan'],
+    user: { name: 'Jack McCoy', roles: [CamsRole.TrialAttorney], offices: [MANHATTAN] },
   },
   {
     sub: 'sgoodman@fake.com',
     label: 'Saul Goodman - Trial Attorney (Buffalo)',
-    user: { name: 'Saul Goodman', roles: [CamsRole.TrialAttorney], offices: BUFFALO },
-    // groups: ['USTP CAMS Trial Attorney', 'USTP CAMS Region 2 Office Buffalo'],
+    user: { name: 'Saul Goodman', roles: [CamsRole.TrialAttorney], offices: [BUFFALO] },
   },
   {
     sub: 'rzane@fake.com',
     label: 'Rachel Zane - Trial Attorney (Buffalo)',
-    user: { name: 'Rachel Zane', roles: [CamsRole.TrialAttorney], offices: BUFFALO },
-    // groups: ['USTP CAMS Trial Attorney', 'USTP CAMS Region 2 Office Buffalo'],
+    user: { name: 'Rachel Zane', roles: [CamsRole.TrialAttorney], offices: [BUFFALO] },
   },
   {
     sub: 'paralegal',
     label: 'Bert - Paralegal',
-    user: { name: 'Bert', roles: [CamsRole.CaseAssignmentManager], offices: MANHATTAN },
-    // groups: ['USTP CAMS Case Assignment Manager', 'USTP CAMS Region 2 Office Manhattan'],
+    user: { name: 'Bert', roles: [CamsRole.CaseAssignmentManager], offices: [MANHATTAN] },
   },
   {
     sub: 'aust',
     label: 'Charlie - Assistant US Trustee',
-    user: { name: 'Charlie', roles: [], offices: MANHATTAN },
-    // groups: ['USTP CAMS Region 2 Office Manhattan'],
+    user: { name: 'Charlie', roles: [], offices: [MANHATTAN] },
   },
   {
     sub: 'user@fake.com',
@@ -59,10 +51,5 @@ export const usersWithRole: MockRole[] = [
       roles: [CamsRole.CaseAssignmentManager, CamsRole.TrialAttorney],
       offices: [],
     },
-    // groups: [
-    //   'USTP CAMS Trial Attorney',
-    //   'USTP CAMS Case Assignment Manager',
-    //   'USTP CAMS Region 2 Office Manhattan',
-    // ],
   },
 ];
