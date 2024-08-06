0ae1fe46d581f9c705f252bbb1a160267e904404
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-08-06 15:27:47 -0400

    WIP - Refactoring attorney model
    [skip ci]
    
    Jira ticket: CAMS-48
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,
    Co-authored-by: Sam Johnson <171059116+samjohnsonflexion@users.noreply.github.com>

--- user-interface/src/lib/models/attorneys-api.ts
+++ user-interface/src/lib/models/attorneys-api.ts
@@ -1,22 +1,12 @@
 import Api from './api';
 import { AttorneyListResponseData } from '@/case-assignment/AssignAttorneyModal';
 import AttorneysMockApi from './attorneys-mock.api.cases';
-import { Attorney } from '../type-declarations/attorneys';
 
 export default class AttorneysApi {
   public static async getAttorneys() {
     const api = import.meta.env['CAMS_PA11Y'] === 'true' ? AttorneysMockApi : Api;
     return api.list('/attorneys').then((response) => {
-      const data = (response.body as AttorneyListResponseData).attorneyList;
-      const attorneys = data.map((atty) => {
-        const attorney = new Attorney(atty.firstName, atty.lastName, atty.office);
-        if (atty.middleName !== undefined) attorney.middleName = atty.middleName;
-        if (atty.generation !== undefined) attorney.generation = atty.generation;
-        if (atty.caseLoad !== undefined) attorney.caseLoad = atty.caseLoad;
-        if (atty.userId !== undefined) attorney.userId = atty.userId;
-        if (atty.userName !== undefined) attorney.userName = atty.userName;
-        return attorney;
-      });
+      const attorneys = (response.body as AttorneyListResponseData).attorneyList;
       return attorneys;
     });
   }
