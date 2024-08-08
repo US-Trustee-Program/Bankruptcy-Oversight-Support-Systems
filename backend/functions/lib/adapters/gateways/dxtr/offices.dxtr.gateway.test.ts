bd62c55fb2ec356ea0a4ffa1e63a09b1a9649b5e
Arthur Morrow <amorrow@flexion.us>
2024-08-08 16:41:55 -0400

    coverage for offices.dxtr.gateway
    Jira ticket: CAMS-48
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

--- backend/functions/lib/adapters/gateways/dxtr/offices.dxtr.gateway.test.ts
+++ backend/functions/lib/adapters/gateways/dxtr/offices.dxtr.gateway.test.ts
@@ -4,6 +4,7 @@ import { ApplicationContext } from '../../types/basic';
 import { createMockApplicationContext } from '../../../testing/testing-utilities';
 import * as database from '../../utils/database';
 import { QueryResults } from '../../types/database';
+import { MANHATTAN, OFFICES } from '../../../../../../common/src/cams/test-utilities/offices.mock';
 
 describe('offices gateway tests', () => {
   describe('getOffice tests', () => {
@@ -38,11 +39,11 @@ describe('offices gateway tests', () => {
       jest.clearAllMocks();
     });
 
-    test('', async () => {
+    test('Should get Offices', async () => {
       const mockResults: QueryResults = {
         success: true,
         results: {
-          recordset: [],
+          recordset: OFFICES,
         },
         message: '',
       };
@@ -53,7 +54,7 @@ describe('offices gateway tests', () => {
       const gateway = new OfficesDxtrGateway();
 
       const offices = await gateway.getOffices(applicationContext);
-      expect(offices).toEqual([]);
+      expect(offices).toEqual(OFFICES);
     });
 
     test('should throw error when success is false', async () => {
@@ -70,6 +71,70 @@ describe('offices gateway tests', () => {
 
       await expect(async () => {
         await gateway.getOffices(applicationContext);
+      }).rejects.toThrow('Some expected SQL error.');
+    });
+  });
+
+  describe('getOfficeByCourtIdAndOfficeCode test', () => {
+    let applicationContext: ApplicationContext;
+    const querySpy = jest.spyOn(database, 'executeQuery');
+
+    beforeEach(async () => {
+      applicationContext = await createMockApplicationContext({ DATABASE_MOCK: 'true' });
+      querySpy.mockImplementation(jest.fn());
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    test('Should get office by office code and courtid', async () => {
+      const mockResults: QueryResults = {
+        success: true,
+        results: {
+          recordset: [MANHATTAN],
+        },
+        message: '',
+      };
+      querySpy.mockImplementation(async () => {
+        return Promise.resolve(mockResults);
+      });
+
+      const gateway = new OfficesDxtrGateway();
+
+      const offices = await gateway.getOfficeByCourtIdAndOfficeCode(
+        applicationContext,
+        '0081',
+        '1',
+      );
+      expect(offices).toEqual(MANHATTAN);
+    });
+
+    test('should throw error when success is false', async () => {
+      const gateway = new OfficesDxtrGateway();
+      await expect(async () => {
+        await gateway.getOfficeByCourtIdAndOfficeCode(applicationContext, '081', '1');
+      }).rejects.toThrow('Invalid court id or office code supplied');
+
+      await expect(async () => {
+        await gateway.getOfficeByCourtIdAndOfficeCode(applicationContext, '0081', '01');
+      }).rejects.toThrow('Invalid court id or office code supplied');
+    });
+
+    test('should throw CamsError when success is false', async () => {
+      const mockResults: QueryResults = {
+        success: false,
+        results: {},
+        message: 'Some expected SQL error.',
+      };
+      querySpy.mockImplementation(async () => {
+        return Promise.resolve(mockResults);
+      });
+
+      const gateway = new OfficesDxtrGateway();
+
+      await expect(async () => {
+        await gateway.getOfficeByCourtIdAndOfficeCode(applicationContext, '0081', '1');
       }).rejects.toThrow('Some expected SQL error.');
     });
   });
