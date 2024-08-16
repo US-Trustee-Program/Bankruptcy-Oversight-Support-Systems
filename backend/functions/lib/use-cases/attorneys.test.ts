7a15d18644c33275b49cfb720a70c62ca1a79afd
Fritz Madden <fmadden@flexion.us>
2024-08-16 10:57:51 -0600

    Fixed all API broken tests
    Jira ticket: CAMS-418
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Sam Johnson <171059116+samjohnsonflexion@users.noreply.github.com>

--- backend/functions/lib/use-cases/attorneys.test.ts
+++ backend/functions/lib/use-cases/attorneys.test.ts
@@ -1,58 +1,35 @@
 import { ApplicationContext } from '../adapters/types/basic';
+// TODO: investigate the duplicates of ATTORNEYS
+import { ATTORNEYS } from '../../../../common/src/cams/test-utilities/attorneys.mock';
 import { createMockApplicationContext } from '../testing/testing-utilities';
 import AttorneysList from './attorneys';
 import { CaseAssignmentUseCase } from './case-assignment';
 
 describe('Test attorneys use-case', () => {
   test('Should use gateway passed to it in constructor', async () => {
-    const mockResult = {
-      success: true,
-      message: '',
-      count: 0,
-      body: {
-        attorneyList: [
-          {
-            id: 'testUserId',
-            name: 'Test User',
-          },
-        ],
-      },
-    };
+    const attorneyUsers = ATTORNEYS;
 
     const gateway = {
       getAttorneys: async (
         _applicationContext: ApplicationContext,
-        _fields: { officeId: string },
-      ) => mockResult,
+        _attorneyOtions: { officeId: string },
+      ) => attorneyUsers,
     };
 
     const mockContext = await createMockApplicationContext();
     const caseList = new AttorneysList(gateway);
     const results = await caseList.getAttorneyList(mockContext, {});
 
-    expect(results).toEqual(mockResult);
+    expect(results).toEqual(attorneyUsers);
   });
 
   test('should log errors when looking up attorney assignments', async () => {
-    const mockResult = {
-      success: true,
-      message: '',
-      count: 0,
-      body: {
-        attorneyList: [
-          {
-            id: 'testUserId',
-            name: 'Test User',
-          },
-        ],
-      },
-    };
-
+    const attorneyUsers = ATTORNEYS;
     const gateway = {
       getAttorneys: async (
         _applicationContext: ApplicationContext,
         _fields: { officeId: string },
-      ) => mockResult,
+      ) => attorneyUsers,
     };
 
     const mockContext = await createMockApplicationContext();
