43e1efebc07b501905551bf7f49b8eab4460f63f
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-08-21 14:56:52 -0400

    Externalize logic from StaffAssignmentRow
    * Externalized logic from the react component
    * Tested the externalized logic in isolation without needing to render the component
    * Added humble function to use in place of React useState hook.
    * Added test utility to handle boilerplate code to mock the humble function
    
    Jira ticket: CAMS-418

--- user-interface/src/lib/testing/testing-utilities.ts
+++ user-interface/src/lib/testing/testing-utilities.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { CamsRole } from '@common/cams/roles';
 import MockData from '@common/cams/test-utilities/mock-data';
 import LocalStorage from '../utils/local-storage';
 import { GlobalAlertRef } from '../components/cams/GlobalAlert/GlobalAlert';
 import * as globalAlertHook from '@/lib/hooks/UseGlobalAlert';
 import { CamsUser } from '@common/cams/users';
+import * as UseStateModule from '@/lib/hooks/UseState';
 
 async function waitFor(condition: () => boolean, timeout = 5000, interval = 50): Promise<void> {
   const startTime = Date.now();
@@ -47,11 +49,47 @@ function spyOnGlobalAlert() {
   return hookFunctions;
 }
 
+const useStateMock: any = (initialValue: any) => {
+  // Credit: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set#implementing_basic_set_operations
+  function difference(setA: Set<string>, setB: Set<string>) {
+    const _difference = new Set(setA);
+    for (const elem of setB) {
+      _difference.delete(elem);
+    }
+    return _difference as Set<string>;
+  }
+
+  let value = typeof initialValue === 'object' ? { ...initialValue } : initialValue;
+  return [
+    value,
+    <T = any>(newValue: T) => {
+      if (typeof value === 'object') {
+        const existingKeys = new Set([...Object.keys(value)]);
+        const newKeys = new Set([...Object.keys(value)]);
+        difference(existingKeys, newKeys).forEach((key) => {
+          delete value[key];
+        });
+        Object.keys(newValue as object).forEach((key) => {
+          value[key] = (newValue as Record<string, any>)[key];
+        });
+      } else {
+        value = newValue;
+      }
+    },
+  ];
+};
+
+function spyOnUseState() {
+  return vi.spyOn(UseStateModule, 'useState').mockImplementation(useStateMock);
+}
+
+export const urlRegex = /https?:\/\/.*\//;
 export const TestingUtilities = {
   waitFor,
   setUser,
   setUserWithRoles,
   spyOnGlobalAlert,
+  spyOnUseState,
 };
 
 export default TestingUtilities;
