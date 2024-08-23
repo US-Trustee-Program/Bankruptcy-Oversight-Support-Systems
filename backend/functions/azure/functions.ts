0892788122b6c145a2117f7ca599df114892e53e
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-08-23 11:59:44 -0400

    Map az request v4 header and query to CamsDict
    Jira ticket: CAMS-356

--- backend/functions/azure/functions.ts
+++ backend/functions/azure/functions.ts
@@ -1,15 +1,23 @@
 import { InvocationContext, HttpRequest } from '@azure/functions';
-import { CamsHttpMethod, CamsHttpRequest } from '../lib/adapters/types/http';
+import { CamsDict, CamsHttpMethod, CamsHttpRequest } from '../lib/adapters/types/http';
 import { MockData } from '../../../common/src/cams/test-utilities/mock-data';
 import { randomUUID } from 'node:crypto';
+
+function azureToCamsDict(it: Iterable<[string, string]>): CamsDict {
+  if (!it) return {};
+  return Array.from(it).reduce((acc, record) => {
+    acc[record[0]] = record[1];
+    return acc;
+  }, {} as CamsDict);
+}
 
 export function httpRequestToCamsHttpRequest(request?: HttpRequest): CamsHttpRequest {
   if (!request) throw new Error('Cannot map undefined request object.');
   return {
     method: request.method as CamsHttpMethod,
     url: request.url,
-    headers: JSON.parse(JSON.stringify(request.headers)),
-    query: JSON.parse(JSON.stringify(request.query)),
+    headers: azureToCamsDict(request.headers),
+    query: azureToCamsDict(request.query),
     params: request.params,
     body: request.body,
   };
