938798cba064568c53c004347c157d3b1b68d7a1
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-08-23 09:20:12 -0500

    WIP - Continue testing case assignment function
    [skip ci]
    
    Jira ticket: CAMS-356
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,
    Co-authored-by: Sam Johnson <171059116+samjohnsonflexion@users.noreply.github.com>

--- backend/functions/case-assignments/case.assignment.function.test.ts
+++ backend/functions/case-assignments/case.assignment.function.test.ts
@@ -1,8 +1,8 @@
 import { handler } from './case.assignment.function';
 // import { CaseAssignmentController } from '../lib/controllers/case-assignment/case.assignment.controller';
-// import * as httpResponseModule from '../lib/adapters/utils/http-response';
-// import { AssignmentError } from '../lib/use-cases/assignment.exception';
-// import { UnknownError } from '../lib/common-errors/unknown-error';
+import * as httpResponseModule from '../lib/adapters/utils/http-response';
+import { AssignmentError } from '../lib/use-cases/assignment.exception';
+import { UnknownError } from '../lib/common-errors/unknown-error';
 import ContextCreator from '../lib/adapters/utils/application-context-creator';
 // import { CaseAssignment } from '../../../common/src/cams/assignments';
 import { MockData } from '../../../common/src/cams/test-utilities/mock-data';
@@ -73,57 +73,57 @@ describe('Case Assignment Function Tests', () => {
     expect(response.jsonBody.body.length).toEqual(2);
   });
 
-  // test('handle any duplicate attorneys passed in the request, not create duplicate assignments', async () => {
-  //   const requestOverride = {
-  //     body: {
-  //       caseId: '081-67-89123',
-  //       attorneyList: ['Jane', 'Jane'],
-  //       role: 'TrialAttorney',
-  //     },
-  //   };
+  test('handle any duplicate attorneys passed in the request, not create duplicate assignments', async () => {
+    const requestOverride = {
+      body: {
+        caseId: '081-67-89123',
+        attorneyList: ['Jane', 'Jane'],
+        role: 'TrialAttorney',
+      },
+    };
 
-  //   const request = createMockAzureFunctionRequest({
-  //     ...defaultRequestProps,
-  //     ...requestOverride,
-  //   });
+    const request = createMockAzureFunctionRequest({
+      ...defaultRequestProps,
+      ...requestOverride,
+    });
 
-  //   const expectedResponse = {
-  //     success: true,
-  //     message: 'Trial attorney assignments created.',
-  //     count: 1,
-  //   };
+    const expectedResponse = {
+      success: true,
+      message: 'Trial attorney assignments created.',
+      count: 1,
+    };
 
-  //   await handler(request, context);
-  //   expect(context.res.body).toEqual(expect.objectContaining(expectedResponse));
-  //   expect(context.res.body.body.length).toEqual(1);
-  // });
+    const response = await handler(request, context);
+    expect(response.jsonBody).toEqual(expect.objectContaining(expectedResponse));
+    expect(response.jsonBody.body.length).toEqual(1);
+  });
 
-  // test('returns bad request 400 when a caseId is not passed in the request', async () => {
-  //   const requestOverride = {
-  //     body: {
-  //       caseId: '',
-  //       attorneyList: ['Bob', 'Denise'],
-  //       role: 'TrialAttorney',
-  //     },
-  //   };
+  test('returns bad request 400 when a caseId is not passed in the request', async () => {
+    const requestOverride = {
+      body: {
+        caseId: '',
+        attorneyList: ['Bob', 'Denise'],
+        role: 'TrialAttorney',
+      },
+    };
 
-  //   const request = createMockAzureFunctionRequest({
-  //     ...defaultRequestProps,
-  //     ...requestOverride,
-  //   });
+    const request = createMockAzureFunctionRequest({
+      ...defaultRequestProps,
+      ...requestOverride,
+    });
 
-  //   const expectedResponse = {
-  //     message: 'Required parameter(s) caseId is/are absent.',
-  //     success: false,
-  //   };
+    const expectedResponse = {
+      message: 'Required parameter(s) caseId is/are absent.',
+      success: false,
+    };
 
-  //   const httpErrorSpy = jest.spyOn(httpResponseModule, 'httpError');
-  //   await handler(request, context);
-  //   expect(context.res.body).toEqual(expectedResponse);
-  //   expect(context.res.statusCode).toEqual(400);
-  //   expect(httpErrorSpy).toHaveBeenCalledWith(expect.any(AssignmentError));
-  //   expect(httpErrorSpy).not.toHaveBeenCalledWith(expect.any(UnknownError));
-  // });
+    const httpErrorSpy = jest.spyOn(httpResponseModule, 'httpError');
+    const response = await handler(request, context);
+    expect(response.jsonBody).toEqual(expectedResponse);
+    expect(response.status).toEqual(400);
+    expect(httpErrorSpy).toHaveBeenCalledWith(expect.any(AssignmentError));
+    expect(httpErrorSpy).not.toHaveBeenCalledWith(expect.any(UnknownError));
+  });
 
   // test('returns bad request 400 when a caseId is invalid format', async () => {
   //   const requestOverride = {
@@ -142,8 +142,8 @@ describe('Case Assignment Function Tests', () => {
   //   const expectedResponse = { message: 'caseId must be formatted like 01-12345.', success: false };
 
   //   const httpErrorSpy = jest.spyOn(httpResponseModule, 'httpError');
-  //   await handler(request, context);
-  //   expect(context.res.body).toEqual(expectedResponse);
+  //   const response = await handler(request, context);
+  //   expect(response.jsonBody).toEqual(expectedResponse);
   //   expect(context.res.statusCode).toEqual(400);
   //   expect(httpErrorSpy).toHaveBeenCalledWith(expect.any(AssignmentError));
   //   expect(httpErrorSpy).not.toHaveBeenCalledWith(expect.any(UnknownError));
@@ -170,8 +170,8 @@ describe('Case Assignment Function Tests', () => {
   //   };
 
   //   const httpErrorSpy = jest.spyOn(httpResponseModule, 'httpError');
-  //   await handler(request, context);
-  //   expect(context.res.body).toEqual(expectedResponse);
+  //   const response = await handler(request, context);
+  //   expect(response.jsonBody).toEqual(expectedResponse);
   //   expect(context.res.statusCode).toEqual(400);
   //   expect(httpErrorSpy).toHaveBeenCalledWith(expect.any(AssignmentError));
   //   expect(httpErrorSpy).not.toHaveBeenCalledWith(expect.any(UnknownError));
@@ -198,8 +198,8 @@ describe('Case Assignment Function Tests', () => {
   //   };
 
   //   const httpErrorSpy = jest.spyOn(httpResponseModule, 'httpError');
-  //   await handler(request, context);
-  //   expect(context.res.body).toEqual(expectedResponse);
+  //   const response = await handler(request, context);
+  //   expect(response.jsonBody).toEqual(expectedResponse);
   //   expect(context.res.statusCode).toEqual(400);
   //   expect(httpErrorSpy).toHaveBeenCalledWith(expect.any(AssignmentError));
   //   expect(httpErrorSpy).not.toHaveBeenCalledWith(expect.any(UnknownError));
@@ -227,11 +227,11 @@ describe('Case Assignment Function Tests', () => {
   //   });
 
   //   const httpErrorSpy = jest.spyOn(httpResponseModule, 'httpError');
-  //   await handler(request, context);
+  //   const response = await handler(request, context);
 
   //   expect(httpErrorSpy).toHaveBeenCalled();
   //   expect(context.res.statusCode).toEqual(500);
-  //   expect(context.res.body.message).toEqual('Unknown error');
+  //   expect(response.jsonBody.message).toEqual('Unknown error');
   //   expect(httpErrorSpy).toHaveBeenCalledWith(expect.any(UnknownError));
   // });
 
@@ -251,7 +251,7 @@ describe('Case Assignment Function Tests', () => {
   //     Object.getPrototypeOf(assignmentController),
   //     'createTrialAttorneyAssignments',
   //   );
-  //   await handler(request, context);
+  //   const response = await handler(request, context);
 
   //   expect(createAssignmentRequestSpy).toHaveBeenCalledWith(expect.objectContaining({ caseId }));
   // });
@@ -278,7 +278,7 @@ describe('Case Assignment Function Tests', () => {
   //   const getAssignmentRequestSpy = jest
   //     .spyOn(Object.getPrototypeOf(assignmentController), 'getTrialAttorneyAssignments')
   //     .mockReturnValue(assignments);
-  //   await handler(request, context);
+  //   const response = await handler(request, context);
 
   //   expect(getAssignmentRequestSpy).toHaveBeenCalledWith(caseId);
   //   expect(getAssignmentRequestSpy).toHaveReturnedWith(assignments);
