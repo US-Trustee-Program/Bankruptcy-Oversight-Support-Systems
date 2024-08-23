f133955cde04c934b90b9ff7b828839ba00d9c9e
samjohnson <171059116+samjohnsonflexion@users.noreply.github.com>
2024-08-23 12:09:44 -0500

    Update mock-oauth2.function to v4
    Jira ticket: CAMS-356

--- backend/functions/oauth2/mock-oauth2.function.ts
+++ backend/functions/oauth2/mock-oauth2.function.ts
@@ -1,23 +1,30 @@
-import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
 import { httpError, httpSuccess } from '../lib/adapters/utils/http-response';
 import ContextCreator from '../lib/adapters/utils/application-context-creator';
 import { mockAuthentication } from '../lib/testing/mock-gateways/mock-oauth2-gateway';
 
-const httpTrigger: AzureFunction = async function (
-  functionContext: Context,
+export async function handler(
   request: HttpRequest,
-): Promise<void> {
+  functionContext: InvocationContext,
+): Promise<HttpResponseInit> {
   const applicationContext = await ContextCreator.applicationContextCreator(
     functionContext,
     request,
   );
   try {
     const token = await mockAuthentication(applicationContext);
-    functionContext.res = httpSuccess({ token });
+    return httpSuccess({ token });
   } catch (camsError) {
     applicationContext.logger.camsError(camsError);
-    functionContext.res = httpError(camsError);
+    return httpError(camsError);
   }
-};
+}
 
-export default httpTrigger;
+app.http('handler', {
+  methods: ['POST'],
+  authLevel: 'anonymous',
+  handler,
+  route: 'oauth2/default',
+});
+
+export default handler;
