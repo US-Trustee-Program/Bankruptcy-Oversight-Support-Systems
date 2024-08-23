5bdcc026499f161b65fba8678f07d01c0111dad4
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-08-23 14:11:12 -0400

    Lift cases function from v3 to v4
    Jira ticket: CAMS-356

--- backend/functions/cases/cases.function.test.ts
+++ backend/functions/cases/cases.function.test.ts
@@ -1,10 +1,9 @@
 import { INTERNAL_SERVER_ERROR } from '../lib/common-errors/constants';
-import httpTrigger from './cases.function';
 import * as httpResponseModule from '../lib/adapters/utils/http-response';
 import { UnknownError } from '../lib/common-errors/unknown-error';
 import { CamsError } from '../lib/common-errors/cams-error';
-import clearAllMocks = jest.clearAllMocks;
 import { createMockAzureFunctionRequest, createMockAzureFunctionContext } from '../azure/functions';
+import handler from './cases.function';
 
 jest.mock('../lib/controllers/cases/cases.controller', () => {
   return {
@@ -22,44 +21,44 @@ jest.mock('../lib/controllers/cases/cases.controller', () => {
 });
 
 describe('Mocking CasesController to get error handling', () => {
-  const request = createMockAzureFunctionRequest();
   const context = createMockAzureFunctionContext();
 
   beforeEach(async () => {
-    clearAllMocks();
+    jest.clearAllMocks();
   });
 
   test('error should be properly handled if httpTrigger throws an error', async () => {
     const httpErrorSpy = jest.spyOn(httpResponseModule, 'httpError');
-    await httpTrigger(context, request);
+    const request = createMockAzureFunctionRequest();
+    const response = await handler(request, context);
 
-    expect(context.res.statusCode).toEqual(INTERNAL_SERVER_ERROR);
-    expect(context.res.body.message).toEqual('Unknown error');
+    expect(response.status).toEqual(INTERNAL_SERVER_ERROR);
+    expect(response.jsonBody.message).toEqual('Unknown error');
     expect(httpErrorSpy).toHaveBeenCalledWith(expect.any(UnknownError));
   });
 
   // TODO rethink how to trigger a CAMS error for this test
   test('should call httpError if a CamsError is caught getting a case', async () => {
-    const requestOverride = {
-      ...request,
+    const request = createMockAzureFunctionRequest({
       params: { caseId: '00000' },
-    };
+    });
 
     const httpErrorSpy = jest.spyOn(httpResponseModule, 'httpError');
-    await httpTrigger(context, requestOverride);
+    const response = await handler(request, context);
 
-    expect(context.res.statusCode).toEqual(INTERNAL_SERVER_ERROR);
-    expect(context.res.body.message).toEqual('Unknown CAMS Error');
+    expect(response.status).toEqual(INTERNAL_SERVER_ERROR);
+    expect(response.jsonBody.message).toEqual('Unknown CAMS Error');
     expect(httpErrorSpy).toHaveBeenCalledWith(expect.any(CamsError));
     expect(httpErrorSpy).not.toHaveBeenCalledWith(expect.any(UnknownError));
   });
 
   test('should call httpError if a CamsError is caught getting all cases', async () => {
     const httpErrorSpy = jest.spyOn(httpResponseModule, 'httpError');
-    await httpTrigger(context, request);
+    const request = createMockAzureFunctionRequest();
+    const response = await handler(request, context);
 
-    expect(context.res.statusCode).toEqual(INTERNAL_SERVER_ERROR);
-    expect(context.res.body.message).toEqual('Unknown error');
+    expect(response.status).toEqual(INTERNAL_SERVER_ERROR);
+    expect(response.jsonBody.message).toEqual('Unknown error');
     expect(httpErrorSpy).toHaveBeenCalledWith(expect.any(UnknownError));
   });
 });
