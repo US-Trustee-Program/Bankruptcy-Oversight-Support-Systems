1c8d3a6aa50d7cdc4157c6a4440f8c5c7b2e07a4
Arthur Morrow <amorrow@flexion.us>
2024-08-28 13:45:13 -0400

    update case-summary function/controller to use camsHttpResponse
    [skip ci]
    
    Jira ticket: CAMS-356

--- backend/functions/case-summary/case-summary.function.test.ts
+++ backend/functions/case-summary/case-summary.function.test.ts
@@ -1,15 +1,14 @@
 import { InvocationContext } from '@azure/functions';
 import { CaseDetail } from '../../../common/src/cams/cases';
 import { MockData } from '../../../common/src/cams/test-utilities/mock-data';
-import { createMockAzureFunctionRequest } from '../azure/functions';
 import { NotFoundError } from '../lib/common-errors/not-found-error';
 import { CaseSummaryController } from '../lib/controllers/case-summary/case-summary.controller';
-import { CamsResponse } from '../lib/controllers/controller-types';
 import handler from './case-summary.function';
 import ContextCreator from '../azure/application-context-creator';
 import { MANHATTAN } from '../../../common/src/cams/test-utilities/offices.mock';
 import { CamsRole } from '../../../common/src/cams/roles';
 import * as httpResponseModule from '../lib/adapters/utils/http-response';
+import { buildTestResponseSuccess, createMockAzureFunctionRequest } from '../azure/testing-helpers';
 
 describe('Case summary function', () => {
   jest.spyOn(ContextCreator, 'getApplicationContextSession').mockResolvedValue(
@@ -37,23 +36,21 @@ describe('Case summary function', () => {
 
   test('Should return case summary for an existing case ID', async () => {
     const caseDetail: CaseDetail = MockData.getCaseDetail();
+    const body = caseDetail;
 
-    const expectedResponse: CamsResponse<CaseDetail> = {
-      success: true,
-      body: caseDetail,
-    };
+    const { azureHttpResponse } = buildTestResponseSuccess(body);
+
     const request = {
       ...baseRequest,
       params: {
-        caseId: '000-00-00000',
+        caseId: caseDetail.caseId,
       },
     };
-    jest
-      .spyOn(CaseSummaryController.prototype, 'getCaseSummary')
-      .mockResolvedValue(expectedResponse);
+    jest.spyOn(CaseSummaryController.prototype, 'getCaseSummary').mockResolvedValue({ body });
 
     const response = await handler(request, context);
-    expect(response.jsonBody).toEqual(expectedResponse);
+    console.log('REsponse:    ', response);
+    expect(response).toEqual(azureHttpResponse);
   });
 
   test('Should return an error response for', async () => {
