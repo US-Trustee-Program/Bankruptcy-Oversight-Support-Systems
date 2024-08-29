f35ddc794c02bd83b020a4068f954ec780c820ee
Arthur Morrow <amorrow@flexion.us>
2024-08-29 13:02:16 -0400

    updated me function and tests with corrects reponse type
    Jira ticket: CAMS-356

--- backend/functions/me/me.function.test.ts
+++ backend/functions/me/me.function.test.ts
@@ -1,9 +1,14 @@
-import { createMockAzureFunctionRequest } from '../azure/testing-helpers';
+import {
+  buildTestResponseError,
+  buildTestResponseSuccess,
+  createMockAzureFunctionRequest,
+} from '../azure/testing-helpers';
 import ContextCreator from '../azure/application-context-creator';
 import MockData from '../../../common/src/cams/test-utilities/mock-data';
 import { ForbiddenError } from '../lib/common-errors/forbidden-error';
 import handler from '../me/me.function';
 import { InvocationContext } from '@azure/functions';
+import { CamsSession } from '../../../common/src/cams/session';
 
 describe('me Function test', () => {
   const context = new InvocationContext({
@@ -16,22 +21,18 @@ describe('me Function test', () => {
   test('should set successful response', async () => {
     const camsSession = MockData.getCamsSession();
     jest.spyOn(ContextCreator, 'getApplicationContextSession').mockResolvedValue(camsSession);
-
+    const { azureHttpResponse } = buildTestResponseSuccess<CamsSession>({ data: camsSession });
     const response = await handler(request, context);
 
-    expect(response.jsonBody).toEqual(camsSession);
+    expect(response).toMatchObject(azureHttpResponse);
   });
 
   test('should handle an error response', async () => {
     const error = new ForbiddenError('FUNCTION_TEST');
     jest.spyOn(ContextCreator, 'getApplicationContextSession').mockRejectedValue(error);
-    const expectedResponseBody = {
-      success: false,
-      message: error.message,
-    };
-
+    const { azureHttpResponse } = buildTestResponseError(error);
     const response = await handler(request, context);
 
-    expect(response.jsonBody).toEqual(expectedResponseBody);
+    expect(response).toEqual(azureHttpResponse);
   });
 });
