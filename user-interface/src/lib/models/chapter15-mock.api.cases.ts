15640b07086ade9501c0507abbfce375b56993fc
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-08-30 11:59:29 -0400

    Replace chapter15 mock API with MockApi2
    [skip ci]
    
    Jira ticket: CAMS-356

--- user-interface/src/lib/models/chapter15-mock.api.cases.ts
+++ user-interface/src/lib/models/chapter15-mock.api.cases.ts
@@ -10,53 +10,53 @@ import { SUPERUSER } from '@common/cams/test-utilities/mock-user';
 import { ResourceActions } from '../../../../common/dist/cams/actions';
 
 export default class Chapter15MockApi extends Api {
-  static caseList = [
-    {
-      caseId: '101-23-44463',
-      caseTitle: 'Flo Esterly and Neas Van Sampson',
-      dateFiled: '2023-05-04',
-    },
-    {
-      caseId: '101-23-44462',
-      caseTitle: 'Bridget Maldonado',
-      dateFiled: '2023-04-14',
-    },
-    {
-      caseId: '101-23-44461',
-      caseTitle: 'Talia Torres and Tylor Stevenson',
-      dateFiled: '2023-04-04',
-    },
-    {
-      caseId: '101-23-44460',
-      caseTitle: 'Asia Hodges',
-      dateFiled: '2023-03-01',
-    },
-    {
-      caseId: '101-23-44459',
-      caseTitle: 'Marilyn Lawson',
-      dateFiled: '2023-02-14',
-    },
-    {
-      caseId: '101-23-44458',
-      caseTitle: 'April Pierce and Leah Pierce',
-      dateFiled: '2023-02-04',
-    },
-    {
-      caseId: '101-23-44457',
-      caseTitle: 'Corinne Gordon',
-      dateFiled: '2023-01-14',
-    },
-    {
-      caseId: '101-23-44456',
-      caseTitle: 'Marilyn Lang and Rudy Bryant',
-      dateFiled: '2023-01-04',
-    },
-    {
-      caseId: '101-23-44455',
-      caseTitle: 'Justin Long and Michael Cera',
-      dateFiled: '2023-02-07',
-    },
-  ];
+  // static caseList = [
+  //   {
+  //     caseId: '101-23-44463',
+  //     caseTitle: 'Flo Esterly and Neas Van Sampson',
+  //     dateFiled: '2023-05-04',
+  //   },
+  //   {
+  //     caseId: '101-23-44462',
+  //     caseTitle: 'Bridget Maldonado',
+  //     dateFiled: '2023-04-14',
+  //   },
+  //   {
+  //     caseId: '101-23-44461',
+  //     caseTitle: 'Talia Torres and Tylor Stevenson',
+  //     dateFiled: '2023-04-04',
+  //   },
+  //   {
+  //     caseId: '101-23-44460',
+  //     caseTitle: 'Asia Hodges',
+  //     dateFiled: '2023-03-01',
+  //   },
+  //   {
+  //     caseId: '101-23-44459',
+  //     caseTitle: 'Marilyn Lawson',
+  //     dateFiled: '2023-02-14',
+  //   },
+  //   {
+  //     caseId: '101-23-44458',
+  //     caseTitle: 'April Pierce and Leah Pierce',
+  //     dateFiled: '2023-02-04',
+  //   },
+  //   {
+  //     caseId: '101-23-44457',
+  //     caseTitle: 'Corinne Gordon',
+  //     dateFiled: '2023-01-14',
+  //   },
+  //   {
+  //     caseId: '101-23-44456',
+  //     caseTitle: 'Marilyn Lang and Rudy Bryant',
+  //     dateFiled: '2023-01-04',
+  //   },
+  //   {
+  //     caseId: '101-23-44455',
+  //     caseTitle: 'Justin Long and Michael Cera',
+  //     dateFiled: '2023-02-07',
+  //   },
+  // ];
 
   static caseDocketEntries = MockData.buildArray(MockData.getDocketEntry, 5);
   static caseActions = [Actions.ManageAssignments];
