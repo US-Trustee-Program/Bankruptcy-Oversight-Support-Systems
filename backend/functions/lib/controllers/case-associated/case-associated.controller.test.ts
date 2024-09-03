c3e68a86a79f63eee778b485be3e2540e94d4e4b
Fritz Madden <fmadden@flexion.us>
2024-09-03 14:41:07 -0600

    Updated arguments passed to case-associated controller
    Removed request argument from case-associated controller and use-case.
    Grabbing caseId off the context.
    
    Jira ticket: CAMS-356

--- backend/functions/lib/controllers/case-associated/case-associated.controller.test.ts
+++ backend/functions/lib/controllers/case-associated/case-associated.controller.test.ts
@@ -25,8 +25,9 @@ describe('Test case-history controller', () => {
       .spyOn(CaseAssociatedUseCase.prototype, 'getAssociatedCases')
       .mockResolvedValue(associatedCases);
     const caseId = NORMAL_CASE_ID;
+    applicationContext.request.params.caseId = caseId;
     const controller = new CaseAssociatedController(applicationContext);
-    const result = await controller.getAssociatedCases(applicationContext, { caseId });
+    const result = await controller.getAssociatedCases(applicationContext);
     expect(result.body.data).toEqual(associatedCases);
   });
 
@@ -35,22 +36,22 @@ describe('Test case-history controller', () => {
       .spyOn(CaseAssociatedUseCase.prototype, 'getAssociatedCases')
       .mockRejectedValue(new NotFoundError('TEST'));
     const caseId = NOT_FOUND_ERROR_CASE_ID;
+    applicationContext.request.params.caseId = caseId;
     const controller = new CaseAssociatedController(applicationContext);
-    await expect(controller.getAssociatedCases(applicationContext, { caseId })).rejects.toThrow(
-      'Not found',
-    );
+    await expect(controller.getAssociatedCases(applicationContext)).rejects.toThrow('Not found');
   });
 
   test('should wrap unexpected errors with CamsError', async () => {
     const expectedMessage = 'Unknown error';
     const caseId = THROW_UNKNOWN_ERROR_CASE_ID;
+    applicationContext.request.params.caseId = caseId;
     const controller = new CaseAssociatedController(applicationContext);
     jest
       .spyOn(CaseAssociatedUseCase.prototype, 'getAssociatedCases')
       .mockImplementation(async () => {
         throw Error(expectedMessage);
       });
-    await expect(controller.getAssociatedCases(applicationContext, { caseId })).rejects.toThrow(
+    await expect(controller.getAssociatedCases(applicationContext)).rejects.toThrow(
       expectedMessage,
     );
   });
