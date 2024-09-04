f46e0888f55fe9cfe3fdac544760cc83433f19b9
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-09-04 17:21:55 -0400

    Remove unused mock requests for resolved values
    Jira ticket: CAMS-356
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

--- backend/functions/lib/controllers/case-history/case-history.controller.test.ts
+++ backend/functions/lib/controllers/case-history/case-history.controller.test.ts
@@ -2,14 +2,7 @@ import { CaseHistoryController } from './case-history.controller';
 import { CaseHistoryUseCase } from '../../use-cases/case-history/case-history';
 import { CASE_HISTORY } from '../../testing/mock-data/case-history.mock';
 import { createMockApplicationContext } from '../../testing/testing-utilities';
-import {
-  NORMAL_CASE_ID,
-  NOT_FOUND_ERROR_CASE_ID,
-  THROW_UNKNOWN_ERROR_CASE_ID,
-} from '../../testing/testing-constants';
 import { NotFoundError } from '../../common-errors/not-found-error';
-import { CamsHttpRequest } from '../../adapters/types/http';
-import { mockCamsHttpRequest } from '../../testing/mock-data/cams-http-request-helper';
 
 describe('Test case-history controller', () => {
   let applicationContext;
@@ -20,13 +13,8 @@ describe('Test case-history controller', () => {
 
   test('should return a case history when getCaseHistory is called', async () => {
     jest.spyOn(CaseHistoryUseCase.prototype, 'getCaseHistory').mockResolvedValue(CASE_HISTORY);
-    const caseId = NORMAL_CASE_ID;
-    const request: CamsHttpRequest = mockCamsHttpRequest({
-      params: { caseId },
-    });
-    applicationContext.request = request;
     const controller = new CaseHistoryController(applicationContext);
-    const result = await controller.getCaseHistory(applicationContext, request);
+    const result = await controller.getCaseHistory(applicationContext, applicationContext.request);
     expect(result.body['data']).toEqual(CASE_HISTORY);
   });
 
@@ -34,30 +22,20 @@ describe('Test case-history controller', () => {
     jest
       .spyOn(CaseHistoryUseCase.prototype, 'getCaseHistory')
       .mockRejectedValue(new NotFoundError('TEST'));
-    const caseId = NOT_FOUND_ERROR_CASE_ID;
-    const request: CamsHttpRequest = mockCamsHttpRequest({
-      params: { caseId },
-    });
-    applicationContext.request = request;
     const controller = new CaseHistoryController(applicationContext);
-    await expect(controller.getCaseHistory(applicationContext, request)).rejects.toThrow(
-      'Not found',
-    );
+    await expect(
+      controller.getCaseHistory(applicationContext, applicationContext.request),
+    ).rejects.toThrow('Not found');
   });
 
   test('should wrap unexpected errors with CamsError', async () => {
     const expectedMessage = 'Unknown error';
-    const caseId = THROW_UNKNOWN_ERROR_CASE_ID;
-    const request: CamsHttpRequest = mockCamsHttpRequest({
-      params: { caseId },
-    });
-    applicationContext.request = request;
     const controller = new CaseHistoryController(applicationContext);
     jest.spyOn(CaseHistoryUseCase.prototype, 'getCaseHistory').mockImplementation(async () => {
       throw Error(expectedMessage);
     });
-    await expect(controller.getCaseHistory(applicationContext, request)).rejects.toThrow(
-      expectedMessage,
-    );
+    await expect(
+      controller.getCaseHistory(applicationContext, applicationContext.request),
+    ).rejects.toThrow(expectedMessage);
   });
 });
