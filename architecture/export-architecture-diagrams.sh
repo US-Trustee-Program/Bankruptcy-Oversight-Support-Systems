7c9c5750ed1a5b4b081d1b0a166638a6d9ebc0fc
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-09-05 11:32:31 -0500

    Document usage of the architecture diagram script

--- architecture/export-architecture-diagrams.sh
+++ architecture/export-architecture-diagrams.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# This script is used to generate architecture diagrams from the dsl.
+# It relies on the Structurizr CLI being installed locally in a particular way.
+# https://docs.structurizr.com/cli/installation#local-installation
+# Following the instructions at the above link, ensuring that the $PATH variable
+# is updated will enable this script to execute properly.
+
+# Usage
+#   From the root directory, run the following command:
+#     sh ./architecture/export-architecture-diagrams.sh
+
 # Export the diagrams from our model
 structurizr.sh export -workspace architecture/cams.dsl -format mermaid
 
