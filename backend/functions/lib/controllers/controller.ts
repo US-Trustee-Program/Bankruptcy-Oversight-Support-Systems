7084361882782178cf14f35964cdb7f3e67d978d
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-09-06 14:19:44 -0400

    Replace arrow function style on CamsController interface
    Jira ticket: CAMS-356

--- backend/functions/lib/controllers/controller.ts
+++ backend/functions/lib/controllers/controller.ts
@@ -2,5 +2,5 @@ import { ApplicationContext } from '../adapters/types/basic';
 import { CamsHttpResponseInit } from '../adapters/utils/http-response';
 
 export interface CamsController {
-  handleRequest: (context: ApplicationContext) => Promise<CamsHttpResponseInit<object | undefined>>;
+  handleRequest(context: ApplicationContext): Promise<CamsHttpResponseInit<object | undefined>>;
 }
