956415ca48e84416430a93c7224e8851fa5297c6
Arthur Morrow <amorrow@flexion.us>
2024-09-06 15:30:01 -0400

    updated case-history to use new handleRequest
    Jira ticket: CAMS-356

--- backend/functions/lib/controllers/case-history/case-history.controller.ts
+++ backend/functions/lib/controllers/case-history/case-history.controller.ts
@@ -3,17 +3,17 @@ import { CaseHistoryUseCase } from '../../use-cases/case-history/case-history';
 import { CaseHistory } from '../../../../../common/src/cams/history';
 import { CamsHttpResponseInit, httpSuccess } from '../../adapters/utils/http-response';
 import { getCamsError } from '../../common-errors/error-utilities';
+import { CamsController } from '../controller';
 
 const MODULE_NAME = 'CASE-HISTORY-CONTROLLER';
 
-export class CaseHistoryController {
+export class CaseHistoryController implements CamsController {
   private readonly useCase: CaseHistoryUseCase;
 
   constructor(applicationContext: ApplicationContext) {
     this.useCase = new CaseHistoryUseCase(applicationContext);
   }
-
-  public async getCaseHistory(
+  public async handleRequest(
     context: ApplicationContext,
   ): Promise<CamsHttpResponseInit<CaseHistory[]>> {
     try {
