9c312db1af50f2d83ca653d269983b18ae18082b
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-09-16 17:29:30 -0400

    Start refactor of attorney model
    Jira ticket: CAMS-283
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

--- backend/functions/lib/use-cases/attorney.gateway.interface.d.ts
+++ backend/functions/lib/use-cases/attorney.gateway.interface.d.ts
@@ -3,4 +3,5 @@ import { AttorneyUser } from '../../../../common/src/cams/users';
 
 export interface AttorneyGatewayInterface {
   getAttorneys(applicationContext: ApplicationContext): Promise<Array<AttorneyUser>>;
+  getAttorneysByUstpOffice(applicationContext: ApplicationContext): Promise<Array<AttorneyUser>>;
 }
