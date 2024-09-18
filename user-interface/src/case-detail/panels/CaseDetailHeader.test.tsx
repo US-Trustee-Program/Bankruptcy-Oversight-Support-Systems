6dc40d1db9669efeef28c8bd6be6fd649d194265
Fritz Madden <fmadden@flexion.us>
2024-09-18 10:19:32 -0600

    Test coverage > 96% for CaseDetailHeader.tsx
    Jira ticket: CAMS-428

--- user-interface/src/case-detail/panels/CaseDetailHeader.test.tsx
+++ user-interface/src/case-detail/panels/CaseDetailHeader.test.tsx
@@ -1,22 +1,25 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import * as useLocationTracker from '@/lib/hooks/UseLocationTracker';
 import CaseDetailHeader from './CaseDetailHeader';
 import CaseDetailScreen from '../CaseDetailScreen';
 import { MockData } from '@common/cams/test-utilities/mock-data';
+import { ResourceActions } from '@common/cams/actions';
+import { CaseDetail } from '@common/cams/cases';
+
+function basicRender(caseDetail: ResourceActions<CaseDetail>, isLoading: boolean) {
+  render(
+    <BrowserRouter>
+      <CaseDetailHeader caseDetail={caseDetail} isLoading={isLoading} caseId={caseDetail.caseId} />
+    </BrowserRouter>,
+  );
+}
 
 describe('Case Detail Header tests', () => {
   const testCaseDetail = MockData.getCaseDetail();
 
   test('should render loading info when isLoading is true', () => {
-    render(
-      <BrowserRouter>
-        <CaseDetailHeader
-          caseDetail={testCaseDetail}
-          isLoading={true}
-          caseId={testCaseDetail.caseId}
-        />
-      </BrowserRouter>,
-    );
+    basicRender(testCaseDetail, true);
 
     const isLoadingH1 = screen.getByTestId('case-detail-heading');
     const isLoadingH2 = screen.getByTestId('loading-h2');
@@ -26,15 +29,7 @@ describe('Case Detail Header tests', () => {
   });
 
   test('should render case detail info when isLoading is false', () => {
-    render(
-      <BrowserRouter>
-        <CaseDetailHeader
-          caseDetail={testCaseDetail}
-          isLoading={false}
-          caseId={testCaseDetail.caseId}
-        />
-      </BrowserRouter>,
-    );
+    basicRender(testCaseDetail, false);
 
     const isLoadingH1 = screen.getByTestId('case-detail-heading');
     const isLoadingH2 = screen.getByTestId('case-detail-heading-title');
@@ -119,6 +114,40 @@ describe('Case Detail Header tests', () => {
         expect(fixedHeader).not.toBeInTheDocument();
       },
       { timeout: 5000 },
+    );
+  });
+
+  describe('back link tests', () => {
+    afterEach(() => {
+      vi.restoreAllMocks();
+    });
+
+    const backLinkTestCases = [
+      ['/my-cases', '', 'My Cases'],
+      ['/search', 'CAMS_WINDOW_012', 'Case Search'],
+      ['/staff-assignment', 'CAMS_WINDOW_012', 'Staff Assignment'],
+      ['/data-verification', 'CAMS_WINDOW_345', 'Data Verification'],
+      ['/foobar', 'CAMS_WINDOW_678', 'Case List'],
+    ];
+
+    test.each(backLinkTestCases)(
+      'back link should be setup to link back to Search in tab CAMS_WINDOW_012',
+      (previousLocation: string, homeTab: string, displayText: string) => {
+        vi.spyOn(useLocationTracker, 'default').mockImplementation(() => {
+          return {
+            previousLocation,
+            homeTab,
+            updateLocation: vi.fn(),
+          };
+        });
+
+        basicRender(testCaseDetail, false);
+
+        const backLink = document.querySelector('.back-button');
+        expect(backLink).toHaveAttribute('href', previousLocation);
+        expect(backLink).toHaveAttribute('target', homeTab);
+        expect(backLink).toHaveTextContent(`Back to ${displayText}`);
+      },
     );
   });
 });
