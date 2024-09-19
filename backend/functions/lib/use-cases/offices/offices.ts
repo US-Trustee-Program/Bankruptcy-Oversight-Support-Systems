eeea11f43ec674f8543e7c6095d0f142e2199e1f
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-09-19 15:00:58 -0400

    WIP - Add syncOfficeStaff to Offices use case
    Jira ticket: CAMS-283

--- backend/functions/lib/use-cases/offices/offices.ts
+++ backend/functions/lib/use-cases/offices/offices.ts
@@ -1,6 +1,8 @@
 import { OfficeDetails } from '../../../../../common/src/cams/courts';
+import { CamsRole } from '../../../../../common/src/cams/roles';
+import { CamsUserReference } from '../../../../../common/src/cams/users';
 import { ApplicationContext } from '../../adapters/types/basic';
-import { getOfficesGateway, getOfficesRepository } from '../../factory';
+import { getOfficesGateway, getUserGroupGateway, getOfficesRepository } from '../../factory';
 import { AttorneyUser } from '../../../../../common/src/cams/users';
 
 export class OfficesUseCase {
@@ -15,5 +17,50 @@ export class OfficesUseCase {
   ): Promise<AttorneyUser[]> {
     const repository = getOfficesRepository(context);
     return repository.getOfficeAttorneys(context, officeCode);
+  }
+
+  public async syncOfficeStaff(applicationContext: ApplicationContext): Promise<object> {
+    const results = {
+      userGroups: [],
+    };
+
+    const gateway = getUserGroupGateway(applicationContext);
+    // TODO: get repo gateway
+
+    const config = applicationContext.config.userGroupGatewayConfig;
+    const userGroups = await gateway.getUserGroups(config);
+    const userMap = new Map<string, CamsUserReference>();
+
+    // TODO: implement the filter.
+    const roleGroups = userGroups.filter(() => true);
+    for (const roleGroup of roleGroups) {
+      const users = await gateway.getUserGroupUsers(config, roleGroup);
+      // TODO: Set the appropriate role for the group.
+      const role = CamsRole.TrialAttorney;
+      for (const user of users) {
+        if (userMap.has(user.id)) {
+          userMap.get(user.id).roles.push(role);
+        } else {
+          user.roles.push(role);
+          userMap.set(user.id, user);
+        }
+      }
+      results.userGroups.push(roleGroup);
+    }
+
+    // TODO: implement the filter.
+    const officeGroups = userGroups.filter(() => true);
+    for (const officeGroup of officeGroups) {
+      // TODO: Map the group name to the office Id.
+      const officeId = '';
+      const users = await gateway.getUserGroupUsers(config, officeGroup);
+      for (const user of users) {
+        const userWithRoles = userMap.has(user.id) ? userMap.get(user.id) : user;
+        console.log(officeId, userWithRoles);
+        // TODO: Write user document to repo
+      }
+    }
+
+    return results;
   }
 }
