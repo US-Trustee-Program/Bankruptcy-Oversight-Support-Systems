c10cc4ec49e0d7632b6420f5fb6d019a441e970e
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-09-20 13:28:25 -0400

    Remove redundant id property from OfficeStaff type
    Jira ticket: CAMS-283

--- common/src/cams/staff.ts
+++ common/src/cams/staff.ts
@@ -3,7 +3,6 @@ import { CamsUserReference } from './users';
 
 export type OfficeStaff = CamsUserReference &
   Auditable & {
-    id: string;
     documentType: 'OFFICE_STAFF';
     officeCode: string;
   };
