a786236185f9b9a79d456e0ba2d79bee6c170818
Arthur Morrow <amorrow@flexion.us>
2024-10-03 15:34:58 -0400

    fix descriptions and remove italics
    Jira ticket: CAMS-441

--- user-interface/src/lib/components/uswds/DateRangePicker.tsx
+++ user-interface/src/lib/components/uswds/DateRangePicker.tsx
@@ -110,6 +110,11 @@ function DateRangePickerComponent(props: DateRangePickerProps, ref: React.Ref<Da
       data-min-date={props.minDate}
       data-max-date={props.maxDate}
     >
+      {ariaDescription && (
+        <span id={`${id}-aria-description`} hidden>
+          {ariaDescription}
+        </span>
+      )}
       <DatePicker
         ref={startDateRef}
         id={`${id}-date-start`}
@@ -140,7 +145,6 @@ function DateRangePickerComponent(props: DateRangePickerProps, ref: React.Ref<Da
       />
       <span id={`${id}-aria-description`} aria-live="polite" hidden>
         <span>Format: numeric month / numeric day / 4-digit year.</span>
-        {ariaDescription ? <span>{ariaDescription}</span> : ''}
         {(minDate || maxDate) && (
           <span>
             (Valid date range is
