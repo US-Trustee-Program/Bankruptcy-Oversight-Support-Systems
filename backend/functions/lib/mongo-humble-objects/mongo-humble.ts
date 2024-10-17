c759bb8692159b02f7c46a2e7085f94bafa01c1e
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-10-17 09:04:46 -0500

    Begin using mongo api
    [skip ci]
    
    Jira ticket: CAMS-442

---
+++ backend/functions/lib/mongo-humble-objects/mongo-humble.ts
@@ -0,0 +1,14 @@
+// // https://cosmos-ustp-cams-mongo.mongo.cosmos.azure.us:443/
+// import { MongoClient } from 'mongodb';
+//
+// type DocumentHumbleClient = MongoClient;
+//
+// export type DocumentQuery = {
+//
+// }
+//
+// export type DocumentCollection<T> = {
+//   upsert: (data: T) => T;
+//   get: (id: string) => T;
+//   query: (query: DocumentQuery) => T;
+// };
