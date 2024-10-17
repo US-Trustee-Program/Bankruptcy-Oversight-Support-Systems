c16fbb46e2972eb03ecce8568a601083c26fe89b
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-10-17 15:50:05 -0400

    WIP - query builder experiment
    Jira ticket: CAMS-442

---
+++ backend/functions/lib/query/query.ts
@@ -0,0 +1,84 @@
+import { CamsSession } from '../../../../common/src/cams/session';
+
+export const QueryDialect = {
+  MONGO: 'MONGO',
+} as const;
+
+class QueryBuilder {
+  log(...params) {
+    console.log(params);
+  }
+}
+
+interface Conjunction<_T> {}
+
+interface Next<T> {
+  and: Conjunction<T>;
+  or: Conjunction<T>;
+}
+
+interface Comparison<T, V> {
+  (value: V): Next<T>;
+}
+
+function _next<T>(_builder: QueryBuilder): Next<T> {
+  return {
+    and: () => {},
+    or: () => {},
+  };
+}
+
+function _equals<T, K, V>(builder: QueryBuilder, key: K): Comparison<T, V> {
+  return function (value: V) {
+    builder.log('==', key, value);
+    return _next<T>(builder);
+  };
+}
+
+interface Comparisons<T, V> {
+  equals: Comparison<T, V>;
+  // greaterThan(): Conjunction;
+  // greaterThanOrEqual(): Conjunction;
+  // lessThan(): Conjunction;
+  // lessThanOrEqual(): Conjunction;
+  // notEqual(): Conjunction;
+  // notContains(): Conjunction;
+  // not(): Conjunction;
+}
+
+function _comparisons<T, K, V>(builder: QueryBuilder, key: K): Comparisons<T, V> {
+  return {
+    equals: _equals<T, K, V>(builder, key),
+    // greaterThan: _greaterThan<T, V>(builder),
+    // greaterThanOrEqual: _greaterThanOrEqual<T, V>(builder),
+    // lessThan: _lessThan<T, V>(builder),
+    // lessThanOrEqual: _lessThanOrEqual<T, V>(builder),
+    // notEqual: _notEqual<T, V>(builder),
+    // notContains: _notContains<T, V>(builder),
+    // not: _not<T, V>(builder),
+  };
+}
+
+function _where<T, K = keyof T, V = unknown>(builder: QueryBuilder): (key: K) => Comparisons<T, V> {
+  // TODO: Need to get the type of K of T
+  return (key: K) => {
+    return _comparisons<T, K, V>(builder, key);
+  };
+}
+
+function find<T>() {
+  const builder = new QueryBuilder();
+  return {
+    where: _where<T>(builder),
+    with: () => {}, // give ourselves the ability to reach a nested property. "with" provides another T sub of original T.
+  };
+}
+
+export const Q = {
+  find,
+};
+
+export default Q;
+
+// Test chaining
+Q.find<CamsSession>().where('accessToken').equals('');
