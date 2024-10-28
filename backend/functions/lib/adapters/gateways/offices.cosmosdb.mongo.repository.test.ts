a6714f20d93da0d261b028d7ac03db7536db80c0
Arthur Morrow <amorrow@flexion.us>
2024-10-28 16:27:51 -0400

    properly test office mongo repo and to 100%
    Jira ticket: CAMS-442

--- backend/functions/lib/adapters/gateways/offices.cosmosdb.mongo.repository.test.ts
+++ backend/functions/lib/adapters/gateways/offices.cosmosdb.mongo.repository.test.ts
@@ -1,33 +1,91 @@
-import { OfficesCosmosMongoDbRepository } from './offices.cosmosdb.mongo.repository';
+import { OfficesCosmosMongoDbRepository, OfficeStaff } from './offices.cosmosdb.mongo.repository';
 import {
   createMockApplicationContext,
   createMockApplicationContextSession,
 } from '../../testing/testing-utilities';
 import { ApplicationContext } from '../types/basic';
+import { MongoCollectionAdapter } from './mongo/mongo-adapter';
+import MockData from '../../../../../common/src/cams/test-utilities/mock-data';
+import QueryBuilder from '../../query/query-builder';
+import { CamsRole } from '../../../../../common/src/cams/roles';
+import { closeDeferred } from '../../defer-close';
+import { createAuditRecord } from '../../../../../common/src/cams/auditable';
+import { getCamsError } from '../../common-errors/error-utilities';
 
 describe('offices repo', () => {
   let context: ApplicationContext;
   let repo: OfficesCosmosMongoDbRepository;
+  const { and, equals, contains } = QueryBuilder;
 
   beforeAll(async () => {
     context = await createMockApplicationContext();
     repo = new OfficesCosmosMongoDbRepository(context);
   });
 
-  beforeEach(() => {
-    jest.clearAllMocks();
+  afterEach(async () => {
+    await closeDeferred(context);
+    jest.restoreAllMocks();
   });
 
   test('getOfficeAttorneys', async () => {
-    const attorneys = await repo.getOfficeAttorneys('my_house');
-
-    expect(attorneys).not.toBeNull();
+    const attorneyUsers = MockData.buildArray(MockData.getCamsUserReference, 3);
+    const officeCode = 'office_code';
+    const findSpy = jest
+      .spyOn(MongoCollectionAdapter.prototype, 'find')
+      .mockResolvedValue(attorneyUsers);
+    const query = QueryBuilder.build(
+      and(
+        equals<OfficeStaff['documentType']>('documentType', 'OFFICE_STAFF'),
+        contains<OfficeStaff['roles']>('roles', [CamsRole.TrialAttorney]),
+        equals<OfficeStaff['officeCode']>('officeCode', officeCode),
+      ),
+    );
+    const attorneys = await repo.getOfficeAttorneys(officeCode);
+    expect(findSpy).toHaveBeenCalledWith(query);
+    expect(attorneys).toEqual(attorneyUsers);
   });
 
   test('putOfficeStaff', async () => {
     const session = await createMockApplicationContextSession();
-    const attorneys = await repo.putOfficeStaff('my_house', session.user);
+    const officeCode = 'test_office_code';
 
-    expect(attorneys).not.toBeNull();
+    const insertOneSpy = jest
+      .spyOn(MongoCollectionAdapter.prototype, 'insertOne')
+      .mockResolvedValue('inserted-id');
+
+    const ttl = 4500;
+    const staff = createAuditRecord<OfficeStaff>({
+      id: session.user.id,
+      documentType: 'OFFICE_STAFF',
+      officeCode,
+      ...session.user,
+      ttl,
+    });
+
+    await repo.putOfficeStaff(officeCode, session.user);
+    expect(insertOneSpy).toHaveBeenCalledWith(staff);
+  });
+
+  describe('error handling', () => {
+    const error = new Error('some error');
+    const camsError = getCamsError(error, 'COSMOS_DB_REPOSITORY_CONSOLIDATION_ORDERS');
+
+    test('getOfficeAttorneys error handling', async () => {
+      const officeCode = 'office_code';
+      jest.spyOn(MongoCollectionAdapter.prototype, 'find').mockRejectedValue(error);
+
+      expect(async () => await repo.getOfficeAttorneys(officeCode)).rejects.toThrow(camsError);
+    });
+
+    test('putOfficeStaff error handling', async () => {
+      const session = await createMockApplicationContextSession();
+      const officeCode = 'test_office_code';
+
+      jest.spyOn(MongoCollectionAdapter.prototype, 'insertOne').mockRejectedValue(error);
+
+      expect(async () => await repo.putOfficeStaff(officeCode, session.user)).rejects.toThrow(
+        camsError,
+      );
+    });
   });
 });
