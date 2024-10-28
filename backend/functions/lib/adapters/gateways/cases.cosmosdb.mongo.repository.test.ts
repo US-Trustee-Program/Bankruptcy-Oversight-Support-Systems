4164e35907852b17c75f2a28b88c451b7b003a07
Fritz Madden <fmadden@flexion.us>
2024-10-28 16:57:26 -0600

    brought coverage of cases mongo repo to 100%
    Jira ticket: CAMS-442

--- backend/functions/lib/adapters/gateways/cases.cosmosdb.mongo.repository.test.ts
+++ backend/functions/lib/adapters/gateways/cases.cosmosdb.mongo.repository.test.ts
@@ -2,7 +2,9 @@ import { SYSTEM_USER_REFERENCE } from '../../../../../common/src/cams/auditable'
 import { TransferFrom, TransferTo } from '../../../../../common/src/cams/events';
 import { CaseAssignmentHistory } from '../../../../../common/src/cams/history';
 import MockData from '../../../../../common/src/cams/test-utilities/mock-data';
+import { CamsError } from '../../common-errors/cams-error';
 import { closeDeferred } from '../../defer-close';
+import { CASE_HISTORY } from '../../testing/mock-data/case-history.mock';
 import { createMockApplicationContext } from '../../testing/testing-utilities';
 import { ApplicationContext } from '../types/basic';
 import { CasesCosmosMongoDbRepository } from './cases.cosmosdb.mongo.repository';
@@ -28,23 +30,100 @@ describe('Cases repository', () => {
     documentType: 'TRANSFER_TO',
   };
 
-  // const transfersArray: Array<TransferFrom | TransferTo> = [transferIn, transferOut];
-
   beforeEach(async () => {
     context = await createMockApplicationContext();
     repo = new CasesCosmosMongoDbRepository(context);
   });
+
   afterEach(async () => {
     await closeDeferred(context);
     jest.restoreAllMocks();
   });
 
   test('should getTransfers', async () => {
-    const caseId = '111-82-80331';
-    const transfers = MockData.buildArray(MockData.getTransferOrder, 2);
-    jest.spyOn(MongoCollectionAdapter.prototype, 'find').mockResolvedValue(transfers);
-    const result = await repo.getTransfers(caseId);
-    expect(result.length).toBeGreaterThan(0);
+    const query = {
+      conjunction: 'AND',
+      values: [
+        {
+          condition: 'REGEX',
+          attributeName: 'documentType',
+          value: '^TRANSFER_',
+        },
+        {
+          condition: 'EQUALS',
+          attributeName: 'caseId',
+          value: '111-82-80331',
+        },
+      ],
+    };
+    const transfers = MockData.buildArray(MockData.getTransferOrder, 5);
+    const findSpy = jest
+      .spyOn(MongoCollectionAdapter.prototype, 'find')
+      .mockResolvedValue(transfers);
+    const result = await repo.getTransfers('111-82-80331');
+    expect(findSpy).toHaveBeenCalledWith(query);
+    expect(result).toEqual(transfers);
+  });
+
+  test('should getConsolidation', async () => {
+    const query = {
+      conjunction: 'AND',
+      values: [
+        {
+          condition: 'REGEX',
+          attributeName: 'documentType',
+          value: '^CONSOLIDATION_',
+        },
+        {
+          condition: 'EQUALS',
+          attributeName: 'caseId',
+          value: '111-82-80331',
+        },
+      ],
+    };
+    const consolidations = MockData.buildArray(MockData.getConsolidationOrder, 5);
+    const findSpy = jest
+      .spyOn(MongoCollectionAdapter.prototype, 'find')
+      .mockResolvedValue(consolidations);
+    const result = await repo.getConsolidation('111-82-80331');
+    expect(findSpy).toHaveBeenCalledWith(query);
+    expect(result).toEqual(consolidations);
+  });
+
+  test('should throw error in getConsolidation when find throws', async () => {
+    jest.spyOn(MongoCollectionAdapter.prototype, 'find').mockRejectedValue(new Error('some error'));
+    expect(async () => await repo.getConsolidation('111-82-80331')).rejects.toThrow(
+      'Unknown Error',
+    );
+  });
+
+  test('should getCaseHistory', async () => {
+    const query = {
+      conjunction: 'AND',
+      values: [
+        {
+          condition: 'REGEX',
+          attributeName: 'documentType',
+          value: '^AUDIT_',
+        },
+        {
+          condition: 'EQUALS',
+          attributeName: 'caseId',
+          value: '111-82-80331',
+        },
+      ],
+    };
+    const findSpy = jest
+      .spyOn(MongoCollectionAdapter.prototype, 'find')
+      .mockResolvedValue(CASE_HISTORY);
+    const result = await repo.getCaseHistory('111-82-80331');
+    expect(findSpy).toHaveBeenCalledWith(query);
+    expect(result).toEqual(CASE_HISTORY);
+  });
+
+  test('should throw error in getCaseHistory when find throws', async () => {
+    jest.spyOn(MongoCollectionAdapter.prototype, 'find').mockRejectedValue(new Error('some error'));
+    expect(async () => await repo.getCaseHistory('111-82-80331')).rejects.toThrow('Unknown Error');
   });
 
   test('should createTransferTo', async () => {
@@ -55,6 +134,7 @@ describe('Cases repository', () => {
     expect(result).not.toBeNull();
     expect(insertOneSpy).toHaveBeenCalledWith(transferOut);
   });
+
   test('should createTransferFrom', async () => {
     const insertOneSpy = jest
       .spyOn(MongoCollectionAdapter.prototype, 'insertOne')
@@ -63,6 +143,7 @@ describe('Cases repository', () => {
     expect(insertOneSpy).toHaveBeenCalledWith(transferIn);
     expect(result).not.toBeNull();
   });
+
   test('should createConsolidationTo', async () => {
     const consolidaitonTo = MockData.getConsolidationTo();
     const insertOneSpy = jest
@@ -73,6 +154,7 @@ describe('Cases repository', () => {
 
     expect(result).not.toBeNull();
   });
+
   test('should createConsolidationFrom', async () => {
     const consolidationFrom = MockData.getConsolidationFrom();
     const insertOneSpy = jest
@@ -82,6 +164,7 @@ describe('Cases repository', () => {
     expect(insertOneSpy).toHaveBeenCalledWith(consolidationFrom);
     expect(result).not.toBeNull();
   });
+
   test('should createCaseHistory', async () => {
     const caseHistory: CaseAssignmentHistory = {
       caseId: caseId1,
@@ -98,5 +181,22 @@ describe('Cases repository', () => {
     expect(insertOneSpy).toHaveBeenCalledWith(caseHistory);
 
     expect(result).not.toBeNull();
+  });
+
+  test('createCaseHistory should throw error when insertOne throws error', async () => {
+    const caseHistory: CaseAssignmentHistory = {
+      caseId: caseId1,
+      documentType: 'AUDIT_ASSIGNMENT',
+      updatedOn: new Date().toISOString(),
+      updatedBy: SYSTEM_USER_REFERENCE,
+      before: [],
+      after: [],
+    };
+    jest
+      .spyOn(MongoCollectionAdapter.prototype, 'insertOne')
+      .mockRejectedValue(new CamsError('COSMOS_DB_REPOSITORY_CASES'));
+    expect(async () => await repo.createCaseHistory(caseHistory)).rejects.toThrow(
+      'Unknown CAMS Error',
+    );
   });
 });
