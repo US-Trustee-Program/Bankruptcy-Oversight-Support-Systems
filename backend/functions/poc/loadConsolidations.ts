2bc10b32aa32a4dd031d7c4e39f10e34591a25a7
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-11-20 10:42:06 -0500

    Apply consistent naming to consolidation migration
    Jira ticket: CAMS-461

--- backend/functions/poc/loadConsolidations.ts
+++ backend/functions/poc/loadConsolidations.ts
@@ -7,23 +7,23 @@ import { subOrchestratorETL } from './orchestration/sub-orchestrator-etl';
 import { subOrchestratorPaging } from './orchestration/sub-orchestrator-paging';
 import getConsolidations from './activity/getConsolidations';
 import getPageCount from './activity/getPageCount';
-import transformAndLoad from './activity/transformAndLoad';
+import migrateConsolidation from './activity/migrateConsolidation';
 import flattenBoundingArrays from './activity/flattenBoundingArrays';
 
 export const SUB_ORCHESTRATOR_ETL = 'SubOrchestratorETL';
 export const SUB_ORCHESTRATOR_PAGING = 'SubOrchestratorPaging';
 export const MAIN_ORCHESTRATOR = 'orchestrator';
-export const PAGE_COUNT_ACTIVITY = 'getPageCountFromACMS';
-export const CONSOLIDATIONS_FROM_ACMS = 'getConsolidationsFromACMS';
-export const TRANSFORM_AND_LOAD = 'transformAndLoad';
+export const GET_PAGE_COUNT = 'getPageCount';
+export const GET_CONSOLIDATIONS = 'getConsolidations';
+export const MIGRATE_CONSOLIDATION = 'migrateConsolidation';
 export const FLATTEN_BOUNDING_ARRAYS = 'flattenBoundingArrays';
 
 df.app.orchestration(MAIN_ORCHESTRATOR, main);
 df.app.orchestration(SUB_ORCHESTRATOR_ETL, subOrchestratorETL);
 df.app.orchestration(SUB_ORCHESTRATOR_PAGING, subOrchestratorPaging);
-df.app.activity(CONSOLIDATIONS_FROM_ACMS, getConsolidations);
-df.app.activity(PAGE_COUNT_ACTIVITY, getPageCount);
-df.app.activity(TRANSFORM_AND_LOAD, transformAndLoad);
+df.app.activity(GET_CONSOLIDATIONS, getConsolidations);
+df.app.activity(GET_PAGE_COUNT, getPageCount);
+df.app.activity(MIGRATE_CONSOLIDATION, migrateConsolidation);
 df.app.activity(FLATTEN_BOUNDING_ARRAYS, flattenBoundingArrays);
 
 app.http('dfClient', {
