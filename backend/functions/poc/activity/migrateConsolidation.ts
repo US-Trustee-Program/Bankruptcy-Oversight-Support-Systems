6358f1c80f8447f8c6a6219a5b49babef6707f57
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-11-21 07:59:31 -0500

    Fix input parameter to migrateConsolidation
    Jira ticket: CAMS-461

--- backend/functions/poc/activity/migrateConsolidation.ts
+++ backend/functions/poc/activity/migrateConsolidation.ts
@@ -1,21 +1,17 @@
 import { InvocationContext } from '@azure/functions';
 import ContextCreator from '../../azure/application-context-creator';
-import { AcmsConsolidation } from '../../lib/use-cases/acms-orders/acms-orders';
 import AcmsOrdersController from '../../lib/controllers/acms-orders/acms-orders.controller';
 import { getCamsError } from '../../lib/common-errors/error-utilities';
 
 const MODULE_NAME = 'IMPORT_ACTION_MIGRATE_CONSOLIDATION';
 
-async function migrateConsolidation(
-  input: AcmsConsolidation,
-  invocationContext: InvocationContext,
-) {
+async function migrateConsolidation(leadCaseId: string, invocationContext: InvocationContext) {
   const logger = ContextCreator.getLogger(invocationContext);
   const appContext = await ContextCreator.getApplicationContext({ invocationContext, logger });
   const controller = new AcmsOrdersController();
 
   try {
-    return controller.migrateConsolidation(appContext, input);
+    return controller.migrateConsolidation(appContext, leadCaseId);
   } catch (originalError) {
     throw getCamsError(originalError, MODULE_NAME);
   }
