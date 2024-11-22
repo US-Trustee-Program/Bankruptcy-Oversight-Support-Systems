690428c6ad45bd99c4bf58e1a2e1f07e9d6e0709
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-11-22 14:01:34 -0500

    Remove test.only
    Jira ticket: CAMS-461

--- backend/functions/lib/use-cases/orders/orders-local-gateway.test.ts
+++ backend/functions/lib/use-cases/orders/orders-local-gateway.test.ts
@@ -113,7 +113,7 @@ describe('orders use case tests', () => {
     mockContext.session = await createMockApplicationContextSession({ user: authorizedUser });
   });
 
-  test.only('should not create a second lead case for an existing consolidation', async () => {
+  test('should not create a second lead case for an existing consolidation', async () => {
     // Spy/mock the factory functions so we can return a LOCAL database of our choosing for the test.
     // We need a generic LOCAL gateway implementation that we return via the mocked factory function.
     const localCasesRepo = new LocalCasesRepository();
