d3508734adfdd46af110a356d8019b6db110c934
Fritz Madden <fmadden@flexion.us>
2024-11-27 10:31:17 -0700

    Removed requirement to override useParams
    if CaseDetailScreen isn't called with caseId in the URL, then grab
    caseId from the caseDetail prop (if it's supplied)
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

--- user-interface/src/case-detail/CaseDetailScreen.tsx
+++ user-interface/src/case-detail/CaseDetailScreen.tsx
@@ -188,7 +188,7 @@ interface CaseDetailProps {
 
 export default function CaseDetailScreen(props: CaseDetailProps) {
   let { caseId } = useParams();
-  if (props.caseDetail) {
+  if (props.caseDetail && props.caseDetail.caseId) {
     caseId = props.caseDetail.caseId;
   }
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -424,13 +424,6 @@ export default function CaseDetailScreen(props: CaseDetailProps) {
                     caseBasicInfo.consolidation.length > 0
                   }
                 />
-                {navState === NavState.CASE_OVERVIEW && (
-                  <div>
-                    <div data-testid="docket-entry-sort"></div>
-                    <div data-testid="docket-entry-search"></div>
-                    <div data-testid="filter" className=".docket-summary-facets"></div>
-                  </div>
-                )}
                 {hasDocketEntries && navState === NavState.COURT_DOCKET && (
                   <div
                     className={`filter-and-search padding-y-4`}
