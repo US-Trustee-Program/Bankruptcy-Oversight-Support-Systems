96fc95bee5d7da74214c4dadf1b1cf71fa2d112d
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-12-04 09:06:13 -0600

    WIP - Use a singleton connection pool
    Jira ticket: CAMS-461
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

--- backend/functions/lib/deferrable/finalize-deferrable.ts
+++ backend/functions/lib/deferrable/finalize-deferrable.ts
@@ -1,9 +1,12 @@
 import { closeDeferred, DeferCloseAccumulator } from './defer-close';
 import { DeferReleaseAccumulator, releaseDeferred } from './defer-release';
+import { LoggerHelper } from '../adapters/types/basic';
 
 export async function finalizeDeferrable(
   accumulator: DeferCloseAccumulator | DeferReleaseAccumulator,
 ) {
+  const logger = accumulator['logger'] as LoggerHelper;
+  logger.debug('FINALIZE_DEFERRABLE', '!!!!!!!!!!!!!!!Finalize deferrable!!!!!!!!!!!!!!!');
   await closeDeferred(accumulator);
   await releaseDeferred(accumulator);
 }
