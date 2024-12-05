e9a9b57888f3198b860dd78e0861ee3f167cd8dd
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-12-05 08:52:50 -0600

    Fix mistaken merge conflict resolution
    Jira ticket: CAMS-461
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>

--- backend/functions/lib/adapters/gateways/mongo/utils/base-mongo-repository.ts
+++ backend/functions/lib/adapters/gateways/mongo/utils/base-mongo-repository.ts
@@ -1,4 +1,3 @@
-import { deferClose } from '../../../../defer-close';
 import { DocumentClient } from '../../../../humble-objects/mongo-humble';
 import { ApplicationContext } from '../../../types/basic';
 import { MongoCollectionAdapter } from './mongo-adapter';
@@ -9,14 +8,12 @@ export abstract class BaseMongoRepository {
   protected readonly client: DocumentClient;
   protected readonly databaseName: string;
 
-  constructor(context: ApplicationContext, moduleName: string, collectionName: string) {
+  protected constructor(context: ApplicationContext, moduleName: string, collectionName: string) {
     this.moduleName = moduleName;
     this.collectionName = collectionName;
     const { connectionString, databaseName } = context.config.documentDbConfig;
     this.databaseName = databaseName;
     this.client = new DocumentClient(connectionString);
-
-    deferClose(this.client, context);
   }
 
   protected getAdapter<T>() {
