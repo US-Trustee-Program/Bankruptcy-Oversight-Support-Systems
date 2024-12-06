d23584f56077b8a2c769656ce73b2a4439926241
Arthur Morrow <amorrow@flexion.us>
2024-12-06 17:47:50 -0500

    separate durable function and workflow accomodations
    refactor to create separate functionApps for durable
    and non durable functions, updated workflows and scripts
    to accomodate for the addition
    
    Jira ticket: CAMS-461
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

---
+++ backend/functions/api/case-docket/case-docket.function.test.ts
@@ -0,0 +1,48 @@
+import handler from './case-docket.function';
+import { DXTR_CASE_DOCKET_ENTRIES } from '../../../lib/testing/mock-data/case-docket-entries.mock';
+import { NORMAL_CASE_ID, NOT_FOUND_ERROR_CASE_ID } from '../../../lib/testing/testing-constants';
+import { InvocationContext } from '@azure/functions';
+import { CamsHttpRequest } from '../../../lib/adapters/types/http';
+import {
+  buildTestResponseError,
+  buildTestResponseSuccess,
+  createMockAzureFunctionRequest,
+} from '../../azure/testing-helpers';
+import { CaseDocketController } from '../../../lib/controllers/case-docket/case-docket.controller';
+import { NotFoundError } from '../../../lib/common-errors/not-found-error';
+import { CaseDocket } from '../../../../common/src/cams/cases';
+
+describe('Case docket function', () => {
+  const context = new InvocationContext({
+    logHandler: () => {},
+    invocationId: 'id',
+  });
+
+  test('Should return a docket consisting of a list of docket entries an existing case ID', async () => {
+    const requestProps: Partial<CamsHttpRequest> = {
+      params: { caseId: NORMAL_CASE_ID },
+    };
+    const request = createMockAzureFunctionRequest(requestProps);
+    const { camsHttpResponse, azureHttpResponse } = buildTestResponseSuccess<CaseDocket>({
+      data: DXTR_CASE_DOCKET_ENTRIES,
+    });
+    jest.spyOn(CaseDocketController.prototype, 'handleRequest').mockResolvedValue(camsHttpResponse);
+
+    const response = await handler(request, context);
+    expect(response).toEqual(azureHttpResponse);
+  });
+
+  test('Should return an error response for a non-existent case ID', async () => {
+    const requestProps: Partial<CamsHttpRequest> = {
+      params: { caseId: NOT_FOUND_ERROR_CASE_ID },
+    };
+    const request = createMockAzureFunctionRequest(requestProps);
+    const error = new NotFoundError('TEST-MODULE');
+    const { azureHttpResponse } = buildTestResponseError(error);
+    jest.spyOn(CaseDocketController.prototype, 'handleRequest').mockRejectedValue(error);
+
+    const response = await handler(request, context);
+
+    expect(response).toEqual(azureHttpResponse);
+  });
+});
---
+++ backend/functions/api/courts/courts.function.ts
@@ -0,0 +1,35 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext } from '@azure/functions';
+import ContextCreator from '../../azure/application-context-creator';
+import { toAzureError, toAzureSuccess } from '../../azure/functions';
+import { CourtsController } from '../../../lib/controllers/courts/courts.controller';
+
+const MODULE_NAME = 'COURTS_FUNCTION';
+
+export default async function handler(
+  request: HttpRequest,
+  invocationContext: InvocationContext,
+): Promise<HttpResponseInit> {
+  const logger = ContextCreator.getLogger(invocationContext);
+  try {
+    const applicationContext = await ContextCreator.applicationContextCreator(
+      invocationContext,
+      logger,
+      request,
+    );
+    const controller = new CourtsController();
+    applicationContext.session =
+      await ContextCreator.getApplicationContextSession(applicationContext);
+
+    const responseBody = await controller.handleRequest(applicationContext);
+    return toAzureSuccess(responseBody);
+  } catch (error) {
+    return toAzureError(logger, MODULE_NAME, error);
+  }
+}
+
+app.http('courts', {
+  methods: ['GET'],
+  authLevel: 'anonymous',
+  handler,
+  route: 'courts',
+});
