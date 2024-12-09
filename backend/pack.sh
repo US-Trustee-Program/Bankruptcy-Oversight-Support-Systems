cf8789cc29f2ab2040030c624b6b094dbf1e717e
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-12-09 17:40:41 -0500

    Refactor building AZ function packages
    Jira ticket: CAMS-461

---
+++ backend/pack.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+mkdir -p dist/node_modules/
+cp -r build/* dist/
+cp -r ../../node_modules/* dist/node_modules/
+cp package.json dist/
+cp host.json dist/
+mkdir -p "$PACK_TEMP_DIR"
+pushd dist || exit
+zip -r "$PACK_TEMP_DIR/$PACK_OUT.zip" . --exclude "*.map" --exclude "*.ts"
+popd || exit
+mv "$PACK_TEMP_DIR/$PACK_OUT.zip" .
