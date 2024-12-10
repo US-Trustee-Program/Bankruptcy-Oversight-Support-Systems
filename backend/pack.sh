055f07a46f006d386b0027bad7d16ff1662f07fa
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-12-09 19:44:03 -0500

    Fix temp archive path and file name for CI pipeline
    Jira ticket: CAMS-461

--- backend/pack.sh
+++ backend/pack.sh
@@ -6,6 +6,10 @@ else
   FILE_NAME=$OUT;
 fi
 
+PACK_TEMP_DIR="/tmp/build/$1"
+
+echo "Creating archive $PACK_TEMP_DIR/$FILE_NAME.zip"
+
 mkdir -p dist/node_modules/
 cp -r build/* dist/
 cp -r ../../node_modules/* dist/node_modules/
@@ -13,6 +17,6 @@ cp dist.package.json dist/package.json
 cp host.json dist/
 mkdir -p "$PACK_TEMP_DIR"
 pushd dist || exit
-zip -r "$PACK_TEMP_DIR/$FILE_NAME.zip" . --exclude "*.map" --exclude "*.ts"
+zip -q -r "$PACK_TEMP_DIR/$FILE_NAME.zip" . --exclude "*.map" --exclude "*.ts"
 popd || exit
 mv "$PACK_TEMP_DIR/$FILE_NAME.zip" .
