e27a24aecabba5fabf5c1d7622c7d3cab9ca309f
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-12-09 19:35:30 -0500

    Use OUT env var for naming package archive
    Jira ticket: CAMS-461

--- backend/pack.sh
+++ backend/pack.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+
+if [[ $OUT == "" ]]; then
+  FILE_NAME=$PACK_OUT;
+else
+  FILE_NAME=$OUT;
+fi
+
 mkdir -p dist/node_modules/
 cp -r build/* dist/
 cp -r ../../node_modules/* dist/node_modules/
@@ -6,6 +13,6 @@ cp dist.package.json dist/package.json
 cp host.json dist/
 mkdir -p "$PACK_TEMP_DIR"
 pushd dist || exit
-zip -r "$PACK_TEMP_DIR/$PACK_OUT.zip" . --exclude "*.map" --exclude "*.ts"
+zip -r "$PACK_TEMP_DIR/$FILE_NAME.zip" . --exclude "*.map" --exclude "*.ts"
 popd || exit
-mv "$PACK_TEMP_DIR/$PACK_OUT.zip" .
+mv "$PACK_TEMP_DIR/$FILE_NAME.zip" .
