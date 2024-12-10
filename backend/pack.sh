3da41aee8734885c765fad82ebeae7c2d8879f24
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-12-10 09:49:14 -0500

    Consolidate build dir back into dist
    Jira ticket: CAMS-461
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>,

--- backend/pack.sh
+++ backend/pack.sh
@@ -10,13 +10,9 @@ PACK_TEMP_DIR="/tmp/build/$1"
 
 echo "Creating archive $PACK_TEMP_DIR/$FILE_NAME.zip"
 
-mkdir -p dist/node_modules/
-cp -r build/* dist/
-cp -r ../../node_modules/* dist/node_modules/
-cp dist.package.json dist/package.json
-cp host.json dist/
+mkdir -p dist/
+mkdir -p node_modules/
+cp -r ../../node_modules/* node_modules/
 mkdir -p "$PACK_TEMP_DIR"
-pushd dist || exit
-zip -q -r "$PACK_TEMP_DIR/$FILE_NAME.zip" . --exclude "*.map" --exclude "*.ts"
-popd || exit
+zip -q -r "$PACK_TEMP_DIR/$FILE_NAME.zip" ./dist ./node_modules ./package.json ./host.json --exclude "*.map" --exclude "*.ts"
 mv "$PACK_TEMP_DIR/$FILE_NAME.zip" .
