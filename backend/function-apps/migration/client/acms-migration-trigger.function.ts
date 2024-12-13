59c2cb186a512ab121bfa519c09c036800b0ebf0
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-12-13 17:18:06 -0500

    Reorganize imports
    Jira ticket: CAMS-461

--- backend/function-apps/migration/client/acms-migration-trigger.function.ts
+++ backend/function-apps/migration/client/acms-migration-trigger.function.ts
@@ -1,13 +1,14 @@
+import * as dotenv from 'dotenv';
 import * as df from 'durable-functions';
 import { HttpRequest, HttpResponse, InvocationContext } from '@azure/functions';
 import { MAIN_ORCHESTRATOR } from '../loadConsolidations';
 import { TriggerRequest } from '../../../lib/use-cases/acms-orders/acms-orders';
 import { BadRequestError } from '../../../lib/common-errors/bad-request';
-import * as dotenv from 'dotenv';
 import { UnauthorizedError } from '../../../lib/common-errors/unauthorized-error';
 
-const MODULE_NAME = 'ACMS_MIGRATION_TRIGGER';
 dotenv.config();
+
+const MODULE_NAME = 'ACMS_MIGRATION_TRIGGER';
 
 export default async function httpStart(
   request: HttpRequest,
@@ -32,6 +33,7 @@ export default async function httpStart(
     input: params,
   });
 
+  // TODO: Need to return message like the other HTTP triggers so we can control the HTTP status code.
   return client.createCheckStatusResponse(request, instanceId);
 }
 
