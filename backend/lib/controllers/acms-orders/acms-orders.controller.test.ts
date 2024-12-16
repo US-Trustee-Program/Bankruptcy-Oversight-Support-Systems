9e8ec3be6c487f610d42603804a6a9f65e016dac
Brian Posey <15091170+btposey@users.noreply.github.com>
2024-12-16 16:01:02 -0500

    Increse test coverage for ACMS migration
    Jira ticket: CAMS-461

--- backend/lib/controllers/acms-orders/acms-orders.controller.test.ts
+++ backend/lib/controllers/acms-orders/acms-orders.controller.test.ts
@@ -5,6 +5,7 @@ import AcmsOrders, {
 import AcmsOrdersController from './acms-orders.controller';
 import { ApplicationContext } from '../../adapters/types/basic';
 import { createMockApplicationContext } from '../../testing/testing-utilities';
+import { NotFoundError } from '../../common-errors/not-found-error';
 
 describe('AcmsOrdersController', () => {
   let context: ApplicationContext;
@@ -13,7 +14,11 @@ describe('AcmsOrdersController', () => {
     context = await createMockApplicationContext();
   });
 
-  test('should return array of lead case ids when calling getConsolidationOrders', async () => {
+  afterEach(async () => {
+    jest.resetAllMocks();
+  });
+
+  test('should return array of lead case ids when calling getLeadCaseIds', async () => {
     const leadCaseIds = ['811100000', '1231111111'];
     const predicate: AcmsPredicate = {
       divisionCode: '000',
@@ -40,6 +45,35 @@ describe('AcmsOrdersController', () => {
     const controller = new AcmsOrdersController();
     await controller.migrateConsolidation(context, leadCaseId);
 
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('should handle failed migration', async () => {
+    const leadCaseId = '811100000';
+    const error = new NotFoundError('TEST_MODULE');
+
+    const spy = jest.spyOn(AcmsOrders.prototype, 'migrateConsolidation').mockRejectedValue(error);
+
+    const controller = new AcmsOrdersController();
+
+    await expect(controller.migrateConsolidation(context, leadCaseId)).rejects.toThrow(error);
+    expect(spy).toHaveBeenCalled();
+  });
+
+  test('should handle failed listing of lead case IDs', async () => {
+    const predicate: AcmsPredicate = {
+      divisionCode: '000',
+      chapter: '00',
+    };
+
+    const error = new Error('some error');
+    const spy = jest.spyOn(AcmsOrders.prototype, 'getLeadCaseIds').mockRejectedValue(error);
+
+    const controller = new AcmsOrdersController();
+
+    await expect(controller.getLeadCaseIds(context, predicate)).rejects.toThrow(
+      'Failed to find lead case ids.',
+    );
     expect(spy).toHaveBeenCalled();
   });
 });
