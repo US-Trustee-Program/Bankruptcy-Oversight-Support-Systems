de5896b0cd424eb532298f0328e037e84f26ed22
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-12-20 09:37:58 -0600

    Clean up debug and comments
    Jira ticket: CAMS-461

--- backend/function-apps/migration/orchestration/sub-orchestrator-queue-etl.ts
+++ backend/function-apps/migration/orchestration/sub-orchestrator-queue-etl.ts
@@ -5,6 +5,5 @@ import { OrchestrationContext } from 'durable-functions';
 export function* subOrchestratorETL(context: OrchestrationContext) {
   const predicate: AcmsPredicate = context.df.getInput();
 
-  // TODO: Should we log the lead case IDs that are queued??
-  const _leadCaseIds = yield context.df.callActivity(QUEUE_MIGRATION, predicate);
+  yield context.df.callActivity(QUEUE_MIGRATION, predicate);
 }
