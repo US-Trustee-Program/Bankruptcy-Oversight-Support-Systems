d675e2d389676e181611456fd49ad6129035d13d
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-12-27 17:17:33 -0600

    Use dynamic import for jsx-a11y plugin too
    Jira ticket: CAMS-393

--- user-interface/eslint.config.mjs
+++ user-interface/eslint.config.mjs
@@ -1,9 +1,9 @@
-import jsxA11y from 'eslint-plugin-jsx-a11y';
 import noJestEslintConfig from '../common/no-jest.eslint.config.mjs';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const tsEslint = require('typescript-eslint');
+const jsxA11y = require('eslint-plugin-jsx-a11y');
 
 const frontendEslintConfig = tsEslint.config(noJestEslintConfig, jsxA11y.flatConfigs.recommended);
 
