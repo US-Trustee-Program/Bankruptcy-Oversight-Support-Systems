da34994eeae79414c281d52e835add1557ef7bd8
Arthur Morrow <amorrow@flexion.us>
2024-12-27 15:22:04 -0500

    added precommit hook for biome-linting future we need to add formatting or a full check
    Jira ticket: CAMS-393

---
+++ ops/scripts/utility/biome-precommit.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# A utility script useful for cleaning all project directories
+
+# Usage
+#   From the root directory, run the following command:
+#     ./ops/scripts/utility/clean-all-projects.sh
+
+# PROJECTS=("./backend" "./common" "./dev-tools" "./test/e2e" "./user-interface") TODO: go through these and validate formatting changes
+PROJECTS=("./backend" "./common" "./dev-tools" "./test/e2e" "./user-interface") # temporarily ones that don't require changes
+
+# this should be the command we run
+#  npm run biome-lint:write && npm run biome-format:write
+
+for str in "${PROJECTS[@]}"; do
+  pushd "${str}" || exit
+  echo "Linting and Formatting ${str}."
+  npm run biome-lint:write
+  popd || exit
+done
+
+exit 0
