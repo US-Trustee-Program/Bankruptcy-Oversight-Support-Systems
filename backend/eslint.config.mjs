ac5237a6b318a529de6cc57a55626020aa2d9d8a
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-12-27 17:10:07 -0600

    Use dynamic import
    Jira ticket: CAMS-393

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,5 +1,8 @@
-import tseslint from 'typescript-eslint';
 import commonEslintConfig from '../common/eslint.config.mjs';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
+const tseslint = require('typescript-eslint');
 
 const backendEslintConfig = tseslint.config(commonEslintConfig);
 
