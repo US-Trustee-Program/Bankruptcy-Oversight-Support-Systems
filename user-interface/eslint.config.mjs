3b628e0c5b989115c77cce37ca09dcfa1e271680
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2024-12-31 14:38:06 -0600

    Add eslint-plugin-react recommended ruleset
    Jira ticket: CAMS-393
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>

--- user-interface/eslint.config.mjs
+++ user-interface/eslint.config.mjs
@@ -4,7 +4,13 @@ const require = createRequire(import.meta.url);
 
 const tsEslint = require('typescript-eslint');
 const jsxA11y = require('eslint-plugin-jsx-a11y');
+const reactPlugin = require('eslint-plugin-react');
 
-const frontendEslintConfig = tsEslint.config(eslintTsConfig, jsxA11y['flatConfigs']['recommended']);
+const frontendEslintConfig = tsEslint.config(
+  eslintTsConfig,
+  jsxA11y['flatConfigs']['recommended'],
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'],
+);
 
 export default frontendEslintConfig;
