fcf72345d9bbc2e03852fbfb7ef2415e793703fa
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2025-01-20 18:40:43 -0600

    Get users helper tests passing
    Note that these tests are not strong yet.
    
    Jira ticket: CAMS-500

--- backend/lib/use-cases/users/users.helpers.ts
+++ backend/lib/use-cases/users/users.helpers.ts
@@ -1,6 +1,9 @@
 import { CamsUser, PrivilegedIdentityUser } from '../../../../common/src/cams/users';
-import { getAuthorizationGateway } from '../../factory';
+import { getUserGroupGateway, getUsersRepository } from '../../factory';
 import { ApplicationContext } from '../../adapters/types/basic';
+import { CamsRole } from '../../../../common/src/cams/roles';
+import { UstpOfficeDetails } from '../../../../common/src/cams/offices';
+import { getOfficesFromGroupNames, getRolesFromGroupNames } from '../user-session/user-session';
 
 export type PrivilegedIdentityHelperOptions = {
   idpUser?: CamsUser;
@@ -9,12 +12,28 @@ export type PrivilegedIdentityHelperOptions = {
 
 async function getPrivilegedIdentityUser(
   context: ApplicationContext,
-  _userId: string,
+  userId: string,
   options?: PrivilegedIdentityHelperOptions,
 ) {
   if (!options) {
-    const _authGateway = getAuthorizationGateway(context);
-    // const user = await authGateway.getUser()
+    const userGroupGateway = await getUserGroupGateway(context);
+    const user = await userGroupGateway.getUserById(context, userId);
+
+    const combined = { roles: user.roles, offices: user.offices };
+
+    const usersRepository = getUsersRepository(context);
+    const pimUser = await usersRepository.getPrivilegedIdentityUser(user.id);
+    if (new Date() < new Date(pimUser.expires)) {
+      const roles = getRolesFromGroupNames(pimUser.claims.groups);
+      const rolesSet = new Set<CamsRole>([...user.roles, ...roles]);
+
+      const offices = await getOfficesFromGroupNames(context, pimUser.claims.groups);
+      const officeSet = new Set<UstpOfficeDetails>([...user.offices, ...offices]);
+      combined.roles = Array.from(rolesSet);
+      combined.offices = Array.from(officeSet);
+    }
+
+    return combined;
   }
 }
 
