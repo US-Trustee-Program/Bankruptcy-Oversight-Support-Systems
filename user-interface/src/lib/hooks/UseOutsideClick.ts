7cb1597df97b318a830c7a16d02c652ac3052e4e
Fritz Madden <fmadden@flexion.us>
2025-01-31 11:32:03 -0700

    Added explanitory comment to UseOutsideClick hook
    Jira ticket: CAMS-516

--- user-interface/src/lib/hooks/UseOutsideClick.ts
+++ user-interface/src/lib/hooks/UseOutsideClick.ts
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// NOTE:  This hook acts on "mousedown" rather than "click" so that it does
+// not disturb other click events. For instance, if a comboBox dropdown
+// popup is open, and a user clicks on a button, we don't want to swallow
+// up the click event on the button, but do want to close the dropdown.
+// (That's quite a run-on sentence huah?) By handling a "mousedown", the click
+// is then handled by the button.
 export default function useOutsideClick(
   refs: Array<React.RefObject<HTMLElement>>,
   callback: (event: MouseEvent) => void,
