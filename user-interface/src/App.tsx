bcd27b00d8be9e733363f7a472d8a622732f8d4b
Fritz Madden <fmadden@flexion.us>
2025-02-11 17:21:50 -0700

    Added routing fix and test coverage
    Jira ticket: CAMS-515

--- user-interface/src/App.tsx
+++ user-interface/src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Header } from './lib/components/Header';
 import { AppInsightsErrorBoundary } from '@microsoft/applicationinsights-react-js';
 import { useAppInsights } from './lib/hooks/UseApplicationInsights';
-import { createContext, useRef } from 'react';
+import { createContext, useEffect, useRef, useState } from 'react';
 import { withLDProvider } from 'launchdarkly-react-client-sdk';
 import { getFeatureFlagConfiguration } from './configuration/featureFlagConfiguration';
 import CaseDetailScreen from './case-detail/CaseDetailScreen';
@@ -21,52 +21,70 @@ import GlobalAlert, { GlobalAlertRef } from './lib/components/cams/GlobalAlert/G
 import { UswdsAlertStyle } from './lib/components/uswds/Alert';
 import { AdminScreen } from './admin/AdminScreen';
 import { GoHome } from './lib/components/GoHome';
-import NotFound from './lib/components/NotFound';
+import { LoadingSpinner } from './lib/components/LoadingSpinner';
 
 const featureFlagConfig = getFeatureFlagConfiguration();
 export const GlobalAlertContext = createContext<React.RefObject<GlobalAlertRef> | null>(null);
 
 function App() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const { reactPlugin } = useAppInsights();
   const flags = useFeatureFlags();
 
   const globalAlertRef = useRef<GlobalAlertRef>(null);
-  const transfersEnabled = flags[TRANSFER_ORDERS_ENABLED];
-  const privilegedIdentityEnabled = flags[PRIVILEGED_IDENTITY_MANAGEMENT];
+  const [transfersEnabled, setTransfersEnabled] = useState<boolean>(false);
+  const [privilegedIdentityEnabled, setPrivilegedIdentityEnabled] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [Object.keys(flags).length > 0]);
+
+  useEffect(() => {
+    setTransfersEnabled(true);
+  }, [flags[TRANSFER_ORDERS_ENABLED] === true]);
+
+  useEffect(() => {
+    setPrivilegedIdentityEnabled(true);
+  }, [flags[PRIVILEGED_IDENTITY_MANAGEMENT] === true]);
+
   return (
-    <AppInsightsErrorBoundary
-      onError={(_error) => {
-        return <h1 data-testid="error-boundary-message">Something Went Wrong</h1>;
-      }}
-      appInsights={reactPlugin}
-    >
-      <div id="app-root" className="App" data-testid="app-component-test-id">
-        <GlobalAlert inline={false} type={UswdsAlertStyle.Info} ref={globalAlertRef} />
-        <Header />
-        <GlobalAlertContext.Provider value={globalAlertRef}>
-          <div className="cams-content">
-            <Routes>
-              <Route index element={<GoHome />}></Route>
-              <Route path="/my-cases" element={<MyCasesScreen />}></Route>
-              <Route path="/search" element={<SearchScreen />}></Route>
-              <Route path="/staff-assignment" element={<StaffAssignmentScreen />}></Route>
-              <Route path="/search/:caseId" element={<SearchScreen />}></Route>
-              <Route path="/case-detail/:caseId/*" element={<CaseDetailScreen />}></Route>
-              {transfersEnabled && (
-                <Route path="/data-verification" element={<DataVerificationScreen />}></Route>
-              )}
-              {privilegedIdentityEnabled && (
-                <Route path="/admin/*" element={<AdminScreen />}></Route>
-              )}
-              {/* Note: we are experiencing what seems like a race condition here */}
-              <Route path="*" element={<NotFound />}></Route>
-            </Routes>
-            <ScrollToTopButton data-testid="scroll-to-top-button" />
+    <>
+      {isLoading && <LoadingSpinner id="app-loading-spinner" caption="Loading application..." />}
+      {!isLoading && (
+        <AppInsightsErrorBoundary
+          onError={(_error) => {
+            return <h1 data-testid="error-boundary-message">Something Went Wrong</h1>;
+          }}
+          appInsights={reactPlugin}
+        >
+          <div id="app-root" className="App" data-testid="app-component-test-id">
+            <GlobalAlert inline={false} type={UswdsAlertStyle.Info} ref={globalAlertRef} />
+            <Header />
+            <GlobalAlertContext.Provider value={globalAlertRef}>
+              <div className="cams-content">
+                <Routes>
+                  <Route path="/my-cases" element={<MyCasesScreen />}></Route>
+                  <Route path="/search" element={<SearchScreen />}></Route>
+                  <Route path="/staff-assignment" element={<StaffAssignmentScreen />}></Route>
+                  <Route path="/search/:caseId" element={<SearchScreen />}></Route>
+                  <Route path="/case-detail/:caseId/*" element={<CaseDetailScreen />}></Route>
+                  {transfersEnabled && (
+                    <Route path="/data-verification" element={<DataVerificationScreen />}></Route>
+                  )}
+                  {privilegedIdentityEnabled && (
+                    <Route path="/admin/*" element={<AdminScreen />}></Route>
+                  )}
+                  <Route index element={<GoHome />}></Route>
+                  <Route path="*" element={<GoHome />}></Route>
+                </Routes>
+                <ScrollToTopButton data-testid="scroll-to-top-button" />
+              </div>
+            </GlobalAlertContext.Provider>
+            <PrivacyActFooter></PrivacyActFooter>
           </div>
-        </GlobalAlertContext.Provider>
-        <PrivacyActFooter></PrivacyActFooter>
-      </div>
-    </AppInsightsErrorBoundary>
+        </AppInsightsErrorBoundary>
+      )}
+    </>
   );
 }
 
