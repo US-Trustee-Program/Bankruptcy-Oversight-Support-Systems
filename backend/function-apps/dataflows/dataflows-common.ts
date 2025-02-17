3e92df907570f2a0046112341a7e797202baaa60
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-02-17 15:12:36 -0500

    Finish refactoring durable functions out of dataflows
    Jira ticket: CAMS-421

--- backend/function-apps/dataflows/dataflows-common.ts
+++ backend/function-apps/dataflows/dataflows-common.ts
@@ -1,4 +1,24 @@
-import { HttpRequest } from '@azure/functions';
+import {
+  HttpRequest,
+  HttpResponse,
+  InvocationContext,
+  StorageQueueOutput,
+  Timer,
+} from '@azure/functions';
+import { toAzureError, toAzureSuccess } from '../azure/functions';
+import ContextCreator from '../azure/application-context-creator';
+import { ForbiddenError } from '../../lib/common-errors/forbidden-error';
+
+export const STORAGE_QUEUE_CONNECTION = 'AzureWebJobsStorage';
+
+export type StartMessage = {
+  invocationId: string;
+};
+
+export type RangeMessage = {
+  start: number;
+  end: number;
+};
 
 export function isAuthorized(request: HttpRequest) {
   const header = request.headers.get('Authorization');
@@ -6,6 +26,49 @@ export function isAuthorized(request: HttpRequest) {
   return process.env.ADMIN_KEY && parts[0] === 'ApiKey' && parts[1] === process.env.ADMIN_KEY;
 }
 
-export function buildUniqueName(...parts): string {
+export function buildFunctionName(...parts): string {
   return parts.join('-').replace(/_/g, '-').replace(' ', '-');
+}
+
+export function buildQueueName(...parts): string {
+  return buildFunctionName(...parts).toLowerCase();
+}
+
+export function buildHttpTrigger(moduleName: string, fn: (context: InvocationContext) => void) {
+  async function httpTrigger(
+    request: HttpRequest,
+    context: InvocationContext,
+  ): Promise<HttpResponse> {
+    try {
+      if (!isAuthorized(request)) {
+        throw new ForbiddenError(moduleName);
+      }
+
+      fn(context);
+
+      return new HttpResponse(toAzureSuccess({ statusCode: 201 }));
+    } catch (error) {
+      return new HttpResponse(toAzureError(ContextCreator.getLogger(context), moduleName, error));
+    }
+  }
+  return httpTrigger;
+}
+
+export function buildStartQueueHttpTrigger(moduleName: string, queue: StorageQueueOutput) {
+  return buildHttpTrigger(moduleName, (context: InvocationContext) => {
+    const startMessage: StartMessage = {
+      invocationId: context.invocationId,
+    };
+    context.extraOutputs.set(queue, startMessage);
+  });
+}
+
+export function buildStartQueueTimerTrigger(_moduleName: string, queue: StorageQueueOutput) {
+  async function timerTrigger(_ignore: Timer, context: InvocationContext) {
+    const startMessage: StartMessage = {
+      invocationId: context.invocationId,
+    };
+    context.extraOutputs.set(queue, startMessage);
+  }
+  return timerTrigger;
 }
--- backend/function-apps/dataflows/dataflows.ts
+++ backend/function-apps/dataflows/dataflows.ts
@@ -1,7 +1,9 @@
 import { setupMigrateCases } from './import/migrate-cases';
+import { setupSyncCases } from './import/sync-cases';
 
 // This can be disabled/removed once case migration is complete.
 setupMigrateCases();
+setupSyncCases();
 
 // TODO: Re-enable setup so consolidation migrations can be executed.
 // import { migrationSetup } from './migration/migration';
