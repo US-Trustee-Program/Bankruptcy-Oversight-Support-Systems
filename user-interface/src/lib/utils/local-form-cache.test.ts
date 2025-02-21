db74c3a34eb7f2e68850d3db283e892e923069d9
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-02-21 17:32:24 -0500

    Add coverage for local-form-cache.ts
    Jira ticket: CAMS-519

---
+++ user-interface/src/lib/utils/local-form-cache.test.ts
@@ -0,0 +1,54 @@
+import { describe, expect, vi, beforeEach } from 'vitest';
+import LocalFormCache from './local-form-cache';
+import LocalCache from './local-cache';
+import { MockLocalStorage } from '../testing/mock-local-storage';
+
+describe('LocalFormCache', () => {
+  const mockLocalStorage = new MockLocalStorage();
+
+  beforeAll(() => {
+    vi.stubEnv('CAMS_DISABLE_LOCAL_CACHE', 'false');
+    vi.stubGlobal('localStorage', mockLocalStorage);
+
+    vi.spyOn(LocalCache, 'removeNamespace').mockImplementation(() => {
+      mockLocalStorage.clear();
+    });
+  });
+
+  beforeEach(() => {
+    mockLocalStorage.clear();
+  });
+
+  const testKey = 'foo';
+  const expectedTestKey = 'cams:cache:form:foo';
+  const formData = {
+    foo: 'bar',
+  };
+
+  test('should save form data', () => {
+    LocalFormCache.saveForm(testKey, formData);
+
+    const jsonInCache = mockLocalStorage.getItem(expectedTestKey);
+    const inCache = JSON.parse(jsonInCache!);
+
+    expect(inCache.value).toEqual(formData);
+  });
+
+  test('should get form data', () => {
+    LocalFormCache.saveForm(testKey, formData);
+
+    const actual = LocalFormCache.getForm(testKey);
+    expect(actual).toEqual(formData);
+  });
+
+  test('should remove all form data', () => {
+    const testKeys = [testKey, testKey + '2', testKey + '3'];
+    testKeys.forEach((key) => {
+      LocalFormCache.saveForm(key, formData);
+    });
+    expect(mockLocalStorage.length).toEqual(testKey.length);
+
+    LocalFormCache.removeAll();
+    expect(mockLocalStorage.length).toEqual(0);
+  });
+});
