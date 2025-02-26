b716c913c0642439b47b0bcd5484daddfab707dc
Arthur Morrow <amorrow@flexion.us>
2025-02-26 15:40:26 -0600

    added new sql records for known good transfers updated script to get those explicitly for transfers load data in
    Jira ticket: CAMS-421

--- test/e2e/scripts/data-generation-utils.ts
+++ test/e2e/scripts/data-generation-utils.ts
@@ -24,7 +24,8 @@ import { ConsolidationOrder, TransferOrder } from '../../../common/src/cams/orde
 //////a. should not change the Case Detail Overview
 //3. When a Transfer order is approved should show 'Approved' on data-verification
 //////a. Case Detail Overview should show information appropriately for either case and properly show 'To' and 'From'
-
+const KNOWN_GOOD_TRANSFER_CASE_ID = '081-65-67641';
+const KNOWN_GOOD_TRANSFER_NEW_CASE_ID = '091-69-12345';
 dotenv.config();
 
 export async function seedCosmosE2eDatabase() {
@@ -34,7 +35,10 @@ export async function seedCosmosE2eDatabase() {
 
   // Get some cases from DXTR
   const dxtrCases = await getCasesFromDxtr(appContext);
-  const dxtrCaseIds = dxtrCases.map((bCase) => bCase.caseId);
+  const knownGoodTransferCases = await getKnownGoodTransferCasesFromDxtr(appContext);
+  const casesToSync = [...new Set([...dxtrCases, ...knownGoodTransferCases])];
+
+  const dxtrCaseIds = casesToSync.map((bCase) => bCase.caseId);
 
   // Create SYNCED_CASE docs in Cosmos for each of the cases.
   const caseEvents = await syncCases(appContext, dxtrCaseIds);
@@ -53,7 +57,7 @@ export async function seedCosmosE2eDatabase() {
   console.log('Created Consolidation Orders....   ', consolidationOrders);
 
   const transfersRepo = getOrdersRepository(appContext);
-  const transferOrders = generateTransferOrders(casesWithTransfers);
+  const transferOrders = generateTransferOrders(casesWithTransfers, knownGoodTransferCases);
   console.log('Created Transfer Orders....   ', consolidationOrders);
   await transfersRepo.createMany(transferOrders);
   consolidationRepo.release();
@@ -66,8 +70,20 @@ async function getCasesFromDxtr(appContext: ApplicationContext) {
     limit: 25,
     offset: 0,
     chapters: ['15'],
+    divisionCodes: ['081'],
   };
+
   return await casesGateway.searchCases(appContext, predicate);
+}
+
+async function getKnownGoodTransferCasesFromDxtr(appContext: ApplicationContext) {
+  const casesGateway = getCasesGateway(appContext);
+  const transferPredicate: CasesSearchPredicate = {
+    limit: 25,
+    offset: 0,
+    caseIds: [KNOWN_GOOD_TRANSFER_CASE_ID, KNOWN_GOOD_TRANSFER_NEW_CASE_ID],
+  };
+  return await casesGateway.searchCases(appContext, transferPredicate);
 }
 
 async function generateConsolidationOrders(
@@ -87,10 +103,12 @@ async function generateConsolidationOrders(
   return Array.from(consolidationsMap.values());
 }
 
-function generateTransferOrders(cases: CaseBasics[]) {
+function generateTransferOrders(cases: CaseBasics[], knownGoodTransferCases: CaseBasics[]) {
   const transferOrders: TransferOrder[] = [];
   const originalCases = cases.slice(0, 5);
   const newCases = cases.slice(5);
+  newCases[0] = { ...originalCases[0] };
+
   for (let i = 0; i < 5; i++) {
     transferOrders.push(
       MockData.getTransferOrder({
@@ -102,7 +120,16 @@ function generateTransferOrders(cases: CaseBasics[]) {
       }),
     );
   }
-
+  //a Manually created SQL record that is known to be a good transfer with corresponding AO_PY, and AO_TX record
+  transferOrders.push(
+    MockData.getTransferOrder({
+      override: {
+        ...knownGoodTransferCases[0],
+        status: 'pending',
+        newCase: { ...MockData.getCaseSummary({ override: { ...knownGoodTransferCases[1] } }) },
+      },
+    }),
+  );
   return transferOrders;
 }
 
