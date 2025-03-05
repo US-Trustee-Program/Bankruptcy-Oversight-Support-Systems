6bbb862d4892bb1f34d83fdf7d13d47e4e40b2fc
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2025-03-05 12:39:34 -0600

    Add handling of expr or simple comparison
    Jira ticket: CAMS-421
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- backend/lib/adapters/gateways/mongo/utils/foo.test.ts
+++ backend/lib/adapters/gateways/mongo/utils/foo.test.ts
@@ -3,7 +3,7 @@ import QueryBuilder from '../../../../query/query-builder';
 import { SyncedCase } from '../../../../../../common/src/cams/cases';
 
 describe('foo', () => {
-  const { greaterThan } = QueryBuilder;
+  const { and, equals, exists, greaterThan } = QueryBuilder;
   test('should do the thing', () => {
     const expected = {
       $expr: {
@@ -13,8 +13,9 @@ describe('foo', () => {
 
     expect(
       toMongoQuery(
-        QueryBuilder.build(greaterThan<SyncedCase['closedDate']>('closedDate', 'reopenedDate')),
-        true,
+        QueryBuilder.build(
+          greaterThan<SyncedCase['closedDate']>('closedDate', 'reopenedDate', true),
+        ),
       ),
     ).toEqual(expected);
 
@@ -28,5 +29,37 @@ describe('foo', () => {
         QueryBuilder.build(greaterThan<SyncedCase['closedDate']>('closedDate', '2025-01-01')),
       ),
     ).toEqual(expected2);
+
+    const expected3 = {
+      $and: [
+        { closedDate: { $exists: true } },
+        { reopenedDate: { $exists: true } },
+        { $expr: { $gt: ['$closedDate', '$reopenedDate'] } },
+        { chapter: { $eq: '7' } },
+        { $expr: { $eq: ['$closedDate', '$reopenedDate'] } },
+      ],
+    };
+
+    type Foo = {
+      bar: number;
+      bar2: number;
+      baz: string;
+    };
+
+    expect(
+      toMongoQuery(
+        QueryBuilder.build(
+          and(
+            exists<SyncedCase>('closedDate', true),
+            exists<SyncedCase>('reopenedDate', true),
+            greaterThan<SyncedCase['closedDate']>('closedDate', 'reopenedDate', true),
+            equals<SyncedCase['chapter']>('chapter', '7'),
+            equals<SyncedCase['closedDate']>('closedDate', 'reopenedDate', true),
+            greaterThan<Foo['bar']>('bar', 7, true),
+            greaterThan<Foo['bar']>('bar', 'bar2', true),
+          ),
+        ),
+      ),
+    ).toEqual(expected3);
   });
 });
