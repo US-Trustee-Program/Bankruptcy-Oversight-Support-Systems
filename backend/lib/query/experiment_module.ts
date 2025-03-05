8cf39928b18c6a9288de3a6822e13acc8a987cff
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-03-05 14:46:52 -0500

    Experiment with nested generics with closures
    Jira ticket: CAMS-285

--- backend/lib/query/experiment_module.ts
+++ backend/lib/query/experiment_module.ts
@@ -1,51 +1,35 @@
-import { Condition, Conjunction } from './query-builder';
+import { Condition } from './query-builder';
 
+// Field would be added to the query builder model.
 export type Field<T> = {
   field: keyof T;
 };
 
-export type UpgradedCondition<T> = Omit<Condition, 'leftOperand'> & {
+// This would replace Condition in the query builder model...
+export type UpgradedCondition<T = unknown> = Omit<Condition, 'leftOperand'> & {
   leftOperand: Field<T>;
-  rightOperand: Field<T> | string | number | boolean | UpgradedCondition<T> | Conjunction;
+  rightOperand: Field<T> | unknown;
 };
 
-// The interface isn't needed if it is always equal to the using function return value.
-interface Builder<T> {
-  equals: (left: keyof T, right: string | number | boolean | Field<T>) => Condition;
-  equals2: (left: Field<T>, right: string | number | boolean | Field<T>) => UpgradedCondition<T>;
-  isField: (obj: unknown) => obj is Field<T>;
-}
+// The functions need to be returned from a closure in order to use function specific generic arguments.
+export function using<T = unknown>() {
+  function equals<R = unknown>(
+    leftOperand: Field<T>,
+    rightOperand: Field<T> | R,
+  ): UpgradedCondition<T> {
+    return {
+      condition: 'EQUALS',
+      leftOperand,
+      rightOperand,
+    };
+  }
 
-function equals<T = unknown>(
-  leftOperand: keyof T,
-  rightOperand: string | number | boolean | Field<T>,
-): Condition {
-  return {
-    condition: 'EQUALS',
-    leftOperand: leftOperand.toString(),
-    rightOperand,
-  };
-}
+  function isField(obj: unknown): obj is Field<T> {
+    return obj instanceof Object && 'field' in obj;
+  }
 
-function equals2<T = unknown>(
-  leftOperand: Field<T>,
-  rightOperand: string | number | boolean | Field<T>, // I would love to get the primitive type from something like: 'typeof T[leftOperand.field]'.
-): UpgradedCondition<T> {
-  return {
-    condition: 'EQUALS',
-    leftOperand,
-    rightOperand,
-  };
-}
-
-function isField<T>(obj: unknown): obj is Field<T> {
-  return obj instanceof Object && 'field' in obj;
-}
-
-export function using<T = unknown>(): Builder<T> {
   return {
     equals,
-    equals2,
     isField,
   };
 }
