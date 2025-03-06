7fc7e4447c332e7ef513f3e4cc7112ad0ab159c2
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-03-06 10:46:03 -0500

    Infix conditional operations
    Jira ticket: CAMS-285
    
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>

--- backend/lib/query/experiment_module.ts
+++ backend/lib/query/experiment_module.ts
@@ -1,9 +1,13 @@
 import { Condition } from './query-builder';
 
 // Field would be added to the query builder model.
-export type Field<T> = {
+export type Field<T = unknown> = {
   field: keyof T;
 };
+
+export function isField(obj: unknown): obj is Field {
+  return obj instanceof Object && 'field' in obj; // && obj.field is keyof T; // how to express field is in T?? If so we can cast Field<T> instead of Field
+}
 
 // This would replace Condition in the query builder model...
 export type UpgradedCondition<T = unknown> = Omit<Condition, 'leftOperand'> & {
@@ -13,23 +17,20 @@ export type UpgradedCondition<T = unknown> = Omit<Condition, 'leftOperand'> & {
 
 // The functions need to be returned from a closure in order to use function specific generic arguments.
 export function using<T = unknown>() {
-  function equals<R = string | number | boolean>(
-    leftOperand: Field<T>,
-    rightOperand: Field<T> | R,
-  ): UpgradedCondition<T> {
+  return (field: keyof T) => {
+    const leftOperand: Field<T> = { field };
+
+    type R = T[typeof field];
+    function equals(rightOperand: Field<T> | R): UpgradedCondition<T> {
+      return {
+        condition: 'EQUALS',
+        leftOperand,
+        rightOperand,
+      };
+    }
+
     return {
-      condition: 'EQUALS',
-      leftOperand,
-      rightOperand,
+      equals,
     };
-  }
-
-  function isField(obj: unknown): obj is Field<T> {
-    return obj instanceof Object && 'field' in obj;
-  }
-
-  return {
-    equals,
-    isField,
   };
 }
