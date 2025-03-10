257a010c94d8a4bc8e2fed3b3cbb4f7c076520fd
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-03-10 16:33:04 -0400

    Remove compare fields flag from query builder
    Jira ticket: CAMS-285
    
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>

--- backend/lib/query/query-builder.test.ts
+++ backend/lib/query/query-builder.test.ts
@@ -8,7 +8,6 @@ import QueryBuilder, {
   Pagination,
   Sort,
   SortedAttribute,
-  using,
 } from './query-builder';
 
 describe('Query Builder', () => {
@@ -18,22 +17,8 @@ describe('Query Builder', () => {
     three: boolean;
   };
 
-  const {
-    equals,
-    greaterThan,
-    greaterThanOrEqual,
-    contains,
-    lessThan,
-    lessThanOrEqual,
-    notEqual,
-    notContains,
-    not,
-    exists,
-    and,
-    or,
-    regex,
-    orderBy,
-  } = QueryBuilder;
+  const { not, and, or, orderBy, using } = QueryBuilder;
+  const q = using<Foo>();
 
   test('should build correct query tree', () => {
     const expected: Conjunction<Foo> = {
@@ -43,7 +28,6 @@ describe('Query Builder', () => {
           condition: 'EQUALS',
           leftOperand: { field: 'uno' },
           rightOperand: 'theValue',
-          compareFields: false,
         },
         {
           conjunction: 'AND',
@@ -52,13 +36,11 @@ describe('Query Builder', () => {
               condition: 'EQUALS',
               leftOperand: { field: 'two' },
               rightOperand: 45,
-              compareFields: false,
             },
             {
               condition: 'EQUALS',
               leftOperand: { field: 'three' },
               rightOperand: true,
-              compareFields: false,
             },
             {
               conjunction: 'OR',
@@ -67,13 +49,11 @@ describe('Query Builder', () => {
                   condition: 'EQUALS',
                   leftOperand: { field: 'uno' },
                   rightOperand: 'hello',
-                  compareFields: false,
                 },
                 {
                   condition: 'EQUALS',
                   leftOperand: { field: 'uno' },
                   rightOperand: 'something',
-                  compareFields: false,
                 },
               ],
             },
@@ -82,12 +62,12 @@ describe('Query Builder', () => {
       ],
     };
 
-    const actual = or<Foo>(
-      equals<Foo, 'uno'>('uno', 'theValue'),
+    const actual = or(
+      q('uno').equals('theValue'),
       and(
-        equals<Foo, 'two'>('two', 45),
-        equals('three', true),
-        or(equals('uno', 'hello'), equals('uno', 'something')),
+        q('two').equals(45),
+        q('three').equals(true),
+        or(q('uno').equals('hello'), q('uno').equals('something')),
       ),
     );
 
@@ -103,82 +83,76 @@ describe('Query Builder', () => {
   const simpleQueryCases = [
     {
       condition: 'EQUALS',
-      query: () => equals<Foo, 'two'>('two', 45),
+      query: () => q('two').equals(45),
       result: {
         condition: 'EQUALS',
         leftOperand: { field: 'two' },
         rightOperand: 45,
-        compareFields: false,
       },
     },
     {
       condition: 'GREATER_THAN',
-      query: () => greaterThan<Foo, 'two'>('two', 45),
+      query: () => q('two').greaterThan(45),
       result: {
         condition: 'GREATER_THAN',
         leftOperand: { field: 'two' },
         rightOperand: 45,
-        compareFields: false,
       },
     },
     {
       condition: 'GREATER_THAN_OR_EQUAL',
-      query: () => greaterThanOrEqual<Foo, 'two'>('two', 45),
+      query: () => q('two').greaterThanOrEqual(45),
       result: {
         condition: 'GREATER_THAN_OR_EQUAL',
         leftOperand: { field: 'two' },
         rightOperand: 45,
-        compareFields: false,
       },
     },
     {
       condition: 'CONTAINS',
-      query: () => contains<Foo, 'two'>('two', [45]),
+      query: () => q('two').contains([45]),
       result: { condition: 'CONTAINS', leftOperand: { field: 'two' }, rightOperand: [45] },
     },
     {
       condition: 'LESS_THAN',
-      query: () => lessThan<Foo, 'two'>('two', 45),
+      query: () => q('two').lessThan(45),
       result: {
         condition: 'LESS_THAN',
         leftOperand: { field: 'two' },
         rightOperand: 45,
-        compareFields: false,
       },
     },
     {
       condition: 'LESS_THAN_OR_EQUAL',
-      query: () => lessThanOrEqual<Foo, 'two'>('two', 45),
+      query: () => q('two').lessThanOrEqual(45),
       result: {
         condition: 'LESS_THAN_OR_EQUAL',
         leftOperand: { field: 'two' },
         rightOperand: 45,
-        compareFields: false,
       },
     },
     {
       condition: 'NOT_EQUAL',
-      query: () => notEqual<Foo, 'two'>('two', 45),
+      query: () => q('two').notEqual(45),
       result: {
         condition: 'NOT_EQUAL',
         leftOperand: { field: 'two' },
         rightOperand: 45,
-        compareFields: false,
       },
     },
     {
       condition: 'NOT_CONTAINS',
-      query: () => notContains<Foo, 'two'>('two', [45]),
+      query: () => q('two').notContains([45]),
       result: { condition: 'NOT_CONTAINS', leftOperand: { field: 'two' }, rightOperand: [45] },
     },
     {
       condition: 'REGEX',
-      query: () => regex('two', '45'),
+      query: () => q('two').regex('45'),
       result: { condition: 'REGEX', leftOperand: { field: 'two' }, rightOperand: '45' },
     },
     {
       condition: 'EXISTS',
-      query: () => exists<Foo, 'two'>('two'),
+      query: () => q('two').exists(),
       result: { condition: 'EXISTS', leftOperand: { field: 'two' }, rightOperand: true },
     },
   ];
@@ -200,7 +174,7 @@ describe('Query Builder', () => {
       result: { conjunction: 'OR', values: [staticEqualCondition] },
     },
     {
-      condition: 'NOT',
+      conjunction: 'NOT',
       query: () => not(staticEqualCondition),
       result: { conjunction: 'NOT', values: [staticEqualCondition] },
     },
@@ -258,117 +232,5 @@ describe('Query Builder', () => {
       foo: 'bar',
     };
     expect(isPagination(notPagination)).toBeFalsy();
-  });
-});
-
-describe('Query Builder using function', () => {
-  type TestPrimitives = {
-    someText: string;
-    howMany: number;
-    howManyMore: number;
-    yesNo: boolean;
-  };
-  const q = using<TestPrimitives>();
-
-  test('should infer the right hand operator type or field from the generic parameter', () => {
-    const expectedNumber: Condition<TestPrimitives> = {
-      condition: 'EQUALS',
-      leftOperand: { field: 'howMany' },
-      rightOperand: 1,
-    };
-
-    expect(q('howMany').equals(1)).toEqual(expectedNumber);
-
-    const expectedString: Condition<TestPrimitives> = {
-      condition: 'EQUALS',
-      leftOperand: { field: 'someText' },
-      rightOperand: 'foo',
-    };
-
-    expect(q('someText').equals('foo')).toEqual(expectedString);
-
-    const expectedBoolean: Condition<TestPrimitives> = {
-      condition: 'EQUALS',
-      leftOperand: { field: 'yesNo' },
-      rightOperand: true,
-    };
-
-    expect(q('yesNo').equals(true)).toEqual(expectedBoolean);
-
-    const expectedField: Condition<TestPrimitives> = {
-      condition: 'EQUALS',
-      leftOperand: { field: 'howMany' },
-      rightOperand: { field: 'howManyMore' },
-    };
-
-    expect(q('howMany').equals({ field: 'howManyMore' })).toEqual(expectedField);
-  });
-
-  const conditionFunctions = [
-    {
-      name: 'EQUALS',
-      fn: q('someText').equals,
-      param: 'foo',
-      expected: {
-        condition: 'EQUALS',
-        leftOperand: { field: 'someText' },
-        rightOperand: 'foo',
-      },
-    },
-    {
-      name: 'GREATER_THAN',
-      fn: q('someText').greaterThan,
-      param: 'foo',
-      expected: {
-        condition: 'GREATER_THAN',
-        leftOperand: { field: 'someText' },
-        rightOperand: 'foo',
-      },
-    },
-    {
-      name: 'GREATER_THAN_OR_EQUAL',
-      fn: q('someText').greaterThanOrEqual,
-      param: 'foo',
-      expected: {
-        condition: 'GREATER_THAN_OR_EQUAL',
-        leftOperand: { field: 'someText' },
-        rightOperand: 'foo',
-      },
-    },
-    {
-      name: 'LESS_THAN',
-      fn: q('someText').lessThan,
-      param: 'foo',
-      expected: {
-        condition: 'LESS_THAN',
-        leftOperand: { field: 'someText' },
-        rightOperand: 'foo',
-      },
-    },
-    {
-      name: 'LESS_THAN_OR_EQUAL',
-      fn: q('someText').lessThanOrEqual,
-      param: 'foo',
-      expected: {
-        condition: 'LESS_THAN_OR_EQUAL',
-        leftOperand: { field: 'someText' },
-        rightOperand: 'foo',
-      },
-    },
-    {
-      name: 'NOT_EQUAL',
-      fn: q('someText').notEqual,
-      param: 'foo',
-      expected: {
-        condition: 'NOT_EQUAL',
-        leftOperand: { field: 'someText' },
-        rightOperand: 'foo',
-      },
-    },
-  ];
-
-  test.each(conditionFunctions)('should return an $name', (params) => {
-    const { fn, param, expected } = params;
-    expect(fn(param)).toEqual(expected);
   });
 });
