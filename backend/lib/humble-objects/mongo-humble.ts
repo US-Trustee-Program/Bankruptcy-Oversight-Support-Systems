28368d171c74d64fad29c2d4b83813b29d22f39c
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-03-11 13:22:42 -0400

    Remove console.log
    Jira ticket: CAMS-285

--- backend/lib/humble-objects/mongo-humble.ts
+++ backend/lib/humble-objects/mongo-humble.ts
@@ -42,7 +42,6 @@ export class CollectionHumble<T> {
 
   public async aggregate(query: DocumentQuery) {
     const queryArray = Array.isArray(query) ? query : [query];
-    console.log('Query excluding Closed Cases..... ', queryArray);
     return this.collection.aggregate(queryArray);
   }
 }
