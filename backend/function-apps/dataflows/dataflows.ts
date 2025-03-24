71881c99b56dd69611f1e49423ba7c48450ade94
Arthur Morrow <amorrow@flexion.us>
2025-03-24 11:41:51 -0500

    added registration for case notes dataflow
    Jira ticket: CAMS-527
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- backend/function-apps/dataflows/dataflows.ts
+++ backend/function-apps/dataflows/dataflows.ts
@@ -6,6 +6,7 @@ import SyncOrders from './import/sync-orders';
 import SyncOfficeStaff from './import/sync-office-staff';
 import MigrateCases from './import/migrate-cases';
 import MigrateConsolidations from './import/migrate-consolidations';
+import HandleLegacyCaseNotes from './import/handle-legacy-case-notes';
 import { LoggerImpl } from '../../lib/adapters/services/logger.service';
 
 /*
@@ -91,7 +92,14 @@ dotenv.config();
 initializeApplicationInsights();
 
 // Register data flows.
-dataflows.register(SyncCases, SyncOfficeStaff, SyncOrders, MigrateCases, MigrateConsolidations);
+dataflows.register(
+  SyncCases,
+  SyncOfficeStaff,
+  SyncOrders,
+  MigrateCases,
+  MigrateConsolidations,
+  HandleLegacyCaseNotes,
+);
 
 // Log the list of registered data flows.
 const registeredDataflows = dataflows.list().join(', ').replace(/-/g, '_');
