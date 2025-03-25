f7e47f14742b8681cfb414ec9c206300f99103af
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-03-25 17:30:20 -0400

    Increase coverage for dataflows-common.ts
    Jira ticket: CAMS-288

--- backend/function-apps/dataflows/dataflows-common.test.ts
+++ backend/function-apps/dataflows/dataflows-common.test.ts
@@ -1,12 +1,27 @@
-import { HttpRequest } from '@azure/functions';
-import { buildFunctionName, isAuthorized } from './dataflows-common';
+import { HttpRequest, InvocationContext, StorageQueueOutput, Timer } from '@azure/functions';
+import {
+  buildFunctionName,
+  buildHttpTrigger,
+  buildQueueName,
+  buildStartQueueHttpTrigger,
+  buildStartQueueTimerTrigger,
+  isAuthorized,
+} from './dataflows-common';
 
 describe('Dataflows Common', () => {
-  describe('buildUniqueName', () => {
-    test('should concatenate arguments with a hyphen delimter', () => {
+  describe('buildFunctionName', () => {
+    test('should return a function name according to our naming convention', () => {
       expect(buildFunctionName()).toEqual('');
-      expect(buildFunctionName('one')).toEqual('one');
-      expect(buildFunctionName('two', 'three')).toEqual('two-three');
+      expect(buildFunctionName('ONE_FOO')).toEqual('ONE-FOO');
+      expect(buildFunctionName('TWO', 'THREE_four')).toEqual('TWO-THREE-four');
+    });
+  });
+
+  describe('buildQueueName', () => {
+    test('should return a queue name conforming to StorageAccount queue name constraints', () => {
+      expect(buildQueueName()).toEqual('');
+      expect(buildQueueName('ONE')).toEqual('one');
+      expect(buildQueueName('TWO', 'THREE')).toEqual('two-three');
     });
   });
 
@@ -14,7 +29,7 @@ describe('Dataflows Common', () => {
     const RIGHT = 'this-is-a-key';
     const WRONG = 'this-is-a-bad-key';
 
-    const env = process.env;
+    const { env } = process;
     beforeAll(() => {
       process.env = {
         ADMIN_KEY: RIGHT,
@@ -58,6 +73,70 @@ describe('Dataflows Common', () => {
         headers: new Map<string, string>([['Authorization', `ApiKey ${WRONG}`]]),
       } as unknown as HttpRequest;
       expect(isAuthorized(request)).toBeFalsy();
+    });
+  });
+
+  describe('buildHttpTrigger', () => {
+    test('should return a http trigger that executes the function passed to the build function', async () => {
+      const invocationContext = {
+        log: jest.fn(),
+      } as unknown as InvocationContext;
+
+      const fnArgumentSpy = jest.fn();
+      const trigger = buildHttpTrigger('TEST', fnArgumentSpy);
+
+      const goodRequest = {
+        headers: new Map<string, string>([['Authorization', `ApiKey ${process.env.ADMIN_KEY}`]]),
+      } as unknown as HttpRequest;
+      await trigger(goodRequest, invocationContext);
+      expect(fnArgumentSpy).toHaveBeenCalledWith(invocationContext, goodRequest);
+
+      fnArgumentSpy.mockClear();
+      const badRequest = {
+        headers: new Map<string, string>(),
+      } as unknown as HttpRequest;
+      await trigger(badRequest, invocationContext);
+      expect(fnArgumentSpy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('buildStartQueueTimerTrigger', () => {
+    test('should return a timer trigger that puts a message on a given start queue', async () => {
+      const setSpy = jest.fn();
+      const invocationContext = {
+        extraOutputs: {
+          set: setSpy,
+        },
+      } as unknown as InvocationContext;
+
+      const storageQueue = {} as unknown as StorageQueueOutput;
+      const trigger = buildStartQueueTimerTrigger('TEST', storageQueue);
+
+      const timer = {} as unknown as Timer;
+      await trigger(timer, invocationContext);
+
+      expect(setSpy).toHaveBeenCalledWith(storageQueue, {});
+    });
+  });
+
+  describe('buildStartQueueHttpTrigger', () => {
+    test('should return a http trigger that puts a message on a given start queue', async () => {
+      const setSpy = jest.fn();
+      const invocationContext = {
+        log: jest.fn(),
+        extraOutputs: {
+          set: setSpy,
+        },
+      } as unknown as InvocationContext;
+
+      const storageQueue = {} as unknown as StorageQueueOutput;
+      const trigger = buildStartQueueHttpTrigger('TEST', storageQueue);
+
+      const goodRequest = {
+        headers: new Map<string, string>([['Authorization', `ApiKey ${process.env.ADMIN_KEY}`]]),
+      } as unknown as HttpRequest;
+      await trigger(goodRequest, invocationContext);
+      expect(setSpy).toHaveBeenCalledWith(storageQueue, {});
     });
   });
 });
