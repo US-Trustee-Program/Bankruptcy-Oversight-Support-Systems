b165035dc94c46772085bd2cb631d64dbf97b8a8
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-03-27 10:37:28 -0400

    Add test coverage for query pipeline library
    Jira ticket: CAMS-288
    
    Co-authored-by: Arthur Morrow <133667008+amorrow-flexion@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- backend/lib/query/query-pipeline.test.ts
+++ backend/lib/query/query-pipeline.test.ts
@@ -4,9 +4,21 @@ import QueryPipeline, {
   Paginate,
   Sort,
   SortedAttribute,
+  Stage,
 } from './query-pipeline';
 
-const { sort } = QueryPipeline;
+const {
+  pipeline,
+  paginate,
+  match,
+  sort,
+  ascending,
+  descending,
+  exclude,
+  join,
+  addFields,
+  additionalField,
+} = QueryPipeline;
 
 describe('Query Pipeline', () => {
   type Foo = {
@@ -15,9 +27,17 @@ describe('Query Pipeline', () => {
     three: boolean;
   };
 
+  type Bar = {
+    uno: string;
+    four: number;
+    five: boolean;
+  };
+
+  const additionalDocs = 'barDocs';
+
   test('should proxy a list of SortDirection when orderBy is called', () => {
-    const attributeFoo: SortedAttribute<Foo> = ['uno', 'ASCENDING'];
-    const attributeBar: SortedAttribute<Foo> = ['uno', 'DESCENDING'];
+    const attributeFoo: SortedAttribute<Foo> = ascending<Foo>('uno');
+    const attributeBar: SortedAttribute<Bar> = descending<Bar>('four');
     expect(sort(attributeFoo)).toEqual({ stage: 'SORT', attributes: [attributeFoo] });
     expect(sort(attributeFoo, attributeBar)).toEqual({
       stage: 'SORT',
@@ -25,10 +45,112 @@ describe('Query Pipeline', () => {
     });
   });
 
+  test('should proxy a Join stage', () => {
+    const expected = {
+      stage: 'JOIN',
+      local: {
+        field: 'uno',
+        source: null,
+      },
+      foreign: {
+        field: 'uno',
+        source: 'bar',
+      },
+      alias: 'barDocs',
+    };
+
+    const actual = join<Bar>('bar', 'uno').onto<Foo>('uno').as(additionalDocs);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should proxy an AddFields stage', () => {
+    const expected = {
+      stage: 'ADD_FIELDS',
+      fields: [
+        {
+          field: 'matchingBars',
+          source: 'barDocs',
+          query: {
+            conjunction: 'AND',
+            values: [],
+          },
+        },
+      ],
+    };
+
+    const actual = addFields(
+      additionalField('matchingBars', additionalDocs, {
+        conjunction: 'AND',
+        values: [],
+      }),
+    );
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should proxy an Exclude stage', () => {
+    const expected = {
+      stage: 'EXCLUDE',
+      fields: ['four', 'five'],
+    };
+
+    const actual = exclude<Bar>(['four', 'five']);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should proxy a Paginate stage', () => {
+    const expected = {
+      stage: 'PAGINATE',
+      skip: 0,
+      limit: 5,
+    };
+
+    const actual = paginate(0, 5);
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should proxy a Match stage', () => {
+    const expected = {
+      conjunction: 'AND',
+      values: [],
+      stage: 'MATCH',
+    };
+
+    const actual = match({
+      conjunction: 'AND',
+      values: [],
+    });
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should coalesce Stage args into a Pipeline', () => {
+    const stageOne: Stage = {
+      stage: 'PAGINATE',
+      skip: 0,
+      limit: 5,
+    };
+    const stageTwo: Stage = {
+      stage: 'EXCLUDE',
+      fields: ['four', 'five'],
+    };
+
+    const expected = {
+      stages: [stageOne, stageTwo],
+    };
+
+    const actual = pipeline(stageOne, stageTwo);
+
+    expect(actual).toEqual(expected);
+  });
+
   test('isSort', () => {
     const sort: Sort = {
       stage: 'SORT',
-      attributes: [['uno', 'ASCENDING']],
+      attributes: [{ field: 'uno', direction: 'ASCENDING' }],
     };
     expect(isSort(sort)).toBeTruthy();
     expect(isSort({})).toBeFalsy();
