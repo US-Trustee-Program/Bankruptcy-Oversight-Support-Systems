293db82cbdccb110dfc80205b9125715a4a0f4cd
Fritz Madden <fmadden@flexion.us>
2025-04-02 14:03:28 -0600

    Converted staff assignment filter to...
    Converted staff assignment filter to be a part of the
    StaffAssignmentScreen presenter pattern.
    
    One test remaining to update a the bottom of
    StaffAssignmentScreen.test.tsx to test the handleAssignmentChange
    method.
    
    Jira ticket: CAMS-288

--- user-interface/src/staff-assignment/screen/staffAssignmentUseCase.ts
+++ user-interface/src/staff-assignment/screen/staffAssignmentUseCase.ts
@@ -1,6 +1,5 @@
 import { CamsUser, CamsUserReference, getCourtDivisionCodes } from '@common/cams/users';
 import { StaffAssignmentStore } from './staffAssignmentStore';
-import { StaffAssignmentControls } from './staffAssignmentControls';
 import useFeatureFlags, {
   CHAPTER_ELEVEN_ENABLED,
   CHAPTER_TWELVE_ENABLED,
@@ -10,10 +9,18 @@ import {
   DEFAULT_SEARCH_LIMIT,
   DEFAULT_SEARCH_OFFSET,
 } from '@common/api/search';
-import { StaffAssignmentScreenFilter } from '../filters/StaffAssignmentFilter';
+import { ComboOption } from '@/lib/components/combobox/ComboBox';
+import LocalStorage from '@/lib/utils/local-storage';
+import useApi2 from '@/lib/hooks/UseApi2';
+import { ResponseBody } from '@common/api/response';
+import { UstpOfficeDetails } from '@common/cams/offices';
+import { useGlobalAlert } from '@/lib/hooks/UseGlobalAlert';
+import { StaffAssignmentScreenFilter } from './staffAssignmentControls';
 
 export interface StaffAssignmentUseCase {
-  handleFilterAssignee(assignee: CamsUserReference | undefined): void;
+  assigneesToComboOptions(officeAssignees: CamsUserReference[]): ComboOption[];
+  fetchAssignees(): void;
+  handleFilterAssignee(assignees: ComboOption[]): void;
   handleAssignmentChange(assignees: CamsUserReference[]): void;
   getChapters(): string[];
   getPredicateByUserContextWithFilter(
@@ -22,12 +29,59 @@ export interface StaffAssignmentUseCase {
   ): CasesSearchPredicate;
 }
 
-const staffAssignmentUseCase = (
-  store: StaffAssignmentStore,
-  controls: StaffAssignmentControls,
-): StaffAssignmentUseCase => {
-  function handleFilterAssignee(assignee: CamsUserReference | undefined) {
-    if (assignee) {
+const staffAssignmentUseCase = (store: StaffAssignmentStore): StaffAssignmentUseCase => {
+  const api = useApi2();
+  const globalAlert = useGlobalAlert();
+
+  function assigneesToComboOptions(officeAssignees: CamsUserReference[]): ComboOption[] {
+    const comboOptions: ComboOption[] = [];
+    officeAssignees.forEach((assignee) => {
+      comboOptions.push({
+        value: assignee.id,
+        label: assignee.name,
+      });
+    });
+    return comboOptions;
+  }
+
+  const fetchAssignees = () => {
+    const session = LocalStorage.getSession();
+    if (session?.user.offices) {
+      const { offices } = session.user;
+      getOfficeAssignees(api.getOfficeAssignees, offices)
+        .then((assignees) => {
+          store.setOfficeAssignees(assignees);
+          store.setOfficeAssigneesError(false);
+        })
+        .catch((_e) => {
+          store.setOfficeAssigneesError(true);
+          globalAlert?.error('There was a problem getting the list of assignees.');
+        });
+    }
+  };
+
+  async function getOfficeAssignees(
+    apiFunction: (office: string) => Promise<ResponseBody<CamsUserReference[]>>,
+    offices: UstpOfficeDetails[],
+  ): Promise<CamsUserReference[]> {
+    const assigneeMap: Map<string, CamsUserReference> = new Map();
+    for (const office of offices) {
+      const assignees = await apiFunction(office.officeCode);
+      assignees.data.forEach((assignee) => {
+        if (!assigneeMap.has(assignee.id)) {
+          assigneeMap.set(assignee.id, assignee);
+        }
+      });
+    }
+    return Array.from(assigneeMap.values()).sort((a, b) => (a.name < b.name ? -1 : 1));
+  }
+
+  function handleFilterAssignee(assignees: ComboOption[]) {
+    if (assignees[0]) {
+      const assignee: CamsUserReference = {
+        id: assignees[0].value,
+        name: assignees[0].label,
+      };
       const newFilter = {
         ...store.staffAssignmentFilter,
         assignee,
@@ -40,7 +94,7 @@ const staffAssignmentUseCase = (
 
   function handleAssignmentChange(assignees: CamsUserReference[]) {
     if (assignees.length) {
-      controls.fetchAssignees();
+      fetchAssignees();
     }
   }
 
@@ -74,10 +128,12 @@ const staffAssignmentUseCase = (
   }
 
   return {
-    handleFilterAssignee,
-    handleAssignmentChange,
+    assigneesToComboOptions,
+    fetchAssignees,
     getChapters,
     getPredicateByUserContextWithFilter,
+    handleAssignmentChange,
+    handleFilterAssignee,
   };
 };
 
