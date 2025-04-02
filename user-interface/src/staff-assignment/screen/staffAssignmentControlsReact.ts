d676b95fc712c21450dd095fa9fa8d6ecdf36bca
Fritz Madden <fmadden@flexion.us>
2025-04-02 14:03:28 -0600

    Converted staff assignment filter to...
    Converted staff assignment filter to be a part of the
    StaffAssignmentScreen presenter pattern.
    
    One test remaining to update a the bottom of
    StaffAssignmentScreen.test.tsx to test the handleAssignmentChange
    method.
    
    Jira ticket: CAMS-288

--- user-interface/src/staff-assignment/screen/staffAssignmentControlsReact.ts
+++ user-interface/src/staff-assignment/screen/staffAssignmentControlsReact.ts
@@ -1,22 +1,13 @@
 import { useRef } from 'react';
-import { StaffAssignmentFilterRef } from '../filters/StaffAssignmentFilter';
 import { AssignAttorneyModalRef } from '../modal/AssignAttorneyModal';
 import { StaffAssignmentControls } from './staffAssignmentControls';
 
 export function useStaffAssignmentControlsReact(): StaffAssignmentControls {
-  const filterRef = useRef<StaffAssignmentFilterRef>(null);
   const infoModalRef = useRef(null);
   const assignmentModalRef = useRef<AssignAttorneyModalRef>(null);
 
-  const fetchAssignees = () => {
-    filterRef.current?.fetchAssignees();
-  };
-
   return {
     assignmentModalRef,
-    filterRef,
     infoModalRef,
-
-    fetchAssignees,
   };
 }
