080c1a4e62aea0c3634a9df640b29a0977874e2b
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-04-03 08:57:15 -0400

    Call migrateAssignments from start queue handler
    Jira ticket: CAMS-288

--- backend/function-apps/dataflows/migrations/migrate-assignees.ts
+++ backend/function-apps/dataflows/migrations/migrate-assignees.ts
@@ -2,18 +2,15 @@ import { app, InvocationContext } from '@azure/functions';
 import { buildFunctionName, StartMessage } from '../dataflows-common';
 import { MIGRATE_ASSIGNEES, MIGRATE_ASSIGNEES_START } from '../storage-queues';
 import ContextCreator from '../../azure/application-context-creator';
+import OfficeAssignees from '../../../lib/use-cases/dataflows/office-assignees';
 
 const MODULE_NAME = MIGRATE_ASSIGNEES;
 
 const START = buildFunctionName(MODULE_NAME, 'start');
 
 async function start(_ignore: StartMessage, invocationContext: InvocationContext) {
-  const _context = await ContextCreator.getApplicationContext({ invocationContext });
-  // const { logger } = context;
-
-  // TODO: Implement the migration logic here.
-  // get all active assignments
-  //
+  const context = await ContextCreator.getApplicationContext({ invocationContext });
+  await OfficeAssignees.migrateAssignments(context);
 }
 
 function setup() {
@@ -21,7 +18,6 @@ function setup() {
     connection: MIGRATE_ASSIGNEES_START.connection,
     queueName: MIGRATE_ASSIGNEES_START.queueName,
     handler: start,
-    // extraOutputs: [HARD_STOP], // DO we need a DLQ??
   });
 }
 
