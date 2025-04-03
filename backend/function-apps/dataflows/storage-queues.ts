4b111cd77dd4e8b2af73cacde7b1ca117eb026ae
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-04-03 11:45:42 -0400

    Add event queues for closed cases and case assignments
    Jira ticket: CAMS-288

--- backend/function-apps/dataflows/storage-queues.ts
+++ backend/function-apps/dataflows/storage-queues.ts
@@ -8,7 +8,18 @@ export const STORAGE_QUEUE_CONNECTION = 'AzureWebJobsStorage';
 
 const connection = STORAGE_QUEUE_CONNECTION;
 
+// TODO: This should probably be defined ONLY in the migration source module.
 export const MIGRATE_ASSIGNEES_START = output.storageQueue({
   queueName: buildQueueName(MIGRATE_ASSIGNEES, 'start'),
+  connection,
+});
+
+export const CASE_CLOSED_EVENT = output.storageQueue({
+  queueName: buildQueueName('CASE_CLOSED_EVENT'),
+  connection,
+});
+
+export const CASE_ASSIGNMENT_EVENT = output.storageQueue({
+  queueName: buildQueueName('CASE_ASSIGNMENT_EVENT'),
   connection,
 });
