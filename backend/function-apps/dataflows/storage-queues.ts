7909db5e127a74c0ca65a58625790861df617802
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-04-04 10:52:13 -0400

    Add case-closed-event handler
    Jira ticket: CAMS-288

--- backend/function-apps/dataflows/storage-queues.ts
+++ backend/function-apps/dataflows/storage-queues.ts
@@ -1,15 +1,10 @@
 import { buildQueueName } from './dataflows-common';
 import { output } from '@azure/functions';
-import { CASE_ASSIGNMENT_EVENT } from './module-names';
+import { CASE_ASSIGNMENT_EVENT, CASE_CLOSED_EVENT } from './module-names';
 
 export const STORAGE_QUEUE_CONNECTION = 'AzureWebJobsStorage';
 
 const connection = STORAGE_QUEUE_CONNECTION;
-
-export const CASE_CLOSED_EVENT = output.storageQueue({
-  queueName: buildQueueName('CASE_CLOSED_EVENT'),
-  connection,
-});
 
 export const CASE_ASSIGNMENT_EVENT_QUEUE = output.storageQueue({
   queueName: buildQueueName(CASE_ASSIGNMENT_EVENT),
@@ -18,5 +13,15 @@ export const CASE_ASSIGNMENT_EVENT_QUEUE = output.storageQueue({
 
 export const CASE_ASSIGNMENT_EVENT_DLQ = output.storageQueue({
   queueName: buildQueueName(CASE_ASSIGNMENT_EVENT, 'DLQ'),
+  connection,
+});
+
+export const CASE_CLOSED_EVENT_QUEUE = output.storageQueue({
+  queueName: buildQueueName(CASE_CLOSED_EVENT),
+  connection,
+});
+
+export const CASE_CLOSED_EVENT_DLQ = output.storageQueue({
+  queueName: buildQueueName(CASE_CLOSED_EVENT, 'DLQ'),
   connection,
 });
---
+++ backend/lib/use-cases/dataflows/case-closed-event.ts
@@ -0,0 +1,34 @@
+import { ApplicationContext } from '../../adapters/types/basic';
+import { getOfficeAssigneesRepository } from '../../factory';
+import { getCamsErrorWithStack } from '../../common-errors/error-utilities';
+
+const MODULE_NAME = 'CASE-CLOSED-EVENT-USE-CASE';
+
+export type CaseClosedEvent = {
+  caseId: string;
+};
+
+async function handleCaseClosedEvent(context: ApplicationContext, event: CaseClosedEvent) {
+  try {
+    await deleteCaseAssignment(context, event.caseId);
+  } catch (originalError) {
+    throw getCamsErrorWithStack(originalError, MODULE_NAME, {
+      camsStackInfo: {
+        message: 'Failed to handle case closed event.',
+        module: MODULE_NAME,
+      },
+      data: event,
+    });
+  }
+}
+
+async function deleteCaseAssignment(context: ApplicationContext, caseId: string) {
+  const repo = getOfficeAssigneesRepository(context);
+  await repo.deleteMany({ caseId });
+}
+
+const CaseClosedEventUseCase = {
+  handleCaseClosedEvent,
+};
+
+export default CaseClosedEventUseCase;
