5f8fb1721a03d95e816ffbd6daaca4327bb11ceb
Fritz Madden <fmadden@flexion.us>
2025-04-08 00:04:59 -0600

    Separated Filter from Staff Assignment presenter
    Filter is now a subcomponent in the filters folder.
    Filter is also using the Presenter pattern.
    
    Jira ticket: CAMS-288
    
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- user-interface/src/staff-assignment/screen/staffAssignmentControls.ts
+++ user-interface/src/staff-assignment/screen/staffAssignmentControls.ts
@@ -1,15 +1,12 @@
 import { RefObject } from 'react';
 import { AssignAttorneyModalRef } from '../modal/AssignAttorneyModal';
 import { ModalRefType } from '@/lib/components/uswds/modal/modal-refs';
-import { CamsUserReference } from '@common/cams/users';
-
-export type StaffAssignmentScreenFilter = {
-  assignee: CamsUserReference;
-};
+import { StaffAssignmentFilterRef } from '../filters/StaffAssignmentFilter';
 
 interface StaffAssignmentControls {
   infoModalRef: React.RefObject<ModalRefType>;
   assignmentModalRef: RefObject<AssignAttorneyModalRef>;
+  filterRef: RefObject<StaffAssignmentFilterRef>;
 }
 
 export type { StaffAssignmentControls };
