a8a41c4599e4b0969f0f5057df590020c01a09e8
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-04-17 14:37:24 -0400

    Export ModuleNames namesspaace from from module-names.ts
    Jira ticket: CAMS-288

--- backend/function-apps/dataflows/events/case-closed-event.ts
+++ backend/function-apps/dataflows/events/case-closed-event.ts
@@ -1,12 +1,12 @@
 import { app, InvocationContext } from '@azure/functions';
-import { CASE_CLOSED_EVENT } from '../module-names';
+import ModuleNames from '../module-names';
 import { CASE_CLOSED_EVENT_DLQ, CASE_CLOSED_EVENT_QUEUE } from '../storage-queues';
 import { buildFunctionName } from '../dataflows-common';
 import { CaseAssignment } from '../../../../common/src/cams/assignments';
 import ContextCreator from '../../azure/application-context-creator';
 import OfficeAssigneesUseCase from '../../../lib/use-cases/offices/office-assignees';
 
-const MODULE_NAME = CASE_CLOSED_EVENT;
+const MODULE_NAME = ModuleNames.CASE_CLOSED_EVENT;
 const HANDLER = buildFunctionName(MODULE_NAME, 'handler');
 
 async function handler(event: CaseAssignment, invocationContext: InvocationContext) {
--- backend/function-apps/dataflows/storage-queues.ts
+++ backend/function-apps/dataflows/storage-queues.ts
@@ -1,27 +1,27 @@
 import { buildQueueName } from './dataflows-common';
 import { output } from '@azure/functions';
-import { CASE_ASSIGNMENT_EVENT, CASE_CLOSED_EVENT } from './module-names';
+import ModuleNames from './module-names';
 
 export const STORAGE_QUEUE_CONNECTION = 'AzureWebJobsStorage';
 
 const connection = STORAGE_QUEUE_CONNECTION;
 
 export const CASE_ASSIGNMENT_EVENT_QUEUE = output.storageQueue({
-  queueName: buildQueueName(CASE_ASSIGNMENT_EVENT),
+  queueName: buildQueueName(ModuleNames.CASE_ASSIGNMENT_EVENT),
   connection,
 });
 
 export const CASE_ASSIGNMENT_EVENT_DLQ = output.storageQueue({
-  queueName: buildQueueName(CASE_ASSIGNMENT_EVENT, 'DLQ'),
+  queueName: buildQueueName(ModuleNames.CASE_ASSIGNMENT_EVENT, 'DLQ'),
   connection,
 });
 
 export const CASE_CLOSED_EVENT_QUEUE = output.storageQueue({
-  queueName: buildQueueName(CASE_CLOSED_EVENT),
+  queueName: buildQueueName(ModuleNames.CASE_CLOSED_EVENT),
   connection,
 });
 
 export const CASE_CLOSED_EVENT_DLQ = output.storageQueue({
-  queueName: buildQueueName(CASE_CLOSED_EVENT, 'DLQ'),
+  queueName: buildQueueName(ModuleNames.CASE_CLOSED_EVENT, 'DLQ'),
   connection,
 });
