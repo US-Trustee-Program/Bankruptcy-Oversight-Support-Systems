b3343bd5a029058fdd934b283ad670f2198f6acb
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2025-04-30 08:53:15 -0500

    Define the e2e report output directory
    This should not be required but the latest run found no report. This
    will keep us from having issues in the future if the default changes
    for some reason.

--- test/e2e/playwright.config.ts
+++ test/e2e/playwright.config.ts
@@ -24,8 +24,8 @@ export default defineConfig({
   retries: process.env.CI ? 3 : 0,
   /* Opt out of parallel tests on CI. */
   workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters. We define the output folder in case the default ever changes. */
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
