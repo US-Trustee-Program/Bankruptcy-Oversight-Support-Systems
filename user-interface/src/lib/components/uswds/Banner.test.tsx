045df109a1c902d542c9b6e7a1701c1b7f9876fd
Nate Elliott <nelliott@flexion.us>
2025-05-08 15:25:30 -0500

    Update Banner tests for window.CAMS_CONFIGURATION
    Jira ticket: CAMS-389

--- user-interface/src/lib/components/uswds/Banner.test.tsx
+++ user-interface/src/lib/components/uswds/Banner.test.tsx
@@ -2,13 +2,24 @@ import { render, screen } from '@testing-library/react';
 import { Banner } from './Banner';
 import { BrowserRouter } from 'react-router-dom';
 
+const originalCamsConfiguration = window.CAMS_CONFIGURATION;
+beforeAll(() => {
+  window.CAMS_CONFIGURATION = {
+    ...originalCamsConfiguration,
+  };
+});
+
+afterAll(() => {
+  window.CAMS_CONFIGURATION = originalCamsConfiguration;
+});
+
 describe('Test Banner Environment', () => {
   beforeEach(() => {
     vi.clearAllMocks();
   });
   test('Should not have a css class based on launchDarklyEnvironment env var', () => {
     const expectedEnv = 'production';
-    vi.stubEnv('CAMS_LAUNCH_DARKLY_ENV', 'production');
+    window.CAMS_CONFIGURATION.CAMS_LAUNCH_DARKLY_ENV = 'production';
     render(
       <BrowserRouter>
         <Banner />
@@ -19,7 +30,7 @@ describe('Test Banner Environment', () => {
   });
   test('Should have a css class based on launchDarklyEnvironment env var', () => {
     const expectedEnv = 'staging';
-    vi.stubEnv('CAMS_LAUNCH_DARKLY_ENV', 'staging');
+    window.CAMS_CONFIGURATION.CAMS_LAUNCH_DARKLY_ENV = 'staging';
     render(
       <BrowserRouter>
         <Banner />
