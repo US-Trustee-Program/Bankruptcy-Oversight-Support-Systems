f0b2088e4ceaf8e4fc4e04df584d39d0997c0269
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-05-08 14:49:49 -0400

    WIP - Fix tests broken by move to window.CAMS_CONFIGURATION
    Jira ticket: CAMS-389
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Nate Elliott <32004008+En-8@users.noreply.github.com>

--- user-interface/src/setupTests.ts
+++ user-interface/src/setupTests.ts
@@ -3,3 +3,21 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const camsConfiguration: Record<string, string> = {};
+
+// Copy environment variables to the config object
+Object.keys(process.env)
+  .filter((key) => key.startsWith('CAMS_'))
+  .forEach((varName) => {
+    if (process.env[varName] !== undefined) {
+      camsConfiguration[varName] = process.env[varName];
+    }
+  });
+
+Object.defineProperty(window, 'CAMS_CONFIGURATION', {
+  value: camsConfiguration,
+  writable: false,
+});
