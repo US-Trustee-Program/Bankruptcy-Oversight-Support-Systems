7d34d387caf8b64a28b7b5eca7bcf911c3e92200
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-05-08 10:41:54 -0400

    Revert eslint config
    Jira ticket: CAMS-389
    
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>

--- user-interface/eslint.config.mjs
+++ user-interface/eslint.config.mjs
@@ -10,32 +10,10 @@ const testConfig = eslintUiTestConfig.map((configObject) => ({
   ...configObject,
 }));
 
-const envToConfigFile = {
-  files: ['**/envToConfig.js'],
-  languageOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'script', // or 'module' if you're using import/export
-    globals: {
-      require: 'readonly',
-      process: 'readonly',
-      console: 'readonly',
-      __dirname: 'readonly',
-      module: 'readonly',
-      exports: 'readonly',
-    },
-  },
-  linterOptions: {
-    rules: {
-      'prettier/prettier': 'off',
-    },
-  },
-};
-
 const frontendEslintConfig = [
   {
     ignores: ['**/build/**/*', '**/dist/**/*', '**/node_modules/**/*', '**/coverage/**/*'],
   },
-  envToConfigFile,
   ...codeConfig,
   ...testConfig,
 ];
