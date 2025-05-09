f555d6a41884ff54352bb56dcd1aedc5113a78a5
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-05-09 17:44:19 -0400

    Simplify feature flag configuration logic
    Jira ticket: CAMS-389
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>

--- user-interface/src/configuration/featureFlagConfiguration.ts
+++ user-interface/src/configuration/featureFlagConfiguration.ts
@@ -1,14 +1,10 @@
 import getAppConfiguration from '@/configuration/appConfiguration';
 
-// DECISION IS TO NOT USE CAMEL CASE FLAGS
-const useCamelCaseFlagKeys = false;
-
 export const getFeatureFlagConfiguration = () => {
   const clientId = getAppConfiguration().featureFlagClientId ?? '';
-  const useExternalProvider = !!clientId;
   return {
     clientId,
-    useExternalProvider,
-    useCamelCaseFlagKeys,
+    useExternalProvider: !!clientId,
+    useCamelCaseFlagKeys: false,
   } as const;
 };
