32060902d41d469700a8ccd20e31d65e58a98b7e
Fritz Madden <fmadden@flexion.us>
2025-05-09 15:06:30 -0600

    Removed import from envToConfig and updated eslint
    Changed eslintTsConfig import to eslintJsConfig import in
    eslint-node.config
    
    Jira ticket: CAMS-389
    
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- user-interface/eslint-node.config.mjs
+++ user-interface/eslint-node.config.mjs
@@ -1,11 +1,11 @@
-import eslintTsConfig from '../common/eslint-ts.config.mjs';
+import eslintJsConfig from '../common/eslint-js.config.mjs';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 const tsEslint = require('typescript-eslint');
 
 const eslintNodeConfig = tsEslint.config(
-  eslintTsConfig,
+  eslintJsConfig,
   {
     rules: {
       'no-undef': 'off',
