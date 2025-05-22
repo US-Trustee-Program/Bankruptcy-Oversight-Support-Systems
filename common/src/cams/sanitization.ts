64c298ecb594cbe844ea2dab260e19df556c612a
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-05-22 17:12:35 -0400

    Add utility function to filter non-ASCII unicode
    Jira ticket: CAMS-549

--- common/src/cams/sanitization.ts
+++ common/src/cams/sanitization.ts
@@ -9,13 +9,30 @@ const MONGO_QUERY_INJECTED_PATTERN = RegExp(
 );
 
 export function isValidUserInput(input: string) {
-  if (
+  return !(
     input.match(MONGO_CONSOLE_INJECTED_PATTERN) ||
     input.match(JAVASCRIPT_INJECTED_PATTERN) ||
     input.match(MONGO_QUERY_INJECTED_PATTERN)
-  ) {
-    return false;
-  } else {
-    return true;
-  }
+  );
+}
+
+/**
+ * Replaces all UTF-8 characters outside the Extended ASCII range (0-255) with the provided replacement.
+ * @param dirty
+ * @param mask
+ */
+export function maskToExtendedAscii(dirty: string, mask: string): string {
+  // TODO: Should we just limit this to the ASCII printable characters (32-126)?
+  // TODO: Is there value to keeping Extended ASCII characters (128-255) in the string?
+  // TODO: Unicode variation selectors cause >1 one-byte character replacements. Coalesce to a single replacement?
+  return Array.from(dirty)
+    .map((char) => {
+      const code = char.charCodeAt(0);
+      return code > 31 && code <= 255 ? char : mask;
+    })
+    .join('');
+}
+
+export function filterToExtendedAscii(dirty: string): string {
+  return maskToExtendedAscii(dirty, '');
 }
