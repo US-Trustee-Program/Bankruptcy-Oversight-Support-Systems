56022b72bfedb9c1b77c9c0115ef5b20f189ebdc
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2025-05-27 14:50:54 -0500

    Restore validateArchiveRequestParameters function
    Jira ticket: CAMS-549
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>

--- backend/lib/controllers/case-notes/case.notes.controller.ts
+++ backend/lib/controllers/case-notes/case.notes.controller.ts
@@ -55,7 +55,7 @@ export class CaseNotesController implements CamsController {
           caseId,
           sessionUser: context.session.user,
         };
-        this.validateRequestParameters({ id: noteId, caseId });
+        this.validateArchiveRequestParameters({ id: noteId, caseId });
         await caseNotesUseCase.archiveCaseNote(archiveNote);
         return httpSuccess({
           statusCode: HttpStatusCodes.CREATED,
@@ -98,9 +98,6 @@ export class CaseNotesController implements CamsController {
   private validateRequestParameters(input: Partial<CaseNoteInput>) {
     const badParams = [];
     const messages = [];
-    if (input.id && !input.id.match(VALID_ID_PATTERN)) {
-      messages.push(INVALID_ID_MESSAGE);
-    }
 
     if (!input.caseId) {
       badParams.push('caseId');
@@ -118,6 +115,32 @@ export class CaseNotesController implements CamsController {
       badParams.push('case note content');
     } else if (!isValidUserInput(input.content)) {
       messages.push(INVALID_NOTE_MESSAGE);
+    }
+
+    if (badParams.length > 0) {
+      const isPlural = badParams.length > 1;
+      const message = `Required ${isPlural ? 'parameters' : 'parameter'} ${badParams.join(', ')} ${isPlural ? 'are' : 'is'} absent.`;
+      messages.push(message);
+    }
+    if (messages.length) {
+      throw new ForbiddenCaseNotesError(MODULE_NAME, { message: messages.join(' ') });
+    }
+  }
+
+  private validateArchiveRequestParameters(request: Partial<CaseNoteDeleteRequest>) {
+    const badParams = [];
+    const messages = [];
+
+    if (!request['id']) {
+      badParams.push('id');
+    } else if (!request['id'].match(VALID_ID_PATTERN)) {
+      messages.push(INVALID_ID_MESSAGE);
+    }
+
+    if (!request['caseId']) {
+      badParams.push('caseId');
+    } else if (!request['caseId'].match(VALID_CASEID_PATTERN)) {
+      messages.push(INVALID_CASEID_MESSAGE);
     }
 
     if (badParams.length > 0) {
