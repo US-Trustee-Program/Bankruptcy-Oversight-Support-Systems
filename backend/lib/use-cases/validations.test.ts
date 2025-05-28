7aeddb7d04ef365cd4426776b2880b33b3f8ac16
Nate Elliott <nelliott@flexion.us>
2025-05-28 08:58:59 -0500

    Enhance unit tests for validations.ts
    Jira ticket: CAMS-549

--- backend/lib/use-cases/validations.test.ts
+++ backend/lib/use-cases/validations.test.ts
@@ -3,14 +3,43 @@ import { sanitizeDeep } from './validations';
 describe('sanitizeDeep', () => {
   const originalEnv = process.env;
 
-  afterAll(() => {
-    process.env = originalEnv;
+  beforeEach(() => {
+    process.env = { ...originalEnv };
+  });
+
+  test('should sanitize a simple string', () => {
+    const testString = 'Hello World 你好 with emoji 🚀';
+    const sanitized = sanitizeDeep(testString, 'TEST-MODULE');
+    expect(sanitized).toEqual('Hello World  with emoji ');
+  });
+
+  test('should sanitize an array of strings', () => {
+    const testArray = ['Hello', 'World 世界', 'Test 😊 emoji'];
+    const sanitized = sanitizeDeep(testArray, 'TEST-MODULE');
+    expect(sanitized).toEqual(['Hello', 'World ', 'Test  emoji']);
+  });
+
+  test('should recursively sanitize an object', () => {
+    const testObject = {
+      name: 'Test 你好',
+      value: 123,
+      nested: { key: 'Nested Value 💡', nestedArray: ['thing1 ©', 'thing2 🔥'] },
+    };
+    const sanitized = sanitizeDeep(testObject, 'TEST-MODULE');
+    expect(sanitized).toEqual({
+      name: 'Test ',
+      value: 123,
+      nested: {
+        key: 'Nested Value ',
+        nestedArray: ['thing1 ', 'thing2 '],
+      },
+    });
   });
 
   test('should detect and ignore cyclic references in objects', () => {
     const testObject = { name: 'Test', testObject: null };
     testObject.testObject = testObject;
-    const sanitized = sanitizeDeep(testObject);
+    const sanitized = sanitizeDeep(testObject, 'TEST-MODULE');
     expect(sanitized).toEqual({ name: 'Test', testObject: null });
   });
 
@@ -20,7 +49,7 @@ describe('sanitizeDeep', () => {
       level1: { level2: { level3: { level4: { level5: { level6: 'deep' } } } } },
     };
 
-    expect(() => sanitizeDeep(testObject)).toThrow('Max depth exceeded');
+    expect(() => sanitizeDeep(testObject, 'TEST-MODULE')).toThrow('Max depth exceeded');
   });
 
   test('should throw an error when max key count of an object is exceeded', () => {
@@ -31,6 +60,36 @@ describe('sanitizeDeep', () => {
       key3: 'value3',
     };
 
-    expect(() => sanitizeDeep(testObject)).toThrow('Max key count exceeded');
+    expect(() => sanitizeDeep(testObject, 'TEST-MODULE')).toThrow('Max key count exceeded');
+  });
+
+  test('should throw BadRequestError for invalid user input', () => {
+    const testStringsWithScriptInjection = [
+      '<script>alert("XSS")</script>',
+      'Check this document.querySelector("input").value',
+      'function() { eval("malicious code"); }',
+      'fetch("/api/sensitive-data")',
+    ];
+
+    const testStringsWithMongoInjection = [
+      'Find me with $where: {evil: true}',
+      'Apply $and operator',
+      'Use $elemMatch to find documents',
+    ];
+
+    testStringsWithScriptInjection.forEach((testString) => {
+      expect(() => sanitizeDeep(testString, 'TEST-MODULE')).toThrow('Invalid user input.');
+    });
+
+    testStringsWithMongoInjection.forEach((testString) => {
+      expect(() => sanitizeDeep(testString, 'TEST-MODULE')).toThrow('Invalid user input.');
+    });
+
+    const testObject = {
+      name: 'Test',
+      description: '<script>alert("XSS")</script>',
+    };
+
+    expect(() => sanitizeDeep(testObject, 'TEST-MODULE')).toThrow('Invalid user input.');
   });
 });
