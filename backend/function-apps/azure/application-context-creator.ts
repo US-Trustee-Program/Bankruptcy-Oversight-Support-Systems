05c998f1b2540e980d4abaf03e3dc0c9a3fb17b2
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-06-06 17:16:52 -0400

    Enable authorization ApiKey scheme in API
    Jira ticket: CAMS-561

--- backend/function-apps/azure/application-context-creator.ts
+++ backend/function-apps/azure/application-context-creator.ts
@@ -1,5 +1,5 @@
 import * as jwt from 'jsonwebtoken';
-import { InvocationContext, HttpRequest } from '@azure/functions';
+import { HttpRequest, InvocationContext } from '@azure/functions';
 import { ApplicationContext } from '../../lib/adapters/types/basic';
 import { ApplicationConfiguration } from '../../lib/configs/application-configuration';
 import { getFeatureFlags } from '../../lib/adapters/utils/feature-flag';
@@ -8,6 +8,8 @@ import { azureToCamsHttpRequest } from './functions';
 import { UnauthorizedError } from '../../lib/common-errors/unauthorized-error';
 import { getUserSessionUseCase } from '../../lib/factory';
 import { sanitizeDeep } from '../../lib/use-cases/validations';
+import { CamsSession } from '../../../common/src/cams/session';
+import * as crypto from 'node:crypto';
 
 const MODULE_NAME = 'APPLICATION-CONTEXT-CREATOR';
 
@@ -71,26 +73,54 @@ async function getApplicationContextSession(context: ApplicationContext) {
     });
   }
 
-  const match = authorizationHeader.match(/Bearer (.+)/);
+  const [scheme, token] = authorizationHeader.split(' ');
 
-  if (!match || match.length !== 2) {
+  if (!scheme || !token) {
     throw new UnauthorizedError(MODULE_NAME, {
-      message: 'Bearer token not found in authorization header',
+      message: 'Authorization scheme and token not found in authorization header',
     });
   }
 
-  let accessToken = '';
-  const jwtToken = jwt.decode(match[1]);
-  if (jwtToken) {
-    accessToken = match[1];
-  } else {
+  switch (scheme) {
+    case 'Bearer':
+      return lookupBearerToken(context, token);
+    case 'ApiKey':
+      return lookupApiKeyToken(context, token);
+    default:
+      throw new UnauthorizedError(MODULE_NAME, { message: 'Unsupported authorization scheme' });
+  }
+}
+
+async function lookupBearerToken(context: ApplicationContext, token: string): Promise<CamsSession> {
+  if (!jwt.decode(token)) {
     throw new UnauthorizedError(MODULE_NAME, {
       message: 'Malformed Bearer token in authorization header',
     });
   }
 
   const sessionUseCase = getUserSessionUseCase(context);
-  return sessionUseCase.lookup(context, accessToken, context.config.authConfig.provider);
+  return sessionUseCase.lookup(context, token, context.config.authConfig.provider);
+}
+
+async function lookupApiKeyToken(
+  _context: ApplicationContext,
+  token: string,
+): Promise<CamsSession> {
+  if (token !== process.env.ADMIN_KEY) {
+    throw new UnauthorizedError(MODULE_NAME, {
+      message: 'Invalid API key in authorization header',
+    });
+  }
+  // TODO: Convert the token to a hashed variant to use as the token to lookup in Cosmos.
+  const hashedToken = crypto.createHash('sha256').update(token).digest('base64');
+  // TODO: For now we will just return a static session since the current API key is also static.
+  return {
+    accessToken: hashedToken,
+    expires: Date.now() + 3600 * 1000, // Now plus one hour.
+    issuer: 'cams',
+    provider: 'apikey',
+    user: { id: 'apiuser', name: 'Api User' },
+  };
 }
 
 const ContextCreator = {
