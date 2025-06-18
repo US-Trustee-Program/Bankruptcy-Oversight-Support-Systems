34d858e7f0f077c9d68cb07f9e017a9f214ea2cd
Nate Elliott <nelliott@flexion.us>
2025-06-18 12:55:18 -0500

    centralize setting innerHTML in test file
    Jira ticket: CAMS-526
    
     Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
     Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

--- user-interface/src/lib/components/cams/RichTextEditor/editor.constants.ts
+++ user-interface/src/lib/components/cams/RichTextEditor/editor.constants.ts
@@ -2,4 +2,24 @@ export const ZERO_WIDTH_SPACE = '​';
 export const ZERO_WIDTH_SPACE_REGEX = new RegExp(ZERO_WIDTH_SPACE, 'g');
 export const EMPTY_TAG_REGEX = /<([a-z][^>]*)>\s*(?:<br\s*\/?>)*\s*<\/\1>/gi;
 
+export const DOMPURIFY_CONFIG = {
+  ALLOWED_TAGS: ['em', 'strong', 'p', 'ul', 'ol', 'li', 'br', 'span', 'a'],
+  ALLOWED_ATTR: ['href', 'class', 'style'],
+  ALLOW_DATA_ATTR: false,
+  FORBID_TAGS: ['style', 'script', 'iframe', 'object', 'embed', 'form', 'input', 'button'],
+  FORBID_ATTR: [
+    'onerror',
+    'onload',
+    'onclick',
+    'onmouseover',
+    'onmousedown',
+    'onfocus',
+    'onblur',
+    'onkeydown',
+    'onkeypress',
+    'srcset',
+    'src',
+  ],
+};
+
 export type RichTextFormat = 'strong' | 'em' | 'u';
