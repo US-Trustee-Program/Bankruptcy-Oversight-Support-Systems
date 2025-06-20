a8e6a2337adadcffec419aacb0219621339dcda5
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-06-20 17:57:54 -0400

    Fix broken dependency, add utils, remove unused file
    Jira ticket: CAMS-526

--- user-interface/src/lib/components/cams/RichTextEditor/editor.constants.ts
+++ user-interface/src/lib/components/cams/RichTextEditor/editor.constants.ts
@@ -1,6 +1,7 @@
 export const ZERO_WIDTH_SPACE = '​';
 export const ZERO_WIDTH_SPACE_REGEX = new RegExp(ZERO_WIDTH_SPACE, 'g');
 export const EMPTY_TAG_REGEX = /<([a-z][^>]*)>\s*(?:<br\s*\/?>)*\s*<\/\1>/gi;
+export const CONTENT_INPUT_SELECTOR = '#rich-text-editor-content';
 
 export const DOMPURIFY_CONFIG = {
   ALLOWED_TAGS: ['em', 'strong', 'p', 'ul', 'ol', 'li', 'br', 'span', 'a'],
