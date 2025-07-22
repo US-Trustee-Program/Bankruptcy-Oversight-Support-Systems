1924692a47489647722babdc5b7aa055ae2671de
Nate Elliott <nelliott@flexion.us>
2025-07-22 11:10:37 -0500

    Set up playwright for UI a11y testing
    Jira ticket: CAMS-526
    
     Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
     Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
     Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>

---
+++ user-interface/playwright.config.ts
@@ -0,0 +1,60 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+/* eslint-disable-next-line @typescript-eslint/no-require-imports */
+require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  expect: {
+    timeout: 10000,
+  },
+  timeout: 30000,
+  testDir: './test/accessibility',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 3 : 0,
+  /* Stop after 2 failures in CI. */
+  maxFailures: process.env.CI ? 2 : undefined,
+  /* Opt out of parallel tests on CI. */
+  workers: 1,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters. We define the output folder in case the default ever changes. */
+  reporter: [['html', { outputFolder: 'playwright-report' }]],
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    /* Test against branded browsers. */
+    {
+      name: 'Microsoft Edge',
+      use: {
+        ...devices['Desktop Edge'],
+        channel: 'msedge',
+      },
+    },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+});
---
+++ user-interface/test/accessibility/case-notes.test.ts
@@ -0,0 +1,32 @@
+import test, { expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+test.describe('Case Notes', () => {
+  test.describe.configure({ retries: 0, mode: 'serial' });
+
+  let addCaseNoteButton;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`/case-detail/999-99-00001/notes`);
+
+    addCaseNoteButton = page.getByTestId('open-modal-button_case-note-add-button');
+    await expect(addCaseNoteButton).toBeVisible();
+  });
+
+  test('should not have accessibility issues', async ({ page }) => {
+    const testNoteTitle = 'Test Note Title';
+    const testNoteContent = 'Test Note Content for E2E purposes';
+
+    //Open Add Note modal to create a new note and submit
+    await page.locator('[data-testid="open-modal-button_case-note-add-button"]').click();
+    await expect(page.locator('[data-testid="modal-content-case-note-form"]')).toBeVisible();
+
+    await page.locator('[data-testid="case-note-title-input"]').fill(testNoteTitle);
+    await page.locator('[data-testid="editor-content"] > div').fill(testNoteContent);
+
+    await page.locator('[data-testid="rich-text-bold-button"]').click();
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
