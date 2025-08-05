94e3bec78c5540df31361ad7a901c975dbd32a25
Fritz Madden <fmadden@flexion.us>
2025-08-05 10:58:47 -0600

    Modified git hook setup script for submodules
    Jira ticket: CAMS-555
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: James Brooks <12275865+jamesobrooks@users.noreply.github.com>
    Co-authored-by: Brian Posey <15091170+btposey@users.noreply.github.com>
    Co-authored-by: Nate Elliott <32004008+En-8@users.noreply.github.com>

--- ops/git-setup/set-up-git-hooks.sh
+++ ops/git-setup/set-up-git-hooks.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
+function usage() {
+    echo "Usage: $0 [-s]"
+    echo "Options:"
+    echo "  -s Set up git hooks for submodule"
+    exit 1
+}
+
+while getopts ":sh" option; do
+  case $option in
+    s)
+      SUBMODULE=true
+      ;;
+    h)
+      usage
+      ;;
+    \?)
+      usage
+      ;;
+  esac
+done
+
+if [ "$SUBMODULE" = true ]; then
+    echo "Setting up git hooks for submodule..."
+    GITHOOKS_DIR="../../../.git/modules/cams/hooks"
+else
+    echo "Setting up git hooks for main repository..."
+    GITHOOKS_DIR="../../.git/hooks"
+fi
+
 # check current working directory
 if [ ! -f "./ops/git-setup/set-up-git-hooks.sh" ]; then
     echo "Error: Incorrect working directory $(pwd) : Execute command from root of repository."
@@ -22,18 +51,18 @@ temp_file=$(mktemp)
 
 ## Read the original file and insert the contents of source-file into it
 {
-    head -n 1 ../../.git/hooks/pre-commit
+    head -n 1 ${GITHOOKS_DIR}/pre-commit
     cat pre-commit-hook-content
-    tail -n +3 ../../.git/hooks/pre-commit
+    tail -n +3 ${GITHOOKS_DIR}/pre-commit
 } > "$temp_file"
 
 ## Move the modified contents back to the original file
-mv "$temp_file" ../../.git/hooks/pre-commit
-chmod +x ../../.git/hooks/pre-commit
+mv "$temp_file" ${GITHOOKS_DIR}/pre-commit
+chmod +x ${GITHOOKS_DIR}/pre-commit
 
 # Set up prepare-commit-msg hook
-cp ./prepare-commit-msg ../../.git/hooks/prepare-commit-msg
-chmod +x ../../.git/hooks/prepare-commit-msg
+cp ./prepare-commit-msg ${GITHOOKS_DIR}/prepare-commit-msg
+chmod +x ${GITHOOKS_DIR}/prepare-commit-msg
 
 popd || exit
 
