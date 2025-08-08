9ae3af9e5bc49a0c0a8f4f0daa139abb33d680b3
Fritz Madden <fmadden@flexion.us>
2025-08-08 13:25:20 -0600

    Updated party types, trustee, assistant, etc.
    Jira ticket: CAMS-582

--- common/src/cams/parties.ts
+++ common/src/cams/parties.ts
@@ -5,25 +5,61 @@ export type Person = {
   generation?: string;
 };
 
-export type Party = {
-  name: string;
+export type Address = {
+  address1: string;
+  address2?: string;
+  address3?: string;
+  city: string;
+  state: string;
+  zipCode: string;
+  countryCode: 'US';
+};
+
+// represents DXTR sourced addresses
+export type FlexAddress = {
   address1?: string;
   address2?: string;
   address3?: string;
   cityStateZipCountry?: string;
+};
+
+export type JustName = {
+  name: string;
+};
+
+type PhoneNumber = string;
+
+export type Phone = {
+  phone: PhoneNumber;
+};
+
+type EmailAddress = string;
+
+export type Email = {
+  email: EmailAddress;
+};
+
+export type TaxIds = {
   taxId?: string;
   ssn?: string;
 };
 
+export type Party = JustName & FlexAddress & TaxIds;
+
 export type Debtor = Party;
 
-export type DebtorAttorney = Omit<Party, 'taxId' | 'ssn'> & {
-  phone?: string;
-  email?: string;
-  office?: string;
-};
+export type DebtorAttorney = JustName &
+  FlexAddress &
+  Phone &
+  Email & {
+    office?: string;
+  };
 
-export type Trustee = Omit<Party, 'taxId' | 'ssn'> & {
-  phone?: string;
-  email?: string;
-};
+export type Trustee = JustName &
+  FlexAddress &
+  Phone &
+  Email & {
+    assistant?: TrusteeAssistant;
+  };
+
+export type TrusteeAssistant = JustName & FlexAddress & Phone & Email;
