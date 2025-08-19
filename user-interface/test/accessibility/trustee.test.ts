d9eeff4028c45bc967bd71ee8ec78693f62cd5f1
Nate Elliott <nelliott@flexion.us>
2025-08-19 08:11:41 -0500

    Fix a11y test for trustees
    Jira ticket: CAMS-582

--- user-interface/test/accessibility/trustee.test.ts
+++ user-interface/test/accessibility/trustee.test.ts
@@ -1,4 +1,3 @@
-/*
 import test, { expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
@@ -7,28 +6,26 @@ test.describe('Trustees', () => {
 
   test.beforeEach(async ({ page }) => {
     await page.goto(`http://localhost:3000/trustees`);
+    await page.waitForSelector('[data-testid="trustees-table"]', { state: 'visible' });
   });
 
   const ANALYZE_DELAY = 1000; // Delay in ms to wait for any transitions to complete to minimize false failures.
 
   test('trustees list should not have accessibility issues', async ({ page }) => {
-    expect(page.getByTestId('trustee-link-trustee-1')).toBeVisible();
-
     await page.waitForTimeout(ANALYZE_DELAY);
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
     expect(accessibilityScanResults.violations).toEqual([]);
   });
 
   test('trustee profile should not have accessibility issues', async ({ page }) => {
-    const trusteeProfileLink = page.getByTestId('trustee-link-trustee-1');
-    expect(trusteeProfileLink).toBeVisible();
+    const trusteeProfileLink = page.locator('[data-testid^="trustee-link-"]').first();
+    await expect(trusteeProfileLink).toBeVisible();
 
     await trusteeProfileLink.click();
-    expect(page.locator('.record-detail-header')).toBeVisible();
+    await expect(page.locator('.record-detail-header')).toBeVisible();
 
     await page.waitForTimeout(ANALYZE_DELAY);
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
     expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
-*/
