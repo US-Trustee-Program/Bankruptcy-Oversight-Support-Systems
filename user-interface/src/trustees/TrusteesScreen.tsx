8c46096fe2e82cfd41b5e8c4e0c643c9830e50bb
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-09-22 17:19:30 -0400

    Fix bad import breaking build
    Jira ticket: CAMS-584

--- user-interface/src/trustees/TrusteesScreen.tsx
+++ user-interface/src/trustees/TrusteesScreen.tsx
@@ -4,7 +4,7 @@ import LocalStorage from '@/lib/utils/local-storage';
 import { CamsRole } from '@common/cams/roles';
 import TrusteesList from './TrusteesList';
 import { MainContent } from '@/lib/components/cams/MainContent/MainContent';
-import { TrusteeFormState } from '@/trustees/TrusteeForm';
+import { TrusteeFormState } from '@/trustees/UseTrusteeFormValidation.types';
 
 export default function TrusteesScreen() {
   const flags = useFeatureFlags();
