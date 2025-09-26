40c305f384f57ce6af2c60c3603bcdda7017c0d1
James Brooks <12275865+jamesobrooks@users.noreply.github.com>
2025-09-25 20:18:13 -0500

    Begin implementing e2e db load as dataflow

--- test/e2e/scripts/load-cosmos-data.ts
+++ test/e2e/scripts/load-cosmos-data.ts
@@ -1,5 +1,7 @@
-import { seedCosmosE2eDatabase } from './data-generation-utils';
+import { seedCosmosE2eDatabase } from '../../../backend/function-apps/dataflows/e2e/data-generation-utils';
 
+// Replace this with http request to '{dataflows base url}/import/load-e2e-db'
+// Do not remove this though, move it to ../../../backend/function-apps/dataflows/e2e/load-e2e-db.ts
 seedCosmosE2eDatabase().then(() => {
   process.exit();
 });
