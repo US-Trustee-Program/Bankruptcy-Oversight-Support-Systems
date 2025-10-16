5de5e744e7e37991734b29dc04ff070ba420090f
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-10-16 17:51:36 -0400

    Fix act warnings in Input component test

--- user-interface/src/lib/components/uswds/Input.test.tsx
+++ user-interface/src/lib/components/uswds/Input.test.tsx
@@ -16,6 +16,10 @@ describe('Tests for USWDS Input component.', () => {
     );
   };
 
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
   test('Should change value when ref.setValue() is called and set value back to original when ref.resetValue() is called.', async () => {
     renderWithoutProps();
     const inputEl = screen.getByTestId('input-1');
@@ -201,7 +205,6 @@ describe('Input additional coverage tests', () => {
     act(() => ref.current?.focus());
     expect(inputEl).toHaveFocus();
 
-    // Test onFocus prop
     fireEvent.focus(inputEl);
     expect(mockOnFocus).toHaveBeenCalled();
   });
