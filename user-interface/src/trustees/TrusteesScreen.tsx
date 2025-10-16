56006390241868275e7a82ce6b26331c977b476d
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-10-16 14:21:16 -0400

    Refactor out the use of react router `location.state`
    Jira ticket: CAMS-645

--- user-interface/src/trustees/TrusteesScreen.tsx
+++ user-interface/src/trustees/TrusteesScreen.tsx
@@ -1,17 +1,15 @@
-import { NavLink, Outlet, useLocation, useOutlet } from 'react-router-dom';
+import { NavLink, Outlet, useOutlet } from 'react-router-dom';
 import useFeatureFlags, { TRUSTEE_MANAGEMENT } from '@/lib/hooks/UseFeatureFlags';
 import LocalStorage from '@/lib/utils/local-storage';
 import { CamsRole } from '@common/cams/roles';
 import TrusteesList from './TrusteesList';
 import { MainContent } from '@/lib/components/cams/MainContent/MainContent';
-import { TrusteeFormState } from '@/trustees/forms/UseTrusteeContactForm';
 
 export default function TrusteesScreen() {
   const flags = useFeatureFlags();
   const session = LocalStorage.getSession();
   const canManage = !!session?.user?.roles?.includes(CamsRole.TrusteeAdmin);
 
-  const location = useLocation();
   const outlet = useOutlet();
 
   // Check if there's a nested route being rendered
@@ -39,7 +37,6 @@ export default function TrusteesScreen() {
           <h1 className="display-inline-block margin-bottom-0">Trustees</h1>
           <NavLink
             to="/trustees/create"
-            state={{ cancelTo: location.pathname, action: 'create' } as TrusteeFormState}
             data-testid="trustees-add-link"
             className="usa-button flex-shrink-0 margin-right-0"
           >
