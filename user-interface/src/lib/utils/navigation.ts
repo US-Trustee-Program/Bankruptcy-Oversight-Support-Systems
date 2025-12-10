3781cb07a9eef4d23cdafcfcc2528f9a4972664c
Prem <pgovinda@flexion.us>
2025-12-10 16:15:31 -0500

    fix lint errors

--- user-interface/src/lib/utils/navigation.ts
+++ user-interface/src/lib/utils/navigation.ts
@@ -12,20 +12,3 @@ export function createNavStateMapper<T>(
     return pathToStateMap[lastSegment] || defaultState;
   };
 }
-
-interface NavigationItem<T> {
-  state: T;
-  path: string;
-  label: string;
-  testId: string;
-  title: string;
-  end?: boolean;
-  isConditional?: boolean;
-  conditionalCheck?: () => boolean;
-}
-
-interface DetailNavigationProps<T> {
-  entityId: string | undefined;
-  initiallySelectedNavLink: T;
-  className?: string;
-}
