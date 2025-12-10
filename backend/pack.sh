e3deaed6075fb334ed8d18b0f5b87f269d39f128
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-12-10 12:45:08 -0500

    Update package scripts to use npm workspace configuration

--- backend/pack.sh
+++ backend/pack.sh
@@ -12,7 +12,8 @@ echo "Creating archive $PACK_TEMP_DIR/$FILE_NAME.zip"
 
 mkdir -p dist/
 mkdir -p node_modules/
-cp -r ../../node_modules/* node_modules/
+# Copy from root node_modules (npm workspaces structure)
+cp -r ../../../node_modules/* node_modules/
 mkdir -p "$PACK_TEMP_DIR"
 zip -q -r "$PACK_TEMP_DIR/$FILE_NAME.zip" ./dist ./node_modules ./package.json ./host.json --exclude "*.map" --exclude "*.ts"
 mv "$PACK_TEMP_DIR/$FILE_NAME.zip" .
