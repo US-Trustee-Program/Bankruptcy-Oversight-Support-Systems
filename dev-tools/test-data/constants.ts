2d6f8261958ec522d02fbe1221684ec916d66ec4
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-12-15 11:27:00 -0500

    Remove stale, unused test data tools

--- dev-tools/test-data/constants.ts
+++
@@ -1,1 +0,0 @@
-export const SUPPORTED_COURT_IDS = ['', ''];
--- dev-tools/test-data/fixtures/noJudgeAssignedCases.ts
+++
@@ -1,7 +0,0 @@
-import { CreateCaseOptions, createCase } from './lib/common';
-
-export function createNoJudgeAssignedCases(options: CreateCaseOptions = {}) {
-  const bCase = createCase(options);
-  delete bCase.judge;
-  return [bCase];
-}
