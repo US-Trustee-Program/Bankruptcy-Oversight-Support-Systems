ad389e9a72ccdbd0598a70756275cf9b896fbfff
Brian Posey <15091170+btposey@users.noreply.github.com>
2025-12-15 11:27:00 -0500

    Remove stale, unused test data tools

--- dev-tools/test-data/fixtures/chapter15Cases.ts
+++
@@ -1,6 +0,0 @@
-import { BCase } from '../domain/bcase';
-import { CreateCaseOptions, createCases } from './lib/common';
-
-export function createChapter15Cases(caseCount: number, options: CreateCaseOptions): Array<BCase> {
-  return createCases(caseCount, { ...options, chapters: ['15'] });
-}
