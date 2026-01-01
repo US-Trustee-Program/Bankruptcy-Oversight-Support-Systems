dbaabf75491272f827c397fde85f5279bbe9ccd6
mattstankey <3545720+mattstankey@users.noreply.github.com>
2026-01-01 14:14:53 -0600

    CAMS-546 added global logout timer back in to ensure the user can timeout without seeing the popup dialog

--- user-interface/src/login/session-timer.ts
+++ user-interface/src/login/session-timer.ts
@@ -5,6 +5,7 @@ import getAppConfiguration from '@/configuration/appConfiguration';
 
 export const SIXTY_SECONDS = 60;
 export const HEARTBEAT = 1000 * SIXTY_SECONDS;
+export const WARNING_THRESHOLD = 1000 * SIXTY_SECONDS;
 export const SESSION_TIMEOUT = 'session-timeout';
 export const AUTH_EXPIRY_WARNING = 'auth-expiry-warning';
 export const SAFE_LIMIT = 300;
@@ -70,8 +71,35 @@ export function getLastInteraction(): number | null {
   return LocalStorage.getLastInteraction();
 }
 
+export function checkForInactivity() {
+  const now = Date.now();
+  const lastInteraction = getLastInteraction();
+
+  if (!lastInteraction) {
+    logout();
+    return;
+  }
+
+  const timeElapsed = now - lastInteraction;
+  const timeUntilTimeout = TIMEOUT - timeElapsed;
+
+  if (timeUntilTimeout <= WARNING_THRESHOLD && timeUntilTimeout > 0) {
+    window.dispatchEvent(new CustomEvent(SESSION_TIMEOUT));
+  }
+
+  if (timeUntilTimeout <= 0) {
+    logout();
+  }
+}
+
 export function logout(): void {
   const { host, protocol } = window.location;
   const logoutUri = protocol + '//' + host + LOGOUT_PATH;
   redirectTo(logoutUri);
+}
+
+export function initializeInactiveLogout() {
+  setInterval(checkForInactivity, 60000);
+  document.body.addEventListener('click', resetLastInteraction);
+  document.body.addEventListener('keypress', resetLastInteraction);
 }
