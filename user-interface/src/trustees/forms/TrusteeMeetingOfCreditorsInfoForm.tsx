14b8377f86402a59e1b9d0bd5305ecfe2e45e958
jalba-flexion <jalba@flexion.us>
2026-01-21 11:49:51 -0600

    Refactor to use Icon instead of iconlabel for copy button
    Jira ticket: CAMS-672
    
    Co-authored-by: Fritz Madden <96319835+fmaddenflx@users.noreply.github.com>
    Co-authored-by: Jonathan Cruz <94860350+cruzjone-flexion@users.noreply.github.com>

--- user-interface/src/trustees/forms/TrusteeMeetingOfCreditorsInfoForm.tsx
+++ user-interface/src/trustees/forms/TrusteeMeetingOfCreditorsInfoForm.tsx
@@ -1,4 +1,3 @@
-import './TrusteeMeetingOfCreditorsInfoForm.scss';
 import Button, { UswdsButtonStyle } from '@/lib/components/uswds/Button';
 import Input from '@/lib/components/uswds/Input';
 import PhoneNumberInput from '@/lib/components/PhoneNumberInput';
