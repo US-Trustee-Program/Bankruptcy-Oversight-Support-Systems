1d34c2a0d7a78580ce3b754009c82356e0fcbe94
Jonathan Cruz <94860350+cruzjone-flexion@users.noreply.github.com>
2026-04-07 13:49:21 -0600

    Remove unused MODULE_NAME export from use case
    Jira ticket: CAMS-725
    
    Co-authored-by: Jonathan Cruz <94860350+cruzjone-flexion@users.noreply.github.com>

--- backend/lib/use-cases/cases/case-trustee-appointment.use-case.ts
+++ backend/lib/use-cases/cases/case-trustee-appointment.use-case.ts
@@ -2,10 +2,6 @@ import { ApplicationContext } from '../../adapters/types/basic';
 import { CaseAppointment } from '@common/cams/trustee-appointments';
 import factory from '../../factory';
 
-const MODULE_NAME = 'CASE-TRUSTEE-APPOINTMENT-USE-CASE';
-
-export { MODULE_NAME };
-
 export class CaseTrusteeAppointmentUseCase {
   private readonly context: ApplicationContext;
 
