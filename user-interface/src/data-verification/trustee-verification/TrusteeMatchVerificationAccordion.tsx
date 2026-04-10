b283015e85f05f54b0addaccffeae1e463d617a1
mattstankey <3545720+mattstankey@users.noreply.github.com>
2026-04-10 12:07:02 -0500

    - Added district filter for trustee search - Fixed styling to more closely match mockups - Fixed error with missing imports where searches weren't working at all - Added a clean-all script to remove all test trustee data
    Jira ticket: CAMS-721
    
    Co-authored-by: Prem Govinda <16512926+bityogi@users.noreply.github.com>
    Co-authored-by: Kelly D <41132296+diabagatekelly@users.noreply.github.com>

--- user-interface/src/data-verification/trustee-verification/TrusteeMatchVerificationAccordion.tsx
+++ user-interface/src/data-verification/trustee-verification/TrusteeMatchVerificationAccordion.tsx
@@ -469,7 +469,7 @@ export function TrusteeMatchVerificationAccordion(props: TrusteeMatchVerificatio
         ref={searchModalRef}
         id={order.id}
         dxtrTrusteeName={order.dxtrTrustee.fullName}
-        courtId={order.courtId}
+        courtId={courtDetails?.courtId ?? order.courtId}
         onConfirm={handleManualMatch}
       />
     </>
