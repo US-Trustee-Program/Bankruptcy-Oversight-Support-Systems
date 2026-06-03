c5e12c9a41b39d17906bdfcd8d761f6b4b5ed021
Brian Posey <15091170+btposey@users.noreply.github.com>
2026-06-03 16:01:17 -0400

    Restore trustee name links to open in new tab
    Trustee profile links in the list should open in a new tab. Removes
    the openNewTab=false override from TrusteesList, corrects unit test
    assertions, restores context/waitForEvent('page') in four a11y tests,
    and updates the E2E navigation test to assert on the new tab page.
    
    Jira ticket: N/A

--- user-interface/src/trustees/TrusteesList.test.tsx
+++ user-interface/src/trustees/TrusteesList.test.tsx
@@ -125,8 +125,8 @@ describe('TrusteesList Component', () => {
       '/trustees/trustee-2',
     );
 
-    expect(screen.getByTestId('trustee-link-trustee-1')).not.toHaveAttribute('target', '_blank');
-    expect(screen.getByTestId('trustee-link-trustee-2')).not.toHaveAttribute('target', '_blank');
+    expect(screen.getByTestId('trustee-link-trustee-1')).toHaveAttribute('target', '_blank');
+    expect(screen.getByTestId('trustee-link-trustee-2')).toHaveAttribute('target', '_blank');
   });
 
   test('should fire analytics event when trustee link is clicked', async () => {
