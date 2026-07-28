416e32c6d4de53609324d3a9b6ce7c4a0ceaf826
Brian Posey <15091170+btposey@users.noreply.github.com>
2026-07-28 16:03:43 -0400

    Remove unused http triggers in dataflows

--- backend/function-apps/dataflows/import/sync-orders.ts
+++ backend/function-apps/dataflows/import/sync-orders.ts
@@ -1,8 +1,8 @@
-import { app, HttpRequest, InvocationContext, Timer } from '@azure/functions';
+import { app, InvocationContext, Timer } from '@azure/functions';
 import ContextCreator from '../../azure/application-context-creator';
 import { OrdersController } from '../../../lib/controllers/orders/orders.controller';
 import { toAzureError } from '../../azure/functions';
-import { buildFunctionName, buildHttpTrigger } from '../dataflows-common';
+import { buildFunctionName } from '@common/queues';
 import { completeDataflowTrace } from '../../../lib/use-cases/dataflows/dataflow-telemetry';
 
 const MODULE_NAME = 'SYNC-ORDERS';
@@ -43,40 +43,10 @@ async function timerTrigger(_ignore: Timer, invocationContext: InvocationContext
   }
 }
 
-/**
- * Used to invoke the orders sync process between DXTR and CAMS CosmosDB from an accessable HTTP endpoint.
- *
- * This endpoint can be invoked from the CLI with the following cURL command. Modify the `txIdOverride` to a
- * specific AO_TX.TX_ID to start the sync from or omit it from the request body to use the last runtime state
- * stored in CosmosDB.
- *
- * curl -v -d '{"txIdOverride": "0"}' -H "Content-Type: application/json" http://localhost:7071/api/sync-orders
- *
- */
-
-const httpTrigger = buildHttpTrigger(
-  MODULE_NAME,
-  async (invocationContext: InvocationContext, request: HttpRequest) => {
-    const context = await ContextCreator.getApplicationContext({
-      invocationContext,
-      request,
-    });
-
-    const ordersController = new OrdersController(context);
-    return ordersController.handleRequest(context);
-  },
-);
-
 function setup() {
   app.timer(buildFunctionName(MODULE_NAME, 'timerTrigger'), {
     schedule: '0 30 9 * * *',
     handler: timerTrigger,
-  });
-
-  app.http(buildFunctionName(MODULE_NAME, 'httpTrigger'), {
-    route: 'sync-orders',
-    methods: ['POST'],
-    handler: httpTrigger,
   });
 }
 
